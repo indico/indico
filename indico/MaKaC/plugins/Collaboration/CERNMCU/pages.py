d44cf55e09e3408d20659cc5ceba97b948fc9f26
Pedro Ferreira <jose.pedro.ferreira@cern.ch>
2009-09-11 14:49:18 +0200

    [FIXED] Unhandled exception

--- indico/MaKaC/plugins/Collaboration/CERNMCU/pages.py
+++ indico/MaKaC/plugins/Collaboration/CERNMCU/pages.py
@@ -32,27 +32,27 @@ from MaKaC.common import info
 from MaKaC.i18n import _
 
 class WNewBookingForm(WCSPageTemplateBase):
-        
+
     def getVars(self):
         vars = WCSPageTemplateBase.getVars( self )
-        
+
         vars["EventTitle"] = self._conf.getTitle()
         vars["EventDescription"] = unescape_html(strip_ml_tags( self._conf.getDescription())).strip()
         vars["DefaultStartDate"] = formatDateTime(self._conf.getAdjustedStartDate())
         vars["DefaultEndDate"] = formatDateTime(self._conf.getAdjustedEndDate())
         vars["MinStartDate"] = formatDateTime(self._conf.getAdjustedStartDate())
         vars["MaxEndDate"] = formatDateTime(self._conf.getAdjustedEndDate())
-        
+
         return vars
 
 class WMain (WJSBase):
-    
+
     def getVars(self):
         vars = WJSBase.getVars( self )
-    
+
         vars["MinStartDate"] = formatDateTime(self._conf.getAdjustedStartDate())
         vars["MaxEndDate"] = formatDateTime(self._conf.getAdjustedEndDate())
-        
+
         location = self._conf.getLocation()
         room = self._conf.getRoom()
         if location and room:
@@ -63,7 +63,7 @@ class WMain (WJSBase):
                 vars["InitialRoomInstitution"] = self._conf.getLocation().getName()
             else:
                 vars["InitialRoomInstitution"] = ""
-            
+
             # TODO: get IP of room from a plugin option instead of querying RB DB every time
             try:
                 minfo = info.HelperMaKaCInfo.getMaKaCInfoInstance()
@@ -71,23 +71,30 @@ class WMain (WJSBase):
                     Logger.get("CERNMCU").info("Connecting with Room Booking DB to get a room's H323 IP")
                     CrossLocationDB.connect()
                     Logger.get("CERNMCU").info("Connection successful")
-                    
+
                     attName = getCERNMCUOptionValueByName("H323_IP_att_name")
-                    
+
                     returnedRooms = CrossLocationQueries.getRooms( location = location.getName(), roomName = room.getName() )
+
                     if isinstance(returnedRooms, list):
-                        returnedRoom = returnedRooms[0]
+                        if len(returnedRooms) == 0:
+                            returnedRoom = None
+                        else:
+                            returnedRoom = returnedRooms[0]
                     else:
                         returnedRoom = returnedRooms
-                    if attName in returnedRoom.customAtts:
+
+                    if (returnedRoom != None) and \
+                           (attName in returnedRoom.customAtts):
+
                         initialRoomIp = returnedRoom.customAtts[attName]
                         if (initialRoomIp.strip() == ""):
                             vars["IPRetrievalResult"] = 1
                         elif not validIP(initialRoomIp):
-                            vars["IPRetrievalResult"] = 2                    
+                            vars["IPRetrievalResult"] = 2
                         else:
                             vars["IPRetrievalResult"] = 0
-                        
+
                     else:
                         initialRoomIp = "IP not defined for this room."
                         vars["IPRetrievalResult"] = 2
@@ -95,43 +102,43 @@ class WMain (WJSBase):
                     initialRoomIp = "Please enter IP"
                     vars["IPRetrievalResult"] = 3
                     Logger.get("CERNMCU").info("Tried to retrieve a room's H323 IP, but Room Booking module was not active.")
-                        
+
             except MaKaCError, e:
                 initialRoomIp = "IP not found."
                 vars["IPRetrievalResult"] = 4
                 Logger.get("CERNMCU").warning("Problem when retrieving a room's H.323 IP: " + e.getMsg())
-                    
-            
+
+
             vars["InitialRoomIP"] = initialRoomIp
-            
+
         else:
             vars["IncludeInitialRoom"] = False
             vars["IPRetrieved"] = False
             vars["InitialRoomName"] = ""
             vars["InitialRoomInstitution"] = ""
             vars["InitialRoomIP"] = ""
-        
+
         vars["CERNGatekeeperPrefix"] = getCERNMCUOptionValueByName("CERNGatekeeperPrefix")
         vars["GDSPrefix"] = getCERNMCUOptionValueByName("GDSPrefix")
         vars["MCU_IP"] = getCERNMCUOptionValueByName("MCU_IP")
         vars["Phone_number"] = getCERNMCUOptionValueByName("Phone_number")
-    
+
         return vars
-    
+
 class WIndexing(WJSBase):
     pass
-    
+
 class WExtra (WJSBase):
     def getVars(self):
         vars = WJSBase.getVars( self )
-        
+
         roomsWithH323IP = []
-        
+
         if self._conf:
-            location = self._conf.getLocation() 
-            
+            location = self._conf.getLocation()
+
             if location:
-            
+
                 # TODO: get list of room from a plugin option instead of querying the RB DB everytime
                 try:
                     minfo = info.HelperMaKaCInfo.getMaKaCInfoInstance()
@@ -139,70 +146,70 @@ class WExtra (WJSBase):
                         Logger.get("CERNMCU").info("Connecting with Room Booking DB to get the list of all rooms with a H323 IP")
                         CrossLocationDB.connect()
                         Logger.get("CERNMCU").info("Connection successful")
-                        
+
                         attName = getCERNMCUOptionValueByName("H323_IP_att_name")
-                        
-                        
+
+
                         returnedRooms = CrossLocationQueries.getRooms( location = location.getName(),
                                                                        customAtts = [{"name":attName, "allowEmpty":False,
                                                                                       "filter": (lambda ip: validIP(ip))}] )
                         if not isinstance(returnedRooms, list):
                             returnedRooms = [returnedRooms]
-        
+
                         for room in returnedRooms:
                             roomsWithH323IP.append(RoomWithH323(location.getName(), room._getName(), room.customAtts[attName]))
-                            
+
                 except MaKaCError, e:
                     Logger.get("CERNMCU").warning("Problem when retrieving the list of all rooms with a H323 IP: " + e.getMsg())
-        
+
         vars["RoomsWithH323IP"] = roomsWithH323IP
         return vars
-    
+
 class WStyle (WCSCSSBase):
     pass
 
 class WInformationDisplay(WCSPageTemplateBase):
-    
+
     def __init__(self, booking, displayTz):
         WCSPageTemplateBase.__init__(self, booking.getConference(), 'CERNMCU', None)
         self._booking = booking
         self._displayTz = displayTz
-    
+
     def getVars(self):
         vars = WCSPageTemplateBase.getVars( self )
-        
+
         vars["Booking"] = self._booking
-        
+
         vars["CERNGatekeeperPrefix"] = getCERNMCUOptionValueByName("CERNGatekeeperPrefix")
         vars["GDSPrefix"] = getCERNMCUOptionValueByName("GDSPrefix")
         vars["MCU_IP"] = getCERNMCUOptionValueByName("MCU_IP")
         vars["Phone_number"] = getCERNMCUOptionValueByName("Phone_number")
-        
+
         return vars
 
 
 class XMLGenerator(object):
-    
+
     @classmethod
     def getDisplayName(cls):
         return "MCU"
-            
+
     @classmethod
     def getCustomBookingXML(cls, booking, displayTz, out):
-                        
+
         out.openTag("information")
-        
+
         if booking.getHasPin():
             out.openTag("section")
             out.writeTag("title", _('Protection:'))
             out.writeTag("line", _('This conference is protected by a PIN'))
             out.closeTag("section")
-        
+
         out.openTag("section")
         out.writeTag("title", _('Description:'))
         out.writeTag("line", booking._bookingParams["description"])
         out.closeTag("section")
-        
+
         out.openTag("section")
         out.writeTag("title", _('Participants:'))
         if booking.getParticipantList():
@@ -211,7 +218,7 @@ class XMLGenerator(object):
         else:
             out.writeTag("line", _("No participants yet"))
         out.closeTag("section")
-        
+
         bookingIdInMCU = str(booking._bookingParams["id"])
         out.openTag("section")
         out.writeTag("title", _("How to join:"))
@@ -240,6 +247,6 @@ class XMLGenerator(object):
             _('" followed by the "#".')
         ]))
         out.closeTag("section")
-        
+
         out.closeTag("information")
-        
+
