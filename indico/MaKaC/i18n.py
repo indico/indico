0b805508d9efd86cd65ab72be98fe3388b7c3c61
David Martín Clavo <david.martin.clavo@cern.ch>
2009-10-09 17:07:02 +0200

    [FIX] Exception was string

--- indico/MaKaC/i18n.py
+++ indico/MaKaC/i18n.py
@@ -3,6 +3,7 @@
 """Contains internationalization subsystem
 """
 import re,os.path
+from MaKaC.errors import MaKaCError
 import gettext, locale
 import operator
 
@@ -61,7 +62,7 @@ def install(name, lang, unicode, localedir=None):
         Lan = gettext.translation("messages", localedir, languages=[lang])
         LanName = lang
     except Exception, e:
-        raise "Can't find the messages.mo for the language ID %s\n%s"%(lang,e)
+        raise MaKaCError("Can't find the messages.mo for the language ID %s\n%s"%(lang,str(e)))
     Lan.install()
     __ = True
 
