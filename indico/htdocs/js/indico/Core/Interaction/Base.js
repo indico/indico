92a333db586c35bb493b1c4612bd056f4c11afa3
Pedro Ferreira <jose.pedro.ferreira@cern.ch>
2010-01-19 18:21:08 +0100

    [IMPROVEMENT] Refactored some decorators
     - added showWithMouse;
     - Generalized highlightWithMouse and showWithMouse so that they use
       highlightWithMouse;

--- indico/htdocs/js/indico/Core/Interaction/Base.js
+++ indico/htdocs/js/indico/Core/Interaction/Base.js
@@ -1,133 +1,69 @@
-type('TouchSensitive', [],
-     {
-         addSense : function(element) {
 
-             var self = this;
-             element.observeEvent('mouseover', function(event) {
-                 if (self.enabled) {
-                     if (!element.ancestorOf($E(eventTarget(event)))) {
-                         return;
-                     }
+function mouseOverSwitch(observable, overAction, outAction) {
 
-                     each(self.actionOnList, function(action) {
-                         action();
-                     });
-                 }
-                 event.preventDefault();
-             });
+    observable.observeEvent('mouseover', function(event) {
+        overAction(event);
+    });
 
-             element.observeEvent('mouseout', function(event) {
-                 if (self.enabled) {
-                     if (element.ancestorOf($E(event.relatedTarget))) {
-                         return;
-                     }
+    observable.observeEvent('mouseout', function(event) {
+        outAction(event);
+    });
 
-                     each(self.actionOffList, function(action) {
-                         action();
-                     });
-                 }
-                 event.preventDefault();
-             });
+}
 
-             return element;
-         },
+function showWithMouse(observable, target) {
 
-         disable : function() {
-             this.enabled = false;
-         },
+    var stored = target.dom.style.display;
+    target.dom.style.display = 'none';
 
-         enable : function() {
-             this.enabled = true;
-         },
+    mouseOverSwitch(observable,
+                    function() {
+                        target.dom.style.display = stored;
+                    },
+                    function() {
+                        target.dom.style.display = 'none';
+                    });
 
-         addActionOn : function(action) {
-             this.actionOnList.push(action);
-         },
-
-         addActionOff : function(action) {
-             this.actionOffList.push(action);
-         }
-
-     },
-     function() {
-         this.enabled = true;
-         this.actionOnList = [];
-         this.actionOffList = [];
-     });
-
-type('TooltipLike', ['TouchSensitive'],
-     {
-         triggerOn: function() {
-             if (!this.state) {
-                 this._triggerOn();
-                 this.state = true;
-             }
-         },
-
-         triggerOff: function() {
-             if (this.state) {
-                 this._triggerOff();
-                 this.state = false;
-             }
-         }
-
-     },
-     function() {
-         var self = this;
-
-         this.state = false;
-
-         this.TouchSensitive();
-         this.addActionOn(function(){
-             self.triggerOn();
-         });
-         this.addActionOff(function(){
-             self.triggerOff();
-         });
-
-     });
-
+}
 
 function highlightWithMouse(observable, target) {
 
     /*
      * observable - trigger element (mouse over, out)
      * target - target element (will look brighter)
-     */ 
+     */
 
     if (target.__highlightSet) {
         return observable;
     }
 
-    observable.observeEvent('mouseover', function(event) {
-	
-        target.__tmpColor = target.dom.style.backgroundColor;
-        if (target.__tmpColor === ''){
-            return;
-        }
-	
-        var bgColor = target.dom.style.backgroundColor;
-        var s = bgColor.match(/rgb\((\d+), (\d+), (\d+)\)/);
-        if (!s) {
-            s = bgColor.match(/#(..)(..)(..)/);
-            if (!s) {
-                return;
-            }
-            s[1] = parseInt('0x'+s[1],16);
-            s[2] = parseInt('0x'+s[2],16);
-            s[3] = parseInt('0x'+s[3],16);
-        }
-	
-        s[1] = Math.floor(s[1]*1.05);
-        s[2] = Math.floor(s[2]*1.05);
-        s[3] = Math.floor(s[3]*1.05);
-        target.setStyle('backgroundColor', 'rgb(' + s[1] + ',' + s[2] + ',' + s[3] + ')');
+    mouseOverSwitch(observable,
+                    function() {
+                        target.__tmpColor = target.dom.style.backgroundColor;
+                        if (target.__tmpColor === ''){
+                            return;
+                        }
 
-    });
-    
-    observable.observeEvent('mouseout', function(event) {
-        target.setStyle('backgroundColor', target.__tmpColor);
-    });
+                        var bgColor = target.dom.style.backgroundColor;
+                        var s = bgColor.match(/rgb\((\d+), (\d+), (\d+)\)/);
+                        if (!s) {
+                            s = bgColor.match(/#(..)(..)(..)/);
+                            if (!s) {
+                                return;
+                            }
+                            s[1] = parseInt('0x'+s[1],16);
+                            s[2] = parseInt('0x'+s[2],16);
+                            s[3] = parseInt('0x'+s[3],16);
+                        }
+
+                        s[1] = Math.floor(s[1]*1.05);
+                        s[2] = Math.floor(s[2]*1.05);
+                        s[3] = Math.floor(s[3]*1.05);
+                        target.setStyle('backgroundColor', 'rgb(' + s[1] + ',' + s[2] + ',' + s[3] + ')');
+                    },
+                    function() {
+                        target.setStyle('backgroundColor', target.__tmpColor);
+                    });
 
     target.__highlightSet = true;
 
@@ -142,15 +78,14 @@ type('Printable', [], {
 
         // Make sure element is visible when printing
         element.dom.className = elementCSSClasses + " print";
-        
+
         // Make sure body is invisible
         body.dom.className = bodyCSSClasses += " noprint";
 
         window.print();
-        
+
         // Restore
         //body.dom.className = bodyCSSClasses;
         //element.dom.className = elementCSSClasses
     }
-    }
-)
+});
