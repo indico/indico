94dbcb73049758641ef8d70a05620c4bb8aa2301
Pedro Ferreira <jose.pedro.ferreira@cern.ch>
2010-02-12 17:16:27 +0100

    [FIX] Added handlers for page unload time

--- indico/htdocs/js/indico/Core/Widgets/RichText.js
+++ indico/htdocs/js/indico/Core/Widgets/RichText.js
@@ -6,7 +6,7 @@ type("RichTextEditor", ["IWidget", "Accessor"],
          draw: function() {
              this.div = Html.div({'id': this.divId});
 
-	     this.postDraw();
+             this.postDraw();
 
              return this.div;
          },
@@ -19,96 +19,96 @@ type("RichTextEditor", ["IWidget", "Accessor"],
              }
          },
 
-	 postDraw: function() {
-	     this.div.set('');
+         postDraw: function() {
+             this.div.set('');
 
-	     this.div.dom.innerHTML = this.editor.CreateHtml();
+             this.div.dom.innerHTML = this.editor.CreateHtml();
 
-	     var self = this;
+             var self = this;
 
-	     this.onLoadFunc = function() {
+             this.onLoadFunc = function() {
 
                  if (self.text) {
-		     self.set(self.text);
+                     self.set(self.text);
                  }
 
-		 each(self.onLoadList,
-		      function(callback) {
-			  callback();
-		      });
+                 each(self.onLoadList,
+                      function(callback) {
+                          callback();
+                      });
 
-		 self.editor.Events.AttachEvent( 'OnSelectionChange',
-						 function() {
-						     if (!self.observing) {
-							 self.startObserving();
-						     }
-						 } ) ;
-	     };
+                 self.editor.Events.AttachEvent( 'OnSelectionChange',
+                                                 function() {
+                                                     if (!self.observing) {
+                                                         self.startObserving();
+                                                     }
+                                                 } ) ;
+             };
 
-	     if (this.editor && this.editor.SetData) {
-		 this.onLoadFunc();
-	     } else {
-		 executeOnLoad = true;
-	     }
+             if (this.editor && this.editor.SetData) {
+                 this.onLoadFunc();
+             } else {
+                 executeOnLoad = true;
+             }
 
-	 },
+         },
 
          set: function(text) {
              var self = this;
 
              if (this.editor && this.editor.SetData) {
                  this.editor.SetData(text);
-		 this.editor.ResetIsDirty();
+                 this.editor.ResetIsDirty();
              } else {
                  this.text = text;
              }
          },
 
          observe: function(callback) {
-	     this.callbacks.append(callback);
+             this.callbacks.append(callback);
          },
 
-	 startObserving: function() {
-	     this.observing = true;
-	     this.editor.ResetIsDirty();
-	     if (this.callbacks.length.get() > 0) {
-		 this.fckTimer = window.setInterval("FCKeditor_OnChange('"+this.divId+"')",2000);
-	     }
-	 },
+         startObserving: function() {
+             this.observing = true;
+             this.editor.ResetIsDirty();
+             if (this.callbacks.length.get() > 0) {
+                 this.fckTimer = window.setInterval("FCKeditor_OnChange('"+this.divId+"')",2000);
+             }
+         },
 
-	 unbind: function() {
-	     this.observing = false;
-	     this.callbacks.clear();
-	     window.clearInterval(this.fckTimer);
-	 },
+         unbind: function() {
+             this.observing = false;
+             this.callbacks.clear();
+             window.clearInterval(this.fckTimer);
+         },
 
          onLoad: function(callback) {
-	     this.onLoadList.append(callback);
+             this.onLoadList.append(callback);
          },
 
-	 destroy: function() {
-	     this.unbind();
-	     delete __globalEditorTable[this.divId];
-	 }
+         destroy: function() {
+             this.unbind();
+             delete __globalEditorTable[this.divId];
+         }
 
      },
      function(width, height, textAreaParams, toolbarSet) {
-	 this.onLoadList = new WatchList();
-	 this.callbacks = new WatchList();
+         this.onLoadList = new WatchList();
+         this.callbacks = new WatchList();
          this.width = width;
          this.height = Browser.IE?height-75:height;
          this.params = exists(textAreaParams)?textAreaParams:{};
 
-	 this.divId = Html.generateId();
+         this.divId = Html.generateId();
 
-	 this.editor = new FCKeditor(this.divId) ;
-	 this.editor.Width = width;
-	 this.editor.Height = height;
-	 this.editor.ToolbarSet = toolbarSet || 'IndicoMinimal' ;
-	 this.editor.BasePath = ScriptRoot + "fckeditor/" ;
-	 this.editor.Config.CustomConfigurationsPath = ScriptRoot + "fckeditor/indicoconfig.js"  ;
-	 __globalEditorTable[this.divId] = this;
-	 var self = this;
+         this.editor = new FCKeditor(this.divId) ;
+         this.editor.Width = width;
+         this.editor.Height = height;
+         this.editor.ToolbarSet = toolbarSet || 'IndicoMinimal' ;
+         this.editor.BasePath = ScriptRoot + "fckeditor/" ;
+         this.editor.Config.CustomConfigurationsPath = ScriptRoot + "fckeditor/indicoconfig.js"  ;
+         __globalEditorTable[this.divId] = this;
+         var self = this;
 
      });
 
@@ -126,24 +126,24 @@ type("RichTextWidget", ["IWidget", "Accessor"],
          observe: function(callback) {
              var self = this;
 
-	     var observeFunc = function(value) {
+             var observeFunc = function(value) {
 
-		 self.plain.unbind();
-		 self.rich.unbind();
+                 self.plain.unbind();
+                 self.rich.unbind();
 
-		 if (value == 'rich') {
-		     self.rich.observe(function() {
-			 callback(self.rich);
-		     });
-		 } else {
-		     self.plain.observe(function() {
-			 callback(self.plain);
-		     });
-		 }
-	     };
+                 if (value == 'rich') {
+                     self.rich.observe(function() {
+                         callback(self.rich);
+                     });
+                 } else {
+                     self.plain.observe(function() {
+                         callback(self.plain);
+                     });
+                 }
+             };
 
-	     this.selected.observe(observeFunc);
-	     observeFunc(this.selected.get());
+             this.selected.observe(observeFunc);
+             observeFunc(this.selected.get());
          },
 
          get: function() {
@@ -152,7 +152,7 @@ type("RichTextWidget", ["IWidget", "Accessor"],
 
          set: function(value, noDetection) {
 
-	     this.currentText = value;
+             this.currentText = value;
 
              if (!any(noDetection, false)) {
                  if ((this.isHtml(value)?'rich':'plain') != this.selected.get()) {
@@ -160,18 +160,18 @@ type("RichTextWidget", ["IWidget", "Accessor"],
                  }
              }
 
-	     if (value && (this.loaded || this.selected.get() == 'plain')) {
-		 this.activeAccessor.set(value);
-	     }
+             if (value && (this.loaded || this.selected.get() == 'plain')) {
+                 this.activeAccessor.set(value);
+             }
          },
 
          synchronizePlain: function() {
-	     this.currentText = this.rich.get();
+             this.currentText = this.rich.get();
              this.plain.set(this.currentText);
          },
 
          synchronizeRich: function() {
-	     this.currentText = this.plain.get();
+             this.currentText = this.plain.get();
              this.rich.set(this.currentText);
          },
 
@@ -183,9 +183,9 @@ type("RichTextWidget", ["IWidget", "Accessor"],
              }
          },
 
-	 postDraw: function() {
-	     this.rich.postDraw();
-	 },
+         postDraw: function() {
+             this.rich.postDraw();
+         },
 
          destroy: function() {
              this.rich.destroy();
@@ -203,11 +203,11 @@ type("RichTextWidget", ["IWidget", "Accessor"],
          this.rich = new RichTextEditor(width, height, textAreaParams, toolbarSet);
          this.richDiv = Html.div({});
          this.currentText = any(initialText, '');
-	 this.loaded = false;
+         this.loaded = false;
 
-	 this.selected = new WatchValue();
+         this.selected = new WatchValue();
 
-	 var toPlainFunc = function(sync) {
+         var toPlainFunc = function(sync) {
              self.plain.setStyle('display', 'block');
              self.richDiv.setStyle('display', 'none');
              self.switchLink.set('toRich');
@@ -218,7 +218,7 @@ type("RichTextWidget", ["IWidget", "Accessor"],
              }
          };
 
-	 var toRichFunc  = function(sync) {
+         var toRichFunc  = function(sync) {
              self.plain.setStyle('display', 'none');
              self.richDiv.setStyle('display', 'block');
              self.switchLink.set('toPlain');
@@ -234,26 +234,27 @@ type("RichTextWidget", ["IWidget", "Accessor"],
              {
                  toPlain: command(
                      toPlainFunc,
-		     $T("switch to plain text")),
+                     $T("switch to plain text")),
                  toRich: command(
                      toRichFunc,
-		     $T("switch to rich text"))
+                     $T("switch to rich text"))
             });
 
-	 if (exists(mode) && mode=='rich') {
-	     toRichFunc();
-	 } else if (exists(mode)){
-	     toPlainFunc();
-	 } else if (self.isHtml(self.currentText)) {
-	     toRichFunc();
-	 } else {
-	     toPlainFunc();
-	 }
+         if (exists(mode) && mode=='rich') {
+             toRichFunc();
+         } else if (exists(mode)){
+             toPlainFunc();
+         } else if (self.isHtml(self.currentText)) {
+             toRichFunc();
+         } else {
+             toPlainFunc();
+         }
 
-	 this.rich.onLoad(function() {
-	     self.loaded = true;
+         this.rich.onLoad(function() {
+             self.loaded = true;
              self.set(self.currentText, true);
-	 });
+         });
+
      });
 
 
@@ -317,10 +318,10 @@ function FCKeditor_OnChange( id ) {
     var value = __globalEditorTable[id];
 
     if(value.editor.IsDirty()) {
-	each(value.callbacks, function(func) {
-	    func();
-	})
-	value.editor.ResetIsDirty();
+        each(value.callbacks, function(func) {
+            func();
+        })
+        value.editor.ResetIsDirty();
     }
 
 }
