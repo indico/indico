0190311ea029bf3bd0c88bfd88e4da1457d0cfe6
Pedro Ferreira <jose.pedro.ferreira@cern.ch>
2010-02-23 15:35:38 +0100

    [FIX] isIPAddress in CERNMCU JS
     - Had missed it when changing function namespace/name;

--- indico/MaKaC/plugins/Collaboration/CERNMCU/tpls/Main.js
+++ indico/MaKaC/plugins/Collaboration/CERNMCU/tpls/Main.js
@@ -1,13 +1,13 @@
 {
     start : function(booking, iframeElement) {
-        
+
     },
-        
+
     checkStart : function(booking) {
         booking.permissionToStart = true;
         return true;
     },
-    
+
     checkParams: function() {
         return {
             'name' : ['text', false, function(name, values){
@@ -46,22 +46,22 @@
             'startDate' : ['datetime', false, function(startDateString, values){
                 var errors = [];
                 var startDate = IndicoUtil.parseDateTime(startDateString);
-                
+
                 //check start date is not in the past
                 // if (beforeNow(startDate)) {
                 //    errors.push("Start date cannot be in the past")
                 // }
-                
+
                 // check start date is not before the minimum start date (event start date)
                 if (startDate < IndicoUtil.parseDateTime("<%= MinStartDate %>")) {
                     errors.push($T("Start date cannot be before the Indico event start date. Please choose it after <%= MinStartDate %>"));
                 }
-                
+
                 // check start date is not after the maximum start date (event end date)
                 if (startDate > IndicoUtil.parseDateTime("<%= MaxEndDate %>")) {
                     errors.push($T("Start date cannot be after the Indico event end date. Please choose it before <%= MaxEndDate %>"));
                 }
-                
+
                 // check start date is not after end date, if end date exists
                 var endDate = IndicoUtil.parseDateTime(values["endDate"]);
                 if (endDate) {
@@ -69,28 +69,28 @@
                         errors.push($T("End date cannot be before start date."));
                     }
                 }
-                
+
                 return errors;
             }],
             'endDate' : ['datetime', false, function(endDateString, values){
                 var errors = [];
                 var endDate = IndicoUtil.parseDateTime(endDateString);
-                
+
                 //check start date is not in the past
                 if (beforeNow(endDate)) {
                     errors.push($T("End date cannot be in the past"));
                 }
-                
+
                 // check start date is not before the minimum start date (event start date)
                 if (endDate < IndicoUtil.parseDateTime("<%= MinStartDate %>")) {
                     errors.push($T("End date cannot be before the Indico event start date. Please choose it after <%= MinStartDate %>"));
                 }
-                
+
                 // check start date is not after the maximum start date (event end date)
                 if (endDate > IndicoUtil.parseDateTime("<%= MaxEndDate %>")) {
                     errors.push($T("End date cannot be after the Indico event end date. Please choose it before <%= MaxEndDate %>"));
                 }
-                
+
                 // check start date is not after end date, if start date exists
                 var startDate = IndicoUtil.parseDateTime(values["startDate"]);
                 if (startDate) {
@@ -98,12 +98,12 @@
                         errors.push($T("End date cannot be before start date."));
                     }
                 }
-                
+
                 return errors;
             }]
         }
     },
-    
+
     errorHandler : function(event, error){
         if (event == 'create' || event == 'edit') {
             switch (error.faultCode) {
@@ -130,7 +130,7 @@
             CSErrorPopup("MCU Error, code: " + error.faultCode, []);
         }
     },
-    
+
     customText: function(booking) {
         if (!booking.error) {
             return "ID: " + booking.bookingParams.id + ". " + booking.bookingParams.participants.length + $T(" participants.");
@@ -139,7 +139,7 @@
             switch(booking.faultCode) {
             case 2:
                 customText += $T("Duplicated Name")
-                break;    
+                break;
             case 18:
                 customText += $T("Duplicated ID")
                 break;
@@ -150,19 +150,19 @@
             return customText;
         }
     },
-    
+
     showInfo : function(booking) {
         infoHTML = '<div><table><tbody>';
-        
+
         if (booking.error) {
             infoHTML +=
                 '<tr><td colspan="2">'+
                     '<span class="collaborationWarning">';
-            
+
             switch(booking.faultCode) {
             case 2:
                 infoHTML += $T("The Conference name already exists in the MCU. Please choose another one.");
-                break;    
+                break;
             case 18:
                 infoHTML += $T("The Conference ID already exists in the MCU. Please choose another one.");
                 break;
@@ -176,81 +176,81 @@
                     '<\/span>' +
                 '<\/td><\/tr>'
         }
-        
+
         infoHTML +=
              '<tr><td class="collaborationInfoLeftCol">' + $T('Conference name:') + '<\/td><td>' +
                 (booking.error && booking.faultCode == 2 ?
                     '<span class="collaborationWarning">' + booking.bookingParams.name + ' (duplicated)<\/span>' :
                     booking.bookingParams.name) +
             '<\/td><\/tr>'+
-            
+
             '<tr><td class="collaborationInfoLeftCol">' + $T('Conference description:') + '<\/td><td>' +
                 booking.bookingParams.description +
             '<\/td><\/tr>'+
-            
+
             '<tr><td class="collaborationInfoLeftCol">' + $T('Conference MCU ID:') + '<\/td><td>' +
                 (booking.error && booking.faultCode == 18 ?
                         '<span class="collaborationWarning">' + booking.bookingParams.id + ' (duplicated)<\/span>' :
                         (booking.bookingParams.id ? booking.bookingParams.id : ('<span class="collaborationWarning">' + $T('No ID yet') + '<\/span>'))) +
             '<\/td><\/tr>'+
-            
+
             '<tr><td class="collaborationInfoLeftCol">' + $T('Start date:') + '<\/td><td>' +
                 formatDateStringCS(booking.bookingParams.startDate) +
             '<\/td><\/tr>'+
-            
+
             '<tr><td class="collaborationInfoLeftCol">' + $T('End date:') + '<\/td><td>' +
                 formatDateStringCS(booking.bookingParams.endDate) +
             '<\/td><\/tr>'+
-            
+
             '<tr><td class="collaborationInfoLeftCol">' + $T('PIN:') + '<\/td><td>' +
-                (booking.bookingParams.hasPin? $T("PIN hidden") : $T("No PIN was defined")) + 
+                (booking.bookingParams.hasPin? $T("PIN hidden") : $T("No PIN was defined")) +
             '<\/td><\/tr>'+
-            
+
             '<tr><td class="collaborationInfoLeftCol">' + $T('Visibility') + '<\/td><td>' +
-                (booking.bookingParams.hidden? $T("Hidden") : $T("Visible")) + 
+                (booking.bookingParams.hidden? $T("Hidden") : $T("Visible")) +
             '<\/td><\/tr>';
-            
+
         if (!booking.error) { infoHTML +=
-            
+
             '<tr><td class="collaborationInfoLeftCol">' + $T('How to join:') + '<\/td><td>' +
                 $T('1) If you are registered in the CERN Gatekeeper, please dial ') + '<%= CERNGatekeeperPrefix %>' + booking.bookingParams.id + '<br \/>' +
                 $T('2) If you have GDS enabled in your endpoint, please call ') + '<%= GDSPrefix %>' + booking.bookingParams.id + '<br \/>' +
-                $T('3) Otherwise dial ') + '<%= MCU_IP %>' + $T(' and using FEC (Far-End Controls) with your remote, enter "') + booking.bookingParams.id + $T('" followed by the "#".<br \/>') + 
+                $T('3) Otherwise dial ') + '<%= MCU_IP %>' + $T(' and using FEC (Far-End Controls) with your remote, enter "') + booking.bookingParams.id + $T('" followed by the "#".<br \/>') +
                 $T('4) To join by phone dial ') + '<%= Phone_number %>' + $T(', enter "') + booking.bookingParams.id + $T('" followed by the "#".') +
              '<\/td><\/tr>';
         }
-            
+
         infoHTML +=
             '<tr><td class="collaborationInfoLeftCol">' + $T('Indico booking ID:') + '<\/td><td>' +
-                booking.id + 
+                booking.id +
             '<\/td><\/tr>' +
-            
+
             '<tr><td class="collaborationInfoLeftCol">' + $T('Booking created on:') + '<\/td><td>' +
                 formatDateTimeCS(booking.creationDate) +
             '<\/td><\/tr>' +
-            
+
             '<tr><td class="collaborationInfoLeftCol">' + $T('Booking last modified on:') + '<\/td><td>' +
                 formatDateTimeCS(booking.modificationDate) +
             '<\/td><\/tr>' +
-            
+
         '<\/tbody><\/table><\/div>'
-        
+
         return infoHTML;
     },
-    
+
     getPopupDimensions: function() {
-	if (Browser.IE) {
-		var height = 370;
-	} else {
-		var height = 390;
-	}
+        if (Browser.IE) {
+                var height = 370;
+        } else {
+                var height = 390;
+        }
         return {width : 600, height: height};
     },
-    
+
     getDateFields : function() {
         return ["startDate", "endDate"]
     },
-    
+
     onCreate : function() {
         $E('autoYesRB').dom.checked = true;
         disableCustomId();
@@ -287,17 +287,17 @@
             default:
                 break;
             }
-            
+
         <% end %>
         <% else: %>
             pf = new ParticipantListField();
         <% end %>
-        
+
         $E('participantsCell').set(pf.draw());
-        
+
         CERNMCUDrawContextHelpIcons();
     },
-    
+
     onEdit: function(booking) {
         // setFormValues has problems with radio buttons constructed with .innerHTML in IE7
         if (Browser.IE) {
@@ -309,23 +309,23 @@
                 $E('autoYesRB').dom.checked = true;
             }
         }
-        
+
         if (booking.bookingParams.autoGenerateId == 'no') {
             enableCustomId();
         } else {
             disableCustomId();
         }
-        
+
         pf = new ParticipantListField(booking.bookingParams.participants)
         $E('participantsCell').set(pf.draw());
-        
+
         CERNMCUDrawContextHelpIcons();
     },
-    
+
     onSave: function(values) {
         var participants = pf.getParticipants();
         values["participants"] = participants;
-        
+
         var ips = {}
         var errors = false;
         for (var i = 0; i < participants.length.get(); i++) {
@@ -335,16 +335,16 @@
                 errors = true;
                 CSErrorPopup("Invalid participants", ["There is more than one participant with the ip " + participant.get("ip")]);
                 break;
-            } else if (!IndicoUtil.isIpAddress(ip)) {
+            } else if (!Util.Validation.isIPAddress(ip)) {
                 CSErrorPopup("Invalid participants", ["The participant " + (i + 1) + " does not have a correct IP"]);
                 errors = true;
                 break;
             } else {
                 ips[participant.get("ip")] = true;
             }
-                
+
         }
-        
+
         return !errors;
     }
 }
