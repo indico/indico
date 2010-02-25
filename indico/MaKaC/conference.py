d165f10601496a04731f5ba6f6919305c5e8c72b
Jose Benito <jose.benito.gonzalez@cern.ch>
2010-02-25 18:34:04 +0100

    [FIX] manager can be a group
    so we cannot unlink elements

--- indico/MaKaC/conference.py
+++ indico/MaKaC/conference.py
@@ -2766,7 +2766,8 @@ class Conference(Persistent, Fossilizable):
         #the link in every manager to the conference then, when the manager goes to his "My profile" he
         #will see a link to a conference that doesn't exist. Therefore, we need to delete that link as well
         for manager in self.getManagerList():
-            manager.unlinkTo(self, "manager")
+            if isinstance(manager, MaKaC.user.Avatar):
+                manager.unlinkTo(self, "manager")
 
         TrashCanManager().add(self)
 
