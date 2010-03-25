c6839d85e78b37552cb86afad697479ce96052f2
Pedro Ferreira <jose.pedro.ferreira@cern.ch>
2010-03-25 18:09:38 +0100

    [FIX] HTTP_REFERER for tz/lang switch
     - no longer mandatory
     - thanks vokac;
     - fixes #249;

--- indico/htdocs/changeLang.py
+++ indico/htdocs/changeLang.py
@@ -1,5 +1,6 @@
 import MaKaC.webinterface.rh.lang as lang
+from MaKaC.common import Config
 def index(req, **args):
-    args['REFERER_URL'] = (str(req.headers_in['Referer']))
+    args['REFERER_URL'] = (str(req.headers_in.get('Referer', Config.getInstance().getBaseURL())))
     return lang.RHChangeLang(req).process(args)
 
