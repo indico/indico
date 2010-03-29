7be949f44329cbe0b5300f2341df8a772772365f
David Martín Clavo <david.martin.clavo@cern.ch>
2010-03-29 14:27:53 +0200

    [IMP] Fossilizing new features
     - Squashed commit of:
    
     [IMP] Fossilize None and tuple
       - Fixed bug for fossilizing sets.
       - Updated testFossilize
       - pylinted testFossilize
    
     [IMP] Default fossil, class:fossil dict
       - Possible to pass None to the 'interface' argument;
       the first fossil for the class will be used.
       - Possible to pass a dictionary to the "interface" argument,
       with class objects as keys and fossil classes as values.
       Depending on the type of object, the corresponding
       fossil will be used.
       - Updated testFossilize.py with tests for default fossil
       and class:fossil dict options.
    
     [IMP] fossiles: 'has' and 'is' method names. produce tag
       - Support for methods such as 'hasXXX' and 'isXXX',
       the generated attributes will be the same as the
       method names. Other method name patterns are rejected.
       - Added 'produce' tag for corner-cases where we want
       to produce a new fossil attribute out of the "self" object
       and we do not want to implement a real method for it,
       or when we need all fossils of a given type / inheriting
       from a given type to all have an attribute with a given value.
       - Updated testFossilize.py
    
     [MIN] Fossilizing method RE. Fossilizing help
       - Updated the API docs for fossilize.
       - Changed method RE to use \w+ instead of .+

--- indico/MaKaC/common/fossilize.py
+++ indico/MaKaC/common/fossilize.py
@@ -13,6 +13,8 @@ Some of the features are:
 import inspect
 import re
 import zope.interface
+from types import NoneType, ClassType, TypeType
+from persistent.interfaces import IPersistent
 
 
 def fossilizes(*classList):
@@ -49,6 +51,7 @@ class WrongFossilTypeException(Exception):
 class InvalidFossilException(Exception):
     """
     The fossil name doesn't follow the convention I(\w+)Fossil
+    or has an invalid method name and did not declare a .name tag for it
     """
 
 class IFossil(zope.interface.Interface):
@@ -62,7 +65,7 @@ class Fossilizable(object):
     """
 
     __fossilNameRE = re.compile('^I(\w+)Fossil$')
-    __methodNameRE = re.compile('^get(\w+)$')
+    __methodNameRE = re.compile('^get(\w+)|(has\w+)|(is\w+)$')
     __methodNameCache = {}
     __fossilNameCache = {}
 
@@ -78,12 +81,12 @@ class Fossilizable(object):
             nmatch = cls.__methodNameRE.match(name)
 
             if not nmatch:
-                raise Exception("method name '%s' is not valid!"
-                                " has to start by 'get' or use "
-                                "'name' tag" % name)
+                raise InvalidFossilException("method name '%s' is not valid! "
+                                             "has to start by 'get', 'has', 'is' "
+                                             "or use 'name' tag" % name)
             else:
-                extractedName = nmatch.group(1)[0:1].lower() + \
-                                nmatch.group(1)[1:]
+                group = nmatch.group(1) or nmatch.group(2) or nmatch.group(3)
+                extractedName = group[0:1].lower() + group[1:]
                 cls.__methodNameCache[name] = extractedName
                 return extractedName
 
@@ -109,6 +112,63 @@ class Fossilizable(object):
         return fossilName
 
 
+    def __obtainInterface(self, interfaceArg):
+        """
+        Obtains the appropriate interface for this object.
+
+        :param interfaceArg: the target fossile type
+        :type interfaceArg: IFossil, NoneType, or dict
+
+            -If IFossil, we will use it.
+            -If None, we will take the default fossil (the first one of this class's "fossilizes" list)
+            -If a dict, we will use the object's class, class name, or full class name as key.
+
+        Also verifies that the interface obtained through these 3 methods is effectively provided by the object.
+        """
+        if interfaceArg is None:
+            #we try to take the 1st interface declared with fossilizes
+            implementedInterfaces = list(zope.interface.implementedBy(self.__class__))
+            if not implementedInterfaces or implementedInterfaces[0] == IPersistent:
+                raise NonFossilizableException("Object %s of class %s cannot be fossilized,"
+                                               "no fossils were declared for it" %
+                                               (str(self),
+                                                self.__class__.__name__))
+            else:
+                interface = implementedInterfaces[0]
+
+        elif type(interfaceArg) is dict:
+
+            found = False
+            clazz = self.__class__
+
+            for key in interfaceArg.iterkeys():
+                if (key == clazz.__name__ or #class name
+                    key == "%s.%s" % (clazz.__module__, clazz.__name__) or #full class name
+                    key == clazz or #class
+                    (type(key) == ClassType or type(key) == TypeType or type(key) == tuple) and isinstance(self, key)): #class (including base classes)
+
+                    interface = interfaceArg[key]
+                    found = True
+                    break
+
+            if not found:
+                raise NonFossilizableException("Object %s of class %s cannot be fossilized; "
+                                               "its class was not a key in the provided fossils dictionary" %
+                                               (str(self),
+                                                self.__class__.__name__))
+
+        else:
+            interface = interfaceArg
+
+        if not interface.providedBy(self):
+            raise WrongFossilTypeException("Interface '%s' not provided"
+                                           " by '%s'" %
+                                           (interface.__name__,
+                                            self.__class__.__name__))
+
+        return interface
+
+
     @classmethod
     def _fossilizeIterable(cls, target, interface, **kwargs):
         """
@@ -120,10 +180,10 @@ class Fossilizable(object):
             return target.fossilize(interface, **kwargs)
         else:
             ttype = type(target)
-            if ttype in [int, str, float]:
+            if ttype in [int, str, float, NoneType]:
                 return target
-            elif ttype in [list, set]:
-                container = ttype()
+            elif ttype in [list, set, tuple]:
+                container = [] #we turn sets and tuples into lists since JSON does not have sets / tuples
                 for elem in target:
                     container.append(fossilize(elem, interface, **kwargs))
                 return container
@@ -137,19 +197,16 @@ class Fossilizable(object):
 
             return fossilize(target, interface)
 
-    def fossilize(self, interface, **kwargs):
+
+    def fossilize(self, interfaceArg = None, **kwargs):
         """
         Fossilizes the object, using the fossil provided by `interface`.
 
-        :param interface: the target fossile type
-        :type interface: IFossil
+        :param interfaceArg: the target fossile type
+        :type interfaceArg: IFossil, NoneType, or dict
         """
 
-        if not interface.providedBy(self):
-            raise WrongFossilTypeException("Interface '%s' not provided"
-                                           " by '%s'" %
-                                           (interface.__name__,
-                                            self.__class__.__name__))
+        interface = self.__obtainInterface(interfaceArg)
 
         name = interface.getName()
         fossilName = self.__extractFossilName(name)
@@ -158,8 +215,12 @@ class Fossilizable(object):
 
         for method in interface:
 
-            methodResult = getattr(self, method)()
             tags = interface[method].getTaggedValueTags()
+            #Please use 'produce' as little as possible; there is almost always a more elegant and modular solution!
+            if 'produce' in tags:
+                methodResult = interface[method].getTaggedValue('produce')(self)
+            else:
+                methodResult = getattr(self, method)()
 
             # Result conversion
             if 'result' in tags:
@@ -197,7 +258,8 @@ class Fossilizable(object):
 
         return result
 
-def fossilize(target, interface, **kwargs):
+
+def fossilize(target, interfaceArg = None, **kwargs):
     """
     Method that allows the "fossilization" process to
     be called on data structures (lists, dictionaries
@@ -205,7 +267,7 @@ def fossilize(target, interface, **kwargs):
 
     :param target: target object to be fossilized
     :type target: Fossilizable
-    :param interface: target fossil type
-    :type interface: IFossil
+    :param interfaceArg: target fossil type
+    :type interfaceArg: IFossil, NoneType, or dict
     """
-    return  Fossilizable._fossilizeIterable(target, interface, **kwargs)
+    return Fossilizable._fossilizeIterable(target, interfaceArg, **kwargs)
--- tests/MaKaC_tests/common_tests/testFossilize.py
+++ tests/MaKaC_tests/common_tests/testFossilize.py
@@ -1,5 +1,6 @@
-from MaKaC.common.fossilize import IFossil, Fossilizable, fossilizes, fossilize,\
-    NonFossilizableException, WrongFossilTypeException, addFossil
+from MaKaC.common.fossilize import IFossil, Fossilizable, fossilizes, fossilize, \
+    NonFossilizableException, WrongFossilTypeException, addFossil,\
+    InvalidFossilException
 import unittest
 
 class ISomeFossil(IFossil):
@@ -40,10 +41,37 @@ class IDynamicFossil(IFossil):
         pass
     getB.convert = lambda x: x.upper()
 
+class IFossilWithProduceFossil(ISimpleFossil1Fossil):
+
+    def getSum(self):
+        pass
+    getSum.produce = lambda self: self.a + self.b
+
+class IClassWithDifferentMethodNamesFossil(IFossil):
+    def getAttributeOne(self):
+        pass
+    def hasAttributeTwo(self):
+        pass
+    def isAttributeThree(self):
+        pass
+    def weirdMethodName(self):
+        pass
+    weirdMethodName.name = 'weirdName'
+
+
+
+class IClassWithDifferentMethodNamesBadFossil(IClassWithDifferentMethodNamesFossil):
+    def weirdMethodName(self):
+        pass
+
+class IClassWithDifferentMethodNamesBad2Fossil(IClassWithDifferentMethodNamesFossil):
+    def get_type(self):
+        pass
+
 
 class SimpleClass(Fossilizable):
 
-    fossilizes(ISimpleFossil1Fossil, ISimpleFossil2Fossil)
+    fossilizes(ISimpleFossil1Fossil, ISimpleFossil2Fossil, IFossilWithProduceFossil)
 
     def __init__(self, a, b, c):
         self.a = a
@@ -64,10 +92,13 @@ class DerivedClass(SimpleClass):
 
 class ComplexClass(Fossilizable):
 
+    def __init__(self):
+        self.simpleInstance = SimpleClass(1, 'foo', 'bar')
+
     fossilizes(IComplexFossil1Fossil, IComplexFossil2Fossil)
 
     def getSimpleInstance(self):
-        return SimpleClass(1,'foo','bar')
+        return self.simpleInstance
 
 
 class ConversionClass(object):
@@ -75,14 +106,14 @@ class ConversionClass(object):
     def multiply(cls, number, factor):
         return number * factor
 
-def sum(number, numberToBeAdded):
+def mysum(number, numberToBeAdded):
     return number + numberToBeAdded
 
 
 class IWithConversion2Fossil(IFossil):
     def getA(self):
         pass
-    getA.convert = sum
+    getA.convert = mysum
 
 class IWithConversionFossil(IFossil):
     def getA(self):
@@ -94,9 +125,9 @@ class IWithConversionFossil(IFossil):
 
 class AnotherClass(Fossilizable):
     fossilizes(IWithConversionFossil)
-    def __init__(self, a , list = []):
+    def __init__(self, a , listArg):
         self.a = a
-        self.list = list
+        self.list = listArg
     def getA(self):
         return self.a
     def getList(self):
@@ -109,24 +140,50 @@ class AnotherChildrenClass(Fossilizable):
     def getA(self):
         return self.a
 
+class ClassWithDifferentMethodNames(Fossilizable):
+    fossilizes(IClassWithDifferentMethodNamesFossil, IClassWithDifferentMethodNamesBadFossil, IClassWithDifferentMethodNamesBad2Fossil)
+    def __init__(self):
+        pass
+    def getAttributeOne(self):
+        return 1
+    def hasAttributeTwo(self):
+        return 2
+    def isAttributeThree(self):
+        return 3
+    def weirdMethodName(self):
+        return 4
+    def get_type(self):
+        pass
+
+
 
 class TestFossilize(unittest.TestCase):
 
     def setUp(self):
         addFossil(SimpleClass, IDynamicFossil)
-        self.s = SimpleClass(1,'a','foo')
+        self.s = SimpleClass(1, 'a', 'foo')
         self.c = ComplexClass()
 
     def testFossilizePrimitives(self):
         self.assertEquals(4, fossilize(4, ISomeFossil))
         self.assertEquals('foo', fossilize('foo', ISomeFossil))
         self.assertEquals(5.0, fossilize(5.0, ISomeFossil))
+        self.assertEquals(None, fossilize(None, ISomeFossil))
 
     def testFossilizeStructuresOfPrimitives(self):
         self.assertEquals([], fossilize([], ISomeFossil))
-        self.assertEquals([4,[4,5,6]], fossilize([4,[4,5,6]], ISomeFossil))
+        self.assertEquals([4, [4, 5, 6], [1]], fossilize([4, (4, 5, 6), set([1])], ISomeFossil))
         self.assertEquals({'a':'foo', 2:'bar'}, fossilize({'a':'foo', 2:'bar'}, ISomeFossil))
         self.assertEquals({'a':'foo', 2:'bar', 3:5.0}, fossilize({'a':'foo', 2:'bar', 3:5.0}, ISomeFossil))
+
+    def testFossilizeNames(self):
+        o1 = ClassWithDifferentMethodNames()
+        self.assertEquals(o1.fossilize(IClassWithDifferentMethodNamesFossil), {'isAttributeThree': 3, 'attributeOne': 1, '_type': 'ClassWithDifferentMethodNames', 'weirdName': 4, 'hasAttributeTwo': 2, '_fossil': 'classWithDifferentMethodNames'})
+
+    def testInvalidNames(self):
+        o1 = ClassWithDifferentMethodNames()
+        self.assertRaises(InvalidFossilException, fossilize, o1, IClassWithDifferentMethodNamesBadFossil)
+        self.assertRaises(InvalidFossilException, fossilize, o1, IClassWithDifferentMethodNamesBad2Fossil)
 
     def testFossilizingNonFossilizable(self):
         self.assertRaises(NonFossilizableException, fossilize, 1+4j, ISomeFossil)
@@ -149,7 +206,7 @@ class TestFossilize(unittest.TestCase):
                           {'_type':'ComplexClass', '_fossil':'complexFossil2', 'mySimpleInstance': [1, 'Bar', 'SimpleClass', 'simpleFossil2']})
 
     def testFossilizingWithInheritance(self):
-        d = DerivedClass(2,'b','bar')
+        d = DerivedClass(2, 'b', 'bar')
         self.assertEquals(d.fossilize(ISimpleFossil1Fossil),
                           {"_type": "DerivedClass", "_fossil": "simpleFossil1", "b": "b", "c":"BAR"})
 
@@ -173,6 +230,32 @@ class TestFossilize(unittest.TestCase):
         aFather = AnotherClass(1000, [a1, a2])
         self.assertEquals(aFather.fossilize(IWithConversionFossil, factor = 10, numberToBeAdded = 1),
                           {'_type':'AnotherClass', '_fossil':'withConversion', 'a': 10000, 'list': [{'_type': 'AnotherChildrenClass', '_fossil': 'withConversion2', 'a': 11}, {'_type': 'AnotherChildrenClass', '_fossil': 'withConversion2', 'a': 21}]})
+
+    def testFossilizeProduce(self):
+        s1 = SimpleClass(10, 20, 'foo')
+        self.assertEquals(s1.fossilize(IFossilWithProduceFossil),
+                          {'_type': 'SimpleClass', '_fossil': 'fossilWithProduce', 'sum': 30, 'b': 20, 'c': 'FOO'})
+
+
+    def testFossilizeDefault(self):
+        s1 = SimpleClass(10, 20, 'foo')
+        d1 = DerivedClass(10, 50, 'bar')
+        self.assertEquals(s1.fossilize(), {'_type':'SimpleClass', '_fossil':'simpleFossil1', "b": 20, "c":"FOO"})
+        self.assertEquals(fossilize(d1), {'_type':'DerivedClass', '_fossil':'simpleFossil1', "b": 50, "c":"BAR"})
+        self.assertEquals(fossilize([s1, d1]), [fossilize(s1), d1.fossilize()])
+
+    def testFossilizeDict(self):
+        s1 = SimpleClass(10, 20, 'foo')
+        d1 = DerivedClass(10, 50, 'bar')
+        self.assertEquals(fossilize([s1, d1], {SimpleClass: ISimpleFossil2Fossil, DerivedClass: ISimpleFossil1Fossil}),
+                          [s1.fossilize(ISimpleFossil2Fossil), d1.fossilize(ISimpleFossil1Fossil)])
+        self.assertEquals(fossilize([s1, d1], {"SimpleClass": ISimpleFossil2Fossil, "DerivedClass": ISimpleFossil1Fossil}),
+                          [s1.fossilize(ISimpleFossil2Fossil), d1.fossilize(ISimpleFossil1Fossil)])
+        self.assertEquals(fossilize([s1, d1], {"MaKaC_tests.common_tests.testFossilize.SimpleClass": ISimpleFossil2Fossil,
+                                               "MaKaC_tests.common_tests.testFossilize.DerivedClass": ISimpleFossil1Fossil}),
+                          [s1.fossilize(ISimpleFossil2Fossil), d1.fossilize(ISimpleFossil1Fossil)])
+        self.assertEquals(fossilize([s1, d1], {SimpleClass: ISimpleFossil2Fossil}),
+                          [s1.fossilize(ISimpleFossil2Fossil), d1.fossilize(ISimpleFossil2Fossil)])
 
 
 if __name__ == '__main__':
