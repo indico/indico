6ebf55543e74771a80f4e871af5b3cfc8f845101
David Martín Clavo <david.martin.clavo@cern.ch>
2010-04-16 16:52:46 +0200

    [FIX] Make RecordingManager work with new plugin system

--- indico/MaKaC/plugins/Collaboration/RecordingManager/__init__.py
+++ indico/MaKaC/plugins/Collaboration/RecordingManager/__init__.py
@@ -19,12 +19,8 @@
 ## along with CDS Indico; if not, write to the Free Software Foundation, Inc.,
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-from MaKaC.plugins import getModules, initModule
 from MaKaC.i18n import _
 
 pluginType = "Collaboration"
 pluginName = "RecordingManager"
 pluginDescription = _("Recording Manager")
-
-modules = {}
-topModule = None
