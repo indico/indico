indico/indico

commit a217dc1f7cb34e7f692e6e077fdcb0e32c875a17

[OPT] Optimization of the timetable display
	- fixes #307
	- Replaced pickling by fossilization for the generation of the data structures needed for the display of the timetable. The fossils contain the minimal amount of information needed to display the timetable, which is less than what the pickling was retrieving
	- New minimal fossils were added for the managment timetable too.
	- Optimized the loop that is grouping entries by day
	- Removed dead code used for the management timetable generation
	- Added a argument to the Fossils.fossilize(), ScheduleToJson.process() and ScheduleToJson.processEntry() method in order to use the attributes cache, useful for fossils that have fields that are repeated.
	- Displaying the 'view event' timetable of a conference like 'CHEP 2009' takes now less than 8 seconds
	- Displaying the 'managment' timetable of a conference like 'CHEP 2009' takes now less than 4 seconds

indico/MaKaC/webinterface/pages/conferences.py
@@ -1153,7 +1153,9 @@ class WConferenceTimeTable(wcomponents.WTemplated):
     def getVars( self ):
         vars = wcomponents.WTemplated.getVars( self )
         tz = DisplayTZ(self._aw,self._conf).getDisplayTZ()
-        vars["ttdata"] = simplejson.dumps(schedule.ScheduleToJson.process(self._conf.getSchedule(), tz, self._aw))
+        vars["ttdata"] = simplejson.dumps(schedule.ScheduleToJson.process(self._conf.getSchedule(),
+                                                                          tz, self._aw,
+                                                                          useAttrCache = True))
         vars['eventInfo'] = simplejson.dumps(DictPickler.pickle(self._conf, timezone=tz))
         return vars
 
@@ -3142,12 +3144,9 @@ class WScheduleSlot(wcomponents.WTemplated):
 
 class WConfModifScheduleGraphic(wcomponents.WTemplated):
 
-    def __init__(self, conference, aw, timetable, dayList, **params):
+    def __init__(self, conference, **params):
         wcomponents.WTemplated.__init__(self, **params)
         self._conf = conference
-        self._aw = aw
-        self._timetable = timetable
-        self._dayList = dayList
 
     def getVars( self ):
         vars=wcomponents.WTemplated.getVars(self)
@@ -3163,24 +3162,8 @@ class WConfModifScheduleGraphic(wcomponents.WTemplated):
         #################################
         vars["editURL"]=quoteattr(str(urlHandlers.UHConfModScheduleDataEdit.getURL(self._conf)))
 
-        # the list of days specified by the user through the option box
-        vars["daysParam"] = self._dayList
-        # the list of days from the timetable
-        vars["dayList"]=self._timetable.getDayList()
-        # the first day of the list
-        vars["dayDate"]=self._dayList[0].getDate()
-
-        vars["addSessionURL"]=urlHandlers.UHConfAddSession.getURL(self._conf)
-        vars["addBreakURL"]=urlHandlers.UHConfAddBreak.getURL(self._conf)
-
-        originURL = urlHandlers.UHConfModifSchedule.getURL(self._conf)
-        newContribURL = urlHandlers.UHConfModScheduleNewContrib.getURL(self._conf)
-        newContribURL.addParam("originURL",originURL)
-        newContribURL.addParam("eventType","meeting")
-        vars["newContribURL"] = newContribURL
-        vars['rbActive'] = info.HelperMaKaCInfo.getMaKaCInfoInstance().getRoomBookingModuleActive()
-
-        vars['ttdata'] = simplejson.dumps(schedule.ScheduleToJson.process(self._conf.getSchedule(), tz, None))
+        vars['ttdata'] = simplejson.dumps(schedule.ScheduleToJson.process(self._conf.getSchedule(), tz, None,
+                                                                          days = None, mgmtMode = True))
         vars['eventInfo'] = simplejson.dumps(DictPickler.pickle(self._conf, timezone=tz))
 
         return vars
@@ -3191,7 +3174,6 @@ class WPConfModifScheduleGraphic( WPConferenceModifBase ):
 
     def __init__(self, rh, conf):
         WPConferenceModifBase.__init__(self, rh, conf)
-        self._session = None
         self._contrib = None
 
     def _setActiveSideMenuItem( self ):
@@ -3201,45 +3183,8 @@ class WPConfModifScheduleGraphic( WPConferenceModifBase ):
         return WPConferenceModifBase.getJSFiles(self) + \
                self._includeJSPackage('Timetable')
 
-    def _generateTimetable(self):
-        tz = self._conf.getTimezone()
-        timeTable = timetable.TimeTable(self._conf.getSchedule(), tz)
-        #######################################
-        # Fermi timezone awareness            #
-        #######################################
-        sDate = self._conf.getSchedule().getStartDate(tz)
-        eDate = self._conf.getSchedule().getEndDate(tz)
-        #######################################
-        # Fermi timezone awareness(end)       #
-        #######################################
-        timeTable.setStartDate(sDate)
-        timeTable.setEndDate(eDate)
-        #timeTable.setStartDayTime(7,0)
-        #timeTable.setEndDayTime(21,59)
-        timeTable.mapEntryList(self._conf.getSchedule().getEntries())
-
-        return timeTable
-
-    def _getSchedule(self):
-        return WConfModifScheduleGraphic( self._conf, self._getAW(), self._timetable, self._days)
-
     def _getTTPage( self, params ):
-
-        params["session"] = self._session
-
-        days = params.get("days", None)
-
-        timeTable= self._generateTimetable()
-
-        if days == None or days == 'all':
-            dayList = timeTable.getDayList()
-        else:
-            dayList = [timeTable.getDayList()[int(days)]]
-
-        self._days = dayList
-        self._timetable = timeTable
-
-        wc = self._getSchedule()
+        wc = WConfModifScheduleGraphic( self._conf)
         return wc.getHTML(params)
 
     def _getPageContent(self, params):

indico/MaKaC/webinterface/pages/sessions.py
@@ -1497,7 +1497,8 @@ class WSessionModifSchedule(wcomponents.WTemplated):
 
         vars['rbActive'] = info.HelperMaKaCInfo.getMaKaCInfoInstance().getRoomBookingModuleActive()
 
-        vars['ttdata'] = simplejson.dumps(schedule.ScheduleToJson.process(self._session.getSchedule(), tz, None))
+        vars['ttdata'] = simplejson.dumps(schedule.ScheduleToJson.process(self._session.getSchedule(), tz,
+                                                                           None, days = None, mgmtMode = True))
 
         eventInfo = DictPickler.pickle(self._session.getConference(), timezone=tz)
         eventInfo['timetableSession'] = DictPickler.pickle(self._session, timezone=tz)

indico/MaKaC/common/Conversion.py
@@ -62,27 +62,12 @@ class Conversion:
     @classmethod
     def parentSlot(cls, entry):
         from MaKaC.schedule import ContribSchEntry, BreakTimeSchEntry
-        from MaKaC.conference import SessionSlot, Session
+        from MaKaC.conference import SessionSlot#, Session
 
         slot = None
 
-        if type(entry) == ContribSchEntry:
-            contrib = entry.getOwner()
-            session = contrib.getSession()
-
-            # If the contribution is not owned by a session return nothing
-            if type(session) != Session:
-                return None
-
-            # TODO: This should be fixed. There's currently no easy way
-            # of knowing in which session slot a contribution belongs.
-            # Loop through all the session slots and look if the contribution
-            # exists in the schedule.
-            for sl in session.getSlotList():
-                for e in sl.getEntries():
-                    if e.getOwner() == contrib:
-                        slot = sl
-                        break
+        if type(entry) == ContribSchEntry and entry.getSchedule() is not None:
+            slot = entry.getSchedule().getOwner()
 
         elif type(entry) == BreakTimeSchEntry:
             slot = entry.getOwner()
@@ -101,6 +86,7 @@ class Conversion:
 
     @classmethod
     def locatorString(cls, obj):
+
         locator = obj.getOwner().getLocator()
         if not locator.has_key('sessionId'):
             if locator.has_key('contribId'):
@@ -118,3 +104,12 @@ class Conversion:
         Converts a timedelta to integer minutes
         """
         return int(obj.seconds / 60)
+
+#    @classmethod
+#    def resourceType(cls, obj):
+#
+#        from MaKaC.conference import Link
+#        if type(obj) == Link:
+#            return 'external'
+#        else:
+#            return 'stored'

indico/MaKaC/common/fossilize.py
@@ -32,7 +32,6 @@ import inspect
 import re
 import zope.interface
 from types import NoneType, ClassType, TypeType
-from persistent.interfaces import IPersistent
 
 
 def fossilizes(*classList):
@@ -85,9 +84,10 @@ class Fossilizable:
 
     __fossilNameRE = re.compile('^I(\w+)Fossil$')
     __methodNameRE = re.compile('^get(\w+)|(has\w+)|(is\w+)$')
-    __methodNameRE = re.compile('^get(.*)|(has.+)|(is.+)$')
     __methodNameCache = {}
     __fossilNameCache = {}
+    __fossilAttrsCache = {} # Attribute Cache for Fossils with
+                            # fields that are repeated
 
     @classmethod
     def __extractName(cls, name):
@@ -147,8 +147,8 @@ class Fossilizable:
         """
         if interfaceArg is None:
             #we try to take the 1st interface declared with fossilizes
-            implementedInterfaces = list(zope.interface.implementedBy(self.__class__))
-            if not implementedInterfaces or implementedInterfaces[0] == IPersistent:
+            implementedInterfaces = list(i for i in zope.interface.implementedBy(self.__class__) if i.extends(IFossil))
+            if not implementedInterfaces:
                 raise NonFossilizableException("Object %s of class %s cannot be fossilized,"
                                                "no fossils were declared for it" %
                                                (str(self),
@@ -158,20 +158,13 @@ class Fossilizable:
 
         elif type(interfaceArg) is dict:
 
-            found = False
-            clazz = self.__class__
+            className = self.__class__.__module__ + '.' + \
+                        self.__class__.__name__
 
-            for key in interfaceArg.iterkeys():
-                if (key == clazz.__name__ or #class name
-                    key == "%s.%s" % (clazz.__module__, clazz.__name__) or #full class name
-                    key == clazz or #class
-                    (type(key) == ClassType or type(key) == TypeType or type(key) == tuple) and isinstance(self, key)): #class (including base classes)
-
-                    interface = interfaceArg[key]
-                    found = True
-                    break
-
-            if not found:
+            # interfaceArg is a dictionary of class:Fossil pairs
+            if className in interfaceArg:
+                interface = interfaceArg[className]
+            else:
                 raise NonFossilizableException("Object %s of class %s cannot be fossilized; "
                                                "its class was not a key in the provided fossils dictionary" %
                                                (str(self),
@@ -180,6 +173,7 @@ class Fossilizable:
             interface = interfaceArg
 
         if not interface.providedBy(self):
+
             raise WrongFossilTypeException("Interface '%s' not provided"
                                            " by '%s'" %
                                            (interface.__name__,
@@ -189,14 +183,14 @@ class Fossilizable:
 
 
     @classmethod
-    def _fossilizeIterable(cls, target, interface, **kwargs):
+    def _fossilizeIterable(cls, target, interface, useAttrCache = False, **kwargs):
         """
         Fossilizes an object, be it a 'direct' fossilizable
         object, or an iterable (dict, list, set);
         """
 
         if isinstance(target, Fossilizable):
-            return target.fossilize(interface, **kwargs)
+            return target.fossilize(interface, useAttrCache, **kwargs)
         else:
             ttype = type(target)
             if ttype in [int, str, float, NoneType]:
@@ -204,25 +198,27 @@ class Fossilizable:
             elif ttype in [list, set, tuple]:
                 container = [] #we turn sets and tuples into lists since JSON does not have sets / tuples
                 for elem in target:
-                    container.append(fossilize(elem, interface, **kwargs))
+                    container.append(fossilize(elem, interface, useAttrCache, **kwargs))
                 return container
             elif ttype is dict:
                 container = {}
                 for key, value in target.iteritems():
-                    container[key] = fossilize(value, interface, **kwargs)
+                    container[key] = fossilize(value, interface, useAttrCache, **kwargs)
                 return container
             else:
                 raise NonFossilizableException()
 
-            return fossilize(target, interface)
+            return fossilize(target, interface, useAttrCache)
 
-
-    def fossilize(self, interfaceArg = None, **kwargs):
+    def fossilize(self, interfaceArg = None, useAttrCache = False, **kwargs):
         """
         Fossilizes the object, using the fossil provided by `interface`.
 
         :param interfaceArg: the target fossile type
         :type interfaceArg: IFossil, NoneType, or dict
+        :param useAttrCache: use caching of attributes if same fields are
+            repeated for a fossil
+        :type useAttrCache: boolean
         """
 
         interface = self.__obtainInterface(interfaceArg)
@@ -235,16 +231,35 @@ class Fossilizable:
         for method in interface:
 
             tags = interface[method].getTaggedValueTags()
-            #Please use 'produce' as little as possible; there is almost always a more elegant and modular solution!
-            if 'produce' in tags:
-                methodResult = interface[method].getTaggedValue('produce')(self)
-            else:
-                methodResult = getattr(self, method)()
+
+            # In some cases it is better to use the attribute cache to
+            # speed up the fossilization
+            cacheUsed = False
+            if useAttrCache:
+                try:
+                    methodResult = self.__fossilAttrsCache[self._p_oid][method]
+                    cacheUsed = True
+                except KeyError:
+                    pass
+            if not cacheUsed:
+                #Please use 'produce' as little as possible; there is almost always a more elegant and modular solution!
+                if 'produce' in tags:
+                    methodResult = interface[method].getTaggedValue('produce')(self)
+                else:
+                    methodResult = getattr(self, method)()
+
+                if hasattr(self, "_p_oid"):
+                    try:
+                        self.__fossilAttrsCache[self._p_oid]
+                    except KeyError:
+                        self.__fossilAttrsCache[self._p_oid] = {}
+                    self.__fossilAttrsCache[self._p_oid][method] = methodResult
 
             # Result conversion
             if 'result' in tags:
                 targetInterface = interface[method].getTaggedValue('result')
                 #targetInterface = globals()[targetInterfaceName]
+
                 methodResult = Fossilizable._fossilizeIterable(
                     methodResult, targetInterface, **kwargs)
 
@@ -263,7 +278,27 @@ class Fossilizable:
             else:
                 attrName = self.__extractName(method)
 
-            result[attrName] = methodResult
+            # In case the name contains dots, each of the 'domains' but the
+            # last one are translated into nested dictionnaries. For example,
+            # if we want to re-name an attribute into "foo.bar.tofu", the
+            # corresponding fossilized attribute will be of the form:
+            # {"foo":{"bar":{"tofu": res,...},...},...}
+            # instead of:
+            # {"foo.bar.tofu": res, ...}
+
+            current = result
+            attrList = attrName.split('.')
+
+            while len(attrList) > 1:
+                attr = attrList.pop(0)
+                try:
+                    current = current[attr]
+                except KeyError:
+                    current[attr] = {}
+                    current = current[attr]
+
+            # For the last attribute level
+            current[attrList[0]] = methodResult
 
         if "_type" in result or "_fossil" in result:
             raise InvalidFossilException('"_type" or "_fossil"'
@@ -278,7 +313,7 @@ class Fossilizable:
         return result
 
 
-def fossilize(target, interfaceArg = None, **kwargs):
+def fossilize(target, interfaceArg = None, useAttrCache = False, **kwargs):
     """
     Method that allows the "fossilization" process to
     be called on data structures (lists, dictionaries
@@ -288,5 +323,7 @@ def fossilize(target, interfaceArg = None, **kwargs):
     :type target: Fossilizable
     :param interfaceArg: target fossil type
     :type interfaceArg: IFossil, NoneType, or dict
+    :param useAttrCache: use the attribute caching
+    :type useAttrCache: boolean
     """
-    return Fossilizable._fossilizeIterable(target, interfaceArg, **kwargs)
+    return Fossilizable._fossilizeIterable(target, interfaceArg, useAttrCache, **kwargs)

indico/MaKaC/conference.py
@@ -24,9 +24,14 @@ from MaKaC.common.utils import formatDateTime
 from MaKaC.fossils.subcontribution import ISubContribParticipationFossil,\
     ISubContributionFossil, ISubContributionWithSpeakersFossil
 from MaKaC.fossils.contribution import IContributionParticipationFossil,\
-    IContributionFossil, IContributionWithSpeakersFossil,\
-    IContributionWithSubContribsFossil
-from MaKaC.fossils.conference import IConferenceMinimalFossil
+    IContributionFossil, IContributionWithSpeakersFossil, IContributionParticipationMinimalFossil, \
+    IContributionWithSubContribsFossil,\
+    IContributionParticipationTTDisplayFossil, \
+    IContributionParticipationTTMgmtFossil
+from MaKaC.fossils.conference import IConferenceMinimalFossil,\
+    ISessionFossil, ISessionSlotFossil, IMaterialFossil,\
+    IConferenceParticipationFossil, IResourceFossil, ILinkFossil,\
+    ILocalFileFossil
 from MaKaC.common.fossilize import fossilizes, Fossilizable
 
 import re, os
@@ -1504,7 +1509,9 @@ class CustomRoom(Persistent):
         return self.name
 
 
-class ConferenceParticipation(Persistent):
+class ConferenceParticipation(Persistent, Fossilizable):
+
+    fossilizes(IConferenceParticipationFossil)
 
     @Retrieves(['MaKaC.conference.ConferenceParticipation',
                  'MaKaC.conference.SessionChair',
@@ -5350,7 +5357,7 @@ class SCIndex(Persistent):
         self._idx._p_changed=1
 
 
-class Session(Persistent):
+class Session(Persistent, Fossilizable):
     """This class implements a conference session, being the different parts
         in which the conference can be divided and the contributions can be
         organised in. The class contains necessary attributes to store session
@@ -5359,6 +5366,7 @@ class Session(Persistent):
         conference but it is allowed for flexibility.
     """
 
+    fossilizes(ISessionFossil)
 
     @Retrieves(['MaKaC.conference.Session'], 'numSlots', lambda x : len(x.getSlotList()))
 
@@ -6840,7 +6848,9 @@ class Session(Persistent):
         return cmp( s1, s2 )
     _cmpTitle=staticmethod(_cmpTitle)
 
-class SessionSlot(Persistent):
+class SessionSlot(Persistent, Fossilizable):
+
+    fossilizes(ISessionSlotFossil)
 
     def __init__(self,session,**sessionSlotData):
         self.session = session
@@ -7487,7 +7497,9 @@ class SessionSlot(Persistent):
 
 class ContributionParticipation(Persistent, Fossilizable):
 
-    fossilizes(IContributionParticipationFossil)
+    fossilizes(IContributionParticipationFossil, IContributionParticipationMinimalFossil,\
+               IContributionParticipationTTDisplayFossil,\
+               IContributionParticipationTTMgmtFossil)
 
     def __init__( self ):
         self._contrib = None
@@ -7901,7 +7913,8 @@ class Contribution(Persistent, Fossilizable):
         attached either to a session or to a conference.
     """
 
-    fossilizes(IContributionFossil, IContributionWithSpeakersFossil, IContributionWithSubContribsFossil)
+    fossilizes(IContributionFossil, IContributionWithSpeakersFossil,\
+                IContributionWithSubContribsFossil)
 
     def __init__(self,**contribData):
         self.parent = None
@@ -9798,6 +9811,19 @@ class Contribution(Persistent, Fossilizable):
         else:
             return maxDatetime()
 
+    @Retrieves(['MaKaC.conference.Contribution'], 'color')
+    def getColor(self):
+        res=""
+        if self.getSession() is not None:
+            res=self.getSession().getColor()
+        return res
+
+    @Retrieves(['MaKaC.conference.Contribution'], 'textColor')
+    def getTextColor(self):
+        res=""
+        if self.getSession() is not None:
+            res=self.getSession().getTextColor()
+        return res
 
 class AcceptedContribution( Contribution ):
     """This class represents a contribution which has been created from an
@@ -10839,7 +10865,7 @@ class SubContribution(Persistent, Fossilizable):
     def setReportNumberHolder(self, rnh):
         self._reportNumberHolder=rnh
 
-class Material(Persistent):
+class Material(Persistent, Fossilizable):
     """This class represents a set of electronic documents (resources) which can
         be attached to a conference, a session or a contribution.
         A material can be of several types (achieved by specialising this class)
@@ -10859,6 +10885,8 @@ class Material(Persistent):
             material. Dictionary of references to Resource objects indexed
             by their unique relative id.
     """
+
+    fossilizes(IMaterialFossil)
 
     def __init__( self, materialData=None ):
         self.id = "not assigned"
@@ -11584,7 +11612,7 @@ class Minutes(Material):
             Material.recoverResource(self, recRes)
 
 
-class Resource(Persistent):
+class Resource(Persistent, Fossilizable):
     """This is the base class for representing individual resources which can
         be included in material containers for lately being attached to
         conference objects (i.e. conferences, sessions or contributions). This
@@ -11602,6 +11630,8 @@ class Resource(Persistent):
         __owner - (Material) reference to the material object in which the
             current resource is included.
     """
+
+    fossilizes(IResourceFossil)
 
     @Retrieves (['MaKaC.conference.Link',
                  'MaKaC.conference.LocalFile'], 'type', lambda r: if_else(type(r) == Link , 'external', 'stored'))
@@ -11944,6 +11974,8 @@ class Link(Resource):
         url -- (string) Contains the URL to the internet target resource.
     """
 
+    fossilizes(ILinkFossil)
+
     def __init__( self, resData = None ):
         Resource.__init__( self, resData )
         self.url = ""
@@ -11980,6 +12012,8 @@ class LocalFile(Resource):
         __archivedId -- (string) It contains a unique identifier for the file
             inside the repository where it is archived.
     """
+
+    fossilizes(ILocalFileFossil)
 
     def __init__( self, resData = None ):
         Resource.__init__( self, resData )

indico/MaKaC/fossils/conference.py
@@ -20,6 +20,7 @@
 
 from MaKaC.common.fossilize import IFossil
 from MaKaC.common.Conversion import Conversion
+from MaKaC.webinterface import urlHandlers
 
 class IConferenceMinimalFossil(IFossil):
 
@@ -54,4 +55,119 @@ class IConferenceMinimalFossil(IFossil):
     def getSupportEmail(self):
         """ Support Email """
 
+class IConferenceParticipationFossil(IFossil):
 
+    def getFirstName( self ):
+        """ Conference Participation First Name """
+
+    def getFamilyName( self ):
+        """ Conference Participation Family Name """
+
+
+class IResourceFossil(IFossil):
+
+    def getName(self):
+        """ Name of the Resource """
+
+
+class ILinkFossil(IResourceFossil):
+
+    def getURL(self):
+        """ URL of the file pointed by the link """
+    getURL.name = "url"
+
+class ILocalFileFossil(IResourceFossil):
+
+    def getURL(self):
+        """ URL of the Local File """
+    getURL.produce = lambda s: str(urlHandlers.UHFileAccess.getURL(s))
+    getURL.name = "url"
+
+
+class IMaterialFossil(IFossil):
+
+    def getTitle( self ):
+        """ Material Title """
+
+    def getResourceList(self):
+        """ Material Resource List """
+    getResourceList.result = {"MaKaC.conference.Link": ILinkFossil, "MaKaC.conference.LocalFile": ILocalFileFossil}
+    getResourceList.name = "resources"
+
+
+class ISessionFossil(IFossil):
+
+    def getTitle(self):
+        """ Session Title """
+
+    def getId(self):
+        """ Session Id """
+    getId.name = "sessionId"
+
+    def getDescription(self):
+        """ Session Description """
+
+    def getAllMaterialList(self):
+        """ Session List of all material """
+    getAllMaterialList.result = IMaterialFossil
+    getAllMaterialList.name = "material"
+
+    def getColor(self):
+        """ Session Color """
+
+    def getTextColor(self):
+        """ Session Text Color """
+
+    def isPoster(self):
+        """ Is self a Poster Session ? """
+    isPoster.produce = lambda s: s.getScheduleType() == 'poster'
+
+
+class ISessionSlotFossil(IFossil):
+
+    def getSession(self):
+        """ Slot Session """
+    getSession.result = ISessionFossil
+
+    def getId(self):
+        """ Session Slot Id """
+    getId.name = "sessionSlotId"
+
+    def getTitle(self):
+        """ Session Slot Title """
+    getTitle.name = "slotTitle"
+
+    def getConference(self):
+        """ Session Slot Conference """
+    getConference.result = IConferenceMinimalFossil
+
+    def getRoom(self):
+        """ Session Slot Room """
+    getRoom.convert = Conversion.roomName
+
+    def getLocationName(self):
+        """ Session Slot Location Name """
+    getLocationName.produce = lambda s: s.getLocation()
+    getLocationName.convert = Conversion.locationName
+    getLocationName.name = "location"
+
+    def getLocationAddress(self):
+        """ Session Slot Location Address """
+    getLocationAddress.produce = lambda s: s.getLocation()
+    getLocationAddress.convert = Conversion.locationAddress
+    getLocationAddress.name = "address"
+
+    def inheritRoom(self):
+        """ Does the Session inherit a Room ?"""
+    inheritRoom.produce = lambda s: s.getOwnRoom() is None
+    inheritRoom.name = "inheritRoom"
+
+    def inheritLocation(self):
+        """ Does the Session inherit a Location ?"""
+    inheritLocation.produce = lambda s: s.getOwnLocation() is None
+    inheritLocation.name = "inheritLoc"
+
+    def getOwnConvenerList(self):
+        """ Session Slot Conveners List """
+    getOwnConvenerList.result = IConferenceParticipationFossil
+    getOwnConvenerList.name = "conveners"

commit 0f44e4c50e72ffb6c0195deb3285a5f33c6101f2

[FIX] Session coordinator timetable display
 - the top level timetable was being shown instead (bug introduced by
   a217dc1f7cb34e7f692e6e077fdcb0e32c875a17);

indico/MaKaC/webinterface/pages/conferences.py
@@ -3185,8 +3185,11 @@ class WPConfModifScheduleGraphic( WPConferenceModifBase ):
         return WPConferenceModifBase.getJSFiles(self) + \
                self._includeJSPackage('Timetable')
 
+    def _getSchedule(self):
+        return WConfModifScheduleGraphic( self._conf )
+
     def _getTTPage( self, params ):
-        wc = WConfModifScheduleGraphic( self._conf)
+        wc = self._getSchedule()
         return wc.getHTML(params)
 
     def _getPageContent(self, params):

indico/MaKaC/webinterface/pages/sessions.py
@@ -1471,31 +1471,21 @@ class WPSessionModifSchedule( WPSessionModifBase, WPConfModifScheduleGraphic  ):
         return timeTable
 
     def _getSchedule(self):
-        return WSessionModifSchedule(self._session, self._timetable, self._days)
+        return WSessionModifSchedule(self._session)
 
     def _getTabContent( self, params ):
         return self._getTTPage(params)
 
 class WSessionModifSchedule(wcomponents.WTemplated):
 
-    def __init__(self, session, timetable, dayList, **params):
+    def __init__(self, session, **params):
         wcomponents.WTemplated.__init__(self, **params)
         self._session = session
-        self._timetable = timetable
-        self._dayList = dayList
 
     def getVars( self ):
         vars=wcomponents.WTemplated.getVars(self)
         tz = self._session.getTimezone()
         vars["timezone"]= tz
-        # the list of days specified by the user through the option box
-        vars["daysParam"] = self._dayList
-        # the list of days from the timetable
-        vars["dayList"]=self._timetable.getDayList()
-        # the first day of the list
-        vars["dayDate"]=self._dayList[0].getDate()
-
-        vars['rbActive'] = info.HelperMaKaCInfo.getMaKaCInfoInstance().getRoomBookingModuleActive()
 
         vars['ttdata'] = simplejson.dumps(schedule.ScheduleToJson.process(self._session.getSchedule(), tz,
                                                                            None, days = None, mgmtMode = True))

indico/MaKaC/webinterface/tpls/SessionModifSchedule.tpl
@@ -17,7 +17,6 @@ var timetable = new SessionManagementTimeTable(ttdata, eventInfo, 900,$E('timeta
 
 IndicoUI.executeOnLoad(function(){
 
-
   $E('timetableDiv').set(timetable.draw());
   timetable.postDraw();
 