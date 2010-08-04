5d34ab116e2787cd3453ab36313df56b5641718a
Kevin Flannery <flannery@fnal.gov>
2010-08-04 15:12:37 -0500

    Removed unnecessary old files from WebEx directory

--- indico/MaKaC/plugins/Collaboration/WebEx/WebEx/tpls/Indexing.js
+++
@@ -1,9 +0,0 @@
-{
-    "customText": function(booking, viewBy) {
-        if (booking.error && booking.errorMessage) {
-            return '<span class="collaborationWarning">' + booking.errorMessage + '<\/span>'
-        } else {
-            return "Title: " + booking.bookingParams.meetingTitle;
-        }
-    }
-}
