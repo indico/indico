cd0497e5c708e48118ad532c260cb5e40aadea20
Nikolche Mihajlovski <nmihajlo@cern.ch>
2010-09-20 17:40:03 +0200

    [MIN] The comma is now not supported in the format.

--- indico/MaKaC/registration.py
+++ indico/MaKaC/registration.py
@@ -42,7 +42,7 @@ import re
 
 import string
 
-PRICE_PATTERN = re.compile(r'^(\d+(?:[\.\,]\d+)?)$')
+PRICE_PATTERN = re.compile(r'^(\d+(?:[\.]\d+)?)$')
 
 def stringToDate( str ):
     months = {  "January":1, "February":2, "March":3, "April":4, "May":5, "June":6, "July":7, "August":8, "September":9, "October":10, "November":11, "December":12 }
