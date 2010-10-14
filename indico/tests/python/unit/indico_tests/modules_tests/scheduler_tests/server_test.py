a2f0ac06d22d9e5c2a40f76acc5f102d6dec344d
Pedro Ferreira <jose.pedro.ferreira@cern.ch>
2010-10-14 15:18:49 +0200

    [FIX] Scheduler - Reduced simulation speed
     * So that tests don't fail;

--- indico/tests/python/unit/indico_tests/modules_tests/scheduler_tests/server_test.py
+++ indico/tests/python/unit/indico_tests/modules_tests/scheduler_tests/server_test.py
@@ -52,7 +52,7 @@ class TestTimeSource(base.TimeSource):
         time.sleep(amount/float(self._factor))
 
 
-base.TimeSource.set(TestTimeSource(5))
+base.TimeSource.set(TestTimeSource(4))
 
 class TestTask(OneShotTask):
 
