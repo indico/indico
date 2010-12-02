d279bfe2a15738540419d771c0d1f3bc2f0972d5
Leszek Syroka <leszek.marek.syroka@cern.ch>
2010-12-02 10:48:31 +0100

    [FIX] Google bot error
    	- resetSessionTZ.py webpage can be opened without passing any parameters
    	- fix#560

--- indico/MaKaC/webinterface/rh/resetTimezone.py
+++ indico/MaKaC/webinterface/rh/resetTimezone.py
@@ -1,25 +1,23 @@
 #import syslog
-from MaKaC.common import DBMgr
 import MaKaC.webinterface.rh.base as base
-from mod_python import util
 import MaKaC.common.info as info
 
 class RHResetTZ(base.RH):
-    
+
     def _process(self):
         sess = self._aw.getSession()
         #syslog.syslog("In RHResetTZ id: " + str(sess.id))
         parms = self._getRequestParams()
-            
+
         tz = None
-        if parms["activeTimezone"] == "My":
+        if not parms.has_key("activeTimezone") or parms["activeTimezone"] == "My":
             if self._aw.getUser():
                 tz = self._aw.getUser().getTimezone()
             else:
                 tz = info.HelperMaKaCInfo.getMaKaCInfoInstance().getTimezone()
         else:
             tz = parms["activeTimezone"]
-        
+
         try:
             if parms["saveToProfile"] == "on":
                 user = sess.getUser()
@@ -30,7 +28,6 @@ class RHResetTZ(base.RH):
                     user.setDisplayTZMode("MyTimezone")
         except:
             pass
-        
         sess.setVar("ActiveTimezone", tz)
         # redirect to the calling URL with the new session tz.
         #DBMgr.getInstance().endRequest(True)
