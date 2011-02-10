33f434fb1ecc9647bc3a267058ca847d66e7313e
Adrian Moennich <jerome.ernst.monnich@cern.ch>
2011-02-10 09:06:27 +0100

    [FIX] Fix "edit session block" dialog in IE
    - do not pass invalid string to Date() constructor but use
      Util.parseJSDateTime() instead
    - also stop tainting the global namespace with local variables
    - fixes #653

--- indico/htdocs/js/indico/Legacy/Dialogs.js
+++ indico/htdocs/js/indico/Legacy/Dialogs.js
@@ -731,9 +731,10 @@ extend(IndicoUI.Dialogs,
                                        slot.confId = slot.conference.id;
                                        slot.title = slot.session.title;
                                        slot.scheduleEntry = slot.sessionSlotId;
-                                       days = [];
-                                       stDay = new Date(slot.conference.startDate.date)
-                                       eDay = new Date(slot.conference.endDate.date)
+                                       var days = [];
+                                       var stDay = Util.parseJSDateTime(slot.conference.startDate.date, '%Y-%m-%d');
+                                       var eDay = Util.parseJSDateTime(slot.conference.endDate.date, '%Y-%m-%d');
+
                                        while(stDay <= eDay){
                                            days.push(IndicoUtil.formatDate2(stDay));
                                            stDay.setDate(stDay.getDate() + 1);
