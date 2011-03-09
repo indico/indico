2c523d93a7d433e4996267401f8bb26ff3b4e5bc
Adrian Moennich <jerome.ernst.monnich@cern.ch>
2011-03-09 14:57:51 +0100

    [FIX] Compress instant messaging javascript

--- etc/js/jsbuild.sh
+++ etc/js/jsbuild.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 rm ../../indico/htdocs/js/indico/pack/*.js
-rm ../indico/htdocs/js/presentation/pack/*.js
+rm ../../indico/htdocs/js/presentation/pack/*.js
+rm ../../indico/MaKaC/plugins/InstantMessaging/htdocs/js/*.pack.js
 
 jsbuild -o ../../indico/htdocs/js/indico/pack -v indico.cfg
 jsbuild -o ../../indico/htdocs/js/presentation/pack -v presentation.cfg
+jsbuild -o ../../indico/MaKaC/plugins/InstantMessaging/htdocs/js -v instantmessaging.cfg
