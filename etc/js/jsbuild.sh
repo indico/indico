1aa778c9cc2bfa56cf08ac06ee1ffa0129f130b1
Jose Benito <jose.benito.gonzalez@cern.ch>
2011-03-10 15:44:32 +0100

    [ADD] Add livesync config for packing

--- etc/js/jsbuild.sh
+++ etc/js/jsbuild.sh
@@ -3,7 +3,9 @@
 rm ../../indico/htdocs/js/indico/pack/*.js
 rm ../../indico/htdocs/js/presentation/pack/*.js
 rm ../../indico/MaKaC/plugins/InstantMessaging/htdocs/js/*.pack.js
+rm ../../indico/ext/livesync/htdocs/js/*.pack.js
 
 jsbuild -o ../../indico/htdocs/js/indico/pack -v indico.cfg
 jsbuild -o ../../indico/htdocs/js/presentation/pack -v presentation.cfg
 jsbuild -o ../../indico/MaKaC/plugins/InstantMessaging/htdocs/js -v instantmessaging.cfg
+jsbuild -o ../../indico/ext/livesync/htdocs/js -v livesync.cfg
