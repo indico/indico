5ad497a7d035e9669995ad2609028d8c5ca4d879
Adrian Moennich <jerome.ernst.monnich@cern.ch>
2011-05-19 11:14:38 +0200

    [MIN] Use tuple instead of list

--- indico/util/metadata/xml.py
+++ indico/util/metadata/xml.py
@@ -41,7 +41,7 @@ class XMLSerializer(Serializer):
     def _convert(self, value):
         if type(value) == datetime:
             return value.isoformat()
-        elif type(value) in [int, float, bool]:
+        elif type(value) in (int, float, bool):
             return str(value)
         else:
             return value.decode('utf-8') if type(value) == str else value
