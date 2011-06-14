384377710a95561d2e7f8051eaaa585850b82c44
Pedro Ferreira <jose.pedro.ferreira@cern.ch>
2011-06-14 10:14:40 +0200

    [VER] Tagged 0.97.1

--- generate.sh
+++ generate.sh
@@ -12,7 +12,7 @@ do
   esac
 done
 
-for EXECUTABLE in python2.6 python2.5 python2.4; do
+for EXECUTABLE in python2.7 python2.6 python2.5 python2.4; do
     $EXECUTABLE setup.py egg_info $DATEOPT bdist_egg
     EGG_NAME=dist/`$EXECUTABLE setup.py egg_filename | tail -n 1`.egg
     md5sum $EGG_NAME | sh -c 'read a; echo ${a%% *}' > $EGG_NAME.md5
--- indico/MaKaC/__init__.py
+++ indico/MaKaC/__init__.py
@@ -19,7 +19,7 @@
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
 DEVELOPMENT = 0
-__version__="0.97.0"
+__version__="0.97.1"
 
 def setEnvironment():
     import os
