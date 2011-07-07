0f6f56b9ddc74b4f13fe58b5aa177e8ed1d70985
Pedro Ferreira <jose.pedro.ferreira@cern.ch>
2011-07-07 11:28:42 +0200

    [FIX] @requires line confuses jstools

--- indico/htdocs/js/jquery/jquery.form.js
+++ indico/htdocs/js/jquery/jquery.form.js
@@ -1,7 +1,6 @@
 /*!
  * jQuery Form Plugin
  * version: 2.82 (15-JUN-2011)
- * @requires jQuery v1.3.2 or later
  *
  * Examples and documentation at: http://malsup.com/jquery/form/
  * Dual licensed under the MIT and GPL licenses:
