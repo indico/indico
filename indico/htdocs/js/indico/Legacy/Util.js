bb7ec8d70af60e6ece3ddb0593c8fbb40854f3d1
Jose Benito <jose.benito.gonzalez@cern.ch>
2011-09-09 12:17:43 +0200

    [FIX] Bug that allowed to skip the regform check

--- indico/htdocs/js/indico/Legacy/Util.js
+++ indico/htdocs/js/indico/Legacy/Util.js
@@ -607,7 +607,7 @@ var IndicoUtil = {
                 else if (dataType == 'shortURL' && !IndicoUtil.parseShortURL(component.get())) {
                     error = Html.span({}, $T("The short URL contains invalid characters. The allowed characters are alphanumeric, _, - and ."));
                 }
-                else if (!allowEmpty && trim(component.get()) === '') {
+                else if (!allowEmpty && component.get() != null && trim(component.get()) === '') {
                     error = Html.span({}, $T("Field is mandatory"));
                 }
                 if (exists(extraCheckFunction)) {
