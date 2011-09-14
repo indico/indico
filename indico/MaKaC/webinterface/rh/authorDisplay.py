223eefd6ae3529b13eb0af1a0476377fbbf48012
Jose Benito <jose.benito.gonzalez@cern.ch>
2011-09-14 09:11:32 +0200

    [FIX] Error in AuthorDisplay with no authId

--- indico/MaKaC/webinterface/rh/authorDisplay.py
+++ indico/MaKaC/webinterface/rh/authorDisplay.py
@@ -18,16 +18,18 @@
 ## along with CDS Indico; if not, write to the Free Software Foundation, Inc.,
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-from MaKaC.webinterface.rh.conferenceDisplay import RHConferenceBaseDisplay
 from MaKaC.webinterface.pages import authors
 from MaKaC.webinterface import urlHandlers
 from MaKaC.webinterface.rh.contribDisplay import RHContributionDisplayBase
+from MaKaC.errors import MaKaCError
 
 class RHAuthorDisplayBase( RHContributionDisplayBase ):
 
     def _checkParams( self, params ):
         RHContributionDisplayBase._checkParams( self, params )
         self._authorId = params.get( "authorId", "" ).strip()
+        if self._authorId == "":
+            raise MaKaCError(_("Author ID not found. The URL you are trying to access might be wrong."))
 
 
 class RHAuthorDisplay( RHAuthorDisplayBase ):
