69e12f8d5771aa9fecff8e73317e70a9ab45ada7
Alberto Resco Perez <alberto.resco.perez@cern.ch>
2011-12-01 18:24:32 +0100

    [FIX] Multiple file upload

---
+++ indico/htdocs/js/indico/Management/Registration.js
@@ -0,0 +1,40 @@
+type("RegistrationUploadFile",[],
+        {
+        draw: function ()
+        {
+            return this.uploadWidget;
+        }
+
+        },
+        function(id, valueDisplay, fileName, mandatory){
+            var imageRemove = Html.img({
+                src: imageSrc("remove"),
+                alt: $T('Remove attachment'),
+                title: $T('Remove this attachment'),
+                id: id,
+                style:{marginLeft:'15px', cursor:'pointer', verticalAlign:'bottom'}
+            });
+
+            var uploadFileInput = Html.input('file', {id: id, name: id});
+            var existingAttachment = Html.div({}, $(valueDisplay)[0],
+                                                  imageRemove,
+                                                  Html.input("hidden",{id: id, name: id}, fileName));
+
+            $(imageRemove.dom).click(function(e) {
+                $E("attachment"+id).set(uploadFileInput);
+                if(mandatory){
+                    addParam($E(id), 'text', false);
+                }
+            });
+
+            if(mandatory){
+                addParam($E(id), 'text', false);
+            }
+
+            if(valueDisplay){
+                this.uploadWidget = existingAttachment
+            } else{
+                this.uploadWidget = uploadFileInput;
+            }
+
+        });
