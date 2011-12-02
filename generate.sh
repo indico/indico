b6f24c9378ea5e262562cc20c1539d66a5eb9dd3
Pedro Ferreira <jose.pedro.ferreira@cern.ch>
2011-12-02 15:33:14 +0100

    [FIX] Generation script

--- generate.sh
+++ generate.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Generates Python eggs for 2.4, 2.5 and 2.6
+# Generates Python eggs for 2.6 and 2.7
 # -d can be passed for 'nighly builds', so that the current date is appended
 
 DATEOPT=
@@ -12,7 +12,7 @@ do
   esac
 done
 
-for EXECUTABLE in python2.6 python2.5 python2.4; do
+for EXECUTABLE in python2.6 python2.7; do
     $EXECUTABLE setup.py egg_info $DATEOPT bdist_egg
     EGG_NAME=dist/`$EXECUTABLE setup.py egg_filename | tail -n 1`.egg
     md5sum $EGG_NAME | sh -c 'read a; echo ${a%% *}' > $EGG_NAME.md5
