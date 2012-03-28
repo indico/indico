ef29b378e34349d24bf39413b82606330fbb54dc
Pedro Ferreira <jose.pedro.ferreira@cern.ch>
2012-03-28 09:41:26 +0200

    [FIX] Do not clone past alarms

--- indico/MaKaC/conference.py
+++ indico/MaKaC/conference.py
@@ -4322,8 +4322,10 @@ class Conference(CommonObjectBase, Locatable):
         # conference's alerts
         if options.get("alerts",False) :
             for alarm in self.getAlarmList() :
-                # .clone takes care of enqueuing it
-                alarm.clone(conf)
+                # only clone alarms that will happen in the future
+                if alarm.getStartOn() > nowutc():
+                    # .clone takes care of enqueuing it
+                    alarm.clone(conf)
         # Meetings' and conferences' contributions cloning
         if options.get("contributions",False) :
             sch = conf.getSchedule()
