a3d7d74f46d76b1b8ffb577c64a6fefe12481ed0
Alberto Resco Perez <alberto.resco.perez@cern.ch>
2012-07-11 11:58:55 +0200

    [FIX] Material list contribution display
    - If the name is deleted, the resource was not being shown.

--- indico/MaKaC/webinterface/pages/contributions.py
+++ indico/MaKaC/webinterface/pages/contributions.py
@@ -77,6 +77,12 @@ class WContributionDisplayBase(WICalExportBase):
         authURL.addParam("authorId", author.getId())
         return authURL
 
+    def _getResourceName(self, resource):
+        if isinstance(resource, conference.Link):
+            return resource.getName() if resource.getName() != "" and resource.getName() != resource.getURL() else resource.getURL()
+        else:
+            return resource.getName() if resource.getName() != "" and resource.getName() != resource.getFileName() else resource.getFileName()
+
     def _getStatusReviewing(self):
         from MaKaC.paperReviewing import ConferencePaperReview as CPR
         versioning = self._contrib.getReviewManager().getVersioning()
@@ -143,6 +149,7 @@ class WContributionDisplayBase(WICalExportBase):
             vars["statusText"] = _("Paper not yet submitted")
             vars["statusClass"] = "contributionReviewingStatusNotSubmitted"
         vars["prefixUpload"] = "Re-" if  statusReviewing not in ["Accept", "Reject", None] else ""
+        vars["getResourceName"] = lambda resource: self._getResourceName(resource)
         return vars
 
 
