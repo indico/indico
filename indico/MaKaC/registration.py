575ae810639e8eb2d52a3705d95800c66f080130
Pedro Ferreira <jose.pedro.ferreira@cern.ch>
2012-10-11 16:07:25 +0200

    [IMP] 'in ' instead of 'has_key'

--- indico/MaKaC/registration.py
+++ indico/MaKaC/registration.py
@@ -2663,7 +2663,7 @@ class GeneralField(Persistent):
         if self.isMandatory() and self.isLocked('mandatory'):
             self.setMandatory(True)
         else:
-            self.setMandatory(data['mandatory'] if data.has_key('mandatory') else False)
+            self.setMandatory(data['mandatory'] if 'mandatory' in data else False)
         if self.isLocked('disable'):
             self.setDisabled(False)
         else:
