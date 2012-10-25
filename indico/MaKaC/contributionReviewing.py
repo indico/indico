564aab05889ca2812816f87b6e900de15a82e627
Jose Benito <jose.benito.gonzalez@cern.ch>
2012-10-25 15:19:54 +0200

    [IMP] CC reviewers when contancting author

--- indico/MaKaC/contributionReviewing.py
+++ indico/MaKaC/contributionReviewing.py
@@ -975,6 +975,7 @@ class ContributionReviewingJudgementNotification(GenericNotification):
         GenericNotification.__init__(self)
         self.setFromAddr("Indico Mailer <%s>"%Config.getInstance().getNoReplyEmail())
         self.setToList([user.getEmail()])
+        self.setCCList([judgement.getAuthor().getEmail()])
 
         if isinstance(judgement, EditorJudgement):
             if contribution.getConference().getConfPaperReview().getChoice() == ConferencePaperReview.LAYOUT_REVIEWING:
@@ -1050,6 +1051,7 @@ class ContributionReviewingJudgementWithdrawalNotification(GenericNotification):
         GenericNotification.__init__(self)
         self.setFromAddr("Indico Mailer <%s>" % Config.getInstance().getNoReplyEmail())
         self.setToList([user.getEmail()])
+        self.setCCList([judgement.getAuthor().getEmail()])
 
         if isinstance(judgement, EditorJudgement):
             if contribution.getConference().getConfPaperReview().getChoice() == ConferencePaperReview.LAYOUT_REVIEWING:
