35004c34048ae3a84762d940c15d9aa4dce93848
Alberto Resco Perez <alberto.resco.perez@cern.ch>
2012-11-08 15:37:21 +0100

    [FIX] Check if exists category

--- indico/MaKaC/webinterface/locators.py
+++ indico/MaKaC/webinterface/locators.py
@@ -316,7 +316,9 @@ class WebLocator:
             obj = CrossLocationQueries.getRooms( roomID = self.__roomID, location = self.__location )
             return obj
         if self.__categId:
-            obj = conference.CategoryManager().getById( self.__categId )
+            if not conference.CategoryManager().hasKey(self.__categId):
+                raise errors.NoReportError(_("""The specified category  with id "%s" does not exist or has been deleted""")%self.__categId)
+            obj = conference.CategoryManager().getById(self.__categId)
             if self.__materialId:
                 obj=obj.getMaterialById(self.__materialId)
             if self.__resId:
