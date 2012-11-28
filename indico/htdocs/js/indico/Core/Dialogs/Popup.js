cdf3248d394692c460b66e9dbe32244c7931aa4c
Pedro Ferreira <jose.pedro.ferreira@cern.ch>
2012-11-28 14:16:39 +0100

    [FIX] Max pop-up size
     * Fixed format too;

--- indico/htdocs/js/indico/Core/Dialogs/Popup.js
+++ indico/htdocs/js/indico/Core/Dialogs/Popup.js
@@ -924,7 +924,8 @@ type("ContributionsPopup", ["ExclusivePopup"], {
             table.append(Html.tr({}, Html.td({style:{verticalAlign: 'top'}}, time), Html.td({}, title, infoDiv)));
         });
         this.innerHTML = Html.table({style: {marginBottom: pixels(10)}}, table).dom.innerHTML;
-        return this.ExclusivePopup.prototype.draw.call(this, Html.table({style: {marginBottom: pixels(10)}}, table), {maxHeight:$(window).height()-50});
+        return this.ExclusivePopup.prototype.draw.call(this, Html.table({style: {marginBottom: pixels(10)}}, table),
+                                                       {maxHeight: Math.min($(window).height() - 50, 700)});
     },
     postDraw: function(){
         this.ExclusivePopup.prototype.postDraw.call(this);
