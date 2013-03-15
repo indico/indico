3bcd5dc02384afceef969c4add3c4642b7c0cbc4
Indico Team <indico-team@cern.ch>
2013-03-15 11:38:26 +0100

    [IMP] Update license headers in every file
    - Update to 2013.
    - Closes #1225

--- indico/ext/livesync/invenio/invenio_connector.py
+++ indico/ext/livesync/invenio/invenio_connector.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 ## This file is part of Invenio.
-## Copyright (C) 2002 - 2012 European Organization for Nuclear Research (CERN).
+## Copyright (C) 2002 - 2013 European Organization for Nuclear Research (CERN).
 ##
 ## Invenio is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
