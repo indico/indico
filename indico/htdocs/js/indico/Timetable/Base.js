a75a564f8b33f5928a46bfc1bcb38feb51cb85ba
Adrian Moennich <adrian.moennich@cern.ch>
2013-04-25 14:43:53 +0200

    [FIX] Wrong JS i18n

--- indico/htdocs/js/indico/Timetable/Base.js
+++ indico/htdocs/js/indico/Timetable/Base.js
@@ -983,7 +983,7 @@ type("ManagementTimeTable",["TimeTable", "UndoMixin"], {
 
         customLinks.appendTo(this.menu);
 
-        var tt_hour_tip = $('<div id="tt_hour_tip"/>').hide().append($('<img/>').prop('src', imageSrc(Indico.SystemIcons.tt_time)).prop('title', $("Add one hour")));
+        var tt_hour_tip = $('<div id="tt_hour_tip"/>').hide().append($('<img/>').prop('src', imageSrc(Indico.SystemIcons.tt_time)).prop('title', $T("Add one hour")));
         var tt_status_info = $('<div id="tt_status_info" />');
 
         this.menu.children('a').addClass('i-button');
