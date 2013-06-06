62145f9d2f58f2b5cdc4077664e5f58918b33a42
Adrian Moennich <adrian.moennich@cern.ch>
2013-06-06 11:56:33 +0200

    [REF] Remove unused WError/WPError classes

--- indico/MaKaC/webinterface/pages/error.py
+++ indico/MaKaC/webinterface/pages/error.py
@@ -17,53 +17,8 @@
 ## You should have received a copy of the GNU General Public License
 ## along with Indico;if not, see <http://www.gnu.org/licenses/>.
 
-import MaKaC.webinterface.urlHandlers as urlHandlers
 from MaKaC.webinterface.pages.base import WPDecorated
 from MaKaC.webinterface.wcomponents import WTemplated
-from MaKaC.i18n import _
-
-class WPError(WPDecorated):
-
-    def __init__(self, rh, ex, params):
-        WPDecorated.__init__(self, rh)
-        self._ex = ex
-        self._RHParams = params
-
-
-    def _getBody(self, params):
-        wc = WError(self._ex)
-        msg = []
-        msg.append("%s: %s"%( "error message", self._ex))
-        msg.append("%s: %s"%( "url", self._rh._req.unparsed_uri))
-        msg.append("%s: %s"%( "redirect from", self._rh._req.prev))
-        msg.append("%s: %s"%( "headers", self._rh._req.headers_in ))
-        for k in self._RHParams.keys():
-            if k.strip() in ["password", "passwordBis"]:
-                msg.append("%s: %s"%(k, self._RHParams[k]))
-            else:
-                #print * instead of password
-                msg.append("%s: %s"%(k, "*"*len(self._RHParams[k])))
-
-        msg.append("%s: %s"%("RequestHandlers", self._rh.__class__))
-
-        params["msg"] = "\n".join(msg)
-        params["email"] = ""
-        if self._getAW().getUser():
-            params["email"] = self._getAW().getUser().getEmail()
-        return wc.getHTML(params)
-
-
-class WError(WTemplated):
-
-    def __init__(self, ex):
-        self._ex = ex
-
-
-    def getVars(self):
-        vars = WTemplated.getVars(self)
-        vars["errorText"] = "%s"%self._ex
-        vars["sendReportURL"] = urlHandlers.UHErrorSendReport.getURL()
-        return vars
 
 
 class WErrorWSGI(WTemplated):
