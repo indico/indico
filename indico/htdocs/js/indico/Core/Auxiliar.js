db75510255a8524d1bb7bb315ad6b521f1a3576d
Alejandro Avilés <omegak@gmail.com>
2013-07-15 17:03:43 +0200

    [NEW] Underscore mixin to move array elements

--- indico/htdocs/js/indico/Core/Auxiliar.js
+++ indico/htdocs/js/indico/Core/Auxiliar.js
@@ -27,6 +27,13 @@ var intToStr = function(id) {
     }
 };
 
+_.mixin({
+    move: function (array, fromIndex, toIndex) {
+        array.splice(toIndex, 0, array.splice(fromIndex, 1)[0]);
+        return array;
+    }
+});
+
 IndicoUI.Aux = {
     globalChangeHandler: null,
 
