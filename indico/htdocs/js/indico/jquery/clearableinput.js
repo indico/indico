b5ec38d95424f0c9fdf7cb0365c66b0f9e2cfcb1
Alejandro Avilés <omegak@gmail.com>
2013-09-18 11:54:34 +0200

    [MIN] Used keymap.js instead of hard-coded int

--- indico/htdocs/js/indico/jquery/clearableinput.js
+++ indico/htdocs/js/indico/jquery/clearableinput.js
@@ -49,8 +49,7 @@
                     }
                 })
                 .on("keyup", function(e) {
-                    var K_ESC = 27;
-                    if (e.which == K_ESC) {
+                    if (e.which == K.ESCAPE) {
                         input.val('value');
                         self._clear();
                     }
