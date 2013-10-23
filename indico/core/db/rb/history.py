f9b9db42a15824f1a311be647eb31cbf02581430
Ferhat Elmas <ferhat.elmas@cern.ch>
2013-10-23 18:21:31 +0200

    [IMP] initialized room booking schema

---
+++ indico/core/db/rb/history.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+##
+##
+## This file is part of Indico.
+## Copyright (C) 2002 - 2013 European Organization for Nuclear Research (CERN).
+##
+## Indico is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 3 of the
+## License, or (at your option) any later version.
+##
+## Indico is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Indico;if not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+"""
+
+from sqlalchemy import Column, String, DateTime, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class History(Base):
+    __tablename__ = 'histories' # 'edits' may be a better name
+
+    historyId = Column(Integer, primary_key=True)
+
+    info = Column(String)
+    timestamp = Column(DateTime)
+    responsibleUser = Column(String)
