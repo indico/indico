6ef04bb4ce7b723fe702e46d8ebaef2f917b20a2
Ferhat Elmas <ferhat.elmas@cern.ch>
2013-11-07 17:52:44 +0100

    [FIX] ambiguity between multiple foreign keys

--- indico/core/db/rb/locations.py
+++ indico/core/db/rb/locations.py
@@ -22,6 +22,7 @@ Holder of rooms in a place and its map view related data
 """
 
 from indico.core.db import db
+from indico.core.db.rb.aspects import Aspect
 
 
 class Location(db.Model):
@@ -34,16 +35,55 @@ class Location(db.Model):
 
     aspects = db.relationship('Aspect',
                               backref='location',
-                              cascade='all, delete-orphan')
+                              cascade='all, delete-orphan',
+                              primaryjoin=id==Aspect.location_id)
 
     default_aspect_id = db.Column(db.Integer, db.ForeignKey('aspects.id',
                                                             use_alter=True,
                                                             name='fk_default_aspect_id'))
 
+    default_aspect = db.relationship('Aspect',
+                                     primaryjoin=default_aspect_id==Aspect.id,
+                                     post_update=True)
+
     rooms = db.relationship('Room',
                             backref='location',
                             cascade='all, delete-orphan')
 
+    def __init__(self, name, support_emails=None):
+        self.name = name
+        self.support_emails = support_emails
+
+    def __str__(self):
+        return self.name
+
     def __repr__(self):
         return '<Location({0}, {1}, {2})>'.format(self.id, self.default_aspect_id,
                                                   self.name)
+
+    def __cmp__(self, other):
+        return cmp(self.name, other.name)
+
+    def getSupportEmails(self):
+        if self.support_emails:
+            return self.getSupportEmails.split(',')
+
+    def setSupportEmails(self, emails):
+        self.support_emails = ','.join(emails)
+
+    def getRooms(self):
+        return self.rooms
+
+    def addRoom(self, room):
+        self.rooms.append(room)
+
+    def deleteRoom(self, room):
+        self.rooms.remove(room)
+
+    @staticmethod
+    def getLocationById(lid):
+        return Location.query.get(lid)
+
+    @staticmethod
+    def getLocationsByName(name):
+        return Location.query.filter(Location.name == name)
