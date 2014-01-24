indico/indico

commit 1a4d1f60483d209bde0a564a6c97ac343512ac43

[FIX] utf-8 encoding of resources PDF conversion
closes #1371

indico/MaKaC/webinterface/rh/conferenceBase.py
@@ -252,7 +252,7 @@ class RHSubmitMaterialBase(object):
                 if type(fileUpload) != str and fileUpload.filename.strip() != "":
                     fDict = {}
 
-                    fDict["fileName"] = fileUpload.filename
+                    fDict["fileName"] = fileUpload.filename.encode("utf-8")
                     estimSize = request.content_length
 
                     if maxUploadFilesTotalSize and estimSize > (maxUploadFilesTotalSize * BYTES_1MB):
@@ -396,7 +396,8 @@ class RHSubmitMaterialBase(object):
                 mat.addResource(resource, forcedFileId=None)
 
             #apply conversion
-            if self._topdf and fileConverter.CDSConvFileConverter.hasAvailableConversionsFor(os.path.splitext(resource.getFileName())[1].strip().lower()):
+            file_ext = os.path.splitext(resource.getFileName())[1].strip().lower()
+            if self._topdf and fileConverter.CDSConvFileConverter.hasAvailableConversionsFor(file_ext):
                 #Logger.get('conv').debug('Queueing %s for conversion' % resource.getFilePath())
                 fileConverter.CDSConvFileConverter.convert(resource.getFilePath(), "pdf", mat)
                 resource.setPDFConversionRequestDate(nowutc())

commit 767b3ea6486a5025d306aeabddfd4dce46ac050e

[FIX] Don't try to pdf-convert links
fixes bug introduced in 1a4d1f60 (when adding a Link)

indico/MaKaC/webinterface/rh/conferenceBase.py
@@ -396,11 +396,12 @@ class RHSubmitMaterialBase(object):
                 mat.addResource(resource, forcedFileId=None)
 
             #apply conversion
-            file_ext = os.path.splitext(resource.getFileName())[1].strip().lower()
-            if self._topdf and fileConverter.CDSConvFileConverter.hasAvailableConversionsFor(file_ext):
-                #Logger.get('conv').debug('Queueing %s for conversion' % resource.getFilePath())
-                fileConverter.CDSConvFileConverter.convert(resource.getFilePath(), "pdf", mat)
-                resource.setPDFConversionRequestDate(nowutc())
+            if self._topdf and not isinstance(resource, Link):
+                file_ext = os.path.splitext(resource.getFileName())[1].strip().lower()
+                if fileConverter.CDSConvFileConverter.hasAvailableConversionsFor(file_ext):
+                    # Logger.get('conv').debug('Queueing %s for conversion' % resource.getFilePath())
+                    fileConverter.CDSConvFileConverter.convert(resource.getFilePath(), 'pdf', mat)
+                    resource.setPDFConversionRequestDate(nowutc())
 
             # store the repo id, for files
             if isinstance(resource, LocalFile) and self._repositoryIds is None: