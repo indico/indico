620aef3e55e6674b60eb584f15f2b33abe0ed0e9
Ferhat Elmas <ferhat.elmas@cern.ch>
2014-02-20 18:08:50 +0100

    [IMP] better date and holiday check

--- indico/modules/rb/services/__init__.py
+++ indico/modules/rb/services/__init__.py
@@ -18,78 +18,33 @@
 ## along with Indico;if not, see <http://www.gnu.org/licenses/>.
 
 from datetime import datetime
-from time import strptime
 
+from MaKaC.common.utils import HolidaysHolder
 from MaKaC.services.implementation.base import ServiceBase
 from MaKaC.services.interface.rpc.common import ServiceError
 
-from MaKaC.common.utils import HolidaysHolder
-
-from indico.core.errors import NoReportError
 from indico.util.i18n import _
 
 from ..models.utils import is_weekend
 
-# TODO
+
 class GetDateWarning(ServiceBase):
 
     def _checkParams(self):
-        """
-        Extracts startDT, endDT and repeatability
-        from the form, if present.
-
-        Assigns these values to self, or Nones if values
-        are not present.
-        """
-
-        if self._params.get('day') == 'today':
-            self._today = True
-            self._startDT = datetime.today().replace(hour=0, minute=0, second=0, microsecond=0)
-            self._endDT = self._startDT.replace(hour=23, minute=59, second=59, microsecond=999999)
-        else:
-            self._today = False
-
-            for param in ['sDay', 'eDay', 'sMonth', 'eMonth', 'sYear', 'eYear', 'sTime', 'eTime']:
-                val = self._params.get(param)
-                if val and val.strip():
-                    val = val.strip()
-                    if not param.endswith('Time'):
-                        val = int(val)
-                locals()[param] = val
-
-            # process sTime and eTime
-            if sTime and eTime:
-                for param in ['sTime', 'eTime']:
-                    try:
-                        locals()[param[0] + 'Hour'], locals()[param[0] + 'Minute'] = map(int, strptime(locals()[param], '%H:%M').split(':'))
-                    except ValueError:
-                        raise NoReportError(
-                            _('{0} must be of the form HH:MM and must be a valid time.').format(
-                                _('The Start Time') if param == 'sTime' else _('The End Time')
-                            )
-                        )
-
-
-            self._startDT, self._endDT = None, None
-            if sYear and sMonth and sDay and sTime and eYear and eMonth and eDay and eTime:
-                # Full period specified
-                self._startDT = datetime(sYear, sMonth, sDay, sHour, sMinute)
-                self._endDT = datetime(eYear, eMonth, eDay, eHour, eMinute)
-            elif sYear and sMonth and sDay and eYear and eMonth and eDay:
-                # There are no times
-                self._startDT = datetime(sYear, sMonth, sDay, 0, 0, 0)
-                self._endDT = datetime(eYear, eMonth, eDay, 23, 59, 59)
-            elif sTime and eTime:
-                # There are no dates
-                self._startDT = datetime(1990, 1, 1, sHour, sMinute)
-                self._endDT = datetime(2030, 12, 31, eHour, eMinute)
+        self._start_date = datetime.strptime(
+            '{} {}'.format(self._params.get('start_date'),
+                           self._params.get('start_time')), '%d-%m-%Y %H:%M')
+        self._end_date = datetime.strptime(
+            '{} {}'.format(self._params.get('end_date'),
+                           self._params.get('end_time')), '%d-%m-%Y %H:%M')
 
     def _getAnswer(self):
-        if not self._startDT or not self._endDT or \
-           (HolidaysHolder.isWorkingDay(self._startDT) and HolidaysHolder.isWorkingDay(self._endDT)):
+        if not self._start_date or not self._end_date or \
+           (HolidaysHolder.isWorkingDay(self._start_date) and
+            HolidaysHolder.isWorkingDay(self._end_date)):
             return ''
 
-        if isWeekend(self._startDT) or isWeekend(self._endDT):
+        if is_weekend(self._start_date) or is_weekend(self._end_date):
             return _('weekend chosen')
 
         return _('holidays chosen')
