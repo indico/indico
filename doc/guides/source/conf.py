41f0799e7b19958e98f02080e5572b8437315bd6
Pedro Ferreira <pedro.ferreira@cern.ch>
2014-03-04 17:49:17 +0100

    [IMP] Removed unnecessary sphinx extensions
     * Causing security warnings in help pages

--- doc/guides/source/conf.py
+++ doc/guides/source/conf.py
@@ -25,7 +25,7 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.pngmath', 'sphinx.ext.mathjax', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
