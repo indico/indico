469fab88e0548cb097e914e4b373ed55607e1e6c
Tommaso Papini <tommaso.papini@cern.ch>
2014-04-25 15:00:27 +0200

    Fixes after WIP

--- indico/htdocs/js/indico/Admin/initial_setup.js
+++ indico/htdocs/js/indico/Admin/initial_setup.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
     var fields = [[$('#name'), $('#surname'), $('#user-email'), $('#login'), $('#password'), $('#password-confirm')],
                   [$('#organisation')],
                   [$('#it-email')]];
+    var itEmailValue = '';
 
     // Scroll the page to a step
     function scrollToStep(step){
@@ -37,11 +38,10 @@ $(document).ready(function(){
     function fillITEmail(){
         var userEmail = $('#user-email');
         var itEmail = $('#it-email');
-        var itEmailHidden = $('#it-email-hidden');
-        if (itEmailHidden.val() == ''){
+        if (itEmailValue == '') {
             itEmail.val(userEmail.val());
-        } else{
-            itEmail.val(itEmailHidden.val());
+        } else {
+            itEmail.val(itEmailValue);
         }
     }
 
@@ -55,9 +55,9 @@ $(document).ready(function(){
     function validateSteps(){
         ok = true;
         invalidStep = 0;
-        for (var i=1; i<=3; i++){
+        for (var i = 1; i <= 3; i++) {
             validateStep(i);
-            if (!ok && invalidStep == 0){
+            if (!ok && invalidStep == 0) {
                 invalidStep = i;
             }
         }
@@ -67,7 +67,7 @@ $(document).ready(function(){
     // Validate a single step
     function validateStep(step){
 
-        for (var i=0; i<fields[step-1].length; i++){
+        for (var i = 0; i < fields[step-1].length; i++) {
             fields[step-1][i].trigger('input').trigger('hideTooltip');
         }
     }
@@ -78,9 +78,9 @@ $(document).ready(function(){
         clicked = true;
         var invalidStep = validateSteps();
 
-        if (invalidStep == 0){
+        if (invalidStep == 0) {
             $('#initial-setup-form').submit();
-        } else{
+        } else {
             scrollToStep(invalidStep);
         }
     });
@@ -101,11 +101,11 @@ $(document).ready(function(){
             event: 'hideTooltip'
         }
     }).on('focus input mouseenter', function(){
-        if ($(this).hasClass('hasError')){
-            $(this).trigger('showTooltip');
-        }
-        else{
-            $(this).trigger('hideTooltip');
+        var $this = $(this);
+        if ($this.hasClass('hasError')) {
+            $this.trigger('showTooltip');
+        } else {
+            $this.trigger('hideTooltip');
         }
     }).on('blur', function(){
         $(this).trigger('hideTooltip');
@@ -125,44 +125,43 @@ $(document).ready(function(){
 
     // Instance Tracking slider
     $('.toggle-button').on('click', function() {
-        $(this).toggleClass('toggled');
-        var toggled = $(this).hasClass('toggled');
+        var $this = $(this);
+        $this.toggleClass('toggled');
+        var toggled = $this.hasClass('toggled');
         var itEmail = $('#it-email');
         var checkbox = $('#enable');
         itEmail.prop('required', toggled);
         itEmail.prop('disabled', !toggled);
         checkbox.prop('checked', toggled);
-        if (toggled){
+        if (toggled) {
             fillITEmail();
             itEmail.trigger('input');
-        }
-        else{
+        } else {
             emptyITEmail();
             markValidField(itEmail);
         }
     });
-    if ($('#enable').prop('checked')){
+    if ($('#enable').prop('checked')) {
         $('.toggle-button').trigger('click');
     }
 
     // Fields validation setup
     $('#initial-setup-form :input:not(#password-confirm)').on('input', function(){
-        if (!this.validity.valid){
-            if (this.id == 'user-email' || this.id == 'it-email'){
-                if (this.validity.valueMissing){
-                    $(this).qtip('option', 'content.attr', "data-error-tooltip");
-                }
-                else{
-                    $(this).qtip('option', 'content.attr', "data-error-tooltip2");
+        var $this = $(this);
+        if (!this.validity.valid) {
+            if (this.id == 'user-email' || this.id == 'it-email') {
+                if (this.validity.valueMissing) {
+                    $this.qtip('option', 'content.attr', 'data-error-tooltip');
+                } else {
+                    $this.qtip('option', 'content.attr', 'data-error-tooltip2');
                 }
             }
-            if (clicked){
-                markInvalidField($(this));
+            if (clicked) {
+                markInvalidField($this);
             }
             ok = false;
-        }
-        else{
-            markValidField($(this));
+        } else {
+            markValidField($this);
         }
     });
     $('#password, #password-confirm').on('input', function(){
@@ -173,8 +172,7 @@ $(document).ready(function(){
                 markInvalidField(passwordConfirm);
             }
             ok = false;
-        }
-        else {
+        } else {
             markValidField(passwordConfirm);
         }
     });
@@ -194,8 +192,7 @@ $(document).ready(function(){
 
     // Listener to store IT Email value
     $('#it-email').on('input', function(){
-        var itEmailHidden = $('#it-email-hidden');
-        itEmailHidden.val(this.value);
+        itEmailValue = this.value;
     });
 
 });
