8c28554e3a7d4c268a5927244db4d4c41d6342c4
Adrian Moennich <adrian.moennich@cern.ch>
2014-05-27 16:11:12 +0200

    Add array func, fix empty rows in array_agg
    array_agg returned [None] insead of [] if there were no rows

--- indico/core/db/sqlalchemy/custom/static_array.py
+++ indico/core/db/sqlalchemy/custom/static_array.py
@@ -54,10 +54,26 @@ class array_agg(expression.ColumnElement):
 @compiles(array_agg, 'postgresql')
 def _compile_array_agg_postgresql(element, compiler, **kw):
     buf = StringIO()
-    buf.write('array_agg(')
+    buf.write('array(select x from unnest(array_agg(')
     buf.write(compiler.process(element.expr))
     if element.order_by is not None:
         buf.write(' ORDER BY ')
         buf.write(compiler.process(element.order_by))
+    buf.write(')) x WHERE x IS NOT NULL)')
+    return buf.getvalue()
+
+
+class array(expression.ColumnElement):
+    type = StaticArray()
+
+    def __init__(self, expr):
+        self.expr = expression._literal_as_binds(expr)
+
+
+@compiles(array, 'postgresql')
+def _compile_array_postgresql(element, compiler, **kw):
+    buf = StringIO()
+    buf.write('array(')
+    buf.write(compiler.process(element.expr))
     buf.write(')')
     return buf.getvalue()
