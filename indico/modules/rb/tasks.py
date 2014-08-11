cb5319f07148021e83e3a33b1e8012fbc2c208d3
Alejandro Avilés <aavilesd@cern.ch>
2014-08-11 11:06:53 +0200

    OccurrenceNotification sends delayed occurrences
    If some notifications were not sent due to the scheduler not being
    running, they will be sent next hour.

--- indico/modules/rb/tasks.py
+++ indico/modules/rb/tasks.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 
 from sqlalchemy.sql import func, cast
 from sqlalchemy import Date
@@ -11,18 +11,25 @@ from indico.modules.rb.notifications.reservation_occurrences import notify_upcom
 from indico.modules.scheduler.tasks.periodic import PeriodicUniqueTask
 
 
+def _build_notification_before_days_filter(notification_before_days):
+    days_until_occurrence = cast(ReservationOccurrence.start_dt, Date) - cast(func.now(), Date)
+    notification_before_days = func.coalesce(Room.notification_for_start, notification_before_days)
+    if datetime.now().hour >= settings.get('notification_hour', 6):
+        # Notify of today and delayed occurrences (happening in N or less days)
+        return days_until_occurrence <= notification_before_days
+    else:
+        # Notify only of delayed occurrences (happening in less than N days)
+        return days_until_occurrence < notification_before_days
+
+
 class OccurrenceNotifications(PeriodicUniqueTask):
     def run(self):
-        if settings.get('notification_hour', 6) != datetime.now().hour:
-            return
-
-        today = cast(func.now(), Date)
-        days_before = func.coalesce(Room.notification_before_days, settings.get('notification_before_days', 0))
         occurrences = ReservationOccurrence.find(
             Reservation.is_accepted,
             ~ReservationOccurrence.notification_sent,
             ReservationOccurrence.is_valid,
-            cast(ReservationOccurrence.start_dt, Date) - days_before * timedelta(days=1) == today,
+            ReservationOccurrence.start_dt >= func.now(),
+            _build_notification_before_days_filter(settings.get('notification_before_days', 0)),
             _join=[Reservation, Room]
         )
 
