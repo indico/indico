1b7f023f89e155abea12837bfe6983884a999c10
Adrian Moennich <adrian.moennich@cern.ch>
2014-09-04 17:03:32 +0200

    wip room tests

--- indico/modules/rb/models/rooms_test.py
+++ indico/modules/rb/models/rooms_test.py
@@ -16,8 +16,55 @@
 
 import pytest
 
+from indico.modules.rb.models.rooms import Room
+
 
 pytest_plugins = 'indico.modules.rb.testing.fixtures'
+
+
+def test_is_auto_confirm(create_room, bool_flag):
+    room = create_room(reservations_need_confirmation=bool_flag)
+    assert room.is_auto_confirm != bool_flag
+    assert Room.find_first(is_auto_confirm=bool_flag) is None
+    assert Room.find_first(is_auto_confirm=not bool_flag) == room
+
+
+@pytest.mark.parametrize(('value', 'expected'), (
+    ('xxx', True),
+    ('', False)
+))
+def test_has_booking_groups(create_room_attribute, dummy_room, value, expected):
+    create_room_attribute('allowed-booking-group')
+    assert not dummy_room.has_booking_groups
+    dummy_room.set_attribute_value('allowed-booking-group', value)
+    assert dummy_room.has_booking_groups == expected
+
+
+def test_has_projector(create_equipment_type, dummy_room, db):
+    assert not dummy_room.has_projector
+    dummy_room.available_equipment.append(create_equipment_type('Computer Projector'))
+    db.session.flush()
+    assert dummy_room.has_projector
+
+
+def test_has_webcast_recording(create_equipment_type, dummy_room, db):
+    assert not dummy_room.has_webcast_recording
+    dummy_room.available_equipment.append(create_equipment_type('Webcast/Recording'))
+    db.session.flush()
+    assert dummy_room.has_webcast_recording
+
+
+@pytest.mark.parametrize(('reservable', 'booking_group', 'expected'), (
+    (True, '', True),
+    (True, 'xxx', False),
+    (False, '', False),
+    (False, 'xxx', False)
+))
+def test_is_public(create_room_attribute, dummy_room, reservable, booking_group, expected):
+    create_room_attribute('allowed-booking-group')
+    dummy_room.set_attribute_value('allowed-booking-group', booking_group)
+    dummy_room.is_reservable = reservable
+    assert dummy_room.is_public == expected
 
 
 def test_owner(dummy_room, dummy_user):
