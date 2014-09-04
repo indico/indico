7e6e98359dba6ce1af89b4edc26698bd2dfdf769
Jacques Dafflon <jacques.dafflon@cern.ch>
2014-09-04 12:00:25 +0200

    Check bad start date in single page booking
     - Check start date in query string of single page booking
     - Change room.check_advance_days() to always return a boolean

--- indico/modules/rb/models/rooms.py
+++ indico/modules/rb/models/rooms.py
@@ -707,9 +707,9 @@ class Room(versioned_cache(_cache, 'id'), db.Model, Serializer):
 
     def check_advance_days(self, end_date, user=None, quiet=False):
         if not self.max_advance_days:
-            return
+            return True
         if user and (user.isRBAdmin() or self.is_owned_by(user)):
-            return
+            return True
         advance_days = (end_date - date.today()).days
         ok = advance_days < self.max_advance_days
         if quiet:
