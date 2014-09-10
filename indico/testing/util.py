4bf6652b138f4dd98358fd6873959ce8f9b5780a
Adrian Moennich <adrian.moennich@cern.ch>
2014-09-10 14:42:50 +0200

    Make bool_matrix more powerful

--- indico/testing/util.py
+++ indico/testing/util.py
@@ -17,7 +17,7 @@
 from itertools import product, imap
 
 
-def bool_matrix(template, expect):
+def bool_matrix(template, expect, mask=None):
     """Creates a boolean matrix suitable for parametrized tests.
 
     This function lets you create a boolean matrix with certain columns being
@@ -34,6 +34,11 @@ def bool_matrix(template, expect):
      (True, False, False, True,  True),
      (True, False, False, False, True))
 
+    The `expect` param can be a boolean value if you always want the same value,
+    a tuple if you want only a single row to be true or a callable receiving the
+    whole row. In some cases the builtin callables `any` or `all` are appropriate
+    callables, in other cases a custom (lambda) function is necessary.
+
     In exclusion mode any row matching the template is skipped. It can be enabled
     by prefixing the template with a `!` character::
 
@@ -45,30 +50,50 @@ def bool_matrix(template, expect):
      (False, True,  True,  False),
      (False, True,  False, False))
 
-    The `expect` param can be a boolean value if you always want the same value,
-    a tuple if you want only a single row to be true or a callable receiving the
-    whole row. In some cases the builtin callables `any` or `all` are appropriate
-    callables, in other cases a custom (lambda) function is necessary.
+    You can also combine both by using the default syntax and specifying the exclusion
+    mask separately::
+
+    >>> bool_matrix('..1.', mask='00..', expect=all)
+    ((True,  True,  True, True,  True),
+     (True,  True,  True, False, False),
+     (True,  False, True, True,  False),
+     (True,  False, True, False, False),
+     (False, True,  True, True,  False),
+     (False, True,  True, False, False))
 
     :param template: row template
     :param expect: bool value, tuple or callable
+    :param mask: exclusion mask
     """
-    # let people align definitions nicely
     template = template.replace(' ', '')
-    exclude = False
+    exclude_all = False
     if template[0] == '!':
-        exclude = True
+        exclude_all = True
         template = template[1:]
+    if mask:
+        if exclude_all:
+            raise ValueError('cannot combine ! with mask')
+        if len(mask) != len(template):
+            raise ValueError('mask length differs from template length')
+        if any(x != '.' and y != '.' for x, y in zip(template, mask)):
+            raise ValueError('mask cannot have a value for a fixed column')
+    else:
+        mask = '.' * len(template)
+
     mapping = {'0': False, '1': True, '.': None}
     template = tuple(imap(mapping.__getitem__, template))
+    mask = tuple(imap(mapping.__getitem__, mask))
     # full truth table
     iterable = product((True, False), repeat=len(template))
-    if exclude:
+    if exclude_all:
         # only use rows which have values not matching the template
         iterable = (x for x in iterable if any(x[i] != v for i, v in enumerate(template) if v is not None))
     else:
         # only use rows where all values match the template
         iterable = (x for x in iterable if all(v is None or x[i] == v for i, v in enumerate(template)))
+        # exclude some rows
+        if any(x is not None for x in mask):
+            iterable = (x for x in iterable if any(x[i] != v for i, v in enumerate(mask) if v is not None))
     # add the "expected" value which can depend on the other values
     if callable(expect):
         iterable = (x + (expect(x),) for x in iterable)
@@ -76,4 +101,7 @@ def bool_matrix(template, expect):
         iterable = (x + (x == expect,) for x in iterable)
     else:
         iterable = (x + (expect,) for x in iterable)
-    return tuple(iterable)
+    matrix = tuple(iterable)
+    if not matrix:
+        raise ValueError('empty matrix')
+    return matrix
