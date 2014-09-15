43f098c7d66a0137eaef5068dab4c6f41f4542ad
Adrian Moennich <adrian.moennich@cern.ch>
2014-09-15 15:42:55 +0200

    Test proxy_to_reservation_if_last_valid_occurrence

--- indico/modules/rb/models/utils_test.py
+++ indico/modules/rb/models/utils_test.py
@@ -14,10 +14,17 @@
 ## You should have received a copy of the GNU General Public License
 ## along with Indico; if not, see <http://www.gnu.org/licenses/>.
 
+from datetime import datetime, date, time, timedelta
+
 import pytest
 
 from indico.core.errors import IndicoError
+from indico.modules.rb.models.reservations import RepeatFrequency
 from indico.modules.rb.models.utils import unimplemented
+from indico.testing.util import bool_matrix
+
+
+pytest_plugins = 'indico.modules.rb.testing.fixtures'
 
 
 @pytest.mark.parametrize(('raised', 'caught', 'message'), (
@@ -32,3 +39,25 @@ def test_unimplemented(raised, caught, message):
 
     exc_info = pytest.raises(caught, _func)
     assert exc_info.value.message == message
+
+
+@pytest.mark.parametrize(
+    ('not_repeating', 'only_one_valid', 'propagate', 'proxied'),
+    bool_matrix('..0', expect=False) +
+    bool_matrix('..1', expect='any_dynamic')
+)
+@pytest.mark.usefixtures('smtp')
+def test_proxy_to_reservation_if_last_valid_occurrence(db, mock, create_reservation, dummy_user,
+                                                       not_repeating, only_one_valid, propagate, proxied):
+    resv = create_reservation(start_dt=datetime.combine(date.today(), time(8)),
+                              end_dt=datetime.combine(date.today() + timedelta(days=1), time(17)),
+                              repeat_frequency=RepeatFrequency.NEVER if not_repeating else RepeatFrequency.DAY)
+    if only_one_valid:
+        for occ in resv.occurrences[1:]:
+            occ.is_cancelled = True
+        db.session.flush()
+
+    occ = resv.occurrences.first()
+    mock.patch.object(resv, 'cancel')
+    occ.cancel(user=dummy_user, propagate=propagate)
+    assert resv.cancel.called == proxied
