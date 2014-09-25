37c75dc7336d91b2c4b60b7a81423d2590fe104f
Adrian Moennich <adrian.moennich@cern.ch>
2014-09-25 14:08:11 +0200

    Support BPython using `indico shell --use-bpython`
    Unlike the default flask-script shell bpython is only enabled if it's
    explictly requested since it does not support the nice colored welcome
    message so far.

--- indico/cli/shell.py
+++ indico/cli/shell.py
@@ -20,13 +20,13 @@ from operator import itemgetter
 
 import transaction
 from flask import current_app
-from flask_script import Shell
+from flask_script import Shell, Option
 from werkzeug.local import LocalProxy
 
 import MaKaC
 from indico.core import signals
 from indico.core.config import Config
-from indico.util.console import colored
+from indico.util.console import colored, strip_ansi
 from indico.core.db import DBMgr, db
 from indico.core.index import Catalog
 from MaKaC.common import HelperMaKaCInfo
@@ -37,7 +37,7 @@ from MaKaC.user import AvatarHolder, GroupHolder
 from indico.web.flask.util import IndicoConfigWrapper
 
 
-def _add_to_context(namespace, element, name=None, doc=None, color='green'):
+def _add_to_context(namespace, info, element, name=None, doc=None, color='green'):
     if not name:
         name = element.__name__
     namespace[name] = element
@@ -48,9 +48,9 @@ def _add_to_context(namespace, element, name=None, doc=None, color='green'):
         attrs['attrs'] = ['bold']
 
     if doc:
-        print colored('+ {0} : {1}'.format(name, doc), color, **attrs)
+        info.append(colored('+ {0} : {1}'.format(name, doc), color, **attrs))
     else:
-        print colored('+ {0}'.format(name), color, **attrs)
+        info.append(colored('+ {0}'.format(name), color, **attrs))
 
 
 class IndicoShell(Shell):
@@ -59,38 +59,59 @@ class IndicoShell(Shell):
                          'yellow', attrs=['bold'])
         super(IndicoShell, self).__init__(banner=banner, use_bpython=False)
         self._context = None
+        self._info = None
+        self._quiet = False
 
-    def run(self, **kwargs):
+    def run(self, no_ipython, use_bpython, quiet):
         context = self.get_context()
+        if not quiet:
+            self.banner = '\n'.join(self._info + [self.banner])
+        if use_bpython:
+            # bpython does not support escape sequences :(
+            # https://github.com/bpython/bpython/issues/396
+            self.banner = strip_ansi(self.banner)
         with context['dbi'].global_connection():
-            super(IndicoShell, self).run(**kwargs)
+            super(IndicoShell, self).run(no_ipython or use_bpython, not use_bpython)
+
+    def get_options(self):
+        return (
+            Option('--no-ipython', action='store_true', dest='no_ipython', default=False,
+                   help="Do not use the IPython shell"),
+            Option('--use-bpython', action='store_true', dest='use_bpython', default=False,
+                   help="Use the BPython shell"),
+            Option('--quiet', '-q', action='store_true', dest='quiet', default=False,
+                   help="Do not print the shell context")
+        )
 
     def get_context(self):
         if self._context is None:
             self._context = context = {}
+            self._info = []
+
+            add_to_context = partial(_add_to_context, context, self._info)
 
             for attr in ('date', 'time', 'datetime', 'timedelta'):
-                _add_to_context(context, getattr(datetime, attr), doc='stdlib datetime.{}'.format(attr), color='yellow')
+                add_to_context(getattr(datetime, attr), doc='stdlib datetime.{}'.format(attr), color='yellow')
 
-            _add_to_context(context, Conference)
-            _add_to_context(context, ConferenceHolder)
-            _add_to_context(context, CategoryManager)
-            _add_to_context(context, AvatarHolder)
-            _add_to_context(context, GroupHolder)
-            _add_to_context(context, PluginsHolder)
-            _add_to_context(context, Catalog)
-            _add_to_context(context, IndexesHolder)
-            _add_to_context(context, IndicoConfigWrapper(Config.getInstance()), 'config')
-            _add_to_context(context, LocalProxy(HelperMaKaCInfo.getMaKaCInfoInstance), 'minfo')
-            _add_to_context(context, current_app, 'app')
+            add_to_context(Conference)
+            add_to_context(ConferenceHolder)
+            add_to_context(CategoryManager)
+            add_to_context(AvatarHolder)
+            add_to_context(GroupHolder)
+            add_to_context(PluginsHolder)
+            add_to_context(Catalog)
+            add_to_context(IndexesHolder)
+            add_to_context(IndicoConfigWrapper(Config.getInstance()), 'config')
+            add_to_context(LocalProxy(HelperMaKaCInfo.getMaKaCInfoInstance), 'minfo')
+            add_to_context(current_app, 'app')
 
             for name, cls in sorted(db.Model._decl_class_registry.iteritems(), key=itemgetter(0)):
                 if hasattr(cls, '__table__'):
-                    _add_to_context(context, cls, color='cyan')
+                    add_to_context(cls, color='cyan')
 
-            _add_to_context(context, DBMgr.getInstance(), 'dbi', doc='zodb db interface', color='cyan!')
-            _add_to_context(context, db, 'db', doc='sqlalchemy db interface', color='cyan!')
-            _add_to_context(context, transaction, doc='transaction module', color='cyan!')
-            signals.shell_context.send(add_to_context=partial(_add_to_context, context))
+            add_to_context(DBMgr.getInstance(), 'dbi', doc='zodb db interface', color='cyan!')
+            add_to_context(db, 'db', doc='sqlalchemy db interface', color='cyan!')
+            add_to_context(transaction, doc='transaction module', color='cyan!')
+            signals.shell_context.send(add_to_context=add_to_context)
 
         return self._context
