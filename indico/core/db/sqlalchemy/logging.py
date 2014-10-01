d92b6072fe1d9053db715435e216fe80a7beb03f
Adrian Moennich <adrian.moennich@cern.ch>
2014-10-01 14:46:49 +0200

    Fix query logging
    - Handle plugin paths correctly
    - Include MaKaC code in stacktrace
    - Include mako templates in stacktrace

--- indico/core/db/sqlalchemy/logging.py
+++ indico/core/db/sqlalchemy/logging.py
@@ -30,6 +30,7 @@ from sqlalchemy.engine import Engine
 from sqlalchemy.event import listens_for
 
 from indico.core.db import db
+from indico.core.plugins import plugin_engine
 
 
 def _prettify_sql(statement):
@@ -40,17 +41,20 @@ def _prettify_params(args):
     return '    ' + pprint.pformat(args).replace('\n', '\n    ')
 
 
+def _interesting_tb_item(item, paths):
+    return (item[0].endswith('.tpl.py') or any(item[0].startswith(p) for p in paths)) and 'sqlalchemy' not in item[0]
+
+
 def _get_sql_line():
     indico_path = current_app.root_path
-    root_path = "{}/".format(os.path.dirname(indico_path))
-    stack = list(reversed(traceback.extract_stack()))
+    makac_path = os.path.abspath(os.path.join(indico_path, '..', 'MaKaC'))
+    paths = [indico_path, makac_path] + [p.root_path for p in plugin_engine.get_active_plugins().itervalues()]
+    stack = [item for item in reversed(traceback.extract_stack()) if _interesting_tb_item(item, paths)]
     for i, item in enumerate(stack):
-        if item[0].startswith(indico_path) and 'logging' not in item[0] and 'sqlalchemy' not in item[0]:
-            module_name = os.path.splitext(item[0].replace(root_path, ''))[0].replace(os.sep, '.')
-            return {'module': module_name,
-                    'line': item[1],
-                    'function': item[2],
-                    'items': stack[i:i+5]}
+        return {'file': item[0],
+                'line': item[1],
+                'function': item[2],
+                'items': stack[i:i+5]}
 
 
 def apply_db_loggers(debug=False):
@@ -67,7 +71,7 @@ def apply_db_loggers(debug=False):
         context._query_start_time = time.time()
         source_line = _get_sql_line()
         if source_line:
-            log_msg = 'Start Query:\n    {0[module]}:{0[line]} {0[function]}\n\n{1}\n{2}'.format(
+            log_msg = 'Start Query:\n    {0[file]}:{0[line]} {0[function]}\n\n{1}\n{2}'.format(
                 source_line,
                 _prettify_sql(statement),
                 _prettify_params(parameters) if parameters else ''
