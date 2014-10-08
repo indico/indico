b5d7f60066688b8f6cc51ce1309ede4a10ca4850
Pedro Ferreira <pedro.ferreira@cern.ch>
2014-10-08 09:23:25 +0200

    Revert "Adjust following entries in moveEntriesBelow"
    This reverts commit 68863de77f46ea9588733b218fcba9ed51cae840.

--- indico/MaKaC/schedule.py
+++ indico/MaKaC/schedule.py
@@ -507,7 +507,7 @@ class TimeSchedule(Schedule, Persistent):
                         if session.getSchedule().getEntries()[0].getOwner() == entry.getOwner():
                             session.setStartDate(session.getStartDate() + diff, check=0, moveEntries=0)
                         sessionsAlreadyModif.append(session)
-                entry.setStartDate(entry.getStartDate() + diff, check=2, moveEntries=1)
+                entry.setStartDate(entry.getStartDate()+diff, check=0, moveEntries=1)
 
 
 class SchEntry(Persistent, Fossilizable):
