ef83f2c4266a034bb67a66253923728d4fa34c17
Jacques Dafflon <jacques.dafflon@cern.ch>
2014-11-03 09:27:05 +0100

    Fix invalid bysetpos type from float to int
    Fix failing tests

--- indico/modules/rb/models/reservation_occurrences.py
+++ indico/modules/rb/models/reservation_occurrences.py
@@ -126,7 +126,10 @@ class ReservationOccurrence(db.Model, Serializer):
             return [start]
 
         if repeat_frequency == RepeatFrequency.DAY:
-            return rrule.rrule(rrule.DAILY, dtstart=start, until=end)
+            if repeat_interval == 1:
+                return rrule.rrule(rrule.DAILY, dtstart=start, until=end)
+            else:
+                raise IndicoError('Unsupported interval')
 
         elif repeat_frequency == RepeatFrequency.WEEK:
             if 0 < repeat_interval < 4:
@@ -135,8 +138,9 @@ class ReservationOccurrence(db.Model, Serializer):
                 raise IndicoError('Unsupported interval')
 
         elif repeat_frequency == RepeatFrequency.MONTH:
+
             if repeat_interval == 1:
-                position = start.day // 7 + 1
+                position = int(ceil(start.day / 7.0))
                 if position == 5:
                     # The fifth weekday of the month will always be the last one
                     position = -1
