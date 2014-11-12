28958ac3f7d0052aa6ce03ecff26edc9e4f1f3a9
Jacques Dafflon <jacques.dafflon@cern.ch>
2014-11-12 17:42:22 +0100

    fix user search
     - prevent user search from failing because of invalid avatar ids
     - check for bad (None) avatars returned from invalid ids
     - fix #1643

--- indico/MaKaC/user.py
+++ indico/MaKaC/user.py
@@ -1150,7 +1150,7 @@ class AvatarHolder(ObjectHolder):
             for userid in match:
                 if self.getById(userid) not in result:
                     av = self.getById(userid)
-                    if not onlyActivated or av.isActivated():
+                    if av and (not onlyActivated or av.isActivated()):
                         result[av.getEmail()] = av
         if searchInAuthenticators:
             for authenticator in AuthenticatorMgr().getList():
@@ -1184,7 +1184,7 @@ class AvatarHolder(ObjectHolder):
                         iset = iset & set(match)
         for userid in iset:
             av=self.getById(userid)
-            if not onlyActivated or av.isActivated():
+            if av and (not onlyActivated or av.isActivated()):
                 result[av.getEmail()]=av
         if searchInAuthenticators:
             for authenticator in AuthenticatorMgr().getList():
