3e2e5491717c2c76fd16a09c1e059bdecda90fcc
Alejandro Avilés <aavilesd@cern.ch>
2014-11-28 15:43:18 +0100

    Fix body title on registration modification

--- indico/MaKaC/webinterface/pages/registrationForm.py
+++ indico/MaKaC/webinterface/pages/registrationForm.py
@@ -929,7 +929,7 @@ class WPRegistrationFormModify(conferences.WPConferenceDefaultDisplayBase):
 class WConfRegistrationFormModify(WConfRegistrationFormDisplay):
 
     def _getTitle(self):
-        title = WConfRegistrationFormDisplay._getTitle(self)
+        title = WConfDisplayBodyBase._getTitle(self)
         return _('{}: Modify'.format(title))
 
     def getVars(self):
