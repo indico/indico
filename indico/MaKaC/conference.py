indico/indico

commit 68863de77f46ea9588733b218fcba9ed51cae840

Adjust following entries in moveEntriesBelow
Fixes the issue described in INC0645144

indico/MaKaC/schedule.py
@@ -507,7 +507,7 @@ class TimeSchedule(Schedule, Persistent):
                         if session.getSchedule().getEntries()[0].getOwner()==entry.getOwner():
                             session.setStartDate(session.getStartDate()+diff, check=0, moveEntries=0)
                         sessionsAlreadyModif.append(session)
-                entry.setStartDate(entry.getStartDate()+diff, check=0, moveEntries=1)
+                entry.setStartDate(entry.getStartDate() + diff, check=2, moveEntries=1)
 
 
 class SchEntry(Persistent, Fossilizable):

commit 088a53a507a690c253c78a1738e79fb3d52f4594

Fix adjust event end time on contrib shifting
This patch avoids the regression introduced by commit 68863de

indico/MaKaC/conference.py
@@ -2807,7 +2807,7 @@ class Conference(CommonObjectBase, Locatable):
             #else:
             #    entries = self.getSchedule().getEntriesOnDay(sDate.astimezone(timezone(self.getTimezone())))[:]
             entries = self.getSchedule().getEntries()[:]
-            self.getSchedule().moveEntriesBelow(diff, entries)
+            self.getSchedule().moveEntriesBelow(diff, entries, check=check)
         #datetime object is non-mutable so we must "force" the modification
         #   otherwise ZODB won't be able to notice the change
         self.notifyModification()

indico/MaKaC/schedule.py
@@ -491,7 +491,7 @@ class TimeSchedule(Schedule, Persistent):
                 return d
         return None
 
-    def moveEntriesBelow(self, diff, entriesList):
+    def moveEntriesBelow(self, diff, entriesList, check=2):
         """diff: the difference we have to increase/decrease each entry of the list.
            entriesList: list of entries for applying the diff"""
 
@@ -507,7 +507,7 @@ class TimeSchedule(Schedule, Persistent):
                         if session.getSchedule().getEntries()[0].getOwner() == entry.getOwner():
                             session.setStartDate(session.getStartDate() + diff, check=0, moveEntries=0)
                         sessionsAlreadyModif.append(session)
-                entry.setStartDate(entry.getStartDate()+diff, check=0, moveEntries=1)
+                entry.setStartDate(entry.getStartDate() + diff, check=check, moveEntries=1)
 
 
 class SchEntry(Persistent, Fossilizable):