be4274a958633031812fcb4dc8c3506a924abd88
Alejandro Avilés <aavilesd@cern.ch>
2015-01-28 15:19:09 +0100

    Add agreements blueprint

--- indico/web/flask/app.py
+++ indico/web/flask/app.py
@@ -66,6 +66,7 @@ from indico.web.flask.blueprints.files import files
 from indico.web.flask.blueprints.admin import admin
 from indico.web.flask.blueprints.rooms_admin import rooms_admin
 from indico.core.plugins.blueprints import plugins_blueprint
+from indico.modules.agreements.blueprint import agreements_blueprint
 from indico.modules.payment.blueprint import payment_blueprint
 from indico.modules.events.registration.blueprint import event_registration_blueprint
 from indico.modules.events.requests.blueprint import requests_blueprint
@@ -73,7 +74,7 @@ from indico.modules.events.requests.blueprint import requests_blueprint
 
 BLUEPRINTS = (legacy, api, misc, user, oauth, rooms, category, category_mgmt, event_display,
               event_creation, event_mgmt, files, admin, rooms_admin, plugins_blueprint, payment_blueprint,
-              event_registration_blueprint, requests_blueprint)
+              event_registration_blueprint, requests_blueprint, agreements_blueprint)
 COMPAT_BLUEPRINTS = map(make_compat_blueprint, (misc, user, oauth, rooms, category, category_mgmt, event_display,
                                                 event_creation, event_mgmt, files, admin, rooms_admin))
 
