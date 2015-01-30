5be063ab6e5e055e1d83a259399125abeebf9465
Adrian Moennich <adrian.moennich@cern.ch>
2015-01-30 15:28:59 +0100

    Test edge cases (00:00) of BookableHours

--- indico/modules/rb/models/room_bookable_hours_test.py
+++ indico/modules/rb/models/room_bookable_hours_test.py
@@ -14,27 +14,41 @@
 # You should have received a copy of the GNU General Public License
 # along with Indico; if not, see <http://www.gnu.org/licenses/>.
 
-from datetime import datetime, time
+from datetime import datetime
 
 import pytest
 
 from indico.modules.rb.models.room_bookable_hours import BookableHours
 
 
-@pytest.mark.parametrize(('start_time', 'end_time', 'expected'), (
-    ('12:00', '13:00', True),
-    ('14:00', '15:00', True),
-    ('12:00', '15:00', True),
-    ('12:30', '14:30', True),
-    ('12:00', '12:00', True),
-    ('15:00', '15:00', True),
-    ('15:00', '15:01', False),
-    ('00:00', '12:00', False),
-    ('11:00', '16:00', False),
-    ('14:00', '16:00', False),
+@pytest.mark.parametrize(('bh_start_time', 'bh_end_time', 'start_time', 'end_time', 'expected'), (
+    ('12:00', '15:00', '12:00', '13:00', True),
+    ('12:00', '15:00', '14:00', '15:00', True),
+    ('12:00', '15:00', '12:00', '15:00', True),
+    ('12:00', '15:00', '12:30', '14:30', True),
+    ('12:00', '15:00', '12:00', '12:00', True),
+    ('12:00', '15:00', '15:00', '15:00', True),
+    ('12:00', '15:00', '15:00', '15:01', False),
+    ('12:00', '15:00', '00:00', '12:00', False),
+    ('12:00', '15:00', '11:00', '16:00', False),
+    ('12:00', '15:00', '14:00', '16:00', False),
+    ('12:00', '15:00', '14:00', '00:00', False),
+    # special cases involving midnight
+    # allow whole day
+    ('00:00', '00:00', '00:00', '00:00', True),
+    # allow until midnight
+    ('20:00', '00:00', '20:00', '21:00', True),
+    ('20:00', '00:00', '21:00', '00:00', True),
+    ('20:00', '00:00', '19:00', '20:00', False),
+    ('20:00', '00:00', '00:00', '01:00', False),
+    # allow from midnight
+    ('00:00', '02:00', '23:00', '00:00', False),
+    ('00:00', '02:00', '01:00', '03:00', False),
 ))
-def test_fits_period(start_time, end_time, expected):
+def test_fits_period(bh_start_time, bh_end_time, start_time, end_time, expected):
+    bh_start_time = datetime.strptime(bh_start_time, '%H:%M').time()
+    bh_end_time = datetime.strptime(bh_end_time, '%H:%M').time()
     start_time = datetime.strptime(start_time, '%H:%M').time()
     end_time = datetime.strptime(end_time, '%H:%M').time()
-    bh = BookableHours(start_time=time(12), end_time=time(15))
+    bh = BookableHours(start_time=bh_start_time, end_time=bh_end_time)
     assert bh.fits_period(start_time, end_time) == expected
