88109c3c4d8a1723b29010db1037b92e678eb4f9
Ilias Trichopoulos <ilias.trichopoulos@cern.ch>
2015-02-12 13:19:00 +0100

    Fix typo bug in RHConfSignIn

--- indico/MaKaC/webinterface/rh/conferenceDisplay.py
+++ indico/MaKaC/webinterface/rh/conferenceDisplay.py
@@ -72,7 +72,7 @@ class RHConfSignIn( conferenceBase.RHConferenceBase, RHSignInBase):
             self._returnURL = urlHandlers.UHConferenceDisplay.getURL( self._conf )
 
         self._disabledAccountURL = lambda av: urlHandlers.UHConfDisabledAccount.getURL(self._conf, av)
-        self._unactivatedAccountURL = lambda av: urlHandlers.UHConfUnactivatedAccount.getURL(self.conf, av)
+        self._unactivatedAccountURL = lambda av: urlHandlers.UHConfUnactivatedAccount.getURL(self._conf, av)
         self._signInPage = conferences.WPConfSignIn( self, self._conf )
         self._signInPageFailed = conferences.WPConfSignIn( self, self._conf, login = self._login, msg = _("Wrong login or password")  )
 
