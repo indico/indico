ba7634d7464037198c0427c0a9e299e90b328056
Adrian Moennich <adrian.moennich@cern.ch>
2015-02-25 20:33:30 +0100

    Create indico vars from a Python dict

--- indico/web/assets/vars_js.py
+++ indico/web/assets/vars_js.py
@@ -1,7 +1,7 @@
 from flask import render_template
 
 from indico.modules.rb.models.locations import Location
-
+from indico.web.flask.util import url_rule_to_js
 from MaKaC.authentication.AuthenticationMgr import AuthenticatorMgr
 from MaKaC.webinterface.common import tools as security_tools
 from MaKaC.export import fileConverter
@@ -10,23 +10,91 @@ from MaKaC.webinterface.materialFactories import MaterialFactoryRegistry
 
 
 def generate_global_file(config):
-
     locations = Location.find_all() if config.getIsRoomBookingActive() else []
+    location_names = {loc.name: loc.name for loc in locations}
     default_location = next((loc.name for loc in locations if loc.is_default), None)
     ext_auths = [(auth.id, auth.name) for auth in AuthenticatorMgr().getList() if auth.id != 'Local']
     file_type_icons = dict((k.lower(), v[2]) for k, v in config.getFileTypes().iteritems())
     material_types = dict((evt_type, [(m, m.title()) for m in MaterialFactoryRegistry._allowedMaterials[evt_type]])
                           for evt_type in ['meeting', 'simple_event', 'conference', 'category'])
 
-    return render_template(
-        'assets/vars_globals.js',
-        config=config,
-        default_location=default_location,
-        ext_authenticators=ext_auths,
-        file_converter=fileConverter,
-        locations={loc.name: loc.name for loc in locations},
-        material_types=material_types,
-        security_tools=security_tools,
-        file_type_icons=file_type_icons,
-        urls=urlHandlers
-    )
+    indico_vars = {
+        'FileTypeIcons': file_type_icons,
+
+        'Urls': {
+            'ImagesBase': config.getImagesBaseURL(),
+            'SecureImagesBase': config.getImagesBaseSecureURL(),
+
+            'Login': urlHandlers.UHSignIn.getURL().js_router,
+            'Favourites': urlHandlers.UHUserBaskets.getURL(_ignore_static=True).js_router,
+
+            'ConferenceDisplay': urlHandlers.UHConferenceDisplay.getURL(_ignore_static=True).js_router,
+            'ContributionDisplay': urlHandlers.UHContributionDisplay.getURL(_ignore_static=True).js_router,
+            'SessionDisplay': urlHandlers.UHSessionDisplay.getURL(_ignore_static=True).js_router,
+
+            'ContribToXML': urlHandlers.UHContribToXML.getURL(_ignore_static=True).js_router,
+            'ContribToPDF': urlHandlers.UHContribToPDF.getURL(_ignore_static=True).js_router,
+
+            'ConfTimeTablePDF': urlHandlers.UHConfTimeTablePDF.getURL(_ignore_static=True).js_router,
+            'ConfTimeTableCustomPDF': urlHandlers.UHConfTimeTableCustomizePDF.getURL(_ignore_static=True).js_router,
+
+            'SessionModification': urlHandlers.UHSessionModification.getURL(_ignore_static=True).js_router,
+            'ContributionModification': urlHandlers.UHContributionModification.getURL(_ignore_static=True).js_router,
+            'SessionProtection': urlHandlers.UHSessionModifAC.getURL(_ignore_static=True).js_router,
+            'ContributionProtection': urlHandlers.UHContribModifAC.getURL(_ignore_static=True).js_router,
+
+            'Reschedule': urlHandlers.UHConfModifReschedule.getURL(_ignore_static=True).js_router,
+            'SlotCalc': urlHandlers.UHSessionModSlotCalc.getURL(_ignore_static=True).js_router,
+            'FitSessionSlot': urlHandlers.UHSessionFitSlot.getURL(_ignore_static=True).js_router,
+
+            'UploadAction': {
+                'subcontribution': urlHandlers.UHSubContribModifAddMaterials.getURL(_ignore_static=True).js_router,
+                'contribution': urlHandlers.UHContribModifAddMaterials.getURL(_ignore_static=True).js_router,
+                'session': urlHandlers.UHSessionModifAddMaterials.getURL(_ignore_static=True).js_router,
+                'conference': urlHandlers.UHConfModifAddMaterials.getURL(_ignore_static=True).js_router,
+                'category': urlHandlers.UHCategoryAddMaterial.getURL(_ignore_static=True).js_router
+            },
+
+            'RoomBookingBookRoom': url_rule_to_js('rooms.room_book'),
+            'RoomBookingBook': url_rule_to_js('rooms.book'),
+            'RoomBookingDetails': urlHandlers.UHRoomBookingRoomDetails.getURL(_ignore_static=True).js_router,
+            'RoomBookingCloneBooking': url_rule_to_js('rooms.roomBooking-cloneBooking'),
+            'ConfModifSchedule': urlHandlers.UHConfModifSchedule.getURL(_ignore_static=True).js_router,
+            'SubcontrModif': urlHandlers.UHContribModifSubCont.getURL(_ignore_static=True).js_router,
+            'AuthorDisplay': urlHandlers.UHContribAuthorDisplay.getURL(_ignore_static=True).js_router,
+            'AuthorEmail': urlHandlers.UHConferenceEmail.getURL(_ignore_static=True).js_router
+        },
+
+        'Data': {
+            'MaterialTypes': material_types,
+            'WeekDays': ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+            'DefaultLocation': default_location,
+            'Locations': location_names
+        },
+
+        'Security': {
+            'allowedTags': ','.join(security_tools.allowedTags),
+            'allowedAttributes': ','.join(security_tools.allowedAttrs),
+            'allowedCssProperties': ','.join(security_tools.allowedCssProperties),
+            'allowedProtocols': ','.join(security_tools.allowedProtocols),
+            'urlProperties': ','.join(security_tools.urlProperties),
+            'sanitizationLevel': config.getSanitizationLevel()
+        },
+
+        'Settings': {
+            'ExtAuthenticators': ext_auths,
+            'RoomBookingModuleActive': config.getIsRoomBookingActive()
+        },
+
+        'FileRestrictions': {
+            'MaxUploadFilesTotalSize': config.getMaxUploadFilesTotalSize(),
+            'MaxUploadFileSize': config.getMaxUploadFileSize()
+        },
+
+        'PDFConversion': {
+            'AvailablePDFConversions': fileConverter.CDSConvFileConverter.getAvailableConversions(),
+            'HasFileConverter': config.hasFileConverter()
+        }
+    }
+
+    return render_template('assets/vars_globals.js', indico_vars=indico_vars, config=config, url_handlers=urlHandlers)
--- indico/web/templates/assets/vars_globals.js
+++ indico/web/templates/assets/vars_globals.js
@@ -1,88 +1,15 @@
-var Indico = {
-    FileTypeIcons:
-        {{ file_type_icons | tojson }},
-    Urls: {
-        JsonRpcService: window.location.protocol == "https:"?"{{ urls.UHJsonRpcService.getURL(secure=True) }}":"{{ urls.UHJsonRpcService.getURL() }}",
+var Indico = {{ indico_vars | tojson }};
 
-        Base: (window.location.protocol == "https:" ? "{{ config.getBaseSecureURL() }}" : "{{ config.getBaseURL() }}"),
-
-        ImagesBase: "{{ config.getImagesBaseURL() }}",
-        SecureImagesBase: "{{ config.getImagesBaseSecureURL() }}",
-
-        ExportAPIBase: (window.location.protocol == "https:" ? "{{ urls.UHAPIExport.getURL(secure=True) }}" : "{{ urls.UHAPIExport.getURL() }}"),
-        APIBase: (window.location.protocol == "https:" ? "{{ urls.UHAPIAPI.getURL(secure=True) }}" : "{{ urls.UHAPIAPI.getURL() }}"),
-
-        Login: {{ urls.UHSignIn.getURL().js_router | tojson }},
-        Favourites: {{ urls.UHUserBaskets.getURL(_ignore_static=True).js_router | tojson }},
-
-        ConferenceDisplay: {{ urls.UHConferenceDisplay.getURL(_ignore_static=True).js_router | tojson }},
-        ContributionDisplay: {{ urls.UHContributionDisplay.getURL(_ignore_static=True).js_router | tojson }},
-        SessionDisplay: {{ urls.UHSessionDisplay.getURL(_ignore_static=True).js_router | tojson }},
-
-        ContribToXML: {{ urls.UHContribToXML.getURL(_ignore_static=True).js_router | tojson }},
-        ContribToPDF: {{ urls.UHContribToPDF.getURL(_ignore_static=True).js_router | tojson }},
-
-        ConfTimeTablePDF: {{ urls.UHConfTimeTablePDF.getURL(_ignore_static=True).js_router | tojson }},
-        ConfTimeTableCustomPDF: {{ urls.UHConfTimeTableCustomizePDF.getURL(_ignore_static=True).js_router | tojson }},
-
-        SessionModification: {{ urls.UHSessionModification.getURL(_ignore_static=True).js_router | tojson }},
-        ContributionModification: {{ urls.UHContributionModification.getURL(_ignore_static=True).js_router | tojson }},
-        SessionProtection: {{ urls.UHSessionModifAC.getURL(_ignore_static=True).js_router | tojson }},
-        ContributionProtection: {{ urls.UHContribModifAC.getURL(_ignore_static=True).js_router | tojson }},
-
-        Reschedule: {{ urls.UHConfModifReschedule.getURL(_ignore_static=True).js_router | tojson }},
-        SlotCalc: {{ urls.UHSessionModSlotCalc.getURL(_ignore_static=True).js_router | tojson }},
-        FitSessionSlot: {{ urls.UHSessionFitSlot.getURL(_ignore_static=True).js_router | tojson }},
-
-        UploadAction: {
-            subcontribution: {{ urls.UHSubContribModifAddMaterials.getURL(_ignore_static=True).js_router | tojson }},
-            contribution: {{ urls.UHContribModifAddMaterials.getURL(_ignore_static=True).js_router | tojson }},
-            session: {{ urls.UHSessionModifAddMaterials.getURL(_ignore_static=True).js_router | tojson }},
-            conference: {{ urls.UHConfModifAddMaterials.getURL(_ignore_static=True).js_router | tojson }},
-            category: {{ urls.UHCategoryAddMaterial.getURL(_ignore_static=True).js_router | tojson }}
-        },
-
-        RoomBookingBookRoom: {{ url_rule_to_js('rooms.room_book') | tojson }},
-        RoomBookingBook: {{ url_rule_to_js('rooms.book') | tojson }},
-        RoomBookingDetails: {{ urls.UHRoomBookingRoomDetails.getURL(_ignore_static=True).js_router | tojson }},
-        RoomBookingCloneBooking: {{ url_rule_to_js('rooms.roomBooking-cloneBooking')  | tojson }},
-        ConfModifSchedule: {{ urls.UHConfModifSchedule.getURL(_ignore_static=True).js_router | tojson }},
-        SubcontrModif: {{ urls.UHContribModifSubCont.getURL(_ignore_static=True).js_router | tojson }},
-        AuthorDisplay: {{ urls.UHContribAuthorDisplay.getURL(_ignore_static=True).js_router | tojson }},
-        AuthorEmail: {{ urls.UHConferenceEmail.getURL(_ignore_static=True).js_router | tojson }}
-    },
-
-    Data: {
-        MaterialTypes: {{ material_types | tojson }},
-        WeekDays: {{ [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ] }},
-        DefaultLocation: {{ default_location | tojson }},
-        Locations: {{ locations | tojson }}
-    },
-
-    Security:{
-        allowedTags: "{{ ",".join(security_tools.allowedTags) }}",
-        allowedAttributes: "{{ ",".join(security_tools.allowedAttrs) }}",
-        allowedCssProperties: "{{ ",".join(security_tools.allowedCssProperties) }}",
-        allowedProtocols: "{{ ",".join(security_tools.allowedProtocols) }}",
-        urlProperties: "{{ ",".join(security_tools.urlProperties) }}",
-        sanitizationLevel: {{ config.getSanitizationLevel() }}
-    },
-
-    Settings: {
-        ExtAuthenticators: {{ ext_authenticators | tojson }},
-        RoomBookingModuleActive: {{ config.getIsRoomBookingActive() | tojson }}
-    },
-
-    FileRestrictions: {
-        MaxUploadFilesTotalSize: {{ config.getMaxUploadFilesTotalSize() }},
-        MaxUploadFileSize: {{ config.getMaxUploadFileSize() }}
-    },
-
-    PDFConversion: {
-        AvailablePDFConversions: {{file_converter.CDSConvFileConverter.getAvailableConversions()}},
-        HasFileConverter: {{ config.hasFileConverter() | tojson }}
-    }
-
-};
+if (location.protocol == 'https:') {
+    Indico.Urls.Base = {{ config.getBaseSecureURL() | string | tojson }};
+    Indico.Urls.JsonRpcService = {{ url_handlers.UHJsonRpcService.getURL(secure=True) | string | tojson }};
+    Indico.Urls.ExportAPIBase = {{ url_handlers.UHAPIExport.getURL(secure=True) | string | tojson }};
+    Indico.Urls.APIBase = {{ url_handlers.UHAPIAPI.getURL(secure=True) | string | tojson }};
+} else {
+    Indico.Urls.Base = {{ config.getBaseURL() | string | tojson }};
+    Indico.Urls.JsonRpcService = {{ url_handlers.UHJsonRpcService.getURL() | string | tojson }};
+    Indico.Urls.ExportAPIBase = {{ url_handlers.UHAPIExport.getURL() | string | tojson }};
+    Indico.Urls.APIBase = {{ url_handlers.UHAPIAPI.getURL() | string | tojson }};
+}
 
 {{ template_hook('vars-js') }}
