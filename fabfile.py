d51a2d2a3e72ceec7caf2f40fa5a303f52f77c44
Ilias Trichopoulos <ilias.trichopoulos@cern.ch>
2015-02-26 11:00:50 +0100

    Replace {} with {0} in fabfile

--- fabfile.py
+++ fabfile.py
@@ -387,10 +387,10 @@ def install_selectize_js():
     with lcd(os.path.join(env.ext_dir, 'selectize.js')):
         dest_js_dir = os.path.join(lib_dir(env.src_dir, 'js'), 'selectize.js/')
         dest_css_dir = os.path.join(lib_dir(env.src_dir, 'css'), 'selectize.js/')
-        local('mkdir -p {} {}'.format(dest_js_dir, dest_css_dir))
-        local('cp dist/js/standalone/selectize.js {}/'.format(dest_js_dir))
-        local('cp dist/css/selectize.css {}/'.format(dest_css_dir))
-        local('cp dist/css/selectize.default.css {}/'.format(dest_css_dir))
+        local('mkdir -p {0} {1}'.format(dest_js_dir, dest_css_dir))
+        local('cp dist/js/standalone/selectize.js {0}/'.format(dest_js_dir))
+        local('cp dist/css/selectize.css {0}/'.format(dest_css_dir))
+        local('cp dist/css/selectize.default.css {0}/'.format(dest_css_dir))
 
 
 # Tasks
