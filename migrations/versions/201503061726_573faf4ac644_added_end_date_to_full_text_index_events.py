fb7754f15a8f0803c5417782e87d6fe153bf6d20
Adrian Moennich <adrian.moennich@cern.ch>
2015-03-09 14:03:00 +0100

    Use index name matching the current naming schema

--- migrations/versions/201503061726_573faf4ac644_added_end_date_to_full_text_index_events.py
+++ migrations/versions/201503061726_573faf4ac644_added_end_date_to_full_text_index_events.py
@@ -16,17 +16,16 @@ down_revision = '342fa3076650'
 
 def upgrade():
     op.alter_column('event_index', 'start_date', nullable=False, schema='events')
-    op.create_index('ix_start_date', 'event_index', ['start_date'], schema='events')
+    op.create_index('ix_events_event_index_start_date', 'event_index', ['start_date'], schema='events')
 
     op.add_column('event_index',
                   sa.Column('end_date', sa.DateTime(), nullable=False, server_default='now()'),
                   schema='events')
     op.alter_column('event_index', 'end_date', server_default=None, schema='events')
-    op.create_index('ix_end_date', 'event_index', ['end_date'], schema='events')
+    op.create_index('ix_events_event_index_end_date', 'event_index', ['end_date'], schema='events')
 
 
 def downgrade():
     op.alter_column('event_index', 'start_date', nullable=True, schema='events')
-    op.drop_index('ix_start_date', table_name='event_index', schema='events')
-
+    op.drop_index('ix_events_event_index_start_date', table_name='event_index', schema='events')
     op.drop_column('event_index', 'end_date', schema='events')
