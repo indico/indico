a5ad5c289d4d5b3494b38b85c4e69227f44c3588
Jacques Dafflon <jacques.dafflon@cern.ch>
2015-03-11 15:38:12 +0100

    ZeroClipboard, clipboard input macro & in tooltip
     - i-action -> i-has-action
     - refactor zeroclipboard in a more generic way
     - clipboard input macro using i-has-action
     - automatically register ZeroClipboard for the macro above
     - support for i-has-action in tooltip (in a dd)

---
+++ indico/htdocs/js/custom/zeroclipboard.js
@@ -0,0 +1,34 @@
+$(function zeroclipboardInit() {
+    'use strict';
+
+    // check if ZeroClipboard is loaded and if the HTML init class is defined
+    if (!window.ZeroClipboard || !$('.zeroclicboard-init').length) { return; }
+
+    ZeroClipboard.config({'swfPath': Indico.Urls.Base + '/js/lib/zeroclipboard/ZeroClipboard.swf'});
+
+    // register i-has-action-clip ZeroClipboard handling
+    var $iHasAction = $('.i-has-action.i-has-action-clip > .i-button');
+    if ($iHasAction.length) {
+        ZeroClipboard.on('ready', function() {
+            var client = new ZeroClipboard($iHasAction);
+            client.on('error', function() { client.destroy(); });
+
+            client.on('ready', function() {
+                client.on('copy', function(event) {
+                    event.clipboardData.setData('text/plain', $(event.target).siblings('input').val());
+                });
+                client.on('aftercopy', function(event) {
+                    $(event.target).parent().siblings('.i-has-action-message')
+                        .css('visibility', 'visible')
+                        .fadeTo(50, 1)
+                        .fadeTo(1500, 0)
+                        .animate({'visibility': 'hidden'}, 0); // animate to have css queued after fadeOut
+                });
+            });
+        });
+    }
+
+    $(document).one('mouseover', '.zeroclicboard-init', function loadZeroClipBoardFlash() {
+        ZeroClipboard.create(); // loads the Flash file
+    });
+});
