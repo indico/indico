f62d4f0a73496a512bebde40296866e398a26bde
Alejandro Avilés <aavilesd@cern.ch>
2015-03-16 11:27:21 +0100

    Add tests to agreements

---
+++ indico/modules/events/agreements/models/agreements_test.py
@@ -0,0 +1,210 @@
+# This file is part of Indico.
+# Copyright (C) 2002 - 2015 European Organization for Nuclear Research (CERN).
+#
+# Indico is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+#
+# Indico is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Indico; if not, see <http://www.gnu.org/licenses/>.
+
+import pytest
+from freezegun import freeze_time
+
+from indico.core.errors import IndicoError
+from indico.modules.events.agreements.models.agreements import Agreement, AgreementState
+from indico.util.date_time import now_utc
+
+pytest_plugins = 'indico.modules.events.agreements.testing.fixtures'
+
+
+@pytest.mark.parametrize(('state', 'expected'), (
+    (AgreementState.pending, False),
+    (AgreementState.accepted, True),
+    (AgreementState.rejected, False),
+    (AgreementState.accepted_on_behalf, True),
+    (AgreementState.rejected_on_behalf, False),
+))
+def test_accepted(dummy_agreement, state, expected):
+    dummy_agreement.state = state
+    assert dummy_agreement.accepted == expected
+    assert Agreement.find_one(accepted=expected) == dummy_agreement
+
+
+@pytest.mark.parametrize(('state', 'expected'), (
+    (AgreementState.pending, True),
+    (AgreementState.accepted, False),
+    (AgreementState.rejected, False),
+    (AgreementState.accepted_on_behalf, False),
+    (AgreementState.rejected_on_behalf, False),
+))
+def test_pending(dummy_agreement, state, expected):
+    dummy_agreement.state = state
+    assert dummy_agreement.pending == expected
+    assert Agreement.find_one(pending=expected) == dummy_agreement
+
+
+@pytest.mark.parametrize(('state', 'expected'), (
+    (AgreementState.pending, False),
+    (AgreementState.accepted, False),
+    (AgreementState.rejected, True),
+    (AgreementState.accepted_on_behalf, False),
+    (AgreementState.rejected_on_behalf, True),
+))
+def test_rejected(dummy_agreement, state, expected):
+    dummy_agreement.state = state
+    assert dummy_agreement.rejected == expected
+    assert Agreement.find_one(rejected=expected) == dummy_agreement
+
+
+@pytest.mark.parametrize(('state', 'expected'), (
+    (AgreementState.pending, False),
+    (AgreementState.accepted, False),
+    (AgreementState.rejected, False),
+    (AgreementState.accepted_on_behalf, True),
+    (AgreementState.rejected_on_behalf, True),
+))
+def test_signed_on_behalf(dummy_agreement, state, expected):
+    dummy_agreement.state = state
+    assert dummy_agreement.signed_on_behalf == expected
+    assert Agreement.find_one(signed_on_behalf=expected) == dummy_agreement
+
+
+def test_definition(mocker):
+    from indico.modules.events.agreements import util
+    mocker.patch.object(util, 'get_agreement_definitions', return_value={'foobar': 'test'})
+    assert Agreement(type='foobar').definition == 'test'
+    assert Agreement(type='barfoo').definition is None
+
+
+def test_event(dummy_event):
+    agreement = Agreement(event_id=int(dummy_event.getId()))
+    assert agreement.event == dummy_event
+
+
+def test_event_setter(dummy_event):
+    agreement = Agreement()
+    agreement.event = dummy_event
+    assert agreement.event_id == int(dummy_event.getId())
+
+
+def test_locator():
+    id_ = 1337
+    event_id = 9000
+    agreement = Agreement(id=id_, event_id=event_id)
+    assert agreement.locator == {'id': id_,
+                                 'confId': event_id}
+
+
+def test_user(dummy_user):
+    agreement = Agreement(user_id=dummy_user.getId())
+    assert agreement.user == dummy_user
+
+
+def test_user_no_user():
+    agreement = Agreement()
+    assert agreement.user is None
+
+
+def test_user_setter(dummy_user):
+    agreement = Agreement()
+    agreement.user = dummy_user
+    assert agreement.user_id == dummy_user.getId()
+
+
+@pytest.mark.parametrize(('person_with_user'), (True, False))
+def test_create_from_data(dummy_event, dummy_person, dummy_user, person_with_user):
+    type_ = 'dummy'
+    dummy_person.user = dummy_user if person_with_user else None
+    agreement = Agreement.create_from_data(event=dummy_event, type_=type_, person=dummy_person)
+    assert agreement.event == dummy_event
+    assert agreement.type == type_
+    assert agreement.state == AgreementState.pending
+    assert agreement.uuid
+    assert agreement.identifier == dummy_person.identifier
+    assert agreement.person_email == dummy_person.email
+    assert agreement.person_name == dummy_person.name
+    assert agreement.user == dummy_person.user
+    assert agreement.data == dummy_person.data
+
+
+@freeze_time(now_utc())
+@pytest.mark.usefixtures('mock_agreement_definition')
+@pytest.mark.parametrize(('reason', 'on_behalf', 'expected_state'), (
+    (None,     True,  AgreementState.accepted_on_behalf),
+    (None,     False, AgreementState.accepted),
+    ('reason', False, AgreementState.accepted),
+))
+def test_accept(reason, on_behalf, expected_state):
+    ip = '127.0.0.1'
+    agreement = Agreement()
+    agreement.accept(from_ip=ip, reason=reason, on_behalf=on_behalf)
+    assert agreement.state == expected_state
+    assert agreement.signed_from_ip == ip
+    assert agreement.reason == reason
+    assert agreement.signed_dt == now_utc()
+    agreement.definition.handle_accepted.assert_called_with(agreement)
+
+
+@freeze_time(now_utc())
+@pytest.mark.usefixtures('mock_agreement_definition')
+@pytest.mark.parametrize(('reason', 'on_behalf', 'expected_state'), (
+    (None,     True,  AgreementState.rejected_on_behalf),
+    (None,     False, AgreementState.rejected),
+    ('reason', False, AgreementState.rejected),
+))
+def test_reject(reason, on_behalf, expected_state):
+    ip = '127.0.0.1'
+    agreement = Agreement()
+    agreement.reject(from_ip=ip, reason=reason, on_behalf=on_behalf)
+    assert agreement.state == expected_state
+    assert agreement.signed_from_ip == ip
+    assert agreement.reason == reason
+    assert agreement.signed_dt == now_utc()
+    agreement.definition.handle_rejected.assert_called_with(agreement)
+
+
+@pytest.mark.usefixtures('mock_agreement_definition')
+def test_reset():
+    agreement = Agreement()
+    agreement.reset()
+    assert agreement.state == AgreementState.pending
+    assert agreement.attachment is None
+    assert agreement.attachment_filename is None
+    assert agreement.data is None
+    assert agreement.reason is None
+    assert agreement.signed_dt is None
+    assert agreement.signed_from_ip is None
+
+
+@pytest.mark.usefixtures('mock_agreement_definition')
+def test_render():
+    agreement = Agreement()
+    agreement.render(None)
+    agreement.definition.render_form.assert_called_with(agreement, None)
+
+
+def test_render_no_definition(monkeypatch):
+    monkeypatch.setattr(Agreement, 'definition', property(lambda s: None))
+    agreement = Agreement()
+    with pytest.raises(IndicoError):
+        agreement.render(None)
+
+
+def test_belongs_to():
+    person_identifier = '1234asdf'
+    agreement = Agreement(identifier=person_identifier)
+    assert agreement.identifier == person_identifier
+
+
+@pytest.mark.usefixtures('mock_agreement_definition')
+def test_is_orphan(dummy_event):
+    agreement = Agreement(event_id=dummy_event.id)
+    agreement.is_orphan()
+    agreement.definition.is_agreement_orphan(agreement.event, agreement)
---
+++ indico/modules/events/agreements/testing/fixtures.py
@@ -0,0 +1,64 @@
+# This file is part of Indico.
+# Copyright (C) 2002 - 2015 European Organization for Nuclear Research (CERN).
+#
+# Indico is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+#
+# Indico is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Indico; if not, see <http://www.gnu.org/licenses/>.
+
+from uuid import uuid4
+
+import pytest
+
+from indico.modules.events.agreements.base import AgreementPersonInfo
+from indico.modules.events.agreements.models.agreements import Agreement, AgreementState
+
+
+@pytest.fixture
+def create_person(dummy_user):
+    """Returns a callable which lets you create AgreementPersonInfo"""
+
+    def _create_person(name, email, data=None):
+        person = AgreementPersonInfo(name=name, email=email, data=data)
+        return person
+
+    return _create_person
+
+
+@pytest.fixture
+def create_agreement(db, mocker, dummy_event, dummy_person):
+    """Returns a a callable which lets you create agreements"""
+
+    def _create_agreement(state):
+        agreement = Agreement(uuid=str(uuid4()), event_id=dummy_event.getId(), type='dummy',
+                              person_email=dummy_person.email, person_name=dummy_person.name, state=state,
+                              identifier=dummy_person.identifier)
+        db.session.add(agreement)
+        db.session.flush()
+        return agreement
+
+    return _create_agreement
+
+
+@pytest.fixture
+def dummy_person(create_person):
+    return create_person('dummy', 'dummy@test.com')
+
+
+@pytest.fixture
+def dummy_agreement(create_agreement):
+    """Gives you a dummy agreement with pending state"""
+    return create_agreement(state=AgreementState.pending)
+
+
+@pytest.fixture
+def mock_agreement_definition(mocker):
+    mocker.patch.object(Agreement, 'definition')
