ec03f384c11c1ba8d958a46acc313edf7f34ed75
Adrian Moennich <adrian.moennich@cern.ch>
2015-03-18 11:23:04 +0100

    Add users module stub

--- indico/web/flask/app.py
+++ indico/web/flask/app.py
@@ -72,13 +72,14 @@ from indico.modules.payment.blueprint import payment_blueprint
 from indico.modules.vc.blueprint import vc_blueprint, vc_compat_blueprint
 from indico.modules.events.registration.blueprint import event_registration_blueprint
 from indico.modules.events.requests.blueprint import requests_blueprint
+from indico.modules.users.blueprint import users_blueprint
 from indico.web.assets.blueprint import assets_blueprint
 
 
 BLUEPRINTS = (legacy, misc, user, oauth, rooms, category, category_mgmt, event_display,
               event_creation, event_mgmt, files, admin, rooms_admin, plugins_blueprint, payment_blueprint,
               event_registration_blueprint, requests_blueprint, agreements_blueprint, vc_blueprint, assets_blueprint,
-              api_blueprint)
+              api_blueprint, users_blueprint)
 COMPAT_BLUEPRINTS = map(make_compat_blueprint, (misc, user, oauth, rooms, category, category_mgmt, event_display,
                                                 event_creation, event_mgmt, files, admin, rooms_admin))
 COMPAT_BLUEPRINTS += (vc_compat_blueprint,)
