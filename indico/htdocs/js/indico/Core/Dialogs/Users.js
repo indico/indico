4df7822ead5bff497fa4f6b15db95439de1c6a6f
Ilias Trichopoulos <ilias.trichopoulos@cern.ch>
2015-05-07 16:24:32 +0200

    Fix conveners list in session basic edit page
    Close #1868

--- indico/htdocs/js/indico/Core/Dialogs/Users.js
+++ indico/htdocs/js/indico/Core/Dialogs/Users.js
@@ -1628,8 +1628,10 @@ type("UserListField", ["IWidget"], {
             each(initialUsers, function(user){
                 if (any(user._type, null) === 'Avatar') {
                     self.userList.set('existingAv' + user.id, $O(user));
-                } else {
+                } else if (user._type === 'LDAPGroupWrapper' || user._type === 'LocalGroupWrapper') {
                     self.userList.set(user.identifier, $O(user));
+                } else {
+                    self.userList.set(user.id, $O(user));
                 }
             });
         }
