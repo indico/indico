3a747001b4c06447bea490b0344274824ac4d33c
Jacques Dafflon <jacques.dafflon@cern.ch>
2015-06-01 16:36:02 +0200

    List Celery tasks in the admin area

--- indico/web/flask/app.py
+++ indico/web/flask/app.py
@@ -70,6 +70,7 @@ from indico.web.flask.blueprints.files import files
 from indico.web.flask.blueprints.admin import admin
 from indico.web.flask.blueprints.rooms_admin import rooms_admin
 
+from indico.core.celery.blueprint import celery_blueprint
 from indico.core.plugins.blueprint import plugins_blueprint
 from indico.modules.api.blueprint import api_blueprint
 from indico.modules.auth.blueprint import auth_blueprint, auth_compat_blueprint
@@ -88,10 +89,10 @@ from indico.web.assets.blueprint import assets_blueprint
 
 
 BLUEPRINTS = (legacy, misc, rooms, category, category_mgmt, event_display,
-              event_creation, event_mgmt, files, admin, rooms_admin, plugins_blueprint, payment_blueprint,
-              event_registration_blueprint, requests_blueprint, agreements_blueprint, evaluation_blueprint,
-              event_reminders_blueprint, static_site_blueprint, vc_blueprint, assets_blueprint, api_blueprint,
-              users_blueprint, oauth_blueprint, auth_blueprint, groups_blueprint)
+              event_creation, event_mgmt, files, admin, rooms_admin, celery_blueprint, plugins_blueprint,
+              payment_blueprint, event_registration_blueprint, requests_blueprint, agreements_blueprint,
+              evaluation_blueprint, event_reminders_blueprint, static_site_blueprint, vc_blueprint, assets_blueprint,
+              api_blueprint, users_blueprint, oauth_blueprint, auth_blueprint, groups_blueprint)
 COMPAT_BLUEPRINTS = map(make_compat_blueprint, (misc, rooms, category, category_mgmt, event_display,
                                                 event_creation, event_mgmt, files, admin, rooms_admin))
 COMPAT_BLUEPRINTS += (vc_compat_blueprint, auth_compat_blueprint)
