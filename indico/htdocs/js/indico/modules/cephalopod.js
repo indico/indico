1491e85bc5c32a4be7c33ad7dafcaf2849f43845
Jacques Dafflon <jacques.dafflon@cern.ch>
2015-06-10 14:27:29 +0200

    Improve UI and use the new sync endpoint

--- indico/htdocs/js/indico/modules/cephalopod.js
+++ indico/htdocs/js/indico/modules/cephalopod.js
@@ -1,4 +1,14 @@
-function _isSynced(cephalopodUrl, localData) {
+/**
+ * Check if the data returned by `cephalopodUrl` matches `localData`
+ *
+ * Performs an ajax request to retrieve the data to check and return a
+ * jQuery promise with the a boolean indicating if the data matches or
+ * not. If the ajax request fails, the promise is rejected wit the ajax
+ * error. An error flash message is also shown on the page unless the
+ * `quiet` option is set to `false` (defaults to `true`).
+ */
+function _isSynced(cephalopodUrl, localData, quiet) {
+    quiet = quiet || false;
     var defer = $.Deferred();
     $.ajax({
         url: cephalopodUrl,
@@ -12,14 +22,16 @@ function _isSynced(cephalopodUrl, localData) {
         });
         defer.resolve(synced);
     }).fail(function onError(response, status, error) {
-        $('<span>', { 'class': 'mono', 'text': error }).appendTo(
-            $('<div>', {
-                'class': 'message-text',
-                'html' : $T('Something went wrong while contacting the instance tracker.<br>It returned the following error: ')
-            }).appendTo(
-                $('<div>', { 'class': 'error-message-box'})
-                    .appendTo($('#flashed-messages')))
-        );
+        if (!quiet) {
+            $('<span>', { 'class': 'mono', 'text': error }).appendTo(
+                $('<div>', {
+                    'class': 'message-text',
+                    'html' : $T('Something went wrong while contacting the instance tracker.<br>It returned the following error: ')
+                }).appendTo(
+                    $('<div>', { 'class': 'error-message-box'})
+                        .appendTo($('#flashed-messages')))
+            );
+        }
         defer.reject(error);
     });
     return defer.promise();
@@ -34,22 +46,59 @@ function initCephalopdOnSettingsPage(cephalopodUrl) {
         $('#sync-button').prop('disabled', !enabled);
     });
 
-    // Only check if synced, if enabled;
-    if (!enabled) { return; }
-
     var defer = $.Deferred();
+    $.when(defer).always(function(label) {
+        $('#tracking-status')
+            .attr('class', 'action-box ' + label.class)
+            .find('.section > .icon')
+                .attr('class', 'icon ' + label.icon)
+                .next('.text')
+                .children('.label')
+                    .html(label.title)
+                    .next()
+                    .html(label.subtitle);
+    });
+
+    // Only check if synced, if enabled;
+    if (!enabled) {
+        defer.resolve({
+            'title'   : $T('Disabled'),
+            'subtitle': $T('Your instance of Indico is not tracked.'),
+            'class'   : 'disabled',
+            'icon'    : 'icon-close'
+        });
+        return;
+    }
+
     _isSynced(cephalopodUrl, {
         'enabled'     : $('#tracked').prop('checked'),
         'contact'     : $('#contact_name').val(),
         'email'       : $('#contact_email').val(),
         'url'         : $('#instance-url').html(),
         'organisation': $('#affiliation').html()
-    }).done(function onSuccess(synced) {
-        $('<span>', {'class' : 'i-label'})
-        defer.resolve(synced ? $T('Synchronized') : $T('Out of sync'))
-        $('#tracking-status').html();
+    }, true).done(function onSuccess(synced) {
+        if (synced) {
+            defer.resolve({
+                'title'   : $T('Synchronized'),
+                'subtitle': $T('Your instance of Indico is tracked and up to date.'),
+                'class'   : 'highlight',
+                'icon'    : 'icon-checkmark'
+            });
+        } else {
+            $('#sync-tracking').show();
+            defer.resolve({
+                'title'   : $T('Out of sync'),
+                'subtitle': $T('Your instance is out of sync. Synchronize your instance with the button on the right.'),
+                'class'   : 'warning',
+                'icon'    : 'icon-warning'
+            });
+        }
     }).fail(function onError(err) {
-        defer.reject(err);
+        defer.reject({
+            'title'   : $T('Error!'),
+            'subtitle': $T('The instance tracker returned the following error: {0}.').format(err),
+            'class'   : 'danger',
+            'icon'    :'icon-disable'});
     });
 }
 
