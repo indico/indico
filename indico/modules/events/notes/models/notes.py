74eb2853aeffd327b319b0ef852b59941a42150f
Adrian Moennich <adrian.moennich@cern.ch>
2015-06-18 16:01:37 +0200

    Add basic event notes module (only models so far)

---
+++ indico/modules/events/notes/models/notes.py
@@ -0,0 +1,228 @@
+# This file is part of Indico.
+# Copyright (C) 2002 - 2015 European Organization for Nuclear Research (CERN).
+#
+# Indico is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+#
+# Indico is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Indico; if not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+
+from functools import partial
+
+from sqlalchemy.event import listens_for, listen
+from sqlalchemy.ext.declarative import declared_attr
+
+from indico.core.db import db
+from indico.core.db.sqlalchemy import PyIntEnum, UTCDateTime
+from indico.core.db.sqlalchemy.links import LinkMixin, LinkType
+from indico.core.db.sqlalchemy.util.models import auto_table_args
+from indico.util.date_time import now_utc
+from indico.util.string import return_ascii, to_unicode, render_markdown
+from indico.util.struct.enum import TitledIntEnum
+
+
+class RenderMode(TitledIntEnum):
+    __titles__ = [None, 'HTML', 'Markdown']
+    html = 1
+    markdown = 2
+
+
+class EventNote(LinkMixin, db.Model):
+    __tablename__ = 'notes'
+    allowed_link_types = LinkMixin.allowed_link_types - {LinkType.category}
+    unique_links = True
+
+    @declared_attr
+    def __table_args__(cls):
+        return auto_table_args(cls, schema='events')
+
+    #: The ID of the note
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+    #: If the note has been deleted
+    is_deleted = db.Column(
+        db.Boolean,
+        nullable=False,
+        default=False
+    )
+    #: The rendered HTML of the note
+    html = db.Column(
+        db.Text,
+        nullable=False
+    )
+    #: The ID of the current revision
+    current_revision_id = db.Column(
+        db.Integer,
+        db.ForeignKey('events.note_revisions.id', use_alter=True),
+        nullable=True  # needed for post_update :(
+    )
+
+    revisions = db.relationship(
+        'EventNoteRevision',
+        primaryjoin=lambda: EventNote.id == EventNoteRevision.note_id,
+        foreign_keys=lambda: EventNoteRevision.note_id,
+        lazy=True,
+        cascade='all, delete-orphan',
+        order_by=lambda: EventNoteRevision.created_dt.desc(),
+        backref=db.backref(
+            'note',
+            lazy=False
+        )
+    )
+    current_revision = db.relationship(
+        'EventNoteRevision',
+        primaryjoin=lambda: EventNote.current_revision_id == EventNoteRevision.id,
+        foreign_keys=current_revision_id,
+        lazy=True,
+        post_update=True
+    )
+
+    @classmethod
+    def get_for_linked_object(cls, linked_object):
+        """Gets the note for the given object.
+
+        This only returns a note that hasn't been deleted.
+        """
+        return cls.find_first(linked_object=linked_object, is_deleted=False)
+
+    @classmethod
+    def get_or_create(cls, linked_object):
+        """Gets the note for the given object or creates a new one.
+
+        If there is an existing note for the object, it will be returned
+        even if it's marked as deleted (in which case it is automatically
+        marked as undeleted).  Otherwise a new note is created.
+        """
+        note = cls.find_first(linked_object=linked_object)
+        if note is None:
+            note = cls(linked_object=linked_object)
+        else:
+            note.is_deleted = False
+        return note
+
+    def delete(self, user):
+        """Marks the note as deleted and adds a new empty revision"""
+        self.create_revision(self.current_revision.render_mode, '', user)
+        self.is_deleted = True
+
+    def create_revision(self, render_mode, source, user):
+        """Creates a new revision of the note if needed.
+
+        Any change to the render mode or the source causes a new
+        revision to be created.  The user is not taken into account
+        since a user "modifying" a note without changing things is
+        not really a change.
+        """
+        with db.session.no_autoflush:
+            current = self.current_revision
+        if current is not None and current.render_mode == render_mode and current.source == source:
+            return current
+        self.current_revision = EventNoteRevision(render_mode=render_mode, source=source, user=user)
+        return self.current_revision
+
+    @return_ascii
+    def __repr__(self):
+        return '<EventNote({})>'.format(self.id)
+
+
+class EventNoteRevision(db.Model):
+    __tablename__ = 'note_revisions'
+    __table_args__ = {'schema': 'events'}
+
+    #: The ID of the revision
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+    #: The ID of the associated note
+    note_id = db.Column(
+        db.Integer,
+        db.ForeignKey('events.notes.id'),
+        nullable=False,
+        index=True
+    )
+    #: The user who created the revision
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('users.users.id'),
+        nullable=False,
+        index=True
+    )
+    #: The date/time when the revision was created
+    created_dt = db.Column(
+        UTCDateTime,
+        nullable=False,
+        default=now_utc
+    )
+    #: How the note is rendered
+    render_mode = db.Column(
+        PyIntEnum(RenderMode),
+        nullable=False
+    )
+    #: The raw source of the note as provided by the user
+    source = db.Column(
+        db.Text,
+        nullable=False
+    )
+    #: The rendered HTML of the note
+    html = db.Column(
+        db.Text,
+        nullable=False
+    )
+
+    #: The user who created the revision
+    user = db.relationship(
+        'User',
+        lazy=True,
+        backref=db.backref(
+            'event_notes_revisions',
+            lazy='dynamic'
+        )
+    )
+
+    @return_ascii
+    def __repr__(self):
+        return '<EventNoteRevision({})>'.format(self.id)
+
+
+@listens_for(EventNote.current_revision, 'set')
+def _add_current_revision(target, value, *unused):
+    if value is None:
+        raise ValueError('current_revision cannot be set to None')
+    with db.session.no_autoflush:
+        target.revisions.append(value)
+    target.html = value.html
+
+
+def _render_revision(attr, target, value, *unused):
+    source = value if attr == 'source' else target.source
+    render_mode = value if attr == 'render_mode' else target.render_mode
+    if source is None or render_mode is None:
+        return
+    if render_mode == RenderMode.html:
+        target.html = source
+    elif render_mode == RenderMode.markdown:
+        target.html = to_unicode(render_markdown(source, extensions=('nl2br',)))
+    else:  # pragma: no cover
+        raise ValueError('Invalid render mode: {}'.format(render_mode))
+
+
+listen(EventNoteRevision.render_mode, 'set', partial(_render_revision, 'render_mode'))
+listen(EventNoteRevision.source, 'set', partial(_render_revision, 'source'))
+
+
+@listens_for(EventNoteRevision.html, 'set')
+def _update_note_html(target, value, *unused):
+    if target.note_id is not None and target == target.note.current_revision:
+        target.note.html = value
---
+++ indico/modules/events/notes/models/notes_test.py
@@ -0,0 +1,164 @@
+# This file is part of Indico.
+# Copyright (C) 2002 - 2015 European Organization for Nuclear Research (CERN).
+#
+# Indico is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+#
+# Indico is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Indico; if not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+
+import pytest
+
+from sqlalchemy import inspect
+from sqlalchemy.exc import IntegrityError
+
+from indico.modules.events.notes.models.notes import EventNote, RenderMode, EventNoteRevision
+
+
+@pytest.yield_fixture
+def note(db, dummy_event):
+    note = EventNote(linked_object=dummy_event)
+    yield note
+    if inspect(note).persistent:
+        db.session.delete(note)
+
+
+def test_revisions(db, note, dummy_user):
+    rev1 = note.create_revision(RenderMode.html, 'first', dummy_user)
+    db.session.add(note)
+    db.session.flush()
+    assert rev1 == note.current_revision
+    assert note.current_revision.html == note.html == 'first'
+    assert set(note.revisions) == {rev1}
+    rev2 = note.create_revision(RenderMode.html, 'second', dummy_user)
+    db.session.flush()
+    assert rev2 == note.current_revision
+    assert note.current_revision.html == note.html == 'second'
+    assert set(note.revisions) == {rev2, rev1}  # order_by is only applied when loading so just check the contents here
+
+
+def test_change_current_revision(db, note, dummy_user):
+    rev1 = note.create_revision(RenderMode.html, 'first', dummy_user)
+    rev2 = note.create_revision(RenderMode.html, 'second', dummy_user)
+    assert note.current_revision == rev2
+    db.session.flush()
+    note.current_revision = rev1
+    db.session.flush()
+    assert note.current_revision.html == note.html == 'first'
+    assert set(note.revisions) == {rev2, rev1}
+
+
+def test_clear_current_revision(note, dummy_user):
+    note.create_revision(RenderMode.html, 'first', dummy_user)
+    with pytest.raises(ValueError):
+        note.current_revision = None
+
+
+def test_delete_current_revision(db, note, dummy_user):
+    note.create_revision(RenderMode.html, 'first', dummy_user)
+    rev = note.create_revision(RenderMode.html, 'second', dummy_user)
+    db.session.add(note)
+    db.session.flush()
+    note.revisions.remove(rev)
+    with pytest.raises(IntegrityError):
+        db.session.flush()
+
+
+def test_create_same_revision(db, create_user, note, dummy_user):
+    user = create_user(123)
+    note.create_revision(RenderMode.html, 'test', dummy_user)
+    note.create_revision(RenderMode.html, 'test', user)
+    db.session.add(note)
+    db.session.flush()
+    assert len(note.revisions) == 1
+    assert note.current_revision.user == dummy_user
+
+
+def test_delete_other_revision(db, note, dummy_user):
+    rev1 = note.create_revision(RenderMode.html, 'first', dummy_user)
+    rev2 = note.create_revision(RenderMode.html, 'second', dummy_user)
+    db.session.add(note)
+    db.session.flush()
+    note.revisions.remove(rev1)
+    db.session.flush()
+    assert set(note.revisions) == {rev2}
+    assert EventNoteRevision.query.count() == 1
+
+
+def test_modify_old_revision_source(db, note, dummy_user):
+    rev1 = note.create_revision(RenderMode.html, 'first', dummy_user)
+    rev2 = note.create_revision(RenderMode.html, 'second', dummy_user)
+    db.session.add(note)
+    db.session.flush()
+    rev1.source = 'rewritten history'
+    assert note.html == rev2.html == 'second'
+
+
+def test_modify_current_revision_source(db, note, dummy_user):
+    note.create_revision(RenderMode.html, 'first', dummy_user)
+    rev = note.create_revision(RenderMode.html, 'second', dummy_user)
+    db.session.add(note)
+    db.session.flush()
+    note.current_revision.source = 'rewritten history'
+    assert note.html == rev.html == 'rewritten history'
+
+
+def test_render_html(note, dummy_user):
+    note.create_revision(RenderMode.html, '<strong>test</strong>', dummy_user)
+    assert note.html == note.current_revision.html == '<strong>test</strong>'
+
+
+def test_render_markdown(note, dummy_user):
+    note.create_revision(RenderMode.markdown, '**test**\n*foo*', dummy_user)
+    assert note.html == note.current_revision.html == '<p><strong>test</strong><br />\n<em>foo</em></p>'
+
+
+def test_get_for_linked_object(note, dummy_user, create_event):
+    note.create_revision(RenderMode.html, 'test', dummy_user)
+    assert EventNote.get_for_linked_object(note.linked_object) == note
+    assert EventNote.get_for_linked_object(create_event('123')) is None
+
+
+def test_get_for_linked_object_deleted(note, dummy_user):
+    note.create_revision(RenderMode.html, 'test', dummy_user)
+    note.is_deleted = True
+    assert EventNote.get_for_linked_object(note.linked_object) is None
+
+
+def test_get_or_create(db, dummy_user, dummy_event, create_event):
+    note = EventNote.get_or_create(dummy_event)
+    assert note is not None
+    assert not inspect(note).persistent  # new object
+    note.create_revision(RenderMode.html, 'test', dummy_user)
+    note.is_deleted = True
+    db.session.flush()
+    # get deleted one
+    assert EventNote.get_or_create(dummy_event) == note
+    assert inspect(note).persistent
+    note.is_deleted = False
+    db.session.flush()
+    # same if it's not deleted
+    assert EventNote.get_or_create(dummy_event) == note
+    assert inspect(note).persistent
+    # other event should create a new one
+    other = EventNote.get_or_create(create_event(123))
+    other.create_revision(RenderMode.html, 'test', dummy_user)
+    assert other != note
+    assert not inspect(other).persistent
+
+
+def test_delete(note, dummy_user):
+    note.create_revision(RenderMode.html, 'test', dummy_user)
+    note.delete(dummy_user)
+    assert len(note.revisions) == 2
+    assert note.html == ''
+    assert note.is_deleted
