indico/indico

commit ad9ab4047a94506254dc2db9783a5a2209c243a6

Fix cloning sessions (maintain ID)
closes #1830

indico/MaKaC/conference.py
@@ -3270,6 +3270,8 @@ class Conference(CommonObjectBase, Locatable):
                  self.setEndDate(newSession.getEndDate())
         if id!=None:
             sessionId = id
+            # Keep ID counter up to date
+            self.__sessionGenerator.newCount()
         else:
             sessionId=self.__generateNewSessionId()
         self.sessions[sessionId]=newSession
@@ -4218,7 +4220,7 @@ class Conference(CommonObjectBase, Locatable):
         # Meetings' and conferences' sessions cloning
         if options.get("sessions",False) :
             for s in self.getSessionList() :
-                newSes = s.clone(timeDelta, conf, options)
+                newSes = s.clone(timeDelta, conf, options, session_id=s.getId())
                 ContextManager.setdefault("clone.unique_id_map", {})[s.getUniqueId()] = newSes.getUniqueId()
                 conf.addSession(newSes)
         # Materials' cloning
@@ -5646,9 +5648,9 @@ class Session(CommonObjectBase, Locatable):
             self.getConference().getSchedule().reSchedule()
             self.notifyModification()
 
-    def clone(self, deltaTime, conf, options):
+    def clone(self, deltaTime, conf, options, session_id=None):
         ses = Session()
-        conf.addSession(ses,check=0)
+        conf.addSession(ses, check=0, id=session_id)
         ses.setTitle(self.getTitle())
         ses.setDescription(self.getDescription())
         startDate = self.getStartDate() + deltaTime

commit 4f9900cad521c01ba9bdea4919523008c8ed1b16

Set session id generator to highest
Fixes issue apparently introduced by ad9ab404
Some formatting/PEP8 fixes as well

indico/MaKaC/conference.py
@@ -3240,7 +3240,7 @@ class Conference(CommonObjectBase, Locatable):
         """
         return str(self.__sessionGenerator.newCount())
 
-    def addSession(self,newSession, check = 2, id = None):
+    def addSession(self, newSession, check=2, session_id=None):
         """Adds a new session object to the conference taking care of assigning
             a new unique id to it
         """
@@ -3253,23 +3253,29 @@ class Conference(CommonObjectBase, Locatable):
             return
         if self.getSchedule().isOutside(newSession):
             if check == 1:
-                 MaKaCError( _("Cannot add this session  (Start:%s - End:%s) Outside of the event's time table(Start:%s - End:%s)") % (newSession.getStartDate(),newSession.getEndDate(),self.getSchedule().getStartDate(), self.getSchedule().getEndDate()),"Event")
+                raise MaKaCError(_("Cannot add this session  (Start:%s - End:%s) "
+                                   "Outside of the event's time table(Start:%s - End:%s)").format(
+                    newSession.getStartDate(),
+                    newSession.getEndDate(),
+                    self.getSchedule().getStartDate(),
+                    self.getSchedule().getEndDate()),
+                    "Event")
             elif check == 2:
-              if self.getSchedule().getStartDate() > newSession.getStartDate():
-                 self.setStartDate(newSession.getStartDate())
-              if self.getSchedule().getEndDate() < newSession.getEndDate():
-                 self.setEndDate(newSession.getEndDate())
-        if id!=None:
-            sessionId = id
+                if self.getSchedule().getStartDate() > newSession.getStartDate():
+                    self.setStartDate(newSession.getStartDate())
+                if self.getSchedule().getEndDate() < newSession.getEndDate():
+                    self.setEndDate(newSession.getEndDate())
+        if session_id is not None:
+            sessionId = session_id
             # Keep ID counter up to date
-            self.__sessionGenerator.newCount()
+            self.__sessionGenerator.sync(session_id)
         else:
-            sessionId=self.__generateNewSessionId()
-        self.sessions[sessionId]=newSession
-        newSession.includeInConference(self,sessionId)
-        #keep the session coordinator index updated
+            sessionId = self.__generateNewSessionId()
+        self.sessions[sessionId] = newSession
+        newSession.includeInConference(self, sessionId)
+        # keep the session coordinator index updated
         for sc in newSession.getCoordinatorList():
-            self.addSessionCoordinator(newSession,sc)
+            self.addSessionCoordinator(newSession, sc)
         self.notifyModification()
 
     def hasSession(self,session):
@@ -5582,7 +5588,7 @@ class Session(CommonObjectBase, Locatable):
 
     def clone(self, deltaTime, conf, options, session_id=None):
         ses = Session()
-        conf.addSession(ses, check=0, id=session_id)
+        conf.addSession(ses, check=0, session_id=session_id)
         ses.setTitle(self.getTitle())
         ses.setDescription(self.getDescription())
         startDate = self.getStartDate() + deltaTime