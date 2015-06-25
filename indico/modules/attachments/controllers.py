833564d5b69f4bd6a441bae14e357cb6d8970ede
Jacques Dafflon <jacques.dafflon@cern.ch>
2015-06-25 10:35:42 +0200

    Attachments mock data

--- indico/modules/attachments/controllers.py
+++ indico/modules/attachments/controllers.py
@@ -22,11 +22,48 @@ from indico.modules.attachments.forms import AddAttachmentsForm, AddLinkForm
 from MaKaC.webinterface.rh.conferenceModif import RHConferenceModifBase
 
 
+def _random_date():
+    from datetime import datetime
+    import random
+    year = random.randint(1950, 2000)
+    month = random.randint(1, 12)
+    day = random.randint(1, 28)
+    return datetime(year, month, day)
+
+
 class RHEventAttachments(RHConferenceModifBase):
     """Shows the attachments of an event"""
 
     def _process(self):
-        return WPEventAttachments.render_template('attachments.html', self._conf, event=self._conf)
+        root_folders = [
+            {'title': 'images', 'content-type': 'text/directory', 'modified_dt': _random_date(), 'content': [
+                {'title': 'img001.jpg', 'content-type': 'image/jpeg', 'modified_dt': _random_date()},
+                {'title': 'img002.jpg', 'content-type': 'image/jpeg', 'modified_dt': _random_date()},
+                {'title': 'img003.jpg', 'content-type': 'image/jpeg', 'modified_dt': _random_date()},
+                {'title': 'here_is_an_image_in_a_nested_folder_with_an_extremely_long_name_and_a_weird_extension.jpag',
+                 'content-type': 'audio/vorbis', 'modified_dt': _random_date()}
+            ]},
+            {'title': 'some folder', 'content-type': 'text/directory', 'modified_dt': _random_date(), 'content': []},
+            {'title': 'Poster', 'content-type': 'text/directory', 'modified_dt': _random_date(), 'content': [
+                {'title': 'poster_final.pdf', 'content-type': 'application/pdf', 'modified_dt': _random_date()}
+            ]},
+
+        ]
+        root_files = [
+            {'title': 'data1.ods', 'content-type': 'application/vnd.oasis.opendocument.spreadsheet',
+             'modified_dt': _random_date()},
+            {'title': 'raw_data1.ods', 'content-type': 'application/vnd.oasis.opendocument.spreadsheet',
+             'modified_dt': _random_date()},
+            {'title': 'final_report.pdf', 'content-type': 'application/pdf', 'modified_dt': _random_date()},
+            {'title': 'Some link', 'content-type': 'text/vnd.indico.link', 'modified_dt': _random_date()},
+            {'title': 'unknown_type.ogg', 'content-type': 'audio/vorbis', 'modified_dt': _random_date()},
+            {'title': 'this_is_a_completely_random_file_whose_name_is_long_extremely_extremely_extremely_long.ogg',
+             'content-type': 'audio/vorbis', 'modified_dt': _random_date()}
+        ]
+        attachments = sorted(root_folders + root_files,
+                             key=lambda a: (a['content-type'] != 'text/directory', a['title'].lower()))
+        return WPEventAttachments.render_template('attachments.html', self._conf, event=self._conf,
+                                                  attachments=attachments)
 
 
 class RHEventAttachmentsUpload(RHConferenceModifBase):
