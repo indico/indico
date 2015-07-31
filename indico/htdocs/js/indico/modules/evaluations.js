2091ee3d26c1139b40ec3d3eb4e174c2466990c5
Alejandro Avilés <aavilesd@cern.ch>
2015-07-31 15:48:20 +0200

    Add js to open evaluation schedule dialog

--- indico/htdocs/js/indico/modules/evaluations.js
+++ indico/htdocs/js/indico/modules/evaluations.js
@@ -2,8 +2,24 @@
     'use strict';
 
     $(document).ready(function() {
+        setupEvaluationScheduleWindows();
         setupQuestionWindows();
     });
+
+    global.setupEvaluationScheduleWindows = function setupEvaluationScheduleWindows() {
+        $('a.js-evaluation-schedule-dialog').on('click', function(evt) {
+            evt.preventDefault();
+            ajaxDialog({
+                url: build_url($(this).data('href')),
+                title: $(this).data('title'),
+                onClose: function(data) {
+                    if (data) {
+                        location.reload();
+                    }
+                }
+            });
+        });
+    };
 
     global.setupQuestionWindows = function setupQuestionWindows() {
         $('a.js-question-dialog').on('click', function(evt) {
