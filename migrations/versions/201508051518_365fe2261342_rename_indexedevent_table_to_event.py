4da502d63ee0b90c517d5d3f95fbdcb28b26890a
Ilias Trichopoulos <ilias.trichopoulos@cern.ch>
2015-08-05 15:59:12 +0200

    Rename event_index table (event) & add logo column

---
+++ migrations/versions/201508051518_365fe2261342_rename_indexedevent_table_to_event.py
@@ -0,0 +1,24 @@
+"""rename IndexedEvent table to Event
+
+Revision ID: 365fe2261342
+Revises: 3778dc365e54
+Create Date: 2015-08-05 15:18:10.748257
+"""
+
+import sqlalchemy as sa
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = '365fe2261342'
+down_revision = '3778dc365e54'
+
+
+def upgrade():
+    op.rename_table('event_index', 'event', schema='events')
+    op.add_column('event', sa.Column('logo', sa.LargeBinary(), nullable=True), schema='events')
+
+
+def downgrade():
+    op.drop_column('event', 'logo', schema='events')
+    op.rename_table('event', 'event_index', schema='events')
