68aacc247723b965178d79ec385032b8b49e62a2
Pedro Ferreira <pedro.ferreira@cern.ch>
2015-08-11 17:36:09 +0200

    Allow zeroclipboard triggers to be updated
    For dynamically-created content.

--- indico/htdocs/js/custom/zeroclipboard.js
+++ indico/htdocs/js/custom/zeroclipboard.js
@@ -1,32 +1,47 @@
-$(function zeroclipboardInit() {
-    'use strict';
+(function(global) {
 
-    // check if ZeroClipboard is loaded and if the HTML init class is defined
-    if (!window.ZeroClipboard || !$('.zeroclipboard-init').length) { return; }
+    var client = null;
 
-    ZeroClipboard.config({'swfPath': Indico.Urls.Base + '/js/lib/zeroclipboard/ZeroClipboard.swf'});
+    global.zeroclipboardInit = function() {
+        'use strict';
 
-    // register i-has-action-clip ZeroClipboard handling
-    var $iHasAction = $('.i-has-action.i-has-action-clip > .i-button');
-    if ($iHasAction.length) {
-        ZeroClipboard.on('ready', function() {
-            var client = new ZeroClipboard($iHasAction);
-            client.on('error', function() { client.destroy(); });
+        // check if ZeroClipboard is loaded and if the HTML init class is defined
+        if (!window.ZeroClipboard || !$('.zeroclipboard-init').length) { return; }
 
-            client.on('ready', function() {
-                client.on('copy', function(event) {
-                    event.clipboardData.setData('text/plain', $(event.target).siblings('input').val());
-                });
-                client.on('aftercopy', function(event) {
-                    $(event.target).parent().siblings('.i-has-action-message')
-                        .css('visibility', 'visible')
-                        .fadeTo(50, 1)
-                        .fadeTo(1500, 0)
-                        .animate({'visibility': 'hidden'}, 0); // animate to have css queued after fadeOut
+        ZeroClipboard.config({'swfPath': Indico.Urls.Base + '/js/lib/zeroclipboard/ZeroClipboard.swf'});
+
+        // register i-has-action-clip ZeroClipboard handling
+        var $iHasAction = $('.i-has-action.i-has-action-clip > .i-button');
+        if ($iHasAction.length) {
+            ZeroClipboard.on('ready', function() {
+                client = new ZeroClipboard($iHasAction);
+                client.on('error', function() { client.destroy(); });
+
+                client.on('ready', function() {
+                    client.on('copy', function(event) {
+                        event.clipboardData.setData('text/plain', $(event.target).siblings('input').val());
+                    });
+                    client.on('aftercopy', function(event) {
+                        $(event.target).parent().siblings('.i-has-action-message')
+                            .css('visibility', 'visible')
+                            .fadeTo(50, 1)
+                            .fadeTo(1500, 0)
+                            .animate({'visibility': 'hidden'}, 0); // animate to have css queued after fadeOut
+                    });
                 });
             });
-        });
-    }
+        }
+    };
+
+    global.zeroclipboardRegister = function(element) {
+        client.clip(element);
+    };
+
+})(window);
+
+
+$(function() {
+    zeroclipboardInit();
 
     $(document).one('mouseover', '.zeroclipboard-init', function loadZeroClipBoardFlash() {
         ZeroClipboard.create(); // loads the Flash file
