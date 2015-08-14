51bdd6ca86c3d4cb464647ad9dd4a56bae98abf9
Ilias Trichopoulos <ilias.trichopoulos@cern.ch>
2015-08-14 11:36:01 +0200

    Fix Dropzone to allow multiple instances in 1 form

--- indico/htdocs/js/custom/dropzone.js
+++ indico/htdocs/js/custom/dropzone.js
@@ -5,115 +5,121 @@
         Dropzone.autoDiscover = false;
     });
 
+    global.setupDropzone = function(element) {
+        var $dz = $(element),
+            $form = $dz.closest('form'),
+            options = {
+                clickable: element + ' .dropzone-inner',
+                previewsContainer: element + ' .dropzone-previews',
+                autoProcessQueue: false,
 
-    global.setupDropzone = function() {
-        $('.dropzone').each(function() {
-            var $form = $(this),
-                $area = $form.find('.dropzone-area'),
-                options = {
-                    clickable: '.dropzone-area',
-                    previewsContainer: '.dropzone-previews',
-                    autoProcessQueue: false,
+                init: function() {
+                    var $button = $form.find('input[type="submit"], .js-dropzone-upload'),
+                        self = this,
+                        file = $dz.data('value');
 
-                    init: function() {
-                        var $button = $form.find('input[type="submit"], .js-dropzone-upload'),
-                            self = this,
-                            file = $area.data('value');
+                    if (file) {
+                        var existingFile = {
+                            name: file.file_name,
+                            size: file.size,
+                            accepted: true
+                        };
 
-                        if (file) {
-                            var existingFile = {
-                                name: file.file_name,
-                                size: file.size,
-                                accepted: true
-                            };
+                        self.emit('addedfile', existingFile);
+                        $dz.find('.dz-progress').hide();
+                        self.files[0] = existingFile;
 
-                            self.emit('addedfile', existingFile);
-                            $('.dz-progress').hide();
-                            self.files[0] = existingFile;
+                        /* Disable opening the upload dialog when clicking on the file's preview element */
+                        $dz.find('.dz-preview').on('click', function(e) {
+                            e.stopPropagation();
+                        });
 
-                            /* Disable opening the upload dialog when clicking on the file's preview element */
-                            $('.dz-preview').on('click', function(e) {
-                                e.stopPropagation();
-                            });
+                        // Check if the existing file is an image and add the image preview.
+                        if (file.content_type.match(/image\/.*/) && file.url) {
+                            $dz.find('.dz-image > img').attr('src', file.url);
+                            $dz.find('.dz-image > img').attr('width', '120px');
+                            $dz.find('.dz-preview').removeClass('dz-file-preview').addClass('dz-image-preview');
+                        }
+                    }
 
-                            // Check if the existing file is an image and add the image preview.
-                            if (file.content_type.match(/image\/.*/) && file.url) {
-                                $('.dz-image > img').attr('src', file.url);
-                                $('.dz-image > img').attr('width', '120px');
-                                $('.dz-preview').removeClass('dz-file-preview').addClass('dz-image-preview');
-                            }
+                    $button.on('click', function(e) {
+                        e.preventDefault();
+                        e.stopPropagation();
+                        $button.prop('disabled', true);
+
+                        if (self.getQueuedFiles().length) {
+                            $dz.find('.dz-progress').show();
+                            self.processQueue();
+                        }
+                        else {
+                            $form.submit();
+                        }
+                    });
+
+                    self.on('addedfile', function(file) {
+                        if (!options.uploadMultiple && self.files.length > 1) {
+                            self.removeFile(self.files[0]);
                         }
 
-                        $button.on('click', function(e) {
-                            e.preventDefault();
+                        // force change in form, so that we can process
+                        // the 'change' event
+                        $button.prop('disabled', false);
+                        $form.find('.change-trigger').val(self.files.length);
+                        $form.trigger('change');
+
+                        $dz.find('.dz-message').hide();
+                        $dz.find('.dz-progress').hide();
+                        $(file.previewElement).on('click', function(e) {
                             e.stopPropagation();
-                            $button.prop('disabled', true);
-
-                            if (self.getQueuedFiles().length) {
-                                $('.dz-progress').show();
-                                self.processQueue();
-                            }
-                            else {
-                                $form.submit();
-                            }
                         });
+                    });
 
-                        self.on('addedfile', function(file) {
-                            if (!options.uploadMultiple && self.files.length > 1) {
-                                self.removeFile(self.files[0]);
-                            }
+                    self.on('removedfile', function(file) {
 
-                            // force change in form, so that we can process
-                            // the 'change' event
-                            $button.prop('disabled', false);
-                            $form.find('.change-trigger').val(self.files.length);
-                            $form.trigger('change');
+                        // force change in form, so that we can process
+                        // the 'change' event
+                        $form.find('.change-trigger').val(self.files.length);
+                        $form.trigger('change');
 
-                            $('.dz-message').hide();
-                            $('.dz-progress').hide();
-                            $(file.previewElement).on('click', function(e) {
-                                e.stopPropagation();
-                            });
-                        });
+                        if (self.files.length === 0) {
+                            $dz.find('.dz-message').show();
+                        }
+                    });
 
-                        self.on('removedfile', function(file) {
+                    self.on('sendingmultiple', function() {
+                        $form.trigger('ajaxDialog:beforeSubmit');
+                    });
 
-                            // force change in form, so that we can process
-                            // the 'change' event
-                            $form.find('.change-trigger').val(self.files.length);
-                            $form.trigger('change');
+                    self.on('success', function(e, response) {
+                        if (options.submit_form) {
+                            $form.submit();
+                        }
+                        $form.trigger('ajaxDialog:success', [response]);
+                    });
 
-                            if (self.files.length === 0) {
-                                $('.dz-message').show();
-                            }
-                        });
+                    self.on('error', function(e, msg, xhr) {
+                        if (xhr) {
+                            $form.trigger('ajaxDialog:error', [xhr]);
+                        }
+                    });
+                }
+            };
 
-                        self.on('sendingmultiple', function() {
-                            $form.trigger('ajaxDialog:beforeSubmit');
-                        });
+        _.extend(options, $dz.data('options'));
+        var param = options.paramName;
+        options.paramName = function() { return param; };
 
-                        self.on('success', function(e, response) {
-                            if (options.submit_form) {
-                                $form.submit();
-                            }
-                            $form.trigger('ajaxDialog:success', [response]);
-                        });
-
-                        self.on('error', function(e, msg, xhr) {
-                            if (xhr) {
-                                $form.trigger('ajaxDialog:error', [xhr]);
-                            }
-                        });
-                    }
-                };
-
-            _.extend(options, $area.data('options'));
-            var param = options.paramName;
-            options.paramName = function() { return param; };
-
+        // include the csrf token in case the dropzone is submitting data to a
+        // seperate endpoint than the form's action
+        if (options.url) {
+            options.params = {
+                'csrf_token': $form.find('#csrf_token').val()
+            };
+            $dz.dropzone(options);
+        }
+        else {
             $form.dropzone(options);
-
-        });
+        }
     };
 
 })(window);
