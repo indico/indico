f108b977ba0346a5c6385ecf05bf10d20b04b4f1
Michal Kolodziejski <michal.kolodziejski@cern.ch>
2015-08-26 11:50:29 +0200

    Resend start notification on reopen/reschedule
    fixes #2007

--- indico/modules/events/surveys/forms.py
+++ indico/modules/events/surveys/forms.py
@@ -79,8 +79,13 @@ class ScheduleSurveyForm(IndicoForm):
                                    description=_("Moment when the survey will open for submissions"))
     end_dt = IndicoDateTimeField(_("End"), [Optional(), LinkedDateTime('start_dt')],
                                  description=_("Moment when the survey will close"))
+    resend_start_notification = BooleanField(_('Resend start notification'), widget=SwitchWidget(),
+                                             description=_("Resend a survey start notification if it hadn't been sent"
+                                                           "before"))
 
     def __init__(self, *args, **kwargs):
         self.survey = kwargs.pop('survey', None)
         self.timezone = self.survey.event.getTimezone()
         super(IndicoForm, self).__init__(*args, **kwargs)
+        if not self.survey.start_notification_sent:
+            del self.resend_start_notification
