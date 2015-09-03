1b7c2c950475af1961f192aaa81fd436d622cb81
Alejandro Avilés <aavilesd@cern.ch>
2015-09-03 14:54:28 +0200

    Style sortable handle for i-box grouped lists

--- indico/htdocs/js/indico/modules/surveys.js
+++ indico/htdocs/js/indico/modules/surveys.js
@@ -191,7 +191,7 @@
         // sort sections
         container.sortable({
             items: '.js-sortable-survey-section',
-            handle: '.i-box-sortable-handle',
+            handle: '.ui-i-box-sortable-handle',
             placeholder: 'i-box-sortable-placeholder',
             tolerance: 'pointer',
             distance: 10,
