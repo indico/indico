5e1022b4dc12e2c025f6853d2d1628651bd3c9c9
Alejandro Avilés <aavilesd@cern.ch>
2015-09-08 11:37:54 +0200

    Move registration tables to dedicated schema

--- migrations/versions/201509041515_f9bb2a03ceb_add_registration_tables.py
+++ migrations/versions/201509041515_f9bb2a03ceb_add_registration_tables.py
@@ -8,6 +8,7 @@ Create Date: 2015-09-04 15:15:03.682231
 import sqlalchemy as sa
 from alembic import op
 from sqlalchemy.dialects import postgresql
+from sqlalchemy.sql.ddl import CreateSchema, DropSchema
 
 from indico.core.db.sqlalchemy import PyIntEnum, UTCDateTime
 from indico.modules.events.registration.models.registration_forms import RegistrationFormModificationMode
@@ -19,6 +20,8 @@ down_revision = '3b6c768b8803'
 
 
 def upgrade():
+    op.execute(CreateSchema('event_registration'))
+
     op.create_table(
         'registration_forms',
         sa.Column('id', sa.Integer(), nullable=False),
@@ -34,7 +37,7 @@ def upgrade():
         sa.Column('registration_limit', sa.Integer(), nullable=True),
         sa.ForeignKeyConstraint(['event_id'], ['events.events.id']),
         sa.PrimaryKeyConstraint('id'),
-        schema='events'
+        schema='event_registration'
     )
 
     op.create_table(
@@ -44,10 +47,10 @@ def upgrade():
         sa.Column('type', sa.Integer(), nullable=False),
         sa.Column('parent_id', sa.Integer(), nullable=True),
         sa.Column('position', sa.Integer(), nullable=False),
-        sa.ForeignKeyConstraint(['parent_id'], ['events.registration_form_items.id']),
-        sa.ForeignKeyConstraint(['registration_form_id'], ['events.registration_forms.id']),
+        sa.ForeignKeyConstraint(['parent_id'], ['event_registration.registration_form_items.id']),
+        sa.ForeignKeyConstraint(['registration_form_id'], ['event_registration.registration_forms.id']),
         sa.PrimaryKeyConstraint('id'),
-        schema='events'
+        schema='event_registration'
     )
 
     op.create_table(
@@ -55,9 +58,9 @@ def upgrade():
         sa.Column('id', sa.Integer(), nullable=False),
         sa.Column('field_id', sa.Integer(), nullable=False),
         sa.Column('data', postgresql.JSON(), nullable=False),
-        sa.ForeignKeyConstraint(['field_id'], ['events.registration_form_items.id']),
+        sa.ForeignKeyConstraint(['field_id'], ['event_registration.registration_form_items.id']),
         sa.PrimaryKeyConstraint('id'),
-        schema='events'
+        schema='event_registration'
     )
 
     op.create_table(
@@ -66,10 +69,10 @@ def upgrade():
         sa.Column('registration_form_id', sa.Integer(), nullable=False, index=True),
         sa.Column('user_id', sa.Integer(), nullable=True, index=True),
         sa.Column('submitted_dt', UTCDateTime, nullable=False),
-        sa.ForeignKeyConstraint(['registration_form_id'], ['events.registration_forms.id']),
+        sa.ForeignKeyConstraint(['registration_form_id'], ['event_registration.registration_forms.id']),
         sa.ForeignKeyConstraint(['user_id'], ['users.users.id']),
         sa.PrimaryKeyConstraint('id'),
-        schema='events'
+        schema='event_registration'
     )
 
     op.create_table(
@@ -77,16 +80,17 @@ def upgrade():
         sa.Column('registration_id', sa.Integer(), nullable=False),
         sa.Column('field_data_id', sa.Integer(), nullable=False),
         sa.Column('data', postgresql.JSON(), nullable=False),
-        sa.ForeignKeyConstraint(['field_data_id'], ['events.registration_form_field_data.id']),
-        sa.ForeignKeyConstraint(['registration_id'], ['events.registrations.id']),
+        sa.ForeignKeyConstraint(['field_data_id'], ['event_registration.registration_form_field_data.id']),
+        sa.ForeignKeyConstraint(['registration_id'], ['event_registration.registrations.id']),
         sa.PrimaryKeyConstraint('registration_id', 'field_data_id'),
-        schema='events'
+        schema='event_registration'
     )
 
 
 def downgrade():
-    op.drop_table('registration_data', schema='events')
-    op.drop_table('registrations', schema='events')
-    op.drop_table('registration_form_field_data', schema='events')
-    op.drop_table('registration_form_items', schema='events')
-    op.drop_table('registration_forms', schema='events')
+    op.drop_table('registration_data', schema='event_registration')
+    op.drop_table('registrations', schema='event_registration')
+    op.drop_table('registration_form_field_data', schema='event_registration')
+    op.drop_table('registration_form_items', schema='event_registration')
+    op.drop_table('registration_forms', schema='event_registration')
+    op.execute(DropSchema('event_registration'))
