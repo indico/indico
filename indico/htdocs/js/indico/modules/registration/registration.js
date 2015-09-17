eef9f11a33eed8f01a7f33dca002d9e4309b68b1
Ilias Trichopoulos <ilias.trichopoulos@cern.ch>
2015-09-17 17:40:45 +0200

    Add registrations list column selector

--- indico/htdocs/js/indico/modules/registration/registration.js
+++ indico/htdocs/js/indico/modules/registration/registration.js
@@ -51,12 +51,65 @@
 
     function setupRegistrationsList() {
         colorizeSelectedRows();
-        $('.registrations .toolbar').dropdown();
+        $('.registrations .toolbar')
+        .dropdown()
+        .on('click', '.js-dialog-action', function(e) {
+            e.preventDefault();
+            var $this = $(this);
+            ajaxDialog({
+                dialogClasses: 'reglist-filter-dialog',
+                trigger: this,
+                url: $this.data('href'),
+                title: $this.data('title'),
+                hidePageHeader: true,
+                onClose: function(data) {
+                    if (data) {
+                        $('.registrations-table-wrapper').html(data.registrations_list);
+                        colorizeSelectedRows();
+                        setupTableSorter();
+                    }
+                }
+            });
+        });
     }
 
     function colorizeSelectedRows() {
         $('table.i-table input.select-row').on('change', function() {
             $(this).closest('tr').toggleClass('selected', $(this).checked);
+        });
+    }
+
+    global.setupRegistrationsListFilter = function setupRegistrationsListFilter() {
+        $('.reglist-filter').dropdown({selector: '.reglist-column .title'});
+
+        var visibleColumnsField = $('#visible-columns');
+        var data = JSON.parse(visibleColumnsField.val());
+
+        $('.reglist-column')
+        .on('click', '.trigger', function() {
+            var field = $(this).closest('.reglist-column');
+            var fieldId = field.data('id');
+            if ($(this).hasClass('enabled')) {
+                data['items'].splice(data['items'].indexOf(fieldId), 1);
+            }
+            else if ($(this).hasClass('not-enabled')) {
+                data['items'].push(fieldId);
+            }
+            $(this).toggleClass('enabled', !$(this).hasClass('enabled'));
+            $(this).toggleClass('not-enabled', !$(this).hasClass('not-enabled'));
+            field.toggleClass('striped', !$(this).hasClass('enabled'));
+            visibleColumnsField.val(JSON.stringify(data)).trigger('change');
+        })
+        .each(function() {
+            var field = $(this).closest('.reglist-column');
+            var fieldId = field.data('id');
+            if (data['items'].indexOf(fieldId) != -1) {
+                field.find('.trigger').addClass('enabled');
+            }
+            else {
+                field.addClass('striped');
+                field.find('.trigger').addClass('not-enabled');
+            }
         });
     }
 })(window);
