a31723a10713417d0aee012a6667611795d7e382
Michal Kolodziejski <michal.kolodziejski@cern.ch>
2015-09-21 14:09:54 +0200

    Add `is_visible` property to the RegistrationForm

--- indico/modules/events/registration/controllers/display.py
+++ indico/modules/events/registration/controllers/display.py
@@ -60,7 +60,7 @@ class RHRegistrationFormList(RHRegistrationFormDisplayBase):
     """List of all registration forms in the event"""
 
     def _process(self):
-        regforms = RegistrationForm.find_all(event_id=int(self.event.id))
+        regforms = RegistrationForm.find_all(RegistrationForm.is_visible, event_id=int(self.event.id))
         if _can_redirect_to_single_regform(regforms):
             return redirect(url_for('.display_regform', regforms[0]))
         return self.view_class.render_template('display/regforms_list.html', self.event, regforms=regforms,
