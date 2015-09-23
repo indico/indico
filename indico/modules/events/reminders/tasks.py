36d1274d5e1dd088ec71b0dbc2632fdcb5ba0687
Adrian Moennich <adrian.moennich@cern.ch>
2015-09-23 13:53:31 +0200

    Reminders: Add event_id FK
    Also don't delete them on event deletion but check whether the event is
    deleted when sending reminders.

--- indico/modules/events/reminders/tasks.py
+++ indico/modules/events/reminders/tasks.py
@@ -18,16 +18,19 @@ from __future__ import unicode_literals
 
 from celery.schedules import crontab
 
-from indico.core.db import db, DBMgr
+from indico.core.db import db
 from indico.util.date_time import now_utc
 from indico.core.celery import celery
+from indico.modules.events import Event
 from indico.modules.events.reminders import logger
 from indico.modules.events.reminders.models.reminders import EventReminder
 
 
 @celery.periodic_task(name='event_reminders', run_every=crontab(minute='*/5'))
 def send_event_reminders():
-    reminders = EventReminder.find_all(~EventReminder.is_sent, EventReminder.scheduled_dt <= now_utc())
+    reminders = EventReminder.find_all(~EventReminder.is_sent, ~Event.is_deleted,
+                                       EventReminder.scheduled_dt <= now_utc(),
+                                       _join=EventReminder.event_new)
     try:
         for reminder in reminders:
             logger.info('Sending event reminder: {}'.format(reminder))
@@ -37,4 +40,3 @@ def send_event_reminders():
         # the is_sent change for already-sent reminders instead of
         # sending them over and over and thus spamming people.
         db.session.commit()
-        DBMgr.getInstance().commit()  # only needed for event email logs
