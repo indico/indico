bdb9fcea90c3a34412f8024b904ad3b4f504ac0a
Adrian Moennich <adrian.moennich@cern.ch>
2015-09-24 11:41:00 +0200

    LegacyEventMapping: Add event_id FK

--- indico/modules/events/__init__.py
+++ indico/modules/events/__init__.py
@@ -58,8 +58,6 @@ event_management_object_url_prefixes = {
 @signals.event.deleted.connect
 def _event_deleted(event, **kwargs):
     event.as_event.is_deleted = True
-    if hasattr(event, '_old_id'):
-        LegacyEventMapping.find(legacy_event_id=event._old_id).delete()
 
 
 @signals.users.merged.connect
