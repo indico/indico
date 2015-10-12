662c1f54096c0bdeaa53b697130ef9cf5614e880
Ilias Trichopoulos <ilias.trichopoulos@cern.ch>
2015-10-12 11:49:11 +0200

    Create boolean filter

--- indico/modules/events/registration/controllers/management/reglists.py
+++ indico/modules/events/registration/controllers/management/reglists.py
@@ -41,10 +41,10 @@ cache = GenericCache('reglist-config')
 def _get_filters_from_request(regform):
     filters = {}
     for field in regform.form_items:
-        if field.is_field and field.input_type in {'radio', 'country'}:
-            radioitems = request.form.getlist('radiogroup_{}'.format(field.id))
-            if radioitems:
-                filters[field.id] = radioitems
+        if field.is_field and field.input_type in {'radio', 'country', 'bool'}:
+            options = request.form.getlist('field_{}'.format(field.id))
+            if options:
+                filters[field.id] = options
     return filters
 
 
