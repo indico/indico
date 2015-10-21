bd8cc7a19af3a451357336fe9e6ad3698721559f
Michal Kolodziejski <michal.kolodziejski@cern.ch>
2015-10-21 14:26:38 +0200

    Download ticket from the registration summary page

--- indico/modules/events/registration/controllers/management/tickets.py
+++ indico/modules/events/registration/controllers/management/tickets.py
@@ -16,13 +16,19 @@
 
 from __future__ import unicode_literals
 
+from io import BytesIO
+
 from flask import redirect
+from werkzeug.exceptions import Forbidden
 
 from indico.core.db import db
+from indico.modules.events.registration.controllers.display import RHRegistrationFormRegistrationBase
 from indico.modules.events.registration.controllers.management import RHManageRegFormBase
 from indico.modules.events.registration.forms import TicketsForm
+from indico.modules.events.registration.models.registrations import RegistrationState
 from indico.modules.events.registration.views import WPManageRegistration
-from indico.web.flask.util import url_for
+from indico.web.flask.util import url_for, send_file, secure_filename
+from MaKaC.PDFinterface.conference import TicketToPDF
 
 
 class RHRegistrationFormTickets(RHManageRegFormBase):
@@ -36,3 +42,25 @@ class RHRegistrationFormTickets(RHManageRegFormBase):
             return redirect(url_for('.manage_regform', self.regform))
         return WPManageRegistration.render_template('management/regform_tickets.html', self.event,
                                                     regform=self.regform, form=form)
+
+
+def generate_ticket(registration):
+    pdf = TicketToPDF(registration.registration_form.event, registration)
+    return BytesIO(pdf.getPDFBin())
+
+
+class RHTicketDownload(RHRegistrationFormRegistrationBase):
+    """Generate ticket for a given registration"""
+
+    def _checkProtection(self):
+        RHRegistrationFormRegistrationBase._checkProtection(self)
+        if self.registration.state != RegistrationState.complete:
+            raise Forbidden
+        if not self.regform.tickets_enabled:
+            raise Forbidden
+        if not self.regform.ticket_on_event_page and not self.regform.ticket_on_summary_page:
+            raise Forbidden
+
+    def _process(self):
+        filename = secure_filename('{}-Ticket.pdf'.format(self.event_new.title), 'ticket.pdf')
+        return send_file(filename, generate_ticket(self.registration), 'application/pdf')
