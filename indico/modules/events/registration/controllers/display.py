128a292f669878256125a100cc0126a7f2c22562
Ilias Trichopoulos <ilias.trichopoulos@cern.ch>
2015-11-03 17:37:56 +0100

    Add title in Participants List

--- indico/modules/events/registration/controllers/display.py
+++ indico/modules/events/registration/controllers/display.py
@@ -103,14 +103,16 @@ class RHParticipantList(RHRegistrationFormDisplayBase):
                        ~Registration.is_deleted,
                        _join=Registration.registration_form)
                  .order_by(db.func.lower(Registration.last_name), db.func.lower(Registration.first_name)))
-        registrations = [(reg.get_full_name(), reg.get_personal_data().get('affiliation'))
+        registrations = [(reg.get_full_name(), reg.get_personal_data().get('title'),
+                         reg.get_personal_data().get('affiliation'))
                          for reg in query]
         return self.view_class.render_template(
             'display/participant_list.html',
             self.event,
             event=self.event,
             regforms=regforms,
-            show_affiliation=any(x[1] for x in registrations),
+            show_title=any(x[1] for x in registrations),
+            show_affiliation=any(x[2] for x in registrations),
             registrations=registrations
         )
 
