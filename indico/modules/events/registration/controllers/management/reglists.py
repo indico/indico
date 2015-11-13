d083cf9d56c827da363c952ff58c49e11fa21045
Michal Kolodziejski <michal.kolodziejski@cern.ch>
2015-11-13 10:56:53 +0100

    Fix error with redundant arg in BulkCheckIn

--- indico/modules/events/registration/controllers/management/reglists.py
+++ indico/modules/events/registration/controllers/management/reglists.py
@@ -635,9 +635,8 @@ class RHRegistrationBulkCheckIn(RHRegistrationsActionBase):
         flash(_("Selected registrations marked as {} successfully.").format(msg), 'success')
         reg_list_config = _get_reg_list_config(regform=self.regform)
         registrations_query = _query_registrations(self.regform)
-        total_regs = registrations_query.count()
         registrations = _filter_registration(self.regform, registrations_query, reg_list_config['filters']).all()
-        return jsonify_data(registration_list=_render_registration_list(self.regform, registrations, total_regs))
+        return jsonify_data(registration_list=_render_registration_list(self.regform, registrations))
 
 
 class RHRegistrationsModifyStatus(RHRegistrationsActionBase):
