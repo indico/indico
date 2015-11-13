0cf8d992e6f57661c269cb5c3be050484f8faf20
Alejandro Avilés <aavilesd@cern.ch>
2015-11-13 15:18:41 +0100

    Put back adapted registration stats

--- indico/modules/events/registration/blueprint.py
+++ indico/modules/events/registration/blueprint.py
@@ -47,6 +47,7 @@ from indico.modules.events.registration.controllers.management.regforms import (
                                                                                 RHRegistrationFormClose,
                                                                                 RHRegistrationFormSchedule,
                                                                                 RHRegistrationFormModify,
+                                                                                RHRegistrationFormStats,
                                                                                 RHRegistrationFormUnschedule)
 from indico.modules.events.registration.controllers.management.sections import (RHRegistrationFormAddSection,
                                                                                 RHRegistrationFormModifySection,
@@ -96,6 +97,7 @@ _bp.add_url_rule('/manage/registration/<int:reg_form_id>/close',
 _bp.add_url_rule('/manage/registration/<int:reg_form_id>/schedule',
                  'schedule_regform', RHRegistrationFormSchedule, methods=('GET', 'POST'))
 _bp.add_url_rule('/manage/registration/<int:reg_form_id>/form/', 'modify_regform', RHRegistrationFormModify)
+_bp.add_url_rule('/manage/registration/<int:reg_form_id>/stats/', 'regform_stats', RHRegistrationFormStats)
 _bp.add_url_rule('/manage/registration/<int:reg_form_id>/unschedule', 'unschedule_regform',
                  RHRegistrationFormUnschedule, methods=('POST',))
 
