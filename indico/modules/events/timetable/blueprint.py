4aa91815b5d97af96d89a39b720f36b2a4ed1a09
Alejandro Avilés <aavilesd@cern.ch>
2015-11-20 17:22:51 +0100

    Basic scaffolding for new timetable module

---
+++ indico/modules/events/timetable/blueprint.py
@@ -0,0 +1,26 @@
+# This file is part of Indico.
+# Copyright (C) 2002 - 2015 European Organization for Nuclear Research (CERN).
+#
+# Indico is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+#
+# Indico is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Indico; if not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+
+from indico.modules.events.timetable.controllers import RHManageTimetable
+from indico.web.flask.wrappers import IndicoBlueprint
+
+
+_bp = IndicoBlueprint('timetable', __name__, template_folder='templates', virtual_template_folder='events/timetable',
+                      url_prefix='/event/<confId>')
+
+_bp.add_url_rule('/manage/timetable', 'timetable', RHManageTimetable)
