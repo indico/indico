def8835b2842b1e5473924c7b7228eff2a4fa6ea
Pedro Ferreira <pedro.ferreira@cern.ch>
2015-11-20 11:43:48 +0100

    Fix Mako UTF8 conversion issues
    Mako has to receive the information in UTF8, otherwise implicit conversion will fail. Unfortunately, Mako's default filters are applied before all others, not after, so, here we have to make sure our filter is well-behaved.

--- indico/MaKaC/common/TemplateExec.py
+++ indico/MaKaC/common/TemplateExec.py
@@ -43,7 +43,7 @@ FILTER_IMPORTS = [
     'from indico.util.string import encode_if_unicode',
     'from indico.util.string import html_line_breaks as html_breaks',
     'from indico.util.string import remove_tags',
-    'from indico.util.string import render_markdown as m',
+    'from indico.util.string import render_markdown_utf8 as m',
     'from indico.util.i18n import _'
 ]
 
