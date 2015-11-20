bd4f58d6f932e4e9246af9496f81c4f7a2d3d9e5
Alejandro Avilés <aavilesd@cern.ch>
2015-11-20 17:22:51 +0100

    Basic scaffolding for new timetable module

---
+++ indico/modules/events/timetable/__init__.py
@@ -0,0 +1,31 @@
+# This file is part of Indico.
+# Copyright (C) 2002 - 2015 European Organization for Nuclear Research (CERN).
+#
+# Indico is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+#
+# Indico is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Indico; if not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+
+from indico.core import signals
+from indico.core.logger import Logger
+from indico.util.i18n import _
+from indico.web.flask.util import url_for
+from indico.web.menu import SideMenuItem
+
+
+logger = Logger.get('events.timetable')
+
+
+@signals.menu.items.connect_via('event-management-sidemenu')
+def _extend_event_management_menu(sender, event, **kwargs):
+    return SideMenuItem('timetable', _('Timetable'), url_for('timetable.timetable', event), weight=80, icon='calendar')
