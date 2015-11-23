0a53f8fd4948f9f7372c4d92466986fb105a8d2a
Alejandro Avilés <aavilesd@cern.ch>
2015-11-23 11:40:41 +0100

    Basic scaffolding for new contributions module

---
+++ indico/modules/events/contributions/blueprint.py
@@ -0,0 +1,26 @@
+# This file is part of Indico.
+# Copyright (C) 2002 - 2015 European Organization for Nuclear Research (CERN).
+#
+# Indico is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+#
+# Indico is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Indico; if not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+
+from indico.modules.events.contributions.controllers import RHManageContributions
+from indico.web.flask.wrappers import IndicoBlueprint
+
+
+_bp = IndicoBlueprint('contributions', __name__, template_folder='templates',
+                      virtual_template_folder='events/contributions', url_prefix='/event/<confId>')
+
+_bp.add_url_rule('/manage/contributions/', 'manage_contributions', RHManageContributions)
