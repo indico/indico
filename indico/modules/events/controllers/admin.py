211eee8159d7e76f3eed34f892eabbf555729a49
Ilias Trichopoulos <ilias.trichopoulos@cern.ch>
2015-12-03 10:57:10 +0100

    Add Reference Types scaffold in server admin area

--- indico/modules/events/controllers/admin.py
+++ indico/modules/events/controllers/admin.py
@@ -1,5 +1,5 @@
 # This file is part of Indico.
-# Copyright (C) 2002 - 2016 European Organization for Nuclear Research (CERN).
+# Copyright (C) 2002 - 2015 European Organization for Nuclear Research (CERN).
 #
 # Indico is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
@@ -14,13 +14,12 @@
 # You should have received a copy of the GNU General Public License
 # along with Indico; if not, see <http://www.gnu.org/licenses/>.
 
-from __future__ import unicode_literals
-
-from indico.modules.events.controllers.admin import RHReferenceTypes
-from indico.web.flask.wrappers import IndicoBlueprint
+from indico.modules.events.views import WPReferenceTypes
+from MaKaC.webinterface.rh.admins import RHAdminBase
 
 
-_bp = IndicoBlueprint('events', __name__, template_folder='templates', virtual_template_folder='events')
+class RHReferenceTypes(RHAdminBase):
+    """Manage reference types in server admin area"""
 
-# Admin
-_bp.add_url_rule('/admin/external-id-types', 'reference_types', RHReferenceTypes, methods=('GET', 'POST'))
+    def _process(self):
+        return WPReferenceTypes.render_template('reference_types.html')
