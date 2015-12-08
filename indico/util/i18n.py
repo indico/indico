cdb68d09306c5c50f313fc8e070266d8102dafb6
Adrian Moennich <adrian.moennich@cern.ch>
2015-12-08 15:02:05 +0100

    Remove global indico imports from indico.util.i18n
    This file is exported in MANY locations and having it import indico
    modules is a great recipe for import issues.

--- indico/util/i18n.py
+++ indico/util/i18n.py
@@ -30,11 +30,6 @@ from flask_babelex import Babel, get_domain, Domain
 from flask_pluginengine import current_plugin
 from speaklater import is_lazy_string, make_lazy_string
 
-from indico.core.config import Config
-from indico.core.db import DBMgr
-
-from MaKaC.common.info import HelperMaKaCInfo
-
 
 LOCALE_ALIASES = dict(LOCALE_ALIASES, en='en_GB')
 RE_TR_FUNCTION = re.compile(r'''_\("([^"]*)"\)|_\('([^']*)'\)''', re.DOTALL | re.MULTILINE)
@@ -178,11 +173,13 @@ class IndicoTranslations(Translations):
             raise RuntimeError(msg)
 
     def ugettext(self, message):
+        from indico.core.config import Config
         if Config.getInstance().getDebug():
             self._check_stack()
         return gettext(message)
 
     def ungettext(self, msgid1, msgid2, n):
+        from indico.core.config import Config
         if Config.getInstance().getDebug():
             self._check_stack()
         return ngettext(msgid1, msgid2, n)
@@ -199,6 +196,9 @@ def set_best_lang(check_session=True):
     language, we will try to guess it from the browser settings and only
     after that fall back to the server's default.
     """
+    from indico.core.db import DBMgr
+    from MaKaC.common.info import HelperMaKaCInfo
+
     if not has_request_context():
         return 'en_GB' if current_app.config['TESTING'] else HelperMaKaCInfo.getMaKaCInfoInstance().getLang()
     elif 'lang' in g:
