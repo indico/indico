906bc0be8773a6d1008b6844034085756d0ca7fa
Indico Team <indico-team@cern.ch>
2016-01-04 09:00:10 +0100

    Happy new year 2016! (#2)
    Bump some more license headers to 2016

--- indico/modules/events/contributions/operations.py
+++ indico/modules/events/contributions/operations.py
@@ -1,4 +1,5 @@
-# This file is part of Indico.# Copyright (C) 2002 - 2015 European Organization for Nuclear Research (CERN).
+# This file is part of Indico.
+# Copyright (C) 2002 - 2016 European Organization for Nuclear Research (CERN).
 #
 # Indico is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
