indico/indico

commit ea74a04d03c24e98a7b6b6ea05a8a9942070c2d9

Optionally publish check-in status
Check-in status can be published on a registration form basis in the
event page. Duplicate might appear if there are several registration
forms but this is very unlikely for meetings and conferences.

indico/modules/events/registration/__init__.py
@@ -69,7 +69,6 @@ def _inject_regform_announcement(event, **kwargs):
 def _inject_event_header(event, **kwargs):
     from indico.modules.events.registration.models.forms import RegistrationForm
     from indico.modules.events.registration.models.registrations import Registration
-    from indico.modules.events.registration.util import get_unique_published_registrations
 
     event = event.as_event
     regforms = (event.registration_forms
@@ -77,9 +76,21 @@ def _inject_event_header(event, **kwargs):
                 .order_by(db.func.lower(RegistrationForm.title))
                 .all())
 
-    participants = get_unique_published_registrations(event)
+    registrations = (Registration
+                     .find(Registration.is_active,
+                           ~RegistrationForm.is_deleted,
+                           RegistrationForm.event_id == event.id,
+                           RegistrationForm.publish_registrations_enabled,
+                           _join=Registration.registration_form,
+                           _eager=Registration.registration_form)
+                     .order_by(db.func.lower(Registration.first_name),
+                               db.func.lower(Registration.last_name),
+                               Registration.friendly_id))
+
+    # A participant could appear more than once in the list in case he register to multiple registration form.
+    # This is deemed very unlikely in the case of meetings and lectures and thus not worth the extra complexity.
     return render_template('events/registration/display/event_header.html', event=event, regforms=regforms,
-                           participants=participants)
+                           participants=registrations)
 
 
 @signals.event.sidemenu.connect

indico/modules/events/registration/util.py
@@ -335,27 +335,6 @@ def get_registrations_with_tickets(user, event):
                              _join=Registration.registration_form).all()
 
 
-def get_unique_published_registrations(event):
-    """Get a list of unique published registrations for an event.
-
-    Uniqueness is determined by associated user, so if someone has
-    registered in more than one registration form in the event, they
-    will be included only once.
-
-    :param event: The Event to get registrations for
-    """
-    registrations = Registration.find_all(Registration.is_active,
-                                          ~RegistrationForm.is_deleted,
-                                          RegistrationForm.event_id == event.id,
-                                          RegistrationForm.publish_registrations_enabled,
-                                          _join=Registration.registration_form)
-
-    linked_participants = {reg.user_id: reg for reg in registrations if reg.user_id is not None}
-    non_linked_participants = {reg for reg in registrations if reg.user_id is None}
-    return sorted(set(linked_participants.viewvalues()) | non_linked_participants,
-                  key=lambda x: (x.first_name.lower(), x.last_name.lower(), x.id))
-
-
 def get_events_registered(user, from_dt=None, to_dt=None):
     """Gets the IDs of events where the user is registered.
 

indico/htdocs/sass/modules/_event_display.scss
@@ -175,6 +175,10 @@ li > table + .note-area-wrapper {
             &:not(first-child) {
                 margin-left: 0.1em;
             }
+
+            &.checked-in {
+                background: lighten($pastel-green, 30%);
+            }
         }
     }
 }

indico/modules/events/registration/forms.py
@@ -73,6 +73,9 @@ class RegistrationFormForm(IndicoForm):
     publish_registrations_enabled = BooleanField(_('Publish registrations'), widget=SwitchWidget(),
                                                  description=_("Registrations from this form will be displayed in the "
                                                                "event page"))
+    publish_checkin_enabled = BooleanField(_('Publish check-in status'), widget=SwitchWidget(),
+                                                 description=_("Check-in status will be shown publicly on the event page "
+                                                               "event page"))
     base_price = DecimalField(_('Registration fee'), [NumberRange(min=0), Optional(), _check_if_payment_required],
                               filters=[lambda x: x if x is not None else 0],
                               widget=NumberInput(step='0.01'),

indico/modules/events/registration/models/forms.py
@@ -131,6 +131,12 @@ class RegistrationForm(db.Model):
         nullable=False,
         default=False
     )
+    #: Whether checked-in status should be displayed in the event pages and participant list
+    publish_checkin_enabled = db.Column(
+        db.Boolean,
+        nullable=False,
+        default=False
+    )
     #: Whether registrations must be approved by a manager
     moderation_enabled = db.Column(
         db.Boolean,

indico/modules/events/registration/templates/display/event_header.html
@@ -55,7 +55,9 @@
             <div class="participant-list-wrapper">
                 <ul class="participant-list">
                     {%- for participant in participants -%}
-                        <li>{{ participant.full_name }}</li>
+                    <li {% if participant.checked_in and participant.registration_form.publish_checkin_enabled %}class="checked-in"{% endif %}>
+                      {{ participant.full_name }}
+                    </li>
                     {%- endfor -%}
                 </ul>
                 <div class="gradient-layer"></div>

migrations/versions/201602021203_27299745573_add_publish_checkin_column_to_reg_form.py
@@ -1,0 +1,24 @@
+"""Add publish_checkin column to reg form
+
+Revision ID: 27299745573
+Revises: 47a8b5324cd6
+Create Date: 2016-02-02 12:03:15.973817
+"""
+
+import sqlalchemy as sa
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = '27299745573'
+down_revision = '47a8b5324cd6'
+
+
+def upgrade():
+    op.add_column('forms', sa.Column('publish_checkin_enabled', sa.Boolean(), nullable=False, server_default='false'),
+                  schema='event_registration')
+    op.alter_column('forms', 'publish_checkin_enabled', server_default=None, schema='event_registration')
+
+
+def downgrade():
+    op.drop_column('forms', 'publish_checkin_enabled', schema='event_registration')

commit e9e2ea4dc6587f4b22b9d852a2314d8717e277f1

Fix cdsagenda/administrative styles (participants)
Broken in ea74a04d03c24e9

indico/MaKaC/webinterface/tpls/events/meetings/administrative/Administrative.tpl
@@ -68,8 +68,8 @@
         % endif
 
         <%
-        from indico.modules.events.registration.util import get_unique_published_registrations
-        participants = get_unique_published_registrations(conf)
+        from indico.modules.events.registration.util import get_published_registrations
+        participants = get_published_registrations(conf)
         %>
         % if participants:
             <br/><br/>

indico/MaKaC/webinterface/tpls/events/meetings/cdsagenda/include/Header.tpl
@@ -87,8 +87,8 @@
         %endif
 
         <%
-        from indico.modules.events.registration.util import get_unique_published_registrations
-        participants = get_unique_published_registrations(conf)
+        from indico.modules.events.registration.util import get_published_registrations
+        participants = get_published_registrations(conf)
         %>
         % if participants:
         <tr>

indico/modules/events/registration/__init__.py
@@ -69,6 +69,7 @@ def _inject_regform_announcement(event, **kwargs):
 def _inject_event_header(event, **kwargs):
     from indico.modules.events.registration.models.forms import RegistrationForm
     from indico.modules.events.registration.models.registrations import Registration
+    from indico.modules.events.registration.util import get_published_registrations
 
     event = event.as_event
     regforms = (event.registration_forms
@@ -76,18 +77,7 @@ def _inject_event_header(event, **kwargs):
                 .order_by(db.func.lower(RegistrationForm.title))
                 .all())
 
-    registrations = (Registration
-                     .find(Registration.is_active,
-                           ~RegistrationForm.is_deleted,
-                           RegistrationForm.event_id == event.id,
-                           RegistrationForm.publish_registrations_enabled,
-                           _join=Registration.registration_form,
-                           _eager=Registration.registration_form)
-                     .order_by(db.func.lower(Registration.first_name),
-                               db.func.lower(Registration.last_name),
-                               Registration.friendly_id)
-                     .all())
-
+    registrations = get_published_registrations(event)
     # A participant could appear more than once in the list in case he register to multiple registration form.
     # This is deemed very unlikely in the case of meetings and lectures and thus not worth the extra complexity.
     return render_template('events/registration/display/event_header.html', event=event, regforms=regforms,

indico/modules/events/registration/util.py
@@ -335,6 +335,25 @@ def get_registrations_with_tickets(user, event):
                              _join=Registration.registration_form).all()
 
 
+def get_published_registrations(event):
+    """Get a list of published registrations for an event.
+
+    :param event: the `Event` to get registrations for
+    :return: list of `Registration` objects
+    """
+    return (Registration
+            .find(Registration.is_active,
+                  ~RegistrationForm.is_deleted,
+                  RegistrationForm.event_id == event.id,
+                  RegistrationForm.publish_registrations_enabled,
+                  _join=Registration.registration_form,
+                  _eager=Registration.registration_form)
+            .order_by(db.func.lower(Registration.first_name),
+                      db.func.lower(Registration.last_name),
+                      Registration.friendly_id)
+            .all())
+
+
 def get_events_registered(user, from_dt=None, to_dt=None):
     """Gets the IDs of events where the user is registered.
 