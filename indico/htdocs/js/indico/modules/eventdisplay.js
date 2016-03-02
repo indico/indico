e73ab467cb917f3418cd8e26fcb669ec371f69c2
Pedro Ferreira <pedro.ferreira@cern.ch>
2016-03-02 12:20:21 +0100

    Fix snake case naming in JS

--- indico/htdocs/js/indico/modules/eventdisplay.js
+++ indico/htdocs/js/indico/modules/eventdisplay.js
@@ -78,10 +78,10 @@
         $('.event-service-row-toggle').on('click', function(e) {
             e.preventDefault();
             var toggler = $(this);
-            var toggler_button = $(this).parent().siblings('.trigger');
+            var togglerButton = $(this).parent().siblings('.trigger');
             toggler.parent().siblings('.event-service-details').slideToggle({
                 start: function() {
-                    toggler_button.toggleClass('icon-expand icon-collapse');
+                    togglerButton.toggleClass('icon-expand icon-collapse');
                 },
                 duration: 'fast'
             });
