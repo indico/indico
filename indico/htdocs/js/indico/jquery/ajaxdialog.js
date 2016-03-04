64d91a642d398ee687e267ec3668e55bdb01829c
Adrian Moennich <adrian.moennich@cern.ch>
2016-03-04 14:22:38 +0100

    Trigger ajaxDialog:closed after closing a dialog

--- indico/htdocs/js/indico/jquery/ajaxdialog.js
+++ indico/htdocs/js/indico/jquery/ajaxdialog.js
@@ -175,6 +175,8 @@
                     popup.close();
                     if (options.confirmCloseUnsaved) {
                         window.onbeforeunload = oldOnBeforeUnload;
+                    if (options.trigger) {
+                        $(options.trigger).trigger('ajaxDialog:closed', [callbackData, customData])
                     }
                 }, function() {
                     ignoreOnBeforeUnload = false;
