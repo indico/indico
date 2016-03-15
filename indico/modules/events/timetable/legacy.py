2e19e14289d524350d5e2c9bbaee6f9036d9df94
Alejandro Avilés <alejandro.aviles@cern.ch>
2016-03-15 11:24:12 +0100

    Adapt timetable serialization for management mode

--- indico/modules/events/timetable/legacy.py
+++ indico/modules/events/timetable/legacy.py
@@ -21,176 +21,198 @@ from collections import defaultdict
 from flask import session
 from sqlalchemy.orm import defaultload
 
+from indico.modules.events.contributions.models.persons import ContributionPersonLink
+from indico.modules.events.sessions.models.persons import SessionBlockPersonLink
 from indico.modules.events.timetable.models.entries import TimetableEntry, TimetableEntryType
 from indico.util.date_time import iterdays
 
 
-def _get_query_options():
-    return (defaultload('contribution').subqueryload('person_links'),
-            defaultload('session_block').subqueryload('person_links'))
+class TimetableSerializer(object):
+    def __init__(self, management=False):
+        self.management = management
 
+    def serialize_timetable(self, event, days=None, hide_weekends=False):
+        timetable = {}
+        start_dt = event.start_dt.astimezone(event.tzinfo)
+        end_dt = event.end_dt.astimezone(event.tzinfo)
+        for day in iterdays(start_dt, end_dt, skip_weekends=hide_weekends, day_whitelist=days):
+            date_str = day.strftime('%Y%m%d')
+            timetable[date_str] = {}
+        query_options = (defaultload('contribution').subqueryload('person_links'),
+                         defaultload('session_block').subqueryload('person_links'))
+        query = (event.timetable_entries
+                 .options(*query_options)
+                 .order_by(TimetableEntry.type != TimetableEntryType.SESSION_BLOCK))
+        for entry in query:
+            day = entry.start_dt.astimezone(event.tzinfo).date()
+            date_str = day.strftime('%Y%m%d')
+            if date_str not in timetable:
+                continue
+            if not entry.can_view(session.user):
+                continue
+            data = self.serialize_timetable_entry(entry)
+            key = self._get_entry_key(entry)
+            if entry.parent:
+                parent_code = 's{}'.format(entry.parent_id)
+                timetable[date_str][parent_code]['entries'][key] = data
+            else:
+                timetable[date_str][key] = data
+        return timetable
 
-def serialize_timetable(event, days=None, hide_weekends=False, for_management=False):
-    # TODO: Management mode
-    timetable = {}
-    start_dt = event.start_dt.astimezone(event.tzinfo)
-    end_dt = event.end_dt.astimezone(event.tzinfo)
-    for day in iterdays(start_dt, end_dt, skip_weekends=hide_weekends, day_whitelist=days):
-        date_str = day.strftime('%Y%m%d')
-        timetable[date_str] = {}
-    query = (event.timetable_entries
-             .options(*_get_query_options())
-             .order_by(TimetableEntry.type != TimetableEntryType.SESSION_BLOCK))
-    for entry in query:
-        day = entry.start_dt.astimezone(event.tzinfo).date()
-        date_str = day.strftime('%Y%m%d')
-        if date_str not in timetable:
-            continue
-        if not entry.can_view(session.user):
-            continue
-        data = serialize_timetable_entry(entry)
-        key = _get_entry_key(entry)
-        if entry.parent:
-            parent_code = 's{}'.format(entry.parent_id)
-            timetable[date_str][parent_code]['entries'][key] = data
+    def serialize_timetable_entry(self, entry):
+        if entry.type == TimetableEntryType.SESSION_BLOCK:
+            return self.serialize_session_block_entry(entry)
+        elif entry.type == TimetableEntryType.CONTRIBUTION:
+            return self.serialize_contribution_entry(entry)
+        elif entry.type == TimetableEntryType.BREAK:
+            return self.serialize_break_entry(entry)
         else:
-            timetable[date_str][key] = data
-    return timetable
+            raise TypeError("Unknown timetable entry type.")
 
-
-def serialize_timetable_entry(entry):
-    if entry.type == TimetableEntryType.SESSION_BLOCK:
-        return serialize_session_block_entry(entry)
-    elif entry.type == TimetableEntryType.CONTRIBUTION:
-        return serialize_contribution_entry(entry)
-    elif entry.type == TimetableEntryType.BREAK:
-        return serialize_break_entry(entry)
-    else:
-        raise TypeError("Unknown timetable entry type.")
-
-
-def serialize_session_block_entry(entry):
-    block = entry.session_block
-    data = {}
-    data.update(_get_date_data(entry))
-    data.update(_get_color_data(block.session))
-    data.update(_get_location_data(block))
-    data.update({'id': _get_entry_key(entry),
-                 'uniqueId': entry.id,
-                 'sessionSlotId': block.id,
-                 'sessionId': block.session_id,
-                 'title': block.session.title,
-                 'slotTitle': block.title,
-                 'entryType': 'Session',
-                 'attachments': _get_attachment_data(block.session),
-                 'code': block.session.code,
-                 'conferenceId': block.session.event_id,
-                 'contribDuration': block.session.default_contribution_duration.seconds / 60,
-                 'conveners': [_get_person_data(x) for x in block.person_links],
-                 'description': block.session.description,
-                 'duration': block.duration.seconds / 60,
-                 'isPoster': block.session.is_poster,
-                 'entries': defaultdict(dict),
-                 'pdf': None,
-                 'url': None})
-    return data
-
-
-def serialize_contribution_entry(entry):
-    contribution = entry.contribution
-    data = {}
-    data.update(_get_date_data(entry))
-    data.update(_get_location_data(contribution))
-    data.update({'id': _get_entry_key(entry),
-                 'uniqueId': entry.id,
-                 'contributionId': contribution.id,
-                 'entryType': 'Contribution',
-                 'title': contribution.title,
-                 'attachments': _get_attachment_data(contribution),
-                 'conferenceId': contribution.event_id,
-                 'description': contribution.description,
-                 'duration': contribution.duration.seconds / 60,
-                 'pdf': None,
-                 'presenters': [_get_person_data(x) for x in contribution.person_links],
-                 'url': None})
-    if entry.parent:
-        block = entry.parent.session_block
-        data.update({'sessionCode': block.session.code,
+    def serialize_session_block_entry(self, entry):
+        block = entry.session_block
+        data = {}
+        data.update(self._get_entry_data(entry))
+        data.update(self._get_color_data(block.session))
+        data.update(self._get_location_data(block))
+        data.update({'entryType': 'Session',
+                     'sessionSlotId': block.id,
                      'sessionId': block.session_id,
-                     'sessionSlotId': block.id})
-    return data
+                     'sessionCode': block.session.code,
+                     'title': block.session.title,
+                     'slotTitle': block.title,
+                     'attachments': self._get_attachment_data(block.session),
+                     'code': block.session.code,
+                     'contribDuration': block.session.default_contribution_duration.seconds / 60,
+                     'conveners': [self._get_person_data(x) for x in block.person_links],
+                     'description': block.session.description,
+                     'duration': block.duration.seconds / 60,
+                     'isPoster': block.session.is_poster,
+                     'entries': defaultdict(dict),
+                     'pdf': None,
+                     'url': None})
+        return data
 
+    def serialize_contribution_entry(self, entry):
+        block = entry.parent.session_block if entry.parent else None
+        contribution = entry.contribution
+        data = {}
+        data.update(self._get_entry_data(entry))
+        data.update(self._get_location_data(contribution))
+        data.update({'entryType': 'Contribution',
+                     'contributionId': contribution.id,
+                     'attachments': self._get_attachment_data(contribution),
+                     'description': contribution.description,
+                     'duration': contribution.duration.seconds / 60,
+                     'pdf': None,
+                     'presenters': [self._get_person_data(x) for x in contribution.person_links],
+                     'sessionCode': block.session.code if block else None,
+                     'sessionId': block.session_id if block else None,
+                     'sessionSlotId': block.id if block else None,
+                     'title': contribution.title,
+                     'url': None})
+        if self.management:
+            data['boardNumber'] = contribution.board_number
+            data['contributionType'] = contribution.type_id
+            data['keywords'] = contribution.keywords
+            # TODO
+            data['reportNumbers'] = None
+            data['fields'] = None
+        return data
 
-def serialize_break_entry(entry):
-    break_ = entry.break_
-    data = {}
-    data.update(_get_date_data(entry))
-    data.update(_get_color_data(break_))
-    data.update(_get_location_data(break_))
-    data.update({'id': break_.id,
-                 'entryType': 'Break',
-                 'title': break_.title,
-                 'description': break_.description,
-                 'duration': break_.duration.seconds / 60})
-    return data
+    def serialize_break_entry(self, entry, management=False):
+        block = entry.parent.session_block if entry.parent else None
+        break_ = entry.break_
+        data = {}
+        data.update(self._get_entry_data(entry))
+        data.update(self._get_color_data(break_))
+        data.update(self._get_location_data(break_))
+        data.update({'entryType': 'Break',
+                     'description': break_.description,
+                     'duration': break_.duration.seconds / 60,
+                     'sessionId': block.session_id if block else None,
+                     'sessionCode': block.session.code if block else None,
+                     'sessionSlotId': block.id if block else None,
+                     'title': break_.title})
+        return data
 
+    def _get_attachment_data(self, obj):
+        def serialize_attachment(attachment):
+            return {'title': attachment.title, 'download_url': attachment.download_url}
 
-def _get_attachment_data(obj):
-    def serialize_attachment(attachment):
-        return {'title': attachment.title, 'download_url': attachment.download_url}
+        def serialize_folder(folder):
+            return {'title': folder.title,
+                    'attachments': map(serialize_attachment, folder.attachments)}
 
-    def serialize_folder(folder):
-        return {'title': folder.title,
-                'attachments': map(serialize_attachment, folder.attachments)}
+        data = {'files': [], 'folders': []}
+        items = obj.attached_items
+        data['files'] = map(serialize_attachment, items.get('files', []))
+        data['folders'] = map(serialize_folder, items.get('folders', []))
+        if not data['files'] and not data['folders']:
+            data['files'] = None
+        return data
 
-    data = {'files': [], 'folders': []}
-    items = obj.attached_items
-    data['files'] = map(serialize_attachment, items.get('files', []))
-    data['folders'] = map(serialize_folder, items.get('folders', []))
-    if not data['files'] and not data['folders']:
-        data['files'] = None
-    return data
+    def _get_color_data(self, obj):
+        return {'color': '#' + obj.background_color,
+                'textColor': '#' + obj.text_color}
 
+    def _get_date_data(self, entry):
+        tzinfo = entry.event_new.tzinfo
+        return {'startDate': self._get_entry_date_dt(entry.start_dt, tzinfo),
+                'endDate': self._get_entry_date_dt(entry.end_dt, tzinfo)}
 
-def _get_color_data(obj):
-    return {'color': '#' + obj.background_color,
-            'textColor': '#' + obj.text_color}
+    def _get_entry_data(self, entry):
+        data = {}
+        data.update(self._get_date_data(entry))
+        data['id'] = self._get_entry_key(entry)
+        data['uniqueId'] = data['id']
+        data['conferenceId'] = entry.event_id,
+        if self.management:
+            data['scheduleEntryId'] = entry.id
+        return data
 
+    def _get_entry_key(self, entry):
+        if entry.type == TimetableEntryType.SESSION_BLOCK:
+            return 's{}'.format(entry.id)
+        elif entry.type == TimetableEntryType.CONTRIBUTION:
+            return 'c{}'.format(entry.id)
+        elif entry.type == TimetableEntryType.BREAK:
+            return 'b{}'.format(entry.id)
+        else:
+            raise ValueError()
 
-def _get_date_data(entry):
-    tzinfo = entry.event_new.tzinfo
-    return {'startDate': _get_entry_date_dt(entry.start_dt, tzinfo),
-            'endDate': _get_entry_date_dt(entry.end_dt, tzinfo)}
+    def _get_entry_date_dt(self, dt, tzinfo):
+        return {'date': dt.astimezone(tzinfo).strftime('%Y-%m-%d'),
+                'time': dt.astimezone(tzinfo).strftime('%H:%M:%S'),
+                'tz': str(tzinfo)}
 
+    def _get_location_data(self, obj):
+        data = {}
+        data['location'] = obj.venue_name
+        data['room'] = obj.room_name
+        data['inheritLoc'] = obj.inherit_location
+        data['inheritRoom'] = obj.inherit_location
+        if self.management:
+            data['address'] = obj.address
+        return data
 
-def _get_entry_key(entry):
-    if entry.type == TimetableEntryType.SESSION_BLOCK:
-        return 's{}'.format(entry.id)
-    elif entry.type == TimetableEntryType.CONTRIBUTION:
-        return 'c{}'.format(entry.id)
-    elif entry.type == TimetableEntryType.BREAK:
-        return 'b{}'.format(entry.id)
-    else:
-        raise ValueError()
-
-
-def _get_entry_date_dt(dt, tzinfo):
-    return {'date': dt.astimezone(tzinfo).strftime('%Y-%m-%d'),
-            'time': dt.astimezone(tzinfo).strftime('%H:%M:%S'),
-            'tz': str(tzinfo)}
-
-
-def _get_location_data(obj):
-    return {'location': obj.venue_name,
-            'room': obj.room_name,
-            'inheritLoc': obj.inherit_location,
-            'inheritRoom': obj.inherit_location}
-
-
-def _get_person_data(person_link):
-    return {'name': person_link.get_full_name(last_name_first=False, last_name_upper=False,
-                                              abbrev_first_name=False, show_title=True),
-            'firstName': person_link.first_name,
-            'familyName': person_link.last_name,
-            'affiliation': person_link.affiliation,
-            'email': person_link.person.email}
+    def _get_person_data(self, person_link):
+        data = {}
+        data['firstName'] = person_link.first_name
+        data['familyName'] = person_link.last_name
+        data['affiliation'] = person_link.affiliation
+        data['email'] = person_link.person.email
+        data['name'] = person_link.get_full_name(last_name_first=False, last_name_upper=False,
+                                                 abbrev_first_name=False, show_title=True),
+        if self.management:
+            data['id'] = person_link.person_id
+            data['address'] = person_link.person.address
+            data['phone'] = person_link.person.phone
+            data['title'] = person_link.person.title
+            if isinstance(person_link, ContributionPersonLink):
+                data['isSubmitter'] = person_link.is_submitter
+                # TODO: URL to person page
+                data['url'] = None
+            if isinstance(person_link, SessionBlockPersonLink):
+                data['fullName'] = data['name']
+        return data
