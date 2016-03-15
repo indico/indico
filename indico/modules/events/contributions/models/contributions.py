0edf2e42b0af5de3f3bda04181f4dbcd053c7ff6
Adrian Moennich <adrian.moennich@cern.ch>
2016-03-15 17:38:42 +0100

    Use friendly_data to show field value

--- indico/modules/events/contributions/models/contributions.py
+++ indico/modules/events/contributions/models/contributions.py
@@ -334,7 +334,7 @@ class Contribution(DescriptionMixin, ProtectionManagersMixin, LocationMixin, Att
         return get_non_inheriting_objects(self)
 
     def get_field_value(self, field_id):
-        return next((v.data for v in self.field_values if v.contribution_field_id == field_id), '')
+        return next((v.friendly_data for v in self.field_values if v.contribution_field_id == field_id), '')
 
 
 @listens_for(mapper, 'after_configured', once=True)
