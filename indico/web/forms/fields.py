f8525c49a22ce1dfc21b3b03907890d51ed95a2b
Alejandro Avilés <alejandro.aviles@cern.ch>
2016-03-24 11:38:59 +0100

    Fix creating duplicate event persons

--- indico/web/forms/fields.py
+++ indico/web/forms/fields.py
@@ -332,7 +332,10 @@ class EventPersonListField(PrincipalListField):
         person_type = data.get('_type')
         if person_type is None:
             user = User.find_first(~User.is_deleted, User.all_emails.contains(data['email'].lower()))
-            return EventPerson.for_user(user, self.event) if user else self._create_event_person(data)
+            if user:
+                return EventPerson.for_user(user, self.event)
+            person = EventPerson.find_first(email=data['email'].lower())
+            return person if person else self._create_event_person(data)
         elif person_type == 'Avatar':
             user = self._convert_principal(data)
             return EventPerson.for_user(user, self.event)
