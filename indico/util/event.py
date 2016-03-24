fb32578bfedb876f43675cc6a9780e5aa05f7c19
Ilias Trichopoulos <ilias.trichopoulos@cern.ch>
2016-03-24 11:47:56 +0100

    Use legacy IDs when possible

--- indico/util/event.py
+++ indico/util/event.py
@@ -38,13 +38,19 @@ def uniqueId(obj):
         return '{}.s{}.{}'.format(obj.getConference().getId(), obj.getSession().getId(), obj.getId())
     # new objects
     elif isinstance(obj, db.m.Contribution):
-        return '{}.{}'.format(obj.event_id, obj.id)
+        return '{}.{}'.format(obj.event_id, obj.legacy_mapping.legacy_contribution_id if obj.legacy_mapping else obj.id)
     elif isinstance(obj, db.m.SubContribution):
-        return '{}.{}.{}'.format(obj.event_new.id, obj.contribution.id, obj.id)
+        return '{}.{}.{}'.format(
+            obj.event_new.id,
+            obj.legacy_mapping.legacy_contribution_id if obj.legacy_mapping else obj.contribution.id,
+            obj.legacy_mapping.legacy_subcontribution_id if obj.legacy_mapping else obj.id
+        )
     elif isinstance(obj, db.m.Session):
-        return '{}.s{}'.format(obj.event_id, obj.id)
+        return '{}.s{}'.format(obj.event_id, obj.legacy_mapping.legacy_session_id if obj.legacy_mapping else obj.id)
     elif isinstance(obj, db.m.SessionBlock):
-        return '{}.s{}.{}'.format(obj.getConference().getId(), obj.getSession().getId(), obj.id)
+        return '{}.s{}.{}'.format(obj.event_new.id,
+                                  obj.legacy_mapping.legacy_session_id if obj.legacy_mapping else obj.session.id,
+                                  obj.legacy_mapping.legacy_session_block_id if obj.legacy_mapping else obj.id)
     elif isinstance(obj, db.m.EventNote):
         return '{}.n{}'.format(uniqueId(obj.object), obj.id)
     else:
