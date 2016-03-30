10697742f7be8ab2239aa06651000916fccaae1c
Florian Vessaz <florian.vessaz@cern.ch>
2016-03-30 15:29:47 +0200

    Add abstracts_settings from 2befa7bc
    pedro/wip/legacy-abstracts

--- indico/modules/events/abstracts/settings.py
+++ indico/modules/events/abstracts/settings.py
@@ -21,7 +21,7 @@ abstracts_settings = EventSettingsProxy('abstracts', {
     'description_settings': {
         'active': True,
         'required': True,
-        'max_chars': 0,
+        'max_chars': None,
         'max_words': None
     }
 }, preload=True)
