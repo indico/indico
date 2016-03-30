f4e5825dcbc9c2b82c88bb46cb94b00653f8bb72
Florian Vessaz <florian.vessaz@cern.ch>
2016-03-30 11:29:52 +0200

    Move sortable_list out of registration module

--- indico/htdocs/js/indico/modules/registration/registration.js
+++ indico/htdocs/js/indico/modules/registration/registration.js
@@ -4,9 +4,6 @@
     $(document).ready(function() {
         setupRegistrationFormScheduleDialogs();
         setupRegistrationFormSummaryPage();
-        $('.js-sortable-list-widget').each(function() {
-            setupRegistrationSortableLists($(this));
-        });
     });
 
     $(window).scroll(function(){
@@ -66,42 +63,6 @@
                     $('#event-sidemenu').html(data.event_menu);
                 }
             });
-        });
-    };
-
-    global.setupRegistrationSortableLists = function setupRegistrationSortableLists($wrapper) {
-        /* Works with the sortable_lists macro defined in
-         * indico/modules/events/registration/templates/management/_sortable_list.html
-         *
-         * Global needed to call it on sortable lists in dialog
-         */
-
-        // Render the lists sortable
-        if ($wrapper.data('disable-dragging') === undefined) {
-            var $lists = $wrapper.find('ul');
-            $lists.sortable({
-                connectWith: $lists,
-                placeholder: 'i-label sortable-item placeholder',
-                containment: $wrapper,
-                forcePlaceholderSize: true
-            });
-        }
-
-        // Move an item from the enabled list to the disabled one (or vice versa).
-        function toggleEnabled($li) {
-            var $list = $li.closest('ul');
-            var targetClass = $list.hasClass('enabled') ? '.disabled' : '.enabled';
-            var $destination = $list.closest('.js-sortable-list-widget').find('ul' + targetClass);
-            $li.detach().appendTo($destination);
-        }
-
-        $wrapper.find('ul li .toggle-enabled').on('click', function() {
-            toggleEnabled($(this).closest('li'));
-        });
-
-        // Prevents dragging the row when the action buttons are clicked.
-        $wrapper.find('.actions').on('mousedown', function(evt) {
-            evt.stopPropagation();
         });
     };
 })(window);
