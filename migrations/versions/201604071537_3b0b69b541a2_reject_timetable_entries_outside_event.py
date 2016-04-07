6610cc58273519062efb084fc1dfb44f247b7f4a
Adrian Moennich <adrian.moennich@cern.ch>
2016-04-07 15:56:03 +0200

    Disallow tt entries outside event start/end time

--- migrations/versions/201604071537_3b0b69b541a2_reject_timetable_entries_outside_event.py
+++ migrations/versions/201604071537_3b0b69b541a2_reject_timetable_entries_outside_event.py
@@ -1,31 +1,22 @@
-# This file is part of Indico.
-# Copyright (C) 2002 - 2016 European Organization for Nuclear Research (CERN).
-#
-# Indico is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 3 of the
-# License, or (at your option) any later version.
-#
-# Indico is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Indico; if not, see <http://www.gnu.org/licenses/>.
+"""Reject timetable entries outside event bounds
 
-from __future__ import unicode_literals
+Revision ID: 3b0b69b541a2
+Revises: 1af04f7ede7a
+Create Date: 2016-04-07 15:37:31.908876
+"""
 
 import textwrap
 
-from sqlalchemy import DDL
-
-from indico.core import signals
+from alembic import op
 
 
-@signals.db_schema_created.connect_via('events')
-def _events_schema_created(sender, connection, **kwargs):
-    sql = textwrap.dedent("""
+# revision identifiers, used by Alembic.
+revision = '3b0b69b541a2'
+down_revision = '1af04f7ede7a'
+
+
+def upgrade():
+    op.execute(textwrap.dedent("""
         CREATE OR REPLACE FUNCTION events.check_timetable_consistency() RETURNS trigger AS
         $BODY$
         DECLARE
@@ -123,5 +114,84 @@ def _events_schema_created(sender, connection, **kwargs):
         END;
         $BODY$
         LANGUAGE plpgsql;
-    """)
-    DDL(sql).execute(connection)
+    """))
+
+
+def downgrade():
+    op.execute(textwrap.dedent("""
+        CREATE OR REPLACE FUNCTION events.check_timetable_consistency() RETURNS trigger AS
+        $BODY$
+        DECLARE
+            src varchar;
+            trigger_event_id int;
+        BEGIN
+        src := TG_ARGV[0];
+
+        IF src = 'break' THEN
+            SELECT tte.event_id INTO STRICT trigger_event_id
+            FROM events.timetable_entries tte
+            WHERE tte.break_id = NEW.id;
+        ELSIF src = 'session_block' THEN
+            SELECT s.event_id INTO STRICT trigger_event_id
+            FROM events.sessions s
+            WHERE s.id = NEW.session_id;
+        ELSE
+            trigger_event_id := NEW.event_id;
+        END IF;
+
+        IF EXISTS (
+            SELECT 1
+            FROM events.timetable_entries te
+            WHERE te.parent_id IS NULL AND te.type = 2 AND EXISTS (
+                SELECT 1
+                FROM events.contributions c
+                WHERE c.id = te.contribution_id AND (c.session_id IS NOT NULL or c.session_block_id IS NOT NULL)
+            ) AND te.event_id = trigger_event_id
+        ) THEN
+            RAISE EXCEPTION 'Found top-level timetable entry for contribution in a session';
+        END IF;
+
+        IF EXISTS (
+            SELECT 1
+            FROM events.timetable_entries te
+            JOIN events.timetable_entries tep ON (tep.id = te.parent_id)
+            JOIN events.session_blocks sb ON (sb.id = tep.session_block_id)
+            WHERE te.parent_id IS NOT NULL AND te.type = 2 AND EXISTS (
+                SELECT 1
+                FROM events.contributions c
+                WHERE (
+                    c.id = te.contribution_id AND (COALESCE(c.session_id, -1) != COALESCE(sb.session_id, -1) OR
+                    COALESCE(c.session_block_id, -1) != COALESCE(tep.session_block_id, -1))
+                )
+            ) AND te.event_id = trigger_event_id
+        ) THEN
+            RAISE EXCEPTION 'Found child timetable entry for contribution in a session that does not match the parent session';
+        END IF;
+
+        IF EXISTS (
+            SELECT 1
+            FROM events.timetable_entries te
+            JOIN events.timetable_entries tep ON (tep.id = te.parent_id)
+            WHERE te.event_id = trigger_event_id AND te.parent_id IS NOT NULL AND tep.start_dt > te.start_dt
+        ) THEN
+            RAISE EXCEPTION 'Found timetable entry starting before its parent block';
+        END IF;
+
+        IF EXISTS (
+            SELECT 1
+            FROM events.timetable_entries te
+            JOIN events.timetable_entries tep ON (tep.id = te.parent_id)
+            JOIN events.session_blocks bl ON (bl.id = tep.session_block_id)
+            LEFT JOIN events.contributions c ON (c.id = te.contribution_id)
+            LEFT JOIN events.breaks b ON (b.id = te.break_id)
+            WHERE te.event_id = trigger_event_id AND te.parent_id IS NOT NULL AND te.type IN (2, 3) AND
+                  (te.start_dt + COALESCE(c.duration, b.duration)) > (tep.start_dt + bl.duration)
+        ) THEN
+            RAISE EXCEPTION 'Found timetable entry ending after its parent block';
+        END IF;
+
+        RETURN NULL;
+        END;
+        $BODY$
+        LANGUAGE plpgsql;
+    """))
