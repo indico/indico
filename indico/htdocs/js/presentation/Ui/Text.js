5f8ede6a35b8e59c39dd0dc5118557d4ad13d6b7
Florian Vessaz <florian.vessaz@cern.ch>
2016-04-08 13:17:39 +0200

    Treat undefined as empty string in String.format
    This prevents '{0}' being displayed for an unset first name in the
    person link widget.

--- indico/htdocs/js/presentation/Ui/Text.js
+++ indico/htdocs/js/presentation/Ui/Text.js
@@ -7,6 +7,6 @@
 String.prototype.format = function() {
     var args = arguments;
     return this.replace(/{(\d+)}/g, function(match, number) {
-        return typeof args[number] != 'undefined' ? args[number] : '{' + number + '}';
+        return typeof args[number] != 'undefined' ? args[number] : '';
     });
 };
