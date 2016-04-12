indico/indico

commit bdc1688f3c3ff7439378b2f46a29b662ff3b31ac

Fix circular import

indico/modules/events/sessions/operations.py
@@ -24,7 +24,6 @@ from indico.modules.events.logs.models.entries import EventLogRealm, EventLogKin
 from indico.modules.events.sessions import logger
 from indico.modules.events.sessions.models.sessions import Session
 from indico.modules.events.sessions.models.blocks import SessionBlock
-from indico.modules.events.timetable.operations import delete_timetable_entry, update_timetable_entry
 
 
 def create_session(event, data):
@@ -93,6 +92,7 @@ def update_session_block(session_block, data):
 
 
 def delete_session_block(session_block):
+    from indico.modules.events.timetable.operations import delete_timetable_entry, update_timetable_entry
     session_ = session_block.session
     for contribution in session_block.contributions:
         contribution.session_block = None

commit 28ca3f8470b8441d012a4eedd75382adb8410be3

Fixed issue introduced in bdc1688f

indico/modules/events/sessions/operations.py
@@ -81,6 +81,8 @@ def delete_session(event_session):
 
 def update_session_block(session_block, data):
     """Update a session block with data passed in the `data` argument"""
+    from indico.modules.events.timetable.operations import update_timetable_entry
+
     start_dt = data.pop('start_dt', None)
     if start_dt is not None:
         update_timetable_entry(session_block.timetable_entry, {'start_dt': start_dt})
@@ -92,7 +94,7 @@ def update_session_block(session_block, data):
 
 
 def delete_session_block(session_block):
-    from indico.modules.events.timetable.operations import delete_timetable_entry, update_timetable_entry
+    from indico.modules.events.timetable.operations import delete_timetable_entry
     session_ = session_block.session
     for contribution in session_block.contributions:
         contribution.session_block = None