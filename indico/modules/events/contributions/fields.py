c80ca3e52a4716ec4a05ab792e796189da9c0077
Alejandro Avilés <alejandro.aviles@cern.ch>
2016-04-15 13:42:46 +0200

    Don't validate author data on form edition
    This is necessary in order to allow modifying a contribution with
    authors in a meeting. This situation happens if the author was added on
    a conference and then converted to a meeting.

--- indico/modules/events/contributions/fields.py
+++ indico/modules/events/contributions/fields.py
@@ -62,7 +62,8 @@ class ContributionPersonLinkListField(PersonLinkListFieldBase):
             if person_link.person in persons:
                 raise ValueError(_("Person with email '{}' is duplicated").format(person_link.person.email))
             if not self.allow_authors and person_link.author_type != AuthorType.none:
-                raise ValueError(_("Author data received"))
+                if not self.object_data or person_link not in self.object_data:
+                    raise ValueError(_("Author data received"))
             if person_link.author_type == AuthorType.none and not person_link.is_speaker:
                 raise ValueError(_("{} has no role").format(person_link.full_name))
             persons.add(person_link.person)
