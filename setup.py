08648d34fa582f25176a6c65badf28f66cbb3bd9
Adrian Moennich <adrian.moennich@cern.ch>
2016-05-26 17:43:56 +0200

    Add zodb importer for IP networks

--- setup.py
+++ setup.py
@@ -362,6 +362,7 @@ if __name__ == '__main__':
             event_dates_titles = indico_zodbimport.modules.event_dates_titles:EventDatesTitlesImporter
             event_abstracts_zodb = indico_zodbimport.modules.event_abstracts_zodb:EventAbstractZODBPatcher
             event_paper_reviewing = indico_zodbimport.modules.event_paper_reviewing:EventPaperReviewingImporter
+            networks = indico_zodbimport.modules.networks:NetworkImporter
             """,
           zip_safe=False,
           packages=foundPackages,
