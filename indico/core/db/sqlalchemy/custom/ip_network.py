cd2fbe6ac9561264aa32048d3c3fb9c37ece87d4
Adrian Moennich <adrian.moennich@cern.ch>
2016-06-02 11:46:27 +0200

    Ensure PyIPNetwork objects are always sortable
    SQLalchemy breaks if it's not the case (e.g. ipv4 and ipv6 networks in
    the same list)

--- indico/core/db/sqlalchemy/custom/ip_network.py
+++ indico/core/db/sqlalchemy/custom/ip_network.py
@@ -16,9 +16,41 @@
 
 from __future__ import unicode_literals
 
-from ipaddress import ip_network
+import ipaddress
 from sqlalchemy import TypeDecorator
 from sqlalchemy.dialects.postgresql import CIDR
+
+
+class _AlwaysSortableMixin(object):
+    def __lt__(self, other):
+        if self._version != other._version:
+            return self._version < other._version
+        else:
+            return super(_AlwaysSortableMixin, self).__lt__(other)
+
+
+class IPv4Network(_AlwaysSortableMixin, ipaddress.IPv4Network):
+    pass
+
+
+class IPv6Network(_AlwaysSortableMixin, ipaddress.IPv6Network):
+    pass
+
+
+def _ip_network(address, strict=True):
+    # based on `ipaddress.ip_network` but returns always-sortable classes
+    # since sqlalchemy needs to be able to sort all values of a type
+    try:
+        return IPv4Network(address, strict)
+    except (ipaddress.AddressValueError, ipaddress.NetmaskValueError):
+        pass
+
+    try:
+        return IPv6Network(address, strict)
+    except (ipaddress.AddressValueError, ipaddress.NetmaskValueError):
+        pass
+
+    raise ValueError('%r does not appear to be an IPv4 or IPv6 network' % address)
 
 
 class PyIPNetwork(TypeDecorator):
@@ -27,13 +59,13 @@ class PyIPNetwork(TypeDecorator):
     impl = CIDR
 
     def process_bind_param(self, value, dialect):
-        return unicode(ip_network(value)) if value is not None else None
+        return unicode(_ip_network(value)) if value is not None else None
 
     def process_result_value(self, value, dialect):
-        return ip_network(value) if value is not None else None
+        return _ip_network(value) if value is not None else None
 
     def coerce_set_value(self, value):
-        return ip_network(value) if value is not None else None
+        return _ip_network(value) if value is not None else None
 
     def alembic_render_type(self, autogen_context):
         imports = autogen_context['imports']
