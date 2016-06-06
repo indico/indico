bdc47de1f761425d6f4a3aae20d504135f4b9fa6
Ilias Trichopoulos <ilias.trichopoulos@cern.ch>
2016-06-06 17:23:17 +0200

    Category picker: List subcategories

--- indico/htdocs/js/indico/jquery/categorypicker.js
+++ indico/htdocs/js/indico/jquery/categorypicker.js
@@ -26,6 +26,58 @@
             var self = this;
             var element = self.element;
             var opt = self.options;
+
+            self.searchInput = $('<input>', {
+                class: 'js-search-category',
+                type: 'text',
+                placeholder: $T.gettext("Search")
+            });
+            self.categoryList = $('<ul>').addClass('group-list');
+            self.categoryNavigator = $('<div>', {
+                class: 'category-navigator i-box just-group-list with-hover-effect'
+            })
+            .append($('<div>').addClass('i-box-content'))
+            .append(self.categoryList);
+
+            this.buildCategoryList(self.element, opt.initialCateg, self.categoryList);
+
+            self.element.append(self.searchInput).append(self.categoryNavigator);
+        },
+
+        buildCategoryList: function($element, categoryId, $categoryList) {
+            $.ajax({
+                url: $element.data('href'),
+                method: 'POST',
+                dataType: 'json',
+                data: {categoryId: categoryId},
+                error: function(data) {
+                    handleAjaxError(data);
+                },
+                success: function(data) {
+                    if (data.category) {
+                        var category = $('<li>', {
+                            text: data.category.title,
+                            class: 'item parent-category',
+                            data: {
+                                id: data.category.id
+                            }
+                        });
+                        $categoryList.append(category);
+                    }
+                    if (data.subcategories) {
+                        for (var i = 0; i < data.subcategories.length; i++) {
+                            var subcategory = $('<li>', {
+                                text: data.subcategories[i].title,
+                                class: 'item subcategory',
+                                data: {
+                                    id: data.subcategories[i].id
+                                }
+                            });
+                            $categoryList.append(subcategory);
+                        };
+                    }
+                }
+            });
         }
     });
 })(jQuery);
