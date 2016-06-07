b512794d8e06271254aabded854acf907d9d0e8b
Florian Vessaz <florian.vessaz@cern.ch>
2016-06-07 10:46:53 +0200

    Adapt redirection after event deletion

--- indico/modules/events/management/controllers.py
+++ indico/modules/events/management/controllers.py
@@ -51,7 +51,7 @@ class RHDeleteEvent(RHConferenceModifBase):
         flash(_('Event "{}" successfully deleted.').format(self._conf.title), 'success')
 
         if category:
-            redirect_url = url_for('category_mgmt.categoryModification', category)
+            redirect_url = url_for('categories.manage', category)
         else:
             redirect_url = url_for('misc.index')
 
