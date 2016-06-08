7f8cd853bbe497bba594fba187c3785de427aa02
Ilias Trichopoulos <ilias.trichopoulos@cern.ch>
2016-06-08 11:51:17 +0200

    Category picker: refactor

--- indico/htdocs/js/indico/jquery/categorypicker.js
+++ indico/htdocs/js/indico/jquery/categorypicker.js
@@ -19,7 +19,7 @@
     $.widget("indico.categorypicker", {
 
         options: {
-            initialCateg: 0
+            categoryId: 0
         },
 
         _create: function() {
@@ -27,107 +27,124 @@
             var element = self.element;
             var opt = self.options;
 
-            self.searchInput = $('<input>', {
+            self.$searchInput = $('<input>', {
                 class: 'js-search-category',
                 type: 'text',
                 placeholder: $T.gettext("Search")
             });
-            self.categoryList = $('<ul>').addClass('group-list');
-            self.categoryNavigator = $('<div>')
-            .addClass('category-navigator i-box just-group-list with-hover-effect fixed-height')
-            .append($('<div>').addClass('i-box-content'))
-            .append(self.categoryList);
+            self.$categoryList = $('<ul>', {class: 'group-list'});
+            self.$categoryNavigator = $('<div>', {class: 'category-navigator i-box just-group-list with-hover-effect fixed-height'})
+                .append($('<div>', {class: 'i-box-content'})
+                    .append(self.$categoryList));
+            self.goToCategory(opt.categoryId);
+            self.element.append(self.$searchInput).append(self.$categoryNavigator);
+        },
 
-            buildCategoryList(opt.initialCateg, self.categoryList);
+        _buildBreadcrumbs: function(path) {
+            var self = this;
+            var $breadcrumbs = $('<ul>', {class: 'breadcrumbs'});
 
-            self.element.append(self.searchInput).append(self.categoryNavigator);
+            _.each(path, function(category) {
+                $breadcrumbs.append($('<li>')
+                    .append($('<a>', {
+                        text: category.title,
+                        data: {id: category.id},
+                        title:  $T.gettext("Go to: {0}".format(category.title)),
+                        href: '#'
+                    }).on('click', function(evt) {
+                        evt.preventDefault()
+                        self.goToCategory($(this).data('id'));
+                    }))
+                );
+            });
 
-            function goToCategory(categoryId, $categoryList) {
-                $('.category-navigator .item').animate({
-                    height: 0,
-                    paddingTop: 0,
-                    paddingBottom: 0,
-                    border: 0
-                }, {
-                    complete: function() {
-                        $(this).remove();
-                    }
-                }).find('.title').fadeOut();
-                buildCategoryList(categoryId, $categoryList);
+            return $breadcrumbs;
+        },
+
+        _buildCurrentCategory: function(category) {
+            var self = this;
+
+            var $category = $('<div>', {
+                class: 'item current-category',
+                data: {id: category.id}
+            });
+            var $titleWrapper = $('<div>', {class: 'title-wrapper'})
+            $titleWrapper.append($('<span>', {
+                class: 'title',
+                text: category.title,
+            }));
+            $titleWrapper.append(self._buildBreadcrumbs(category.path));
+            $category.append($titleWrapper);
+            if (category.path.length) {
+                var parent = _.last(category.path);
+                var $buttonWrapper = $('<div>', {class: 'button-wrapper'});
+                $buttonWrapper.append($('<a>', {
+                    class: 'icon-arrow-up navigate-up',
+                    title: $T.gettext("Go to parent: {0}".format(parent.title))
+                }).on('click', function() {
+                    self.goToCategory(parent.id);
+                }));
+                $category.append($buttonWrapper);
             }
 
-            function buildCategoryList(categoryId, $categoryList) {
-                $.ajax({
-                    url: build_url(Indico.Urls.Categories.info, {categId: categoryId}),
-                    dataType: 'json',
-                    error: function(data) {
-                        handleAjaxError(data);
-                    },
-                    success: function(data) {
-                        if (data.category) {
-                            var category = $('<li>', {
-                                class: 'item parent-category',
-                                data: {
-                                    id: data.category.id
-                                }
-                            });
-                            var titleWrapper = $('<div>').addClass('title-wrapper');
-                            titleWrapper.append($('<span>', {
-                                class: 'title',
-                                text: data.category.title,
-                            }));
-                            var breadcrumb = $('<ul>').addClass('breadcrumb');
-                            _.each(data.category.breadcrumb, function(category) {
-                                breadcrumb.append($('<li>')
-                                    .append($('<a>', {
-                                        text: category.title,
-                                        data: {
-                                            id: category.id
-                                        },
-                                        title:  $T.gettext("Go to: {0}".format(category.title)),
-                                        href: '#'
-                                    }).on('click', function(evt) {
-                                        evt.preventDefault()
-                                        goToCategory($(this).data('id'), $categoryList);
-                                    }))
-                                );
-                            });
-                            titleWrapper.append(breadcrumb);
-                            category.append(titleWrapper);
-                            if (data.category.breadcrumb.length) {
-                                var parent = _.last(data.category.breadcrumb);
+            return $category;
+        },
 
-                                var buttonWrapper = $('<div>').addClass('button-wrapper');
-                                buttonWrapper.append($('<a>', {
-                                    class: 'icon-arrow-up navigate-up',
-                                    title: $T.gettext("Go to parent: {0}".format(parent.title))
-                                }).on('click', function() {
-                                    goToCategory(parent.id, $categoryList);
-                                }));
-                                category.append(buttonWrapper);
-                            }
-                            $categoryList.append(category);
-                        }
-                        if (data.subcategories) {
-                            _.each(data.subcategories, function(subcat) {
-                                var subcategory = $('<li>', {
-                                    class: 'item subcategory',
-                                    data: {
-                                        id: subcat.id
-                                    }
-                                }).append($('<span>', {
-                                    class: 'title',
-                                    text: subcat.title,
-                                }));
-                                subcategory.on('click', function() {
-                                    goToCategory($(this).data('id'), $categoryList);
-                                });
-                                $categoryList.append(subcategory);
-                            });
-                        }
-                    }
+        _buildSubcategory: function(subcategory) {
+            var self = this;
+
+            var $subcategory = $('<li>', {
+                class: 'item subcategory',
+                data: {id: subcategory.id}
+            }).append($('<span>', {
+                class: 'title',
+                text: subcategory.title,
+            })).on('click', function() {
+                self.goToCategory($(this).data('id'));
+            });
+
+            return $subcategory;
+        },
+
+        _renderNavigator: function(data) {
+            var self = this;
+
+            if (data.category) {
+                self.$categoryList.before(self._buildCurrentCategory(data.category));
+            }
+            if (data.subcategories) {
+                _.each(data.subcategories, function(subcategory) {
+                    self.$categoryList.append(self._buildSubcategory(subcategory));
                 });
             }
+        },
+
+        goToCategory: function(id) {
+            var self = this;
+
+            $('.category-navigator .item').animate({
+                height: 0,
+                paddingTop: 0,
+                paddingBottom: 0,
+                border: 0
+            }, {
+                complete: function() {
+                    $(this).remove();
+                }
+            }).find('.title').fadeOut();
+
+            $.ajax({
+                url: build_url(Indico.Urls.Categories.info, {categId: id}),
+                dataType: 'json',
+                error: function(data) {
+                    handleAjaxError(data);
+                },
+                success: function(data) {
+                    if (data) {
+                        self._renderNavigator(data);
+                    }
+                }
+            });
         }
     });
 })(jQuery);
