a8d86af40029d359988379df32fa148ea7d1598f
Michal Kolodziejski <michal.kolodziejski@cern.ch>
2016-06-13 13:11:34 +0200

    Add operations.py to categories module

---
+++ indico/modules/categories/operations.py
@@ -0,0 +1,28 @@
+# This file is part of Indico.
+# Copyright (C) 2002 - 2016 European Organization for Nuclear Research (CERN).
+#
+# Indico is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+#
+# Indico is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Indico; if not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+
+from flask import session
+
+from indico.core.db import db
+from indico.modules.categories import logger
+
+
+def update_category(category, data):
+    category.populate_from_dict(data)
+    db.session.flush()
+    logger.info('Category %s updated by %s', category, session.user)
