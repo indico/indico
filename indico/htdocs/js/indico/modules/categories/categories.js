5ce0e93aee7f95142b627f1fddd7bea8c03b1781
Alejandro Avilés <alejandro.aviles@cern.ch>
2016-06-15 16:25:55 +0200

    Move category JS into same dir

--- indico/htdocs/js/indico/modules/categories/categories.js
+++ indico/htdocs/js/indico/modules/categories/categories.js
@@ -28,4 +28,87 @@
             $('<div>').categorypicker({openInDialog: true});
         });
     }
+
+    global.setupCategoryTable = function setupCategoryTable() {
+        var $table = $('table.categories-management');
+        var $tbody = $table.find('tbody');
+        var categoryRowSelector = 'tr[data-category-id]';
+
+        $('.js-sort-categories').on('click', function() {
+            var currentOrder = getSortedCategories();
+            function undo() {
+                restoreCategoryOrder(currentOrder);
+                return setOrderAjax(currentOrder);
+            }
+            sortCategories();
+            setOrderAjax(getSortedCategories());
+            cornerMessage({
+                actionLabel: $T.gettext("Undo"),
+                actionCallback: undo,
+                feedbackMessage: $T.gettext("The category order has been restored."),
+                duration: 10000,
+                message: $T.gettext("The category list has been sorted.")
+            });
+        });
+
+        $('.js-delete-category').on('indico:confirmed', function(evt) {
+            evt.preventDefault();
+            var $this = $(this);
+            $.ajax({
+                url: $this.data('href'),
+                method: 'POST',
+                error: handleAjaxError,
+                success: function(data) {
+                    $this.closest(categoryRowSelector).remove();
+                    if (data.is_empty) {
+                        $('.banner .js-delete-category').removeClass('disabled')
+                            .attr('title', $T.gettext("Delete category"));
+                    }
+                }
+            });
+        });
+
+        $tbody.sortable({
+            axis: 'y',
+            containment: 'parent',
+            cursor: 'move',
+            handle: '.js-handle',
+            items: '> tr',
+            tolerance: 'pointer',
+            update: function() {
+                setOrderAjax(getSortedCategories());
+            }
+        });
+
+        function getSortedCategories() {
+            return $tbody.find(categoryRowSelector).map(function() {
+                return $(this).data('category-id');
+            }).toArray();
+        }
+
+        function restoreCategoryOrder(order) {
+            $.each(order, function(index, id) {
+                $tbody.find('[data-category-id=' + id + ']').detach().appendTo($tbody);
+            });
+        }
+
+        function sortCategories() {
+            $tbody.find(categoryRowSelector).sort(function(a, b) {
+                return $(a).data('category-title').localeCompare($(b).data('category-title'));
+            }).detach().appendTo($tbody);
+        }
+
+        function setOrderAjax(order) {
+            return $.ajax({
+                url: $table.data('sort-url'),
+                method: 'POST',
+                data: JSON.stringify({
+                    categories: order
+                }),
+                dataType: 'json',
+                contentType: 'application/json',
+                error: handleAjaxError
+            });
+        }
+    };
 })(window);
