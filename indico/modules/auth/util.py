0ea4eadab80ed58d6164b1358f285e432b9e402c
Michal Kolodziejski <michal.kolodziejski@cern.ch>
2016-06-22 13:30:17 +0200

    Move send_confirmation to auth/util.py

--- indico/modules/auth/util.py
+++ indico/modules/auth/util.py
@@ -16,12 +16,16 @@
 
 from __future__ import unicode_literals
 
-from flask import session, redirect, request
+from flask import session, redirect, request, flash
 from werkzeug.datastructures import MultiDict
 
 from indico.core.config import Config
+from indico.core.notifications import make_email, send_email
+from indico.modules.users.models.users import User
+from indico.util.i18n import _
 from indico.util.signing import secure_serializer
 from indico.web.flask.util import url_for
+from indico.web.flask.templating import get_template_module
 
 
 def save_identity_info(identity_info, user):
@@ -89,3 +93,38 @@ def url_for_register(next_url=None, email=None):
 
     external_url = Config.getInstance().getExternalRegistrationURL()
     return external_url or url_for_login()
+
+
+def send_confirmation(email, salt, endpoint, template, template_args=None, url_args=None, data=None,
+                      redirect_endpoint=None):
+    template_args = template_args or {}
+    url_args = url_args or {}
+    token = secure_serializer.dumps(data or email, salt=salt)
+    url = url_for(endpoint, token=token, _external=True, _secure=True, **url_args)
+    template_module = get_template_module(template, email=email, url=url, **template_args)
+    send_email(make_email(email, template=template_module))
+
+    flash(_('We have sent you a verification email. Please check your mailbox within the next hour and open '
+            'the link in that email.'))
+
+    if redirect_endpoint:
+        return redirect(url_for(redirect_endpoint, **url_args))
+    else:
+        return redirect(url_for(endpoint, **url_args))
+
+
+def send_verification_email(email, template, salt, endpoint, data=None, url_args=None):
+    token = secure_serializer.dumps(data or email, salt=salt)
+    url = url_for(endpoint, email=email, token=token, _external=True, _secure=True)
+    template = get_template_module(template, email=email, url=url)
+    send_email(make_email(email, template=template))
+
+
+def notify_of_registration_request_approval(user, endpoint):
+    template = get_template_module('auth/emails/registration_request_approved.txt', recipient=user)
+    send_email(make_email(user.email, template=template))
+
+
+def send_notification_to_admins(template):
+    for admin in User.find(is_admin=True):
+        send_email(make_email(admin.email, template=template))
