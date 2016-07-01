86418046c75a11194279cfd071d5337bb909f4f3
Adrian Moennich <adrian.moennich@cern.ch>
2016-07-01 13:03:17 +0200

    Add unit tests for effective protection mode

--- indico/modules/categories/models/categories_test.py
+++ indico/modules/categories/models/categories_test.py
@@ -15,8 +15,10 @@
 # along with Indico; if not, see <http://www.gnu.org/licenses/>.
 
 import pytest
+from sqlalchemy.orm import undefer
 
 from indico.core.db.sqlalchemy.protection import ProtectionMode
+from indico.modules.categories import Category
 
 
 @pytest.mark.parametrize(('protection_mode', 'creation_restricted', 'acl', 'allowed'), (
@@ -42,3 +44,43 @@ def test_can_create_events(dummy_category, dummy_user, protection_mode, creation
 
 def test_can_create_events_no_user(dummy_category):
     assert not dummy_category.can_create_events(None)
+
+
+def test_effective_protection_mode(db):
+    def _cat(id_, protection_mode=ProtectionMode.inheriting, children=None):
+        return Category(id=id_, title='cat-{}'.format(id_), protection_mode=protection_mode, children=children or [])
+    root = Category.get_root()
+    root.protection_mode = ProtectionMode.protected
+    root.children = [
+        _cat(1),
+        _cat(2, ProtectionMode.public, children=[
+            _cat(3, children=[
+                _cat(4, ProtectionMode.inheriting),
+                _cat(5, ProtectionMode.public),
+                _cat(6, ProtectionMode.protected),
+            ]),
+            _cat(7, ProtectionMode.protected, children=[
+                _cat(8, ProtectionMode.inheriting),
+                _cat(9, ProtectionMode.public),
+                _cat(10, ProtectionMode.protected),
+            ]),
+            _cat(11)
+        ])
+    ]
+    db.session.add(root)
+    db.session.flush()
+    data = {c.id: c.effective_protection_mode for c in Category.query.options(undefer('effective_protection_mode'))}
+    assert data == {
+        0: ProtectionMode.protected,
+        1: ProtectionMode.protected,
+        2: ProtectionMode.public,
+        3: ProtectionMode.public,
+        4: ProtectionMode.public,
+        5: ProtectionMode.public,
+        6: ProtectionMode.protected,
+        7: ProtectionMode.protected,
+        8: ProtectionMode.protected,
+        9: ProtectionMode.public,
+        10: ProtectionMode.protected,
+        11: ProtectionMode.public
+    }
