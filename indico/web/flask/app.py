5c4d70fc45222f085eb4f2d7f9b7f81e06027c23
Florian Vessaz <florian.vessaz@cern.ch>
2016-07-01 09:04:05 +0200

    Add format_number filter

--- indico/web/flask/app.py
+++ indico/web/flask/app.py
@@ -188,6 +188,7 @@ def setup_jinja(app):
     app.add_template_filter(EnsureUnicodeExtension.wrap_func(date_time_util.format_datetime))
     app.add_template_filter(EnsureUnicodeExtension.wrap_func(date_time_util.format_human_date))
     app.add_template_filter(EnsureUnicodeExtension.wrap_func(date_time_util.format_timedelta))
+    app.add_template_filter(EnsureUnicodeExtension.wrap_func(date_time_util.format_number))
     # Filters (new ones returning unicode)
     app.add_template_filter(date_time_util.format_human_timedelta)
     app.add_template_filter(lambda d: Markup(html_params(**d)), 'html_params')
