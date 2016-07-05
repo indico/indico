a4abb196e51dd4cc9e9126048b618685108b0e74
Florian Vessaz <florian.vessaz@cern.ch>
2016-07-05 15:08:18 +0200

    Don't set per-event setting when enabled globally
    Otherwise enabling "Show past events" then displaying an event then
    disabling "Show past events" and loading the event again would result on
    the past events being loaded.

--- indico/htdocs/js/indico/modules/categories/display.js
+++ indico/htdocs/js/indico/modules/categories/display.js
@@ -1,4 +1,4 @@
-function setupCategoryDisplayEventList(show_past_events) {
+function setupCategoryDisplayEventList(showPastEvents) {
     'use strict';
 
     var $eventList = $('.event-list');
@@ -8,8 +8,8 @@ function setupCategoryDisplayEventList(show_past_events) {
     setupToggleEventListButton($futureEvents);
     setupToggleEventListButton($pastEvents, onTogglePastEvents);
 
-    if (show_past_events) {
-        $pastEvents.find('.js-toggle-list').first().trigger('click');
+    if (showPastEvents) {
+        $pastEvents.find('.js-toggle-list').first().trigger('click', true);
     }
 
     function onTogglePastEvents(shown) {
@@ -36,9 +36,10 @@ function setupCategoryDisplayEventList(show_past_events) {
         }
         displaySpinner(false);
 
-        $wrapper.find('.js-toggle-list').on('click', function(evt) {
+        $wrapper.find('.js-toggle-list').on('click', function(evt, triggeredAutomatically) {
             evt.preventDefault();
-            if ($content.is(':empty')) {
+            var isEmpty = $content.is(':empty');
+            if (isEmpty) {
                 displaySpinner(true);
                 $.ajax({
                     url: $content.data('event-list-url'),
@@ -52,18 +53,18 @@ function setupCategoryDisplayEventList(show_past_events) {
                         $content.show();
                         updateMessage(true);
                         displaySpinner(false);
-                        callback(true);
                     }
                 });
             } else if ($content.is(':visible')) {
                 $content.hide();
                 updateMessage(false);
-                callback(false);
 
             } else {
                 $content.show();
                 updateMessage(true);
-                callback(true);
+            }
+            if (!triggeredAutomatically) {
+                callback(isEmpty);
             }
         });
     }
