6c86c10ec73da3322652ae618c6442fd9d27032a
Adrian Moennich <adrian.moennich@cern.ch>
2016-07-18 17:37:24 +0200

    Fix DeprecationWarnings in bootstrap

--- indico/modules/bootstrap/forms.py
+++ indico/modules/bootstrap/forms.py
@@ -14,10 +14,9 @@
 # You should have received a copy of the GNU General Public License
 # along with Indico; if not, see <http://www.gnu.org/licenses/>.
 
-from operator import itemgetter
-
-from wtforms import validators, TextField, SelectField, BooleanField
+from wtforms import StringField, BooleanField
 from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired, Email
 
 from indico.modules.auth.forms import LocalRegistrationForm
 from indico.util.i18n import _
@@ -26,12 +25,10 @@ from indico.web.forms.widgets import SwitchWidget
 
 
 class BootstrapForm(LocalRegistrationForm):
-    first_name = TextField('First Name', [validators.Required()])
-    last_name = TextField('Last Name', [validators.Required()])
-    email = EmailField(_('Email address'), [validators.Required()])
-    affiliation = TextField('Affiliation', [validators.Required()])
+    first_name = StringField('First Name', [DataRequired()])
+    last_name = StringField('Last Name', [DataRequired()])
+    email = EmailField(_('Email address'), [DataRequired()])
+    affiliation = StringField('Affiliation', [DataRequired()])
     enable_tracking = BooleanField('Join the community', widget=SwitchWidget())
-    contact_name = TextField('Contact Name',
-                             [UsedIfChecked('enable_tracking'), validators.Required()])
-    contact_email = EmailField('Contact Email Address',
-                               [UsedIfChecked('enable_tracking'), validators.Required(), validators.Email()])
+    contact_name = StringField('Contact Name', [UsedIfChecked('enable_tracking'), DataRequired()])
+    contact_email = EmailField('Contact Email Address',  [UsedIfChecked('enable_tracking'), DataRequired(), Email()])
