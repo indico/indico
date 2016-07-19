7e0efc2e7d59f831b3a155087aba31235d5566a6
Adrian Moennich <adrian.moennich@cern.ch>
2016-07-19 13:42:18 +0200

    Add a bunch of missing unicode_literals imports

--- indico/modules/events/payment/views.py
+++ indico/modules/events/payment/views.py
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with Indico; if not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import unicode_literals
+
 from MaKaC.webinterface.pages.admins import WPAdminsBase
 from MaKaC.webinterface.pages.base import WPJinjaMixin
 from MaKaC.webinterface.pages.conferences import WPConferenceDefaultDisplayBase, WPConferenceModifBase
