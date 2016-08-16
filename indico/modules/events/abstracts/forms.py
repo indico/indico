dca2bf7b2b3b96dc0b4c0515d15a3c3052ae3881
Adrian Moennich <adrian.moennich@cern.ch>
2016-08-16 11:53:32 +0200

    Use WMD & MathJax in BOA config form

--- indico/modules/events/abstracts/forms.py
+++ indico/modules/events/abstracts/forms.py
@@ -39,7 +39,7 @@ class AbstractContentSettingsForm(IndicoForm):
 class BOASettingsForm(IndicoForm):
     """Settings form for the 'Book of Abstracts'"""
 
-    extra_text = IndicoMarkdownField(_('Additional text'))
+    extra_text = IndicoMarkdownField(_('Additional text'), wmd=True)
     sort_by = SelectField(_('Sort by'), [DataRequired()], choices=[
         ('id', _('ID')),
         ('title', _('Title')),
