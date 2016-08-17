934af99c940b8e297f44a07132bda52d57fddb4c
Adrian Moennich <adrian.moennich@cern.ch>
2016-08-17 11:52:23 +0200

    Remove useless kwarg from CategoryField
    Setting an ID that's not 0 at import time makes no sense; this attribute
    should always be set on the instance after creating a form.

--- indico/web/forms/fields.py
+++ indico/web/forms/fields.py
@@ -252,9 +252,6 @@ class IndicoPasswordField(PasswordField):
 class CategoryField(HiddenField):
     """WTForms field that lets you select a category.
 
-    :param category_id: The ID of the category to initialize the category
-                        navigator with. The root category is used in case the
-                        category_id is not provided.
     :param allow_events: Whether to allow selecting a category that
                          contains events.
     :param allow_subcats: Whether to allow selecting a category that
@@ -264,7 +261,7 @@ class CategoryField(HiddenField):
     widget = JinjaWidget('forms/category_picker_widget.html')
 
     def __init__(self, *args, **kwargs):
-        self.navigator_category_id = kwargs.pop('navigator_category_id', 0)
+        self.navigator_category_id = 0
         self.allow_events = kwargs.pop('allow_events', True)
         self.allow_subcats = kwargs.pop('allow_subcats', True)
         super(CategoryField, self).__init__(*args, **kwargs)
