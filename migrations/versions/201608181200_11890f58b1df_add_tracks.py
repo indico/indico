1a4c44773719918f2043ad11ac5c87ee50198880
Adrian Moennich <adrian.moennich@cern.ch>
2016-08-17 17:50:51 +0200

    Fix incorrect indexes in alembic revision

--- migrations/versions/201608181200_11890f58b1df_add_tracks.py
+++ migrations/versions/201608181200_11890f58b1df_add_tracks.py
@@ -17,9 +17,9 @@ down_revision = '4d4b95748173'
 def upgrade():
     op.create_table(
         'tracks',
-        sa.Column('id', sa.Integer(), nullable=False, index=True),
+        sa.Column('id', sa.Integer(), nullable=False),
         sa.Column('title', sa.String(), nullable=False),
-        sa.Column('event_id', sa.Integer(), nullable=False),
+        sa.Column('event_id', sa.Integer(), nullable=False, index=True),
         sa.Column('position', sa.Integer(), nullable=False),
         sa.Column('description', sa.Text(), nullable=False),
         sa.ForeignKeyConstraint(['event_id'], ['events.events.id']),
