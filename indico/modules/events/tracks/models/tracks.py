3e72af2d4937ac7875b2c230a14c83fcd941b4e2
Alejandro Avilés <alejandro.aviles@cern.ch>
2016-08-19 15:35:31 +0200

    Add abstract reviewers ACL and per-track table

--- indico/modules/events/tracks/models/tracks.py
+++ indico/modules/events/tracks/models/tracks.py
@@ -61,6 +61,16 @@ class Track(DescriptionMixin, db.Model):
             order_by=position
         )
     )
+    abstract_reviewers = db.relationship(
+        'User',
+        secondary='events.track_abstract_reviewers',
+        lazy=True,
+        collection_class=set,
+        backref=db.backref(
+            'reviewer_for_tracks',
+            lazy='dynamic'
+        )
+    )
 
     @return_ascii
     def __repr__(self):
