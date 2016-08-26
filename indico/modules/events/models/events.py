a649b8e40dfee4062d413b9ba71d49089fa801db
Adrian Moennich <adrian.moennich@cern.ch>
2016-08-26 14:06:05 +0200

    Update outdated backref comment

--- indico/modules/events/models/events.py
+++ indico/modules/events/models/events.py
@@ -336,8 +336,8 @@ class Event(SearchableTitleMixin, DescriptionMixin, LocationMixin, ProtectionMan
     )
 
     # relationship backrefs:
+    # - abstract_email_templates (AbstractEmailTemplate.event_new)
     # - abstracts (Abstract.event_new)
-    # - abstract_notification_templates (AbstractEmailTemplate.event_new)
     # - agreements (Agreement.event_new)
     # - all_attachment_folders (AttachmentFolder.event_new)
     # - all_legacy_attachment_folder_mappings (LegacyAttachmentFolderMapping.event_new)
