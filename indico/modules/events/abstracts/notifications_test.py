8907ac1bbb89a804df8b9c25a981bae840987340
Pedro Ferreira <pedro.ferreira@cern.ch>
2016-09-20 18:42:56 +0200

    Unit tests for notifications

--- indico/modules/events/abstracts/notifications_test.py
+++ indico/modules/events/abstracts/notifications_test.py
@@ -16,15 +16,23 @@
 
 from __future__ import unicode_literals
 
+import re
+
 import pytest
 
 from indico.modules.events.abstracts.models.abstracts import Abstract, AbstractState
 from indico.modules.events.abstracts.models.persons import AbstractPersonLink
-from indico.modules.events.abstracts.models.email_templates import AbstractEmailTemplate
 from indico.modules.events.abstracts.notifications import send_abstract_notifications
 from indico.modules.events.abstracts.util import build_default_email_template
+from indico.modules.events.contributions.models.types import ContributionType
+from indico.modules.events.tracks.models.tracks import Track
 from indico.modules.events.contributions.models.persons import AuthorType
 from indico.modules.users.models.users import UserTitle
+
+
+def text_compare(v1, v2):
+    """Compare two strings, ignoring white space and line breaks."""
+    return re.sub(r'\s', '', v1) == re.sub(r'\s', '', v2)
 
 
 @pytest.fixture
@@ -40,12 +48,31 @@ def create_dummy_person(db, create_event_person):
 
 
 @pytest.fixture
-def dummy_abstract(db, dummy_contribution, dummy_user, create_dummy_person):
+def create_dummy_track(db):
+    def _create(event):
+        track = Track(title='Dummy Track', event_new=event)
+        db.session.add(track)
+        db.session.flush()
+        return track
+    return _create
+
+
+@pytest.fixture
+def create_dummy_contrib_type(db):
+    def _create(event, name='Poster'):
+        contrib_type = ContributionType(name=name, event_new=event)
+        db.session.add(contrib_type)
+        db.session.flush()
+        return contrib_type
+    return _create
+
+
+@pytest.fixture
+def dummy_abstract(db, dummy_event_new, dummy_user, create_dummy_person):
     abstract = Abstract(friendly_id=314,
                         title="Broken Symmetry and the Mass of Gauge Vector Mesons",
-                        event_new=dummy_contribution.event_new,
+                        event_new=dummy_event_new,
                         submitter=dummy_user,
-                        contribution=dummy_contribution,
                         locator={'confId': -314, 'abstract_id': 1234},
                         judgment_comment='Vague but interesting!')
 
@@ -74,16 +101,151 @@ def create_email_template(db):
     return _create
 
 
-def test_accept_abstract(mocker, dummy_abstract, create_email_template):
-    send_email = mocker.patch('indico.modules.events.abstracts.notifications.send_email')
+@pytest.fixture
+def abstract_objects(dummy_abstract, create_dummy_contrib_type, create_dummy_track):
     event = dummy_abstract.event_new
+    return event, dummy_abstract, create_dummy_track(event), create_dummy_contrib_type(event)
+
+
+@pytest.mark.usefixtures('request_context')
+def test_abstract_notification(mocker, abstract_objects, create_email_template):
+    send_email = mocker.patch('indico.modules.events.abstracts.notifications.send_email')
+
+    event, abstract, track, contrib_type = abstract_objects
 
     event.abstract_email_templates.append(
-        create_email_template(event, 0, 'accept', 'accept', [{'state': 'accepted'}], True))
+        create_email_template(event, 0, 'accept', 'accepted', [{'state': [AbstractState.accepted.value]}], True))
 
-    send_abstract_notifications(dummy_abstract)
+    send_abstract_notifications(abstract)
     assert send_email.call_count == 0
 
-    dummy_abstract.state = AbstractState.accepted
-    send_abstract_notifications(dummy_abstract)
+    abstract.accepted_contrib_type = contrib_type
+    abstract.state = AbstractState.accepted
+    send_abstract_notifications(abstract)
+    assert send_email.call_count == 1
+
+
+@pytest.mark.usefixtures('request_context')
+def test_notification_rules(mocker, abstract_objects, create_email_template):
+    send_email = mocker.patch('indico.modules.events.abstracts.notifications.send_email')
+
+    event, abstract, track, contrib_type = abstract_objects
+    event.abstract_email_templates.append(
+        create_email_template(event, 0, 'merge', 'merged poster for track', [
+            {'state': [AbstractState.merged.value], 'track': [track.id]}
+        ], True))
+
+    send_abstract_notifications(abstract)
     assert send_email.call_count == 0
+
+    abstract.state = AbstractState.accepted
+    abstract.accepted_track = track
+    send_abstract_notifications(abstract)
+    assert send_email.call_count == 0
+
+    abstract.state = AbstractState.merged
+    abstract.accepted_track = None
+    abstract.submitted_for_tracks = {track}
+    send_abstract_notifications(abstract)
+    assert send_email.call_count == 1
+
+
+@pytest.mark.usefixtures('request_context')
+def test_notification_several_conditions(db, mocker, abstract_objects, create_email_template, create_dummy_track,
+                                         create_dummy_contrib_type):
+    event, abstract, track, contrib_type = abstract_objects
+    event.abstract_email_templates = [
+        create_email_template(event, 0, 'accept', 'accepted', [
+            {'state': [AbstractState.accepted.value], 'track': [track.id], 'contribution_type': [contrib_type.id]},
+            {'state': [AbstractState.accepted.value], 'contribution_type': []}
+        ], True)
+    ]
+
+    send_email = mocker.patch('indico.modules.events.abstracts.notifications.send_email')
+    abstract.state = AbstractState.accepted
+    abstract.accepted_track = track
+    send_abstract_notifications(abstract)
+    assert send_email.call_count == 1
+
+    send_email.reset_mock()
+    abstract.accepted_contrib_type = contrib_type
+    send_abstract_notifications(abstract)
+    assert send_email.call_count == 1
+
+    send_email.reset_mock()
+    abstract.accepted_track = create_dummy_track(event)
+    abstract.accepted_contrib_type = create_dummy_contrib_type(event, name='Presentation')
+    db.session.flush()
+    send_abstract_notifications(abstract)
+    assert send_email.call_count == 0
+
+
+@pytest.mark.usefixtures('request_context')
+def test_notification_any_conditions(mocker, abstract_objects, create_email_template):
+    event, abstract, track, contrib_type = abstract_objects
+    event.abstract_email_templates = [
+        create_email_template(event, 0, 'accept', 'accepted', [
+            {'state': [AbstractState.accepted.value]}
+        ], True)
+    ]
+
+    send_email = mocker.patch('indico.modules.events.abstracts.notifications.send_email')
+    abstract.state = AbstractState.accepted
+    abstract.accepted_track = track
+    send_abstract_notifications(abstract)
+    assert send_email.call_count == 1
+
+
+@pytest.mark.usefixtures('request_context')
+def test_notification_stop_on_match(mocker, abstract_objects, create_email_template):
+    event, abstract, track, contrib_type = abstract_objects
+    event.abstract_email_templates = [
+        create_email_template(event, 0, 'merge', 'merged poster', [
+            {'state': [AbstractState.accepted.value]}
+        ], False),
+        create_email_template(event, 0, 'merge', 'merged poster 2', [
+            {'state': [AbstractState.accepted.value]}
+        ], True)
+    ]
+
+    send_email = mocker.patch('indico.modules.events.abstracts.notifications.send_email')
+    abstract.state = AbstractState.accepted
+    send_abstract_notifications(abstract)
+    assert send_email.call_count == 2
+
+    send_email.reset_mock()
+    event.abstract_email_templates[0].stop_on_match = True
+    send_abstract_notifications(abstract)
+    assert send_email.call_count == 1
+
+
+@pytest.mark.usefixtures('request_context')
+def test_email_content(monkeypatch, abstract_objects, create_email_template):
+    def _mock_send_email(email, event, user):
+        assert email['subject'] == '[Indico] Abstract Acceptance notification (#314)'
+        assert text_compare(email['body'], """
+            Dear Guinea Pig,
+
+            We're pleased to announce that your abstract "Broken Symmetry and the Mass of Gauge Vector Mesons" with ID
+            #314 has been accepted in track "Dummy Track" (Poster).
+
+            Kind regards,
+            The organizers of dummy#0
+
+            --
+            Indico :: Email Notifier
+            http://localhost/
+        """)
+        assert event == ev
+
+    ev, abstract, track, contrib_type = abstract_objects
+    monkeypatch.setattr('indico.modules.events.abstracts.notifications.send_email', _mock_send_email)
+
+    ev = abstract.event_new
+    ev.abstract_email_templates.append(
+        create_email_template(ev, 0, 'accept', 'accept', [{'state': [AbstractState.accepted.value]}], True))
+
+    abstract.accepted_contrib_type = contrib_type
+    abstract.accepted_track = track
+    abstract.state = AbstractState.accepted
+    send_abstract_notifications(abstract)
