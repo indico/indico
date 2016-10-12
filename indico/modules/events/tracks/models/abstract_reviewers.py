bb6422ba58bfc7e53ed5481047ed026feb00ed83
Pedro Ferreira <pedro.ferreira@cern.ch>
2016-10-12 11:12:50 +0200

    Add global reviewers/conveners
    Reviewers/conveners with access to all tracks

--- indico/modules/events/tracks/models/abstract_reviewers.py
+++ indico/modules/events/tracks/models/abstract_reviewers.py
@@ -23,20 +23,29 @@ _track_abstract_reviewers_table = db.Table(
     'track_abstract_reviewers',
     db.metadata,
     db.Column(
+        'id',
+        db.Integer,
+        primary_key=True
+    ),
+    db.Column(
         'user_id',
         db.Integer,
         db.ForeignKey('users.users.id'),
-        primary_key=True,
-        autoincrement=False,
+        index=True,
+        nullable=False
+    ),
+    db.Column(
+        'event_id',
+        db.Integer,
+        db.ForeignKey('events.events.id'),
         index=True
     ),
     db.Column(
         'track_id',
         db.Integer,
         db.ForeignKey('events.tracks.id'),
-        primary_key=True,
-        autoincrement=False,
         index=True
     ),
+    db.CheckConstraint('(track_id IS NULL) != (event_id IS NULL)', name='track_xor_event_id_null'),
     schema='events'
 )
