34b625e05b9e46e41f79dc065117620e3c8b610c
Pedro Ferreira <pedro.ferreira@cern.ch>
2016-10-21 17:55:00 +0200

    Hide items in abstract page based on role
    Reviewers/submitters shouldn't see who judged nor be able to judge.

--- indico/modules/events/abstracts/controllers/display.py
+++ indico/modules/events/abstracts/controllers/display.py
@@ -36,7 +36,7 @@ class RHDisplayAbstract(AbstractMixin, RHConferenceBaseDisplay):
         AbstractMixin._checkProtection(self)
 
     def _process(self):
-        return WPDisplayAbstracts.render_template('abstract.html', self._conf, abstract=self.abstract)
+        return WPDisplayAbstracts.render_template('abstract.html', self._conf, abstract=self.abstract, management=False)
 
 
 class RHDisplayAbstractExportPDF(RHDisplayAbstract):
