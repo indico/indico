256ad334594450aac3f9ea82bf4a64ee2d8680d6
Adrian Moennich <adrian.moennich@cern.ch>
2016-10-31 13:39:31 +0100

    Use proper float division for abstract scores

--- indico/modules/events/abstracts/models/abstracts.py
+++ indico/modules/events/abstracts/models/abstracts.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Indico; if not, see <http://www.gnu.org/licenses/>.
 
-from __future__ import unicode_literals
+from __future__ import unicode_literals, division
 
 from sqlalchemy.ext.declarative import declared_attr
 
--- indico/modules/events/abstracts/models/reviews.py
+++ indico/modules/events/abstracts/models/reviews.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Indico; if not, see <http://www.gnu.org/licenses/>.
 
-from __future__ import unicode_literals
+from __future__ import unicode_literals, division
 
 from indico.core.db.sqlalchemy import db, PyIntEnum, UTCDateTime
 from indico.util.date_time import now_utc
