c9c390be8cc8ea4c08cc8a7c86ead9a26052e236
Adrian Moennich <adrian.moennich@cern.ch>
2016-11-03 14:50:17 +0100

    Move abstract editing RH into common.py
    Also fix inheritance so it works fine in both management and display
    areas.

---
+++ indico/modules/events/abstracts/controllers/common.py
@@ -0,0 +1,51 @@
+# This file is part of Indico.
+# Copyright (C) 2002 - 2016 European Organization for Nuclear Research (CERN).
+#
+# Indico is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+#
+# Indico is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Indico; if not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+
+from flask import flash, session
+from werkzeug.exceptions import Forbidden
+
+from indico.modules.events.abstracts.controllers.reviewing import RHAbstractReviewBase
+from indico.modules.events.abstracts.operations import update_abstract
+from indico.modules.events.abstracts.util import make_abstract_form
+from indico.modules.events.tracks.models.tracks import Track
+from indico.modules.events.util import get_field_values
+from indico.util.i18n import _
+from indico.web.util import jsonify_data, jsonify_form
+
+
+class RHEditAbstract(RHAbstractReviewBase):
+    def _checkProtection(self):
+        RHAbstractReviewBase._checkProtection(self)
+        if not self.abstract.can_edit(session.user):
+            raise Forbidden
+
+    def _process(self):
+        abstract_form_class = make_abstract_form(self.event_new)
+        form = abstract_form_class(obj=self.abstract, abstract=self.abstract, event=self.event_new)
+        # TODO: Handle reviewed_for_tracks properly!
+        if form.validate_on_submit():
+            data = form.data
+            if isinstance(data['submitted_for_tracks'], Track):
+                data['submitted_for_tracks'] = {data['submitted_for_tracks']}
+            elif data['submitted_for_tracks'] is None:
+                data['submitted_for_tracks'] = set()
+            update_abstract(self.abstract, *get_field_values(data))
+            flash(_("Abstract modified successfully"), 'success')
+            return jsonify_data(flash=False)
+        self.commit = False
+        return jsonify_form(form)
