f2980b82aede79b019beaab923067eccaccb2628
Adrian Moennich <adrian.moennich@cern.ch>
2016-11-04 15:22:27 +0100

    Simplify imports in abstracts blueprint

--- indico/modules/events/abstracts/blueprint.py
+++ indico/modules/events/abstracts/blueprint.py
@@ -16,45 +16,7 @@
 
 from __future__ import unicode_literals
 
-from indico.modules.events.abstracts.controllers.boa import RHManageBOA, RHExportBOA
-from indico.modules.events.abstracts.controllers.common import RHEditAbstract
-from indico.modules.events.abstracts.controllers.display import (RHDisplayAbstract, RHDisplayAbstractExportPDF,
-                                                                 RHMyAbstracts, RHMyAbstractsExportPDF,
-                                                                 RHSubmitAbstract)
-from indico.modules.events.abstracts.controllers.email_templates import (RHAddEmailTemplate, RHEditEmailTemplateRules,
-                                                                         RHEditEmailTemplateText, RHEmailTemplateList,
-                                                                         RHDeleteEmailTemplate, RHSortEmailTemplates,
-                                                                         RHEmailTemplateREST)
-from indico.modules.events.abstracts.controllers.management import (RHAbstractList,
-                                                                    RHAbstracts, RHAbstractListCustomize,
-                                                                    RHAbstractListStaticURL,
-                                                                    RHManageAbstractSubmission, RHManageAbstract,
-                                                                    RHManageAbstractReviewing, RHCreateAbstract,
-                                                                    RHDeleteAbstracts, RHAbstractPersonList,
-                                                                    RHAbstractsDownloadAttachments,
-                                                                    RHAbstractsExportPDF, RHAbstractExportPDF,
-                                                                    RHAbstractsExportCSV, RHAbstractsExportExcel,
-                                                                    RHAbstractsExportJSON,
-                                                                    RHScheduleCFA, RHOpenCFA, RHCloseCFA,
-                                                                    RHManageReviewingRoles, RHBulkAbstractJudgment,
-                                                                    RHAbstractNotificationLog)
-from indico.modules.events.abstracts.controllers.reviewing import (RHAbstractsDownloadAttachment,
-                                                                   RHResetAbstractState,
-                                                                   RHWithdrawAbstract,
-                                                                   RHLeaveComment,
-                                                                   RHDeleteAbstractComment,
-                                                                   RHEditAbstractComment,
-                                                                   RHDisplayReviewableTracks,
-                                                                   RHDisplayReviewableTrackAbstracts,
-                                                                   RHListOtherAbstracts,
-                                                                   RHReviewAbstractForTrack,
-                                                                   RHJudgeAbstract,
-                                                                   RHDisplayAbstractListCustomize,
-                                                                   RHDisplayAbstractsExportPDF,
-                                                                   RHDisplayAbstractsExportCSV,
-                                                                   RHDisplayAbstractsExportExcel,
-                                                                   RHDisplayAbstractsDownloadAttachments,
-                                                                   RHEditReviewedForTrackList)
+from indico.modules.events.abstracts.controllers import boa, common, display, email_templates, management, reviewing
 from indico.web.flask.util import make_compat_redirect_func
 from indico.web.flask.wrappers import IndicoBlueprint
 
@@ -62,105 +24,117 @@ _bp = IndicoBlueprint('abstracts', __name__, url_prefix='/event/<confId>', templ
                       virtual_template_folder='events/abstracts')
 
 # Display pages
-_bp.add_url_rule('/abstracts/<int:abstract_id>/', 'display_abstract', RHDisplayAbstract)
-_bp.add_url_rule('/abstracts/<int:abstract_id>/notifications', 'notification_log', RHAbstractNotificationLog)
-_bp.add_url_rule('/abstracts/<int:abstract_id>/abstract.pdf', 'display_abstract_pdf_export', RHDisplayAbstractExportPDF)
+_bp.add_url_rule('/abstracts/<int:abstract_id>/', 'display_abstract', display.RHDisplayAbstract)
+_bp.add_url_rule('/abstracts/<int:abstract_id>/notifications', 'notification_log', management.RHAbstractNotificationLog)
+_bp.add_url_rule('/abstracts/<int:abstract_id>/abstract.pdf', 'display_abstract_pdf_export',
+                 display.RHDisplayAbstractExportPDF)
 _bp.add_url_rule('/abstracts/<int:abstract_id>/attachments/<file_id>/<filename>', 'download_attachment',
-                 RHAbstractsDownloadAttachment)
-_bp.add_url_rule('/abstracts/mine', 'my_abstracts', RHMyAbstracts)
-_bp.add_url_rule('/abstracts/mine.pdf', 'my_abstracts_pdf', RHMyAbstractsExportPDF)
-_bp.add_url_rule('/abstracts/submit', 'submit', RHSubmitAbstract, methods=('GET', 'POST'))
+                 reviewing.RHAbstractsDownloadAttachment)
+_bp.add_url_rule('/abstracts/mine', 'my_abstracts', display.RHMyAbstracts)
+_bp.add_url_rule('/abstracts/mine.pdf', 'my_abstracts_pdf', display.RHMyAbstractsExportPDF)
+_bp.add_url_rule('/abstracts/submit', 'submit', display.RHSubmitAbstract, methods=('GET', 'POST'))
 
 # Reviewing pages (display area)
-_bp.add_url_rule('/call-for-abstracts/reviewing/', 'display_reviewable_tracks', RHDisplayReviewableTracks)
+_bp.add_url_rule('/call-for-abstracts/reviewing/', 'display_reviewable_tracks', reviewing.RHDisplayReviewableTracks)
 _bp.add_url_rule('/call-for-abstracts/reviewing/<int:track_id>/', 'display_reviewable_track_abstracts',
-                 RHDisplayReviewableTrackAbstracts)
+                 reviewing.RHDisplayReviewableTrackAbstracts)
 _bp.add_url_rule('/call-for-abstracts/reviewing/<int:track_id>/customize', 'display_customize_abstract_list',
-                 RHDisplayAbstractListCustomize, methods=('GET', 'POST'))
+                 reviewing.RHDisplayAbstractListCustomize, methods=('GET', 'POST'))
 _bp.add_url_rule('/call-for-abstracts/reviewing/<int:track_id>/attachments', 'display_download_attachments',
-                 RHDisplayAbstractsDownloadAttachments, methods=('POST',))
+                 reviewing.RHDisplayAbstractsDownloadAttachments, methods=('POST',))
 _bp.add_url_rule('/call-for-abstracts/reviewing/<int:track_id>/abstracts.pdf', 'display_abstracts_pdf_export',
-                 RHDisplayAbstractsExportPDF, methods=('POST',))
+                 reviewing.RHDisplayAbstractsExportPDF, methods=('POST',))
 _bp.add_url_rule('/call-for-abstracts/reviewing/<int:track_id>/abstracts.csv', 'display_abstracts_csv_export',
-                 RHDisplayAbstractsExportCSV, methods=('POST',))
+                 reviewing.RHDisplayAbstractsExportCSV, methods=('POST',))
 _bp.add_url_rule('/call-for-abstracts/reviewing/<int:track_id>/abstracts.xlsx', 'display_abstracts_xlsx_export',
-                 RHDisplayAbstractsExportExcel, methods=('POST',))
+                 reviewing.RHDisplayAbstractsExportExcel, methods=('POST',))
 
 # Book of Abstracts
-_bp.add_url_rule('/manage/abstracts/boa', 'manage_boa', RHManageBOA, methods=('GET', 'POST'))
-_bp.add_url_rule('/book-of-abstracts.pdf', 'export_boa', RHExportBOA)
+_bp.add_url_rule('/manage/abstracts/boa', 'manage_boa', boa.RHManageBOA, methods=('GET', 'POST'))
+_bp.add_url_rule('/book-of-abstracts.pdf', 'export_boa', boa.RHExportBOA)
 
-_bp.add_url_rule('/abstracts/other-list', 'other_abstracts', RHListOtherAbstracts, methods=('POST',))
+_bp.add_url_rule('/abstracts/other-list', 'other_abstracts', reviewing.RHListOtherAbstracts, methods=('POST',))
 
 # Management
-_bp.add_url_rule('/manage/abstracts/', 'manage_abstracts', RHAbstracts)
-_bp.add_url_rule('/manage/abstracts/schedule', 'schedule_abstracts_call', RHScheduleCFA, methods=('GET', 'POST'))
-_bp.add_url_rule('/manage/abstracts/open', 'open_abstracts_call', RHOpenCFA, methods=('POST',))
-_bp.add_url_rule('/manage/abstracts/close', 'close_abstracts_call', RHCloseCFA, methods=('POST',))
-_bp.add_url_rule('/manage/abstracts/settings', 'manage_submission_settings', RHManageAbstractSubmission,
+_bp.add_url_rule('/manage/abstracts/', 'manage_abstracts', management.RHAbstracts)
+_bp.add_url_rule('/manage/abstracts/schedule', 'schedule_abstracts_call', management.RHScheduleCFA,
                  methods=('GET', 'POST'))
-_bp.add_url_rule('/manage/abstracts/review-settings', 'manage_reviewing_settings', RHManageAbstractReviewing,
+_bp.add_url_rule('/manage/abstracts/open', 'open_abstracts_call', management.RHOpenCFA, methods=('POST',))
+_bp.add_url_rule('/manage/abstracts/close', 'close_abstracts_call', management.RHCloseCFA, methods=('POST',))
+_bp.add_url_rule('/manage/abstracts/settings', 'manage_submission_settings', management.RHManageAbstractSubmission,
                  methods=('GET', 'POST'))
-_bp.add_url_rule('/manage/abstracts/teams', 'manage_reviewing_roles', RHManageReviewingRoles, methods=('GET', 'POST'))
-_bp.add_url_rule('/manage/abstracts/list/', 'manage_abstract_list', RHAbstractList)
-_bp.add_url_rule('/manage/abstracts/list/customize', 'customize_abstract_list', RHAbstractListCustomize,
+_bp.add_url_rule('/manage/abstracts/review-settings', 'manage_reviewing_settings', management.RHManageAbstractReviewing,
                  methods=('GET', 'POST'))
-_bp.add_url_rule('/manage/abstracts/abstracts.pdf', 'abstracts_pdf_export', RHAbstractsExportPDF,
+_bp.add_url_rule('/manage/abstracts/teams', 'manage_reviewing_roles', management.RHManageReviewingRoles,
+                 methods=('GET', 'POST'))
+_bp.add_url_rule('/manage/abstracts/list/', 'manage_abstract_list', management.RHAbstractList)
+_bp.add_url_rule('/manage/abstracts/list/customize', 'customize_abstract_list', management.RHAbstractListCustomize,
+                 methods=('GET', 'POST'))
+_bp.add_url_rule('/manage/abstracts/abstracts.pdf', 'abstracts_pdf_export', management.RHAbstractsExportPDF,
                  methods=('POST',))
-_bp.add_url_rule('/manage/abstracts/abstracts.csv', 'abstracts_csv_export', RHAbstractsExportCSV,
+_bp.add_url_rule('/manage/abstracts/abstracts.csv', 'abstracts_csv_export', management.RHAbstractsExportCSV,
                  methods=('POST',))
-_bp.add_url_rule('/manage/abstracts/abstracts.xlsx', 'abstracts_xlsx_export', RHAbstractsExportExcel,
+_bp.add_url_rule('/manage/abstracts/abstracts.xlsx', 'abstracts_xlsx_export', management.RHAbstractsExportExcel,
                  methods=('POST',))
-_bp.add_url_rule('/manage/abstracts/abstracts.json', 'abstracts_json_export', RHAbstractsExportJSON,
+_bp.add_url_rule('/manage/abstracts/abstracts.json', 'abstracts_json_export', management.RHAbstractsExportJSON,
                  methods=('POST',))
-_bp.add_url_rule('/manage/abstracts/create', 'manage_create_abstract', RHCreateAbstract, methods=('GET', 'POST'))
-_bp.add_url_rule('/manage/abstracts/delete', 'manage_delete_abstracts', RHDeleteAbstracts, methods=('POST',))
-_bp.add_url_rule('/manage/abstracts/person-list', 'person_list', RHAbstractPersonList, methods=('POST',))
-_bp.add_url_rule('/manage/abstracts/attachments', 'download_attachments', RHAbstractsDownloadAttachments,
+_bp.add_url_rule('/manage/abstracts/create', 'manage_create_abstract', management.RHCreateAbstract,
+                 methods=('GET', 'POST'))
+_bp.add_url_rule('/manage/abstracts/delete', 'manage_delete_abstracts', management.RHDeleteAbstracts, methods=('POST',))
+_bp.add_url_rule('/manage/abstracts/person-list', 'person_list', management.RHAbstractPersonList, methods=('POST',))
+_bp.add_url_rule('/manage/abstracts/attachments', 'download_attachments', management.RHAbstractsDownloadAttachments,
                  methods=('POST',))
-_bp.add_url_rule('/manage/abstracts/judge', 'manage_judge_abstracts', RHBulkAbstractJudgment, methods=('POST',))
-_bp.add_url_rule('/manage/abstracts/list/static-url', 'generate_static_url', RHAbstractListStaticURL, methods=('POST',))
+_bp.add_url_rule('/manage/abstracts/judge', 'manage_judge_abstracts', management.RHBulkAbstractJudgment,
+                 methods=('POST',))
+_bp.add_url_rule('/manage/abstracts/list/static-url', 'generate_static_url', management.RHAbstractListStaticURL,
+                 methods=('POST',))
 
 # E-mail templates
-_bp.add_url_rule('/manage/abstracts/email-templates/', 'email_tpl_list', RHEmailTemplateList)
-_bp.add_url_rule('/manage/abstracts/email-templates/add', 'email_tpl_add', RHAddEmailTemplate, methods=('GET', 'POST'))
-_bp.add_url_rule('/manage/abstracts/email-templates/sort', 'email_tpl_sort', RHSortEmailTemplates, methods=('POST',))
-_bp.add_url_rule('/manage/abstracts/email-templates/<email_tpl_id>', 'email_tpl_delete', RHDeleteEmailTemplate,
-                 methods=('DELETE',))
-_bp.add_url_rule('/manage/abstracts/email-templates/<email_tpl_id>/edit', 'email_tpl_rule_edit',
-                 RHEditEmailTemplateRules, methods=('GET', 'POST'))
-_bp.add_url_rule('/manage/abstracts/email-templates/<email_tpl_id>/edit-text', 'email_tpl_text_edit',
-                 RHEditEmailTemplateText, methods=('GET', 'POST'))
-_bp.add_url_rule('/manage/abstracts/email-templates/<email_tpl_id>', 'email_tpl_rest',
-                 RHEmailTemplateREST, methods=('PATCH',))
+_bp.add_url_rule('/manage/abstracts/email-templates/', 'email_tpl_list', email_templates.RHEmailTemplateList)
+_bp.add_url_rule('/manage/abstracts/email-templates/add',
+                 'email_tpl_add', email_templates.RHAddEmailTemplate, methods=('GET', 'POST'))
+_bp.add_url_rule('/manage/abstracts/email-templates/sort',
+                 'email_tpl_sort', email_templates.RHSortEmailTemplates, methods=('POST',))
+_bp.add_url_rule('/manage/abstracts/email-templates/<email_tpl_id>',
+                 'email_tpl_delete', email_templates.RHDeleteEmailTemplate, methods=('DELETE',))
+_bp.add_url_rule('/manage/abstracts/email-templates/<email_tpl_id>/edit',
+                 'email_tpl_rule_edit', email_templates.RHEditEmailTemplateRules, methods=('GET', 'POST'))
+_bp.add_url_rule('/manage/abstracts/email-templates/<email_tpl_id>/edit-text',
+                 'email_tpl_text_edit', email_templates.RHEditEmailTemplateText, methods=('GET', 'POST'))
+_bp.add_url_rule('/manage/abstracts/email-templates/<email_tpl_id>',
+                 'email_tpl_rest', email_templates.RHEmailTemplateREST, methods=('PATCH',))
 
 # Abstract-specific management
-_bp.add_url_rule('/manage/abstracts/<int:abstract_id>/', 'manage_abstract', RHManageAbstract, methods=('GET', 'POST'))
-_bp.add_url_rule('/manage/abstracts/<int:abstract_id>/abstract.pdf', 'manage_abstract_pdf_export', RHAbstractExportPDF)
+_bp.add_url_rule('/manage/abstracts/<int:abstract_id>/', 'manage_abstract', management.RHManageAbstract,
+                 methods=('GET', 'POST'))
+_bp.add_url_rule('/manage/abstracts/<int:abstract_id>/abstract.pdf', 'manage_abstract_pdf_export',
+                 management.RHAbstractExportPDF)
 
 # Abstract-specific
-_bp.add_url_rule('/manage/abstracts/<int:abstract_id>/comment', 'comment_abstract', RHLeaveComment, methods=('POST',))
+_bp.add_url_rule('/manage/abstracts/<int:abstract_id>/comment', 'comment_abstract', reviewing.RHLeaveComment,
+                 methods=('POST',))
 _bp.add_url_rule('/manage/abstracts/<int:abstract_id>/comments/<int:comment_id>',
-                 'edit_abstract_comment', RHEditAbstractComment, methods=('GET', 'POST'))
+                 'edit_abstract_comment', reviewing.RHEditAbstractComment, methods=('GET', 'POST'))
 _bp.add_url_rule('/manage/abstracts/<int:abstract_id>/comments/<int:comment_id>',
-                 'delete_abstract_comment', RHDeleteAbstractComment, methods=('DELETE',))
+                 'delete_abstract_comment', reviewing.RHDeleteAbstractComment, methods=('DELETE',))
 
 for prefix in ('/manage/abstracts', '/abstracts'):
     defaults = {'management': prefix != '/abstracts'}
     _bp.add_url_rule(prefix + '/<int:abstract_id>/edit', 'edit_abstract',
-                     RHEditAbstract, methods=('GET', 'POST'), defaults=defaults)
+                     common.RHEditAbstract, methods=('GET', 'POST'), defaults=defaults)
     _bp.add_url_rule(prefix + '/<int:abstract_id>/withdraw', 'withdraw_abstract',
-                     RHWithdrawAbstract, methods=('POST',), defaults=defaults)
+                     reviewing.RHWithdrawAbstract, methods=('POST',), defaults=defaults)
     _bp.add_url_rule(prefix + '/<int:abstract_id>/reset', 'reset_abstract_state',
-                     RHResetAbstractState, methods=('POST',), defaults=defaults)
+                     reviewing.RHResetAbstractState, methods=('POST',), defaults=defaults)
     _bp.add_url_rule(prefix + '/<int:abstract_id>/abstract.pdf', 'manage_abstract_pdf_export',
-                     RHAbstractExportPDF, defaults=defaults)
+                     management.RHAbstractExportPDF, defaults=defaults)
     _bp.add_url_rule(prefix + '/<int:abstract_id>/review/track/<int:track_id>', 'review_abstract',
-                     RHReviewAbstractForTrack, methods=('POST',), defaults=defaults)
+                     reviewing.RHReviewAbstractForTrack, methods=('POST',), defaults=defaults)
     _bp.add_url_rule(prefix + '/<int:abstract_id>/judge', 'judge_abstract',
-                     RHJudgeAbstract, methods=('POST',), defaults=defaults)
+                     reviewing.RHJudgeAbstract, methods=('POST',), defaults=defaults)
     _bp.add_url_rule(prefix + '/<int:abstract_id>/reviewing-tracks', 'edit_review_tracks',
-                     RHEditReviewedForTrackList, methods=('POST',), defaults=defaults)
+                     reviewing.RHEditReviewedForTrackList, methods=('POST',), defaults=defaults)
+
 
 # Legacy URLs
 _compat_bp = IndicoBlueprint('compat_abstracts', __name__, url_prefix='/event/<event_id>')
