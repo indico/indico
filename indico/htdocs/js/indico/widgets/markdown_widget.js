e8d4551e9fece0540508ea6f5de099e7f2051db1
Adrian Moennich <adrian.moennich@cern.ch>
2016-11-09 13:33:53 +0100

    Show word/char count/limits in markdown field

--- indico/htdocs/js/indico/widgets/markdown_widget.js
+++ indico/htdocs/js/indico/widgets/markdown_widget.js
@@ -15,19 +15,63 @@
  * along with Indico; if not, see <http://www.gnu.org/licenses/>.
  */
 
+/* global countWords:false */
+
 (function(global) {
     'use strict';
+
+    function getLimitClass(remaining, max) {
+        if (remaining < 0) {
+            return 'limit-exceeded';
+        } else if (remaining <= max * 0.2) {
+            return 'limit-close';
+        } else {
+            return '';
+        }
+    }
+
+    function updateLimits($field, options) {
+        var $maxLengthInfo = $('#{0}-max-length-info'.format(options.fieldId));
+        var value = $field.val().trim();
+        $maxLengthInfo.empty();
+        if (options.maxLength) {
+            var charsLeft = options.maxLength - value.length;
+            $('<span>', {
+                html: $T.ngettext('<strong>1</strong> char left',
+                                  '<strong>{0}</strong> chars left', charsLeft).format(charsLeft),
+                class: getLimitClass(charsLeft, options.maxLength)
+            }).appendTo($maxLengthInfo);
+        }
+        if (options.maxWords) {
+            var wordCount = countWords(value);
+            var wordsLeft = options.maxWords - wordCount;
+            $('<span>', {
+                html: $T.ngettext('<strong>1</strong> word left',
+                                  '<strong>{0}</strong> words left', wordsLeft).format(wordsLeft),
+                class: getLimitClass(wordsLeft, options.maxWords)
+            }).appendTo($maxLengthInfo);
+        }
+    }
 
     global.setupMarkdownWidget = function setupMarkdownWidget(options) {
         options = $.extend(true, {
             fieldId: null,
-            useMarkdownEditor: false
+            useMarkdownEditor: false,
+            maxLength: 0,
+            maxWords: 0
         }, options);
 
         if (options.useMarkdownEditor) {
             var field = $('#' + options.fieldId);
             var container = field.closest('[data-field-id]');
             field.pagedown();
+
+            if (options.maxLength || options.maxWords) {
+                updateLimits(field, options);
+                field.on('change input', function() {
+                    updateLimits($(this), options);
+                });
+            }
 
             ['markdown-info', 'latex-info', 'wmd-help-button'].forEach(function(name) {
                 var content = container.find('.{0}-text'.format(name));
