76ccf3aeef73effc6a9f541fbf3c608711eb44eb
Adrian Moennich <adrian.moennich@cern.ch>
2016-11-23 16:59:39 +0100

    Fix typos

--- indico/modules/events/fields.py
+++ indico/modules/events/fields.py
@@ -79,7 +79,7 @@ class ReferencesField(MultipleItemsField):
 
 
 class EventPersonListField(PrincipalListField):
-    """"A field that lets you select a list Indico user and EventPersons
+    """A field that lets you select a list Indico user and EventPersons
 
     Requires its form to have an event set.
     """
