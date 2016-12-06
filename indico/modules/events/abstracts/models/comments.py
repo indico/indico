9163fa22f38d42d361ba822c8af229e06a2ed8e2
Adrian Moennich <adrian.moennich@cern.ch>
2016-12-06 17:44:38 +0100

    Abstract out review comments

--- indico/modules/events/abstracts/models/comments.py
+++ indico/modules/events/abstracts/models/comments.py
@@ -16,10 +16,11 @@
 
 from __future__ import unicode_literals
 
+from sqlalchemy.ext.declarative import declared_attr
+
 from indico.core.db import db
-from indico.core.db.sqlalchemy import PyIntEnum, UTCDateTime
-from indico.core.db.sqlalchemy.descriptions import RenderModeMixin, RenderMode
-from indico.util.date_time import now_utc
+from indico.core.db.sqlalchemy import PyIntEnum
+from indico.core.db.sqlalchemy.review_comments import ReviewCommentMixin
 from indico.util.locators import locator_property
 from indico.util.string import format_repr, return_ascii, text_to_repr
 from indico.util.struct.enum import TitledIntEnum
@@ -41,94 +42,42 @@ class AbstractCommentVisibility(TitledIntEnum):
     users = 5
 
 
-class AbstractComment(RenderModeMixin, db.Model):
-    possible_render_modes = {RenderMode.markdown}
-    default_render_mode = RenderMode.markdown
-
+class AbstractComment(ReviewCommentMixin, db.Model):
     __tablename__ = 'abstract_comments'
     __table_args__ = {'schema': 'event_abstracts'}
-    TIMELINE_TYPE = 'comment'
+    user_backref_name = 'abstract_comments'
+    user_modified_backref_name = 'modified_abstract_comments'
 
-    id = db.Column(
-        db.Integer,
-        primary_key=True
-    )
-    abstract_id = db.Column(
-        db.Integer,
-        db.ForeignKey('event_abstracts.abstracts.id'),
-        index=True,
-        nullable=False
-    )
-    user_id = db.Column(
-        db.Integer,
-        db.ForeignKey('users.users.id'),
-        index=True,
-        nullable=False
-    )
-    _text = db.Column(
-        'text',
-        db.Text,
-        nullable=False
-    )
-    #: ID of the user who last modified the comment
-    modified_by_id = db.Column(
-        db.Integer,
-        db.ForeignKey('users.users.id'),
-        index=True,
-        nullable=True
-    )
-    created_dt = db.Column(
-        UTCDateTime,
-        nullable=False,
-        default=now_utc
-    )
-    modified_dt = db.Column(
-        UTCDateTime,
-        nullable=True
-    )
-    visibility = db.Column(
-        PyIntEnum(AbstractCommentVisibility),
-        nullable=False,
-        default=AbstractCommentVisibility.contributors
-    )
-    is_deleted = db.Column(
-        db.Boolean,
-        nullable=False,
-        default=False
-    )
-    abstract = db.relationship(
-        'Abstract',
-        lazy=True,
-        backref=db.backref(
-            'comments',
-            primaryjoin='(AbstractComment.abstract_id == Abstract.id) & ~AbstractComment.is_deleted',
-            order_by=created_dt,
-            cascade='all, delete-orphan',
+    @declared_attr
+    def abstract_id(cls):
+        return db.Column(
+            db.Integer,
+            db.ForeignKey('event_abstracts.abstracts.id'),
+            index=True,
+            nullable=False
+        )
+
+    @declared_attr
+    def visibility(cls):
+        return db.Column(
+            PyIntEnum(AbstractCommentVisibility),
+            nullable=False,
+            default=AbstractCommentVisibility.contributors
+        )
+
+    @declared_attr
+    def abstract(cls):
+        return db.relationship(
+            'Abstract',
             lazy=True,
+            backref=db.backref(
+                'comments',
+                primaryjoin='(AbstractComment.abstract_id == Abstract.id) & ~AbstractComment.is_deleted',
+                order_by=cls.created_dt,
+                cascade='all, delete-orphan',
+                lazy=True,
+            )
         )
-    )
-    user = db.relationship(
-        'User',
-        lazy=True,
-        foreign_keys=user_id,
-        backref=db.backref(
-            'abstract_comments',
-            primaryjoin='(AbstractComment.user_id == User.id) & ~AbstractComment.is_deleted',
-            lazy='dynamic'
-        )
-    )
-    modified_by = db.relationship(
-        'User',
-        lazy=True,
-        foreign_keys=modified_by_id,
-        backref=db.backref(
-            'modified_abstract_comments',
-            primaryjoin='(AbstractComment.modified_by_id == User.id) & ~AbstractComment.is_deleted',
-            lazy='dynamic'
-        )
-    )
-
-    text = RenderModeMixin.create_hybrid_property('_text')
 
     @locator_property
     def locator(self):
