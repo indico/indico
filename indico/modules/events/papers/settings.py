6df5902f0de95a374cd5c64fbf67f7f1f5ec854b
Pedro Ferreira <pedro.ferreira@cern.ch>
2016-12-12 17:36:40 +0100

    Add notifications to PR settings

--- indico/modules/events/papers/settings.py
+++ indico/modules/events/papers/settings.py
@@ -16,8 +16,26 @@
 
 from __future__ import unicode_literals
 
-from indico.core.settings.converters import DatetimeConverter
+from indico.core.settings.converters import DatetimeConverter, SettingConverter
 from indico.modules.events.settings import EventSettingsProxy
+from indico.util.struct.enum import IndicoEnum
+
+
+class PaperReviewingRole(int, IndicoEnum):
+    judge = 1
+    layout_reviewer = 2
+    content_reviewer = 3
+
+
+class RoleConverter(SettingConverter):
+    @staticmethod
+    def from_python(value):
+        return [v.name for v in value]
+
+    @staticmethod
+    def to_python(value):
+        return {PaperReviewingRole.get(v) for v in value}
+
 
 paper_reviewing_settings = EventSettingsProxy('paper_reviewing', {
     'start_dt': None,
@@ -28,10 +46,20 @@ paper_reviewing_settings = EventSettingsProxy('paper_reviewing', {
     'judge_deadline': None,
     'layout_reviewer_deadline': None,
     'content_reviewer_deadline': None,
+
+    # Notifications
+    'notify_on_added_to_event': {},
+    'notify_on_assigned_contrib': {},
+    'notify_on_paper_submission': {PaperReviewingRole.layout_reviewer, PaperReviewingRole.content_reviewer},
+    'notify_judge_on_review': True,
+    'notify_author_on_judgment': True
 }, converters={
     'start_dt': DatetimeConverter,
     'end_dt': DatetimeConverter,
     'judge_deadline': DatetimeConverter,
     'layout_reviewer_deadline': DatetimeConverter,
     'content_reviewer_deadline': DatetimeConverter,
+    'notify_on_added_to_event': RoleConverter,
+    'notify_on_assigned_contrib': RoleConverter,
+    'notify_on_paper_submission': RoleConverter
 })
