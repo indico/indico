f188c64eb786954cc61416cb6fd171cbb3435152
Indico Team <indico-team@cern.ch>
2017-01-11 14:52:05 +0100

    Happy new year 2017! (#2)

--- indico/modules/events/papers/models/files.py
+++ indico/modules/events/papers/models/files.py
@@ -1,5 +1,5 @@
 # This file is part of Indico.
-# Copyright (C) 2002 - 2016 European Organization for Nuclear Research (CERN).
+# Copyright (C) 2002 - 2017 European Organization for Nuclear Research (CERN).
 #
 # Indico is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
@@ -15,9 +15,8 @@
 # along with Indico; if not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import unicode_literals
-
 # This file is part of Indico.
-# Copyright (C) 2002 - 2016 European Organization for Nuclear Research (CERN).
+# Copyright (C) 2002 - 2017 European Organization for Nuclear Research (CERN).
 #
 # Indico is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
