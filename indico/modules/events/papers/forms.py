40826d758c7ed78c60e9f8f15d17309ecf1356e4
Ilias Trichopoulos <ilias.trichopoulos@cern.ch>
2017-01-26 10:59:57 +0100

    Create paper comment and review forms

--- indico/modules/events/papers/forms.py
+++ indico/modules/events/papers/forms.py
@@ -18,21 +18,64 @@ from __future__ import unicode_literals
 
 from datetime import time
 
-from flask import request
-from wtforms.fields import TextAreaField, BooleanField, HiddenField, StringField
+from flask import request, session
+from wtforms.fields import TextAreaField, BooleanField, HiddenField, StringField, RadioField
 from wtforms.validators import Optional, DataRequired
 
 from indico.modules.events.fields import ReviewQuestionsField
 from indico.modules.events.papers.fields import PaperEmailSettingsField
 from indico.modules.events.papers.models.review_questions import PaperReviewQuestion
-from indico.modules.events.papers.models.reviews import PaperAction, PaperReviewType
+from indico.modules.events.papers.models.reviews import PaperAction, PaperReviewType, PaperCommentVisibility
 from indico.util.i18n import _
 from indico.web.flask.util import url_for
-from indico.web.forms.base import IndicoForm
+from indico.web.forms.base import IndicoForm, FormDefaults
 from indico.web.forms.fields import (PrincipalListField, IndicoDateTimeField, IndicoTagListField, HiddenEnumField,
-                                     HiddenFieldList, IndicoMarkdownField, FileField, EditableFileField)
+                                     HiddenFieldList, IndicoMarkdownField, FileField, EditableFileField,
+                                     IndicoEnumSelectField)
 from indico.web.forms.util import inject_validators
 from indico.web.forms.validators import LinkedDateTime
+from indico.web.forms.widgets import JinjaWidget
+
+
+def make_review_form(event, review_type):
+    """Extends the paper WTForm to add the extra fields.
+
+    Each extra field will use a field named ``custom_ID``.
+
+    :param event: The `Event` for which to create the paper review form.
+    :param review_type: The `PaperReviewType` for which to create the paper review form.
+    :return: A `PaperReviewForm` subclass.
+    """
+    form_class = type(b'_PaperReviewForm', (PaperReviewForm,), {})
+    for idx, question in enumerate(event.cfp.get_questions_from_review_type(review_type), start=1):
+        name = 'question_{}'.format(question.id)
+        range_ = event.cfp.rating_range
+        field = RadioField(question.text, validators=[DataRequired()],
+                           choices=[(unicode(n), unicode(n)) for n in range(range_[0], range_[1] + 1)],
+                           widget=JinjaWidget('events/reviews/rating_widget.html',
+                                              question=question, rating_range=event.cfp.rating_range, inline_js=True,
+                                              question_idx=idx))
+        setattr(form_class, name, field)
+    return form_class
+
+
+def build_review_form(paper_revision=None, review_type=None, review=None):
+    if review:
+        paper_revision = review.revision
+        review_type = review.type
+    review_form_class = make_review_form(paper_revision.paper.event_new, review_type=review_type)
+    reviews = paper_revision.get_reviews(user=session.user, group=review_type)
+    latest_user_review = reviews[0] if reviews else None
+
+    if latest_user_review:
+        answers = {'question_{}'.format(rating.question.id): rating.value
+                   for rating in latest_user_review.ratings}
+        defaults = FormDefaults(obj=latest_user_review, **answers)
+    else:
+        defaults = FormDefaults()
+
+    return review_form_class(prefix='type-{}'.format(review_type.value), obj=defaults, paper=paper_revision.paper,
+                             edit=review is not None)
 
 
 def make_competences_form(event):
@@ -145,3 +188,42 @@ class PaperTemplateForm(IndicoForm):
         if template is None:
             inject_validators(self, 'template', [DataRequired()])
         super(PaperTemplateForm, self).__init__(*args, **kwargs)
+
+
+class PaperCommentForm(IndicoForm):
+    text = TextAreaField(_("Comment"), [DataRequired()], render_kw={'placeholder': _("Leave a comment...")})
+    visibility = IndicoEnumSelectField(_("Visibility"), [DataRequired()], enum=PaperCommentVisibility,
+                                       skip={PaperCommentVisibility.users})
+
+    def __init__(self, *args, **kwargs):
+        comment = kwargs.get('obj')
+        user = comment.user if comment else kwargs.pop('user')
+        paper = kwargs.pop('paper')
+        super(IndicoForm, self).__init__(*args, **kwargs)
+        if not paper.can_judge(user):
+            self.visibility.skip.add(PaperCommentVisibility.judges)
+        if not paper.can_review(user):
+            del self.visibility
+
+
+class PaperReviewForm(IndicoForm):
+    """Form for reviewing a paper"""
+
+    _order = ('proposed_action', 'comment')
+
+    comment = TextAreaField(_("Comment"), render_kw={'placeholder': _("You may leave a comment (only visible to "
+                                                                      "reviewers and judges)...")})
+    proposed_action = IndicoEnumSelectField(_("Proposed Action"), [DataRequired()], enum=PaperAction)
+
+    def __init__(self, edit=False, *args, **kwargs):
+        paper = kwargs.pop('paper')
+        super(PaperReviewForm, self).__init__(*args, **kwargs)
+        self.event = paper.event_new
+        if not edit:
+            self.proposed_action.none = _("Propose an action...")
+
+    @property
+    def split_data(self):
+        data = self.data
+        return {'questions_data': {k: v for k, v in data.iteritems() if k.startswith('question_')},
+                'review_data': {k: v for k, v in data.iteritems() if not k.startswith('question_')}}
