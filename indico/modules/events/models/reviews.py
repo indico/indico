47bd2c916feb2493b35f723c3e71f0c9e9ec6db2
Ilias Trichopoulos <ilias.trichopoulos@cern.ch>
2017-01-26 10:56:14 +0100

    Improve proposal mixins and proxies

--- indico/modules/events/models/reviews.py
+++ indico/modules/events/models/reviews.py
@@ -20,43 +20,64 @@ from sqlalchemy.ext.hybrid import hybrid_property
 
 from indico.util.caching import memoize_request
 from indico.util.i18n import _
+from indico.util.locators import locator_property
 from indico.util.struct.enum import RichIntEnum
 from indico.web.flask.util import url_for
 
 
-class ProposalMixin(object):
-    proposal_type = None
-    call_for_proposals_attr = None
+class ProposalGroupProxy(object):
+    """Represents the object that the proposals can be grouped by.
 
-    # endpoints
-    delete_comment_endpoint = None
-    create_comment_endpoint = None
-    edit_comment_endpoint = None
-    create_review_endpoint = None
-    edit_review_endpoint = None
+    It provides all necessary methods for building the URLs, displaying the
+    grouping information, etc.
+    """
+
+    title_attr = 'title'
+    full_title_attr = 'full_title'
+
+    def __init__(self, group):
+        self.instance = group
+
+    def __eq__(self, other):
+        if not isinstance(other, ProposalGroupProxy):
+            return False
+        return self.instance == other.instance
+
+    def __hash__(self):
+        return hash(self.instance)
+
+    def __ne__(self, other):
+        return not (self == other)
 
     @property
-    def cfp(self):
-        return getattr(self.event_new, self.call_for_proposals_attr)
+    def title(self):
+        return getattr(self.instance, self.title_attr)
 
-    def can_comment(self, user):
-        raise NotImplementedError
+    @property
+    def full_title(self):
+        return (getattr(self.instance, self.full_title_attr)
+                if hasattr(self.instance, self.full_title_attr)
+                else self.title)
 
-    def can_review(self, user, check_state=False):
-        raise NotImplementedError
+    @locator_property
+    def locator(self):
+        return self.instance.locator
 
-    def get_delete_comment_url(self, comment):
-        return url_for(self.delete_comment_endpoint, comment)
 
-    def get_save_comment_url(self, comment=None):
-        return (url_for(self.edit_comment_endpoint, comment)
-                if comment
-                else url_for(self.create_comment_endpoint, self))
+class ProposalRevisionMixin(object):
+    """ Properties and methods of a proposal revision."""
+    #: The attribute  of the revision used to fetch the proposal object.
+    proposal_attr = None
+    #: Whether the reviewing process supports multiple revisions per proposal.
+    #: If set to false it is assumed that the reviewing process supports only
+    #: one revision per proposal.
+    revisions_enabled = True
 
-    def get_save_review_url(self, group=None, review=None):
-        return (url_for(self.edit_review_endpoint, review)
-                if review
-                else url_for(self.create_review_endpoint, self, group))
+    @property
+    def proposal(self):
+        # Property to fetch the proposal object. If multiple revisions are
+        # disabled, the revision is represented by the proposal object itself.
+        return getattr(self, self.proposal_attr) if self.revisions_enabled else self
 
     def get_timeline(self, user=None):
         raise NotImplementedError
@@ -84,6 +105,61 @@ class ProposalMixin(object):
                 'reviews': reviews}
 
 
+class ProposalMixin(object):
+    """Classes that represent a proposal object should extend this class (ex:
+    Abstract, Paper).
+    """
+    #: A unique identifier to handle rendering differences between proposal
+    #: types
+    proposal_type = None
+    #: Attribute to retrieve the object with access to the reviewing settings
+    call_for_proposals_attr = None
+    #: Whether there is support for multiple revisions per proposal or just one
+    revisions_enabled = True
+
+    # endpoints
+    delete_comment_endpoint = None
+    create_comment_endpoint = None
+    edit_comment_endpoint = None
+    create_review_endpoint = None
+    edit_review_endpoint = None
+
+    @property
+    def cfp(self):
+        return getattr(self.event_new, self.call_for_proposals_attr)
+
+    def get_revisions(self):
+        if self.revisions_enabled:
+            raise NotImplementedError
+        else:
+            return [self]
+
+    def get_last_revision(self):
+        if self.revisions_enabled:
+            raise NotImplementedError
+        else:
+            return self
+
+    def can_comment(self, user):
+        raise NotImplementedError
+
+    def can_review(self, user, check_state=False):
+        raise NotImplementedError
+
+    def get_delete_comment_url(self, comment):
+        return url_for(self.delete_comment_endpoint, comment)
+
+    def get_save_comment_url(self, comment=None):
+        return (url_for(self.edit_comment_endpoint, comment)
+                if comment
+                else url_for(self.create_comment_endpoint, self))
+
+    def get_save_review_url(self, group=None, review=None):
+        return (url_for(self.edit_review_endpoint, review)
+                if review
+                else url_for(self.create_review_endpoint, self, group))
+
+
 class ProposalCommentMixin(object):
     timeline_item_type = 'comment'
 
@@ -107,17 +183,29 @@ class ProposalCommentVisibility(RichIntEnum):
 
 
 class ProposalReviewMixin(object):
+    """Mixin for proposal reviews
+
+    Classes that represent a review of a proposal should extend this class
+    (ex: AbstractReview, PaperReview).
+    """
+    #: A unique identifier to handle rendering differences between timeline
+    #: items
     timeline_item_type = 'review'
-    proposal_relationship = None
-    group_relationship = None
+    #: The revision object that the review refers to
+    revision_attr = None
+    #: Object used to group reviews together
+    group_attr = None
+    #: Proxy class to provide the necessary properties and methods to the
+    #: review grouping object
+    group_proxy_cls = ProposalGroupProxy
 
     @hybrid_property
-    def proposal(self):
-        return getattr(self, self.proposal_relationship)
+    def revision(self):
+        return getattr(self, self.revision_attr)
 
     @property
     def group(self):
-        return getattr(self, self.group_relationship)
+        return self.group_proxy_cls(getattr(self, self.group_attr))
 
     @property
     def visibility(self):
