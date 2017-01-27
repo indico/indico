5e1dc0cdaa6d63e00bedd2bc1296b698697615fd
David Andreev <david.andreev@cern.ch>
2017-01-27 16:08:15 +0100

    Add css classes to PaperRevisionState rich enum

--- indico/modules/events/papers/models/revisions.py
+++ indico/modules/events/papers/models/revisions.py
@@ -33,7 +33,7 @@ from indico.util.struct.enum import RichIntEnum
 
 class PaperRevisionState(RichIntEnum):
     __titles__ = [None, _("Submitted"), _("Accepted"), _("Rejected"), _("To be corrected")]
-    __css_classes__ = [None, '', 'success', 'error', 'warning']
+    __css_classes__ = [None, 'highlight', 'success', 'error', 'warning']
     submitted = 1
     accepted = 2
     rejected = 3
