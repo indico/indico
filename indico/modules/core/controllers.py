eb59b4369a1bfa19fce2642cfa8d59b41f31d6e9
Adrian Moennich <adrian.moennich@cern.ch>
2017-01-27 17:05:40 +0100

    Use single non-ajax form for admin settings

--- indico/modules/core/controllers.py
+++ indico/modules/core/controllers.py
@@ -16,27 +16,25 @@
 
 from __future__ import unicode_literals
 
-from flask import flash
+from flask import flash, redirect
 from werkzeug.urls import url_join
 
 from indico.core.config import Config
+from indico.core.settings.proxy import PrefixSettingsProxy
 from indico.modules.cephalopod import cephalopod_settings
-from indico.modules.core.forms import SiteSettingsForm, SocialSettingsForm
+from indico.modules.core.forms import SettingsForm
 from indico.modules.core.settings import core_settings, social_settings
 from indico.modules.core.views import WPSettings
 from indico.util.i18n import _
-from indico.web.flask.templating import get_template_module
+from indico.web.flask.util import url_for
 from indico.web.forms.base import FormDefaults
-from indico.web.util import jsonify_form, jsonify_data
 from MaKaC.webinterface.rh.admins import RHAdminBase
 
 
-class RHCoreAdminBase(RHAdminBase):
-    CSRF_ENABLED = True
-
-
-class RHSettings(RHCoreAdminBase):
+class RHSettings(RHAdminBase):
     """General settings"""
+
+    CSRF_ENABLED = True
 
     def _get_cephalopod_data(self):
         if not cephalopod_settings.get('joined'):
@@ -51,42 +49,17 @@ class RHSettings(RHCoreAdminBase):
         return url, data
 
     def _process(self):
+        proxy = PrefixSettingsProxy({'core': core_settings, 'social': social_settings})
+        form = SettingsForm(obj=FormDefaults(**proxy.get_all()))
+        if form.validate_on_submit():
+            proxy.set_multi(form.data)
+            flash(_('Settings have been saved'), 'success')
+            return redirect(url_for('.settings'))
+
         cephalopod_url, cephalopod_data = self._get_cephalopod_data()
         return WPSettings.render_template('settings.html',
+                                          form=form,
                                           core_settings=core_settings.get_all(),
                                           social_settings=social_settings.get_all(),
                                           cephalopod_url=cephalopod_url,
                                           cephalopod_data=cephalopod_data)
-
-
-class RHSettingsBase(RHCoreAdminBase):
-    """Base class to edit settings"""
-
-    form_class = None
-    macro = None
-    settings_proxy = None
-
-    def _process(self):
-        form = self.form_class(obj=FormDefaults(**self.settings_proxy.get_all()))
-        if form.validate_on_submit():
-            self.settings_proxy.set_multi(form.data)
-            flash(_('Settings have been saved'), 'success')
-            tpl = get_template_module('core/_settings.html')
-            return jsonify_data(html=getattr(tpl, self.macro)(self.settings_proxy.get_all()))
-        return jsonify_form(form)
-
-
-class RHSiteSettings(RHSettingsBase):
-    """Edit site settings"""
-
-    form_class = SiteSettingsForm
-    settings_proxy = core_settings
-    macro = 'render_site_settings'
-
-
-class RHSocialSettings(RHSettingsBase):
-    """Edit social settings"""
-
-    form_class = SocialSettingsForm
-    settings_proxy = social_settings
-    macro = 'render_social_settings'
