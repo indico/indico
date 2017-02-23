cb8a9dc15e950a019f9f14bab7294c1d9a4d231e
Adrian Moennich <adrian.moennich@cern.ch>
2017-02-23 11:56:54 +0100

    Remove legacy getLocator support

--- indico/util/locators_test.py
+++ indico/util/locators_test.py
@@ -27,10 +27,6 @@ def test_get_locator_none():
 
 
 def test_get_locator():
-    class _Legacy(object):
-        def getLocator(self):
-            return {'foo': 'bar'}
-
     class _Prop(object):
         @property
         def locator(self):
@@ -41,7 +37,6 @@ def test_get_locator():
         def locator(self):
             return {'foo': 'bar'}
 
-    assert get_locator(_Legacy()) == {'foo': 'bar'}
     assert get_locator(_Prop()) == {'foo': 'bar'}
     assert get_locator(_Locator()) == {'foo': 'bar'}
     assert get_locator({'foo': 'bar'}) == {'foo': 'bar'}
