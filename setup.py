51bf1e31874f2069cc74c556ef4cd93008fe9a89
Adrian Moennich <adrian.moennich@cern.ch>
2017-03-02 12:09:07 +0100

    Cleanup setup.py

--- setup.py
+++ setup.py
@@ -14,63 +14,74 @@
 # You should have received a copy of the GNU General Public License
 # along with Indico; if not, see <http://www.gnu.org/licenses/>.
 
-# Autoinstalls setuptools if the user doesn't have them already
-import ez_setup
-ez_setup.use_setuptools()
 
 import ast
 import os
 import re
-import shutil
-import sys
-from distutils.sysconfig import get_python_version
-from distutils.cmd import Command
-from distutils.command import bdist
+from setuptools import setup, find_packages
 
 
-import pkg_resources
-from setuptools.command import develop, sdist, bdist_egg, easy_install
-from setuptools import setup, find_packages, findall
-
-
-DEPENDENCY_URLS = ["https://github.com/indico/indico-fonts/releases/"]
-
-
-def compile_languages(cmd):
-    """
-    Compile all language files
-    Needed to generate binary distro
-    """
-    from babel.messages import frontend
-
-    compile_cmd = frontend.compile_catalog(cmd.distribution)
-    cmd.distribution._set_command_options(compile_cmd)
-    compile_cmd.finalize_options()
-    compile_cmd.run()
+DEPENDENCY_URLS = ['https://github.com/indico/indico-fonts/releases/']
+ENTRY_POINTS = {
+    'console_scripts': [
+        'indico = indico.cli.manage:main',
+        'indico-zodbimport = indico_zodbimport.cli:main',
+        'indico_initial_setup = indico.legacy.consoleScripts.indicoInitialSetup:main',
+    ],
+    'pytest11': [
+        'indico = indico.testing.pytest_plugin',
+    ],
+    'indico.zodb_importers': [
+        'api = indico_zodbimport.modules.api:APIImporter',
+        'attachments = indico_zodbimport.modules.attachments:AttachmentImporter',
+        'badges_posters = indico_zodbimport.modules.badges_posters:BadgePosterImporter',
+        'categories = indico_zodbimport.modules.categories:CategoryImporter',
+        'event_abstracts = indico_zodbimport.modules.event_abstracts:EventAbstractsImporter',
+        'event_abstracts_zodb = indico_zodbimport.modules.event_abstracts_zodb:EventAbstractZODBPatcher',
+        'event_acls = indico_zodbimport.modules.event_acls:EventACLImporter',
+        'event_alarms = indico_zodbimport.modules.event_alarms:EventAlarmImporter',
+        'event_categories = indico_zodbimport.modules.event_categories:EventCategoriesImporter',
+        'event_data = indico_zodbimport.modules.event_data:EventDataImporter',
+        'event_dates_titles = indico_zodbimport.modules.event_dates_titles:EventDatesTitlesImporter',
+        'event_images = indico_zodbimport.modules.event_images:EventImageImporter',
+        'event_keywords_ctimes_visibility = indico_zodbimport.modules.event_keywords_ctimes_visibility:EventKeywordsCreationTimesVisibilityImporter',
+        'event_layout = indico_zodbimport.modules.event_layout:EventLayoutImporter',
+        'event_locations = indico_zodbimport.modules.event_locations:EventLocationsImporter',
+        'event_logs = indico_zodbimport.modules.event_logs:EventLogImporter',
+        'event_managers = indico_zodbimport.modules.event_managers:EventManagerImporter',
+        'event_menus = indico_zodbimport.modules.event_menus:EventMenuImporter',
+        'event_notes = indico_zodbimport.modules.event_notes:EventNoteImporter',
+        'event_paper_reviewing_legacy = indico_zodbimport.modules.event_paper_reviewing_legacy:LegacyEventPaperReviewingImporter',
+        'event_papers = indico_zodbimport.modules.event_papers:EventPapersImporter',
+        'event_participants = indico_zodbimport.modules.event_participants:EventParticipantsImporter',
+        'event_regforms = indico_zodbimport.modules.event_regforms:EventRegformImporter',
+        'event_series = indico_zodbimport.modules.event_series:EventSeriesImporter',
+        'event_shorturls = indico_zodbimport.modules.event_shorturls:EventShorturlsImporter',
+        'event_stubs = indico_zodbimport.modules.event_stubs:EventStubImporter',
+        'event_surveys = indico_zodbimport.modules.event_surveys:SurveyImporter',
+        'event_timetable = indico_zodbimport.modules.event_timetable:EventTimetableImporter',
+        'event_types = indico_zodbimport.modules.event_types:EventTypeImporter',
+        'global_ip_acl = indico_zodbimport.modules.global_ip_acl:GlobalIPACLImporter',
+        'global_settings = indico_zodbimport.modules.global_settings:GlobalSettingsImporter',
+        'groups = indico_zodbimport.modules.groups:GroupImporter',
+        'legacy_categories = indico_zodbimport.modules.legacy_categories:LegacyCategoryImporter',
+        'legacy_events = indico_zodbimport.modules.legacy_events:LegacyEventImporter',
+        'legal = indico_zodbimport.modules.legal:LegalImporter',
+        'networks = indico_zodbimport.modules.networks:NetworkImporter',
+        'news = indico_zodbimport.modules.news:NewsImporter',
+        'payment = indico_zodbimport.modules.payment:PaymentImporter',
+        'roombooking = indico_zodbimport.modules.roombooking:RoomBookingImporter',
+        'static_sites = indico_zodbimport.modules.static_sites:StaticSitesImporter',
+        'upcoming_events = indico_zodbimport.modules.upcoming_events:UpcomingEventsImporter',
+        'user_management = indico_zodbimport.modules.user_management:UserManagementImporter',
+        'users = indico_zodbimport.modules.users:UserImporter',
+    ]
+}
 
 
 def read_requirements_file(fname):
     with open(fname, 'r') as f:
         return [dep.strip() for dep in f.readlines() if not (dep.startswith('-') or '://' in dep)]
-
-
-def _generateDataPaths(x):
-
-    dataFilesDict = {}
-
-    for (baseDstDir, srcDir) in x:
-        for f in findall(srcDir):
-            dst_dir = os.path.join(baseDstDir,
-                                   os.path.relpath(os.path.dirname(f), srcDir))
-            if dst_dir not in dataFilesDict:
-                dataFilesDict[dst_dir] = []
-            dataFilesDict[dst_dir].append(f)
-
-    dataFiles = []
-    for k, v in dataFilesDict.items():
-        dataFiles.append((k, v))
-
-    return dataFiles
 
 
 def get_requirements():
@@ -83,223 +94,23 @@ def get_version():
         return str(ast.literal_eval(_version_re.search(f.read().decode('utf-8')).group(1)))
 
 
-# Commands
-class sdist_indico(sdist.sdist):
-    user_options = (sdist.sdist.user_options +
-                    [('version=', None, 'version to distribute')])
-    version = 'dev'
-
-    def run(self):
-        sdist.sdist.run(self)
-
-
-def _bdist_indico(dataFiles):
-    class bdist_indico(bdist.bdist):
-        def run(self):
-            compile_languages(self)
-            bdist.bdist.run(self)
-
-    bdist_indico.dataFiles = dataFiles
-    return bdist_indico
-
-
-def _bdist_egg_indico(dataFiles):
-    class bdist_egg_indico(bdist_egg.bdist_egg):
-        def run(self):
-            compile_languages(self)
-            bdist_egg.bdist_egg.run(self)
-
-    bdist_egg_indico.dataFiles = dataFiles
-    return bdist_egg_indico
-
-
-class develop_indico(develop.develop):
-    def run(self):
-        develop.develop.run(self)
-
-        # install dev dependencies
-        env = pkg_resources.Environment()
-        easy_install.main(read_requirements_file(os.path.join(os.path.dirname(__file__), 'requirements.dev.txt')))
-        env.scan()
-
-
-class develop_config(develop_indico):
-    description = "prepares the current directory for Indico development"
-    user_options = (develop.develop.user_options +
-                    [('www-uid=', None, "Set user for cache/log/db (typically apache user)"),
-                     ('www-gid=', None, "Set group for cache/log/db (typically apache group)"),
-                     ('http-port=', None, "Set port used by HTTP server"),
-                     ('https-port=', None, "Set port used by HTTP server in HTTPS mode"),
-                     ('smtp-port=', None, "Set port used for SMTP (e-mail sending)"),
-                     ('use-apache', None, "Use apache (will chmod directories accordingly)")])
-
-    www_uid = None
-    www_gid = None
-    http_port = 8000
-    https_port = 8443
-    smtp_port = 8025
-
-    def run(self):
-        # dependencies, links, etc...
-        develop_indico.run(self)
-
-        local = 'etc/indico.conf'
-        if os.path.exists(local):
-            print 'Upgrading existing etc/indico.conf...'
-        else:
-            print 'Creating new etc/indico.conf..'
-            shutil.copy('etc/indico.conf.sample', local)
-
-        if not os.path.exists('etc/logging.conf'):
-            shutil.copy('etc/logging.conf.sample', 'etc/logging.conf')
-
-        print """\nIndico needs to store some information in the filesystem (database, cache, temporary files, logs...)
-Please specify the directory where you'd like it to be placed.
-(Note that putting it outside of your sourcecode tree is recommended)"""
-        prefixDirDefault = os.path.dirname(os.getcwd())
-        prefixDir = raw_input('Full path [%s]: ' % prefixDirDefault).strip()
-
-        if prefixDir == '':
-            prefixDir = prefixDirDefault
-
-        directories = dict((d, os.path.join(prefixDir, d)) for d in
-                           ['db', 'log', 'tmp', 'cache', 'archive'])
-
-        print 'Creating directories...',
-        for d in directories.values():
-            if not os.path.exists(d):
-                os.makedirs(d)
-        print 'Done!'
-
-        # add existing dirs
-        directories.update(dict((d, os.path.join(os.getcwd(), 'indico', d)) for d in ['htdocs', 'bin', 'etc', 'doc']))
-
-        self._update_conf_dir_paths(local, directories)
-
-        # avoid modifying the htdocs folder permissions (it brings problems with git)
-        directories.pop('htdocs')
-
-        compile_languages(self)
-
-    def _update_conf_dir_paths(self, filePath, dirs):
-        fdata = open(filePath).read()
-        for dir in dirs.items():
-            d = dir[1].replace("\\", "/")  # For Windows users
-            fdata = re.sub('\/opt\/indico\/%s' % dir[0], d, fdata)
-        open(filePath, 'w').write(fdata)
-
-
-class egg_filename(Command):
-    description = "Get the file name of the generated egg"
-    user_options = []
-    boolean_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        ei_cmd = self.ei_cmd = self.get_finalized_command("egg_info")
-        self.egg_info = ei_cmd.egg_info
-
-        basename = pkg_resources.Distribution(
-            None, None, ei_cmd.egg_name, ei_cmd.egg_version,
-            get_python_version(),
-            self.distribution.has_ext_modules() and pkg_utils.get_build_platform).egg_name()
-
-        print basename
-
-    def run(self):
-        pass
-
-
 if __name__ == '__main__':
-    # Always load source from the current folder
-    sys.path = [os.path.abspath('indico')] + sys.path
-
-    #PWD_INDICO_CONF = 'etc/indico.conf'
-    #if not os.path.exists(PWD_INDICO_CONF):
-    #    shutil.copy('etc/indico.conf.sample', PWD_INDICO_CONF)
-
-    dataFiles = _generateDataPaths((('bin', 'bin'), ('doc', 'doc'), ('etc', 'etc')))
-
-    cmdclass = {'sdist': sdist_indico,
-                'bdist': _bdist_indico(dataFiles),
-                'bdist_egg': _bdist_egg_indico(dataFiles),
-                'develop_config': develop_config,
-                'develop': develop_indico,
-                'egg_filename': egg_filename}
-
-    setup(name="indico",
-          cmdclass=cmdclass,
-          version=get_version(),
-          description="Indico is a full-featured conference lifecycle management and meeting/lecture scheduling tool",
-          author="Indico Team",
-          author_email="indico-team@cern.ch",
-          url="http://indico-software.org",
-          download_url="http://indico-software.org/wiki/Releases/Indico1.2",
-          platforms=["any"],
-          long_description="Indico allows you to schedule conferences, from single talks to complex meetings with "
-                           "sessions and contributions. It also includes an advanced user delegation mechanism, "
-                           "allows paper reviewing, archival of conference information and electronic proceedings",
-          license="http://www.gnu.org/licenses/gpl-3.0.txt",
-          entry_points="""
-            [console_scripts]
-            indico_initial_setup = indico.legacy.consoleScripts.indicoInitialSetup:main
-            indico = indico.cli.manage:main
-            indico-zodbimport = indico_zodbimport.cli:main
-
-            [pytest11]
-            indico = indico.testing.pytest_plugin
-
-            [indico.zodb_importers]
-            roombooking = indico_zodbimport.modules.roombooking:RoomBookingImporter
-            payment = indico_zodbimport.modules.payment:PaymentImporter
-            api = indico_zodbimport.modules.api:APIImporter
-            users = indico_zodbimport.modules.users:UserImporter
-            groups = indico_zodbimport.modules.groups:GroupImporter
-            static_sites = indico_zodbimport.modules.static_sites:StaticSitesImporter
-            event_alarms = indico_zodbimport.modules.event_alarms:EventAlarmImporter
-            legacy_events = indico_zodbimport.modules.legacy_events:LegacyEventImporter
-            legacy_categories = indico_zodbimport.modules.legacy_categories:LegacyCategoryImporter
-            event_logs = indico_zodbimport.modules.event_logs:EventLogImporter
-            event_notes = indico_zodbimport.modules.event_notes:EventNoteImporter
-            attachments = indico_zodbimport.modules.attachments:AttachmentImporter
-            event_images = indico_zodbimport.modules.event_images:EventImageImporter
-            event_stubs = indico_zodbimport.modules.event_stubs:EventStubImporter
-            event_menus = indico_zodbimport.modules.event_menus:EventMenuImporter
-            event_layout = indico_zodbimport.modules.event_layout:EventLayoutImporter
-            event_surveys = indico_zodbimport.modules.event_surveys:SurveyImporter
-            event_managers = indico_zodbimport.modules.event_managers:EventManagerImporter
-            event_regforms = indico_zodbimport.modules.event_regforms:EventRegformImporter
-            event_participants = indico_zodbimport.modules.event_participants:EventParticipantsImporter
-            event_timetable = indico_zodbimport.modules.event_timetable:EventTimetableImporter
-            event_locations = indico_zodbimport.modules.event_locations:EventLocationsImporter
-            event_categories = indico_zodbimport.modules.event_categories:EventCategoriesImporter
-            event_dates_titles = indico_zodbimport.modules.event_dates_titles:EventDatesTitlesImporter
-            event_abstracts_zodb = indico_zodbimport.modules.event_abstracts_zodb:EventAbstractZODBPatcher
-            event_paper_reviewing_legacy = indico_zodbimport.modules.event_paper_reviewing_legacy:LegacyEventPaperReviewingImporter
-            legal = indico_zodbimport.modules.legal:LegalImporter
-            networks = indico_zodbimport.modules.networks:NetworkImporter
-            categories = indico_zodbimport.modules.categories:CategoryImporter
-            event_acls = indico_zodbimport.modules.event_acls:EventACLImporter
-            upcoming_events = indico_zodbimport.modules.upcoming_events:UpcomingEventsImporter
-            news = indico_zodbimport.modules.news:NewsImporter
-            event_types = indico_zodbimport.modules.event_types:EventTypeImporter
-            user_management = indico_zodbimport.modules.user_management:UserManagementImporter
-            event_series = indico_zodbimport.modules.event_series:EventSeriesImporter
-            event_keywords_ctimes_visibility = indico_zodbimport.modules.event_keywords_ctimes_visibility:EventKeywordsCreationTimesVisibilityImporter
-            event_abstracts = indico_zodbimport.modules.event_abstracts:EventAbstractsImporter
-            event_papers = indico_zodbimport.modules.event_papers:EventPapersImporter
-            event_shorturls = indico_zodbimport.modules.event_shorturls:EventShorturlsImporter
-            global_ip_acl = indico_zodbimport.modules.global_ip_acl:GlobalIPACLImporter
-            global_settings = indico_zodbimport.modules.global_settings:GlobalSettingsImporter
-            badges_posters = indico_zodbimport.modules.badges_posters:BadgePosterImporter
-            event_data = indico_zodbimport.modules.event_data:EventDataImporter
-            """,
-          zip_safe=False,
-          packages=find_packages(),
-          package_data={'indico': ['*.*']},
-          include_package_data=True,
-          install_requires=get_requirements(),
-          data_files=dataFiles,
-          dependency_links=DEPENDENCY_URLS)
+    setup(
+        name='indico',
+        version=get_version(),
+        description='Indico is a full-featured conference lifecycle management and meeting/lecture scheduling tool',
+        author='Indico Team',
+        author_email='indico-team@cern.ch',
+        url='http://indico-software.org',
+        download_url='http://indico-software.org/wiki/Releases/Indico1.2',
+        platforms=['any'],
+        long_description="Indico allows you to schedule conferences, from single talks to complex meetings with "
+                         "sessions and contributions. It also includes an advanced user delegation mechanism, "
+                         "allows paper reviewing, archival of conference information and electronic proceedings",
+        license='https://www.gnu.org/licenses/gpl-3.0.txt',
+        zip_safe=False,
+        packages=find_packages(),
+        include_package_data=True,
+        install_requires=get_requirements(),
+        dependency_links=DEPENDENCY_URLS,
+        entry_points=ENTRY_POINTS)
