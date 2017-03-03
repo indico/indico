b2fcad3fd3b9c4a9e59f78ec6b34ecd687707492
Adrian Moennich <adrian.moennich@cern.ch>
2017-03-03 16:44:01 +0100

    Do not suggest pkgdir config location in prod

---
+++ indico/util/packaging.py
@@ -0,0 +1,33 @@
+# This file is part of Indico.
+# Copyright (C) 2002 - 2017 European Organization for Nuclear Research (CERN).
+#
+# Indico is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+#
+# Indico is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Indico; if not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+
+import os
+import sys
+
+import pkg_resources
+
+
+def package_is_editable(package):
+    """Check whether the Python package is installed in 'editable' mode"""
+    # based on pip.dist_is_editable
+    dist = pkg_resources.get_distribution(package)
+    for path_item in sys.path:
+        egg_link = os.path.join(path_item, dist.project_name + '.egg-link')
+        if os.path.isfile(egg_link):
+            return True
+    return False
