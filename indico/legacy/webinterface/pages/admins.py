2fc3cf17dda173d11c55eacdd3f915f4bc2d9a0f
Adrian Moennich <adrian.moennich@cern.ch>
2017-05-12 15:15:34 +0200

    Use Jinja instead of legacy AdminFrame

--- indico/legacy/webinterface/pages/admins.py
+++ indico/legacy/webinterface/pages/admins.py
@@ -14,59 +14,23 @@
 # You should have received a copy of the GNU General Public License
 # along with Indico; if not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import unicode_literals
+
+from flask import render_template_string
+
 from indico.legacy.webinterface import wcomponents
 from indico.legacy.webinterface.pages.main import WPMainBase
-from indico.legacy.webinterface.wcomponents import render_header
 from indico.util.i18n import _
-from indico.web.menu import render_sidemenu
 
 
 class WPAdminsBase(WPMainBase):
-    def getJSFiles(self):
-        return WPMainBase.getJSFiles(self) + self._includeJSPackage('Management')
-
-    def _getHeader(self):
-        return render_header()
-
     def _getBody(self, params):
-        self._createTabCtrl()
-        self._setActiveTab()
-
-        frame = WAdminFrame()
-
-        return frame.getHTML({
-            "body": self._getPageContent(params),
-            "sideMenu": render_sidemenu('admin-sidemenu', active_item=self.sidemenu_option)
-        })
+        body = self._getPageContent(params)
+        tpl = "{% extends 'layout/admin_page.html' %}{% block content %}{{ body | safe }}{% endblock %}"
+        return render_template_string(tpl, active_menu_item=self.sidemenu_option, body=body)
 
     def _getNavigationDrawer(self):
-        return wcomponents.WSimpleNavigationDrawer(_("Administration"), bgColor="white")
-
-    def _createTabCtrl(self):
-        pass
-
-    def _getTabContent(self):
-        raise NotImplementedError
-
-    def _setActiveTab(self):
-        pass
+        return wcomponents.WSimpleNavigationDrawer(_('Administration'), bgColor='white')
 
     def _getPageContent(self, params):
         raise NotImplementedError
-
-
-class WAdminFrame(wcomponents.WTemplated):
-    def __init__( self ):
-        pass
-
-    def getVars( self ):
-        vars = wcomponents.WTemplated.getVars( self )
-        vars["titleTabPixels"] = self.getTitleTabPixels()
-        vars["intermediateVTabPixels"] = self.getIntermediateVTabPixels()
-        return vars
-
-    def getIntermediateVTabPixels( self ):
-        return 0
-
-    def getTitleTabPixels( self ):
-        return 260
--- indico/legacy/webinterface/tpls/AdminFrame.tpl
+++
@@ -1,9 +0,0 @@
-<div class="layout-side-menu">
-    <div class="menu-column">
-        ${ sideMenu }
-    </div>
-    <div class="content-column">
-        ${ render_template('flashed_messages.html') }
-        ${ body }
-    </div>
-</div>
