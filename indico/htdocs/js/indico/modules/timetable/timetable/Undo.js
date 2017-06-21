d48d8fd458f79e8f3bea73596cea2078ab215d93
Ilias Trichopoulos <ilias.trichopoulos@cern.ch>
2017-06-21 16:15:45 +0200

    Fix display of undo icon

--- indico/htdocs/js/indico/modules/timetable/timetable/Undo.js
+++ indico/htdocs/js/indico/modules/timetable/timetable/Undo.js
@@ -19,7 +19,8 @@ type("UndoMixin", [], {
 
     updateUndoDiv: function(tt_status_info) {
         /* A "button" that appears after an action is performed */
-        var undo_contents =  $('<a href="#" class="i-button icon-undo warning"/>').text($T('Undo last operation')).
+        // TODO: remove the class 'hidden' once Undo is working fine again
+        var undo_contents =  $('<a href="#" class="i-button icon-undo warning hidden"/>').text($T('Undo last operation')).
             click(function() {
                 // "Undo" should be executed over the _current_ timetable, not the original source of
                 // the event. Being so, we call a global event that will then invoke the correct TT
