f9a190204da7b54b5b09710655e131bf4414c8c0
Adrian Moennich <adrian.moennich@cern.ch>
2017-07-13 11:21:40 +0200

    Check for the correct database encoding

--- indico/core/db/sqlalchemy/migration.py
+++ indico/core/db/sqlalchemy/migration.py
@@ -79,6 +79,15 @@ def _require_pg_version(version):
     return False
 
 
+def _require_encoding(encoding):
+    cur_encoding = db.engine.execute("SELECT current_setting('server_encoding')").scalar()
+    if cur_encoding >= encoding:
+        return True
+    print cformat('%{red}Database encoding must be {}; got {}').format(encoding, cur_encoding)
+    print cformat('%{yellow}Recreate your database using `createdb -E {} -T template0 ...`').format(encoding)
+    return False
+
+
 def prepare_db(empty=False, root_path=None, verbose=True):
     """Initialize an empty database (create tables, set alembic rev to HEAD)."""
     root_path = root_path or current_app.root_path
@@ -113,6 +122,8 @@ def prepare_db(empty=False, root_path=None, verbose=True):
                     print cformat('  * %{cyan}{}%{reset}.%{cyan!}{}%{reset}').format(schema, t)
         return
     if not _require_pg_version('9.4'):
+        return
+    if not _require_encoding('UTF8'):
         return
     if not _require_extensions('unaccent', 'pg_trgm'):
         return
