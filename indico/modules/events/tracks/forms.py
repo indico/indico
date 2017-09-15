c729332d10d973a943e97b2be00aaa5039bb432f
Marco Vidal Garcia <marco.vidal@cern.ch>
2017-09-15 14:38:58 +0200

    Add session selector to Programme interface

--- indico/modules/events/tracks/forms.py
+++ indico/modules/events/tracks/forms.py
@@ -16,10 +16,12 @@
 
 from __future__ import unicode_literals
 
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
 from wtforms.fields import StringField
 from wtforms.validators import DataRequired
 
 from indico.core.db.sqlalchemy.descriptions import RenderMode
+from indico.modules.events.sessions.models.sessions import Session
 from indico.util.i18n import _
 from indico.web.forms.base import IndicoForm, generated_data
 from indico.web.forms.fields import IndicoMarkdownField
@@ -28,7 +30,15 @@ from indico.web.forms.fields import IndicoMarkdownField
 class TrackForm(IndicoForm):
     title = StringField(_('Title'), [DataRequired()])
     code = StringField(_('Code'))
+    default_session = QuerySelectField(_('Default session'), default='', allow_blank=True, get_label='title',
+                                       description=_('Indico will preselect this session whenever an abstract is '
+                                                     'accepted for the track'))
     description = IndicoMarkdownField(_('Description'), editor=True)
+
+    def __init__(self, *args, **kwargs):
+        event = kwargs.pop('event')
+        super(TrackForm, self).__init__(*args, **kwargs)
+        self.default_session.query = Session.query.with_parent(event)
 
 
 class ProgramForm(IndicoForm):
