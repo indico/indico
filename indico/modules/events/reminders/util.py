d984b15db3ecfd654d1bd935d00fc92833555bf6
Björn Pedersen <bjoern.pedersen@frm2.tum.de>
2017-11-17 10:48:26 +0100

    Event reminders: Allow to include description
    For some reminders the event descritpion is a useful addition,
    so allow to include it.

--- indico/modules/events/reminders/util.py
+++ indico/modules/events/reminders/util.py
@@ -20,18 +20,13 @@ from indico.modules.events.models.events import EventType
 from indico.web.flask.templating import get_template_module
 
 
-def make_reminder_email(event, with_agenda, note):
+def make_reminder_email(event, with_agenda, with_description, note):
     """Returns the template module for the reminder email.
 
     :param event: The event
     :param with_agenda: If the event's agenda should be included
     :param note: A custom message to include in the email
     """
-    if event.type_ == EventType.lecture:
-        with_description = with_agenda
-        with_agenda = False
-    else:
-        with_description = False
     return get_template_module('events/reminders/emails/event_reminder.txt', event=event,
                                url=event.short_external_url, note=note, with_agenda=with_agenda,
                                with_description = with_description,
