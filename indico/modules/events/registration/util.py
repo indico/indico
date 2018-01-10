14a8ff0892119146d31f99b99fcb2570735dd6b9
Marco Vidal Garcia <marco.vidal@cern.ch>
2018-01-10 14:17:22 +0100

    Prevent repeated positions when disabling sections

--- indico/modules/events/registration/util.py
+++ indico/modules/events/registration/util.py
@@ -16,7 +16,9 @@
 
 from __future__ import unicode_literals
 
+import itertools
 from collections import OrderedDict
+from operator import attrgetter
 
 from flask import current_app, json, session
 from qrcode import QRCode, constants
@@ -493,3 +495,18 @@ def generate_ticket(registration):
 
 def get_ticket_attachments(registration):
     return [('Ticket.pdf', generate_ticket(registration).getvalue())]
+
+
+def update_regform_item_positions(regform):
+    """Update positions when deleting/disabling an item in order to prevent gaps"""
+    section_positions = itertools.count(1)
+    disabled_section_positions = itertools.count(1000)
+    for section in sorted(regform.sections, key=attrgetter('position')):
+        section_active = section.is_enabled and not section.is_deleted
+        section.position = next(section_positions if section_active else disabled_section_positions)
+        # ensure consistent field ordering
+        positions = itertools.count(1)
+        disabled_positions = itertools.count(1000)
+        for child in section.children:
+            child_active = child.is_enabled and not child.is_deleted
+            child.position = next(positions if child_active else disabled_positions)
