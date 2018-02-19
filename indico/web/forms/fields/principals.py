7853d39f62f0bf807309deef75b468c782983eda
Marco Vidal Garcia <marco.vidal@cern.ch>
2018-02-19 18:42:17 +0100

    Prevent widget from failing when acl is empty

--- indico/web/forms/fields/principals.py
+++ indico/web/forms/fields/principals.py
@@ -158,4 +158,4 @@ class PermissionsField(JSONField):
         return get_permissions_info(PermissionsField.type_mapping[self.object_type])[0]
 
     def _value(self):
-        return self.data if self.data else '[]'
+        return self.data if self.data else None
