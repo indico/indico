22285b37aa558e41c2223fc44416c827e2b12091
Adrian Moennich <adrian.moennich@cern.ch>
2018-03-06 10:53:46 +0100

    Do not use webassets for custom assets
    This means custom assets from CUSTOMIZATION_DIR are now plain JS/CSS,
    without any postprocessing/bundling/etc.

--- indico/web/assets/util.py
+++ indico/web/assets/util.py
@@ -17,6 +17,10 @@
 from __future__ import unicode_literals
 
 import os
+from glob import glob
+
+from indico.core.config import config
+from indico.web.flask.util import url_for
 
 
 def get_asset_path(path, theme=None):
@@ -25,3 +29,20 @@ def get_asset_path(path, theme=None):
     else:
         base = 'core'
     return os.path.join(base, path)
+
+
+def _get_custom_files(subdir, pattern):
+    customization_dir = config.CUSTOMIZATION_DIR
+    if not customization_dir:
+        return []
+    customization_dir = os.path.join(customization_dir, subdir)
+    if not os.path.exists(customization_dir):
+        return []
+    return sorted(os.path.relpath(x, customization_dir) for x in glob(os.path.join(customization_dir, pattern)))
+
+
+def get_custom_assets(type_):
+    if type_ not in ('js', 'css'):
+        raise ValueError('Invalid custom asset type')
+    return [url_for('assets.custom', folder=type_, filename=name)
+            for name in _get_custom_files(type_, '*.{}'.format(type_))]
