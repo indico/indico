193a2dbb5815ecbd7b2b86101ab2118306ef2bfa
Adrian Moennich <adrian.moennich@cern.ch>
2018-03-07 12:31:39 +0100

    Fix mathjax paths

--- indico/web/client/js/jquery/compat/mathjax.js
+++ indico/web/client/js/jquery/compat/mathjax.js
@@ -16,5 +16,10 @@
  */
 
 import 'mathjax';
-
 import '../utils/pagedown_mathjax';
+
+// pretend this is not an "unpacked" setup
+MathJax.isPacked = true;
+// the default values for these paths use isPacked before we can set it
+MathJax.OutputJax.fontDir = '[MathJax]/fonts';
+MathJax.OutputJax.imageDir = '[MathJax]/images';
