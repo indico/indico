984f9899608509f26141a8e8fa12a04eb4eacf1c
Adrian Moennich <adrian.moennich@cern.ch>
2018-03-12 18:07:07 +0100

    Fix custom css/js handling in static sites
    Also use a fake manifest entry to handle these custom files so more code
    can be reused for asset and customization files

--- indico/core/webpack.py
+++ indico/core/webpack.py
@@ -16,7 +16,28 @@
 
 from __future__ import unicode_literals
 
+from flask import current_app
 from flask_webpackext import FlaskWebpackExt
+from flask_webpackext.manifest import JinjaManifestLoader
+from pywebpack import ManifestLoader
+
+from indico.web.assets.util import get_custom_assets
+
+
+class IndicoManifestLoader(JinjaManifestLoader):
+    def load(self, filepath):
+        if current_app.debug or filepath not in JinjaManifestLoader.cache:
+            JinjaManifestLoader.cache[filepath] = manifest = ManifestLoader.load(self, filepath)
+            self._add_custom_assets(manifest)
+        return JinjaManifestLoader.cache[filepath]
+
+    def _add_custom_assets(self, manifest):
+        # custom assets (from CUSTOMIZATION_DIR) are not part of the webpack manifest
+        # since they are not build with webpack (it's generally not available on the
+        # machine running indico), but we add them here anyway so they can be handled
+        # without too much extra code, e.g. when building a static site.
+        manifest.add(self.entry_cls('__custom.css', get_custom_assets('css')))
+        manifest.add(self.entry_cls('__custom.js', get_custom_assets('js')))
 
 
 webpack = FlaskWebpackExt()
