2a31369fcea9d2034c3c7e8ba37e14129045c721
Pedro Ferreira <pedro.ferreira@cern.ch>
2018-03-16 17:52:47 +0100

    Event log: add text filtering

--- indico/modules/events/logs/controllers.py
+++ indico/modules/events/logs/controllers.py
@@ -20,12 +20,19 @@ from collections import defaultdict
 
 from flask import jsonify, request
 
+from indico.core.db import db
+from indico.core.db.sqlalchemy.util.queries import preprocess_ts_string
 from indico.modules.events.logs.models.entries import EventLogEntry, EventLogRealm
 from indico.modules.events.logs.util import serialize_log_entry
 from indico.modules.events.logs.views import WPEventLogs
 from indico.modules.events.management.controllers import RHManageEventBase
 
 LOG_PAGE_SIZE = 10
+
+
+def _contains(field, text):
+    return db.func.to_tsvector('simple', field).match(preprocess_ts_string(text),
+                                                      postgresql_regconfig='simple')
 
 
 class RHEventLogs(RHManageEventBase):
@@ -42,12 +49,27 @@ class RHEventLogsJSON(RHManageEventBase):
         entries = defaultdict(list)
         page = int(request.args.get('page', 1))
         filters = request.args.getlist('filters')
+        text = request.args.get('q')
+
         query = self.event.log_entries.order_by(EventLogEntry.logged_dt.desc())
 
         if filters:
             realms = {EventLogRealm.get(f) for f in filters if EventLogRealm.get(f)}
             if realms:
                 query = query.filter(EventLogEntry.realm.in_(realms))
+
+        if text:
+            query = query.filter(
+                db.or_(_contains(EventLogEntry.module, text),
+                       _contains(EventLogEntry.type, text),
+                       _contains(EventLogEntry.summary, text),
+                       _contains(db.m.User.first_name + " " + db.m.User.last_name, text),
+                       _contains(EventLogEntry.data['body'].astext, text),
+                       _contains(EventLogEntry.data['subject'].astext, text),
+                       _contains(EventLogEntry.data['from'].astext, text),
+                       _contains(EventLogEntry.data['to'].astext, text),
+                       _contains(EventLogEntry.data['cc'].astext, text))
+            ).outerjoin(db.m.User)
 
         query = query.paginate(page, LOG_PAGE_SIZE)
 
