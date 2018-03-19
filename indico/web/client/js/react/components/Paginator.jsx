aae116471064a7da5e7eef85430ebfebb30669b4
Pedro Ferreira <pedro.ferreira@cern.ch>
2018-03-19 11:32:11 +0100

    Move paginator scss to components dir

--- indico/web/client/js/react/components/Paginator.jsx
+++ indico/web/client/js/react/components/Paginator.jsx
@@ -19,6 +19,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import IButton from "../IButton";
 
+import './style/paginator.scss';
+
 export default class Paginator extends React.Component {
     static propTypes = {
         currentPage: PropTypes.number.isRequired,
