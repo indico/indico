e0b9a4499534a952b1eb9385fb924a8ea310deb6
Pedro Ferreira <pedro.ferreira@cern.ch>
2018-04-09 18:27:56 +0200

    Move reducer handling to util function

---
+++ indico/web/client/js/utils/redux.js
@@ -0,0 +1,35 @@
+/* This file is part of Indico.
+ * Copyright (C) 2002 - 2018 European Organization for Nuclear Research (CERN).
+ *
+ * Indico is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Indico is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Indico; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import loggerMiddleware from 'redux-logger';
+import {composeWithDevTools} from 'redux-devtools-extension';
+
+
+export default function createReduxStore(reducers, initialData = {}) {
+    const middleware = [thunkMiddleware];
+    if (process.env.NODE_ENV === 'development') {
+        middleware.push(loggerMiddleware);
+    }
+    const enhancer = composeWithDevTools(applyMiddleware(...middleware));
+
+    return createStore(combineReducers({
+        staticData: (state = {}) => state,
+        ...reducers
+    }), initialData, enhancer);
+}
