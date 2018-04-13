31c543c81d8897a4c6e6de468b0698dda1b68d88
Adrian Moennich <adrian.moennich@cern.ch>
2018-04-13 14:53:03 +0200

    Add link to admin area

--- indico/web/client/js/react/containers/UserMenu.jsx
+++ indico/web/client/js/react/containers/UserMenu.jsx
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with Indico; if not, see <http://www.gnu.org/licenses/>.
  */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider, connect} from 'react-redux';
