63ec256aab60ebeaabdeea8a0ef176c1a1ecfd37
Michal Kolodziejski <michal.kolodziejski@cern.ch>
2018-04-24 12:39:31 +0200

    Fix the user avatar label in the top menu

--- indico/web/client/js/react/components/UserMenu.jsx
+++ indico/web/client/js/react/components/UserMenu.jsx
@@ -52,7 +52,7 @@ export default function UserMenu({userData, staticData}) {
     const {availableLanguages} = staticData;
     const {firstName, lastName, email, language, isAdmin, avatarBgColor} = userData;
     const avatar = (
-        <Label style={{
+        <Label circular size="large" style={{
             backgroundColor: avatarBgColor
         }} styleName="user-menu-avatar">
             {firstName[0]}
