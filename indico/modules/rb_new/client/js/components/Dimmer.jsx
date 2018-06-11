c7d98f3b216bcb6d203e3988ea51ffc962fe5f3b
Natalia Juszka <natalia.karina.juszka@cern.ch>
2018-06-11 19:30:58 +0200

    Add DimmableImage component

---
+++ indico/modules/rb_new/client/js/components/Dimmer.jsx
@@ -0,0 +1,51 @@
+/* This file is part of Indico.
+ * Copyright (C) 2002 - 2018 European Organization for Nuclear Research (CERN).
+ *
+ * Indico is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Indico is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Indico; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Image} from 'semantic-ui-react';
+
+import './Dimmer.module.scss';
+
+
+export default class DimmableImage extends React.Component {
+    static propTypes = {
+        src: PropTypes.string.isRequired,
+        content: PropTypes.node,
+        hoverContent: PropTypes.node
+    };
+
+    static defaultProps = {
+        content: null,
+        hoverContent: null
+    };
+
+    render() {
+        const {src, content, hoverContent} = this.props;
+        return (
+            <div styleName="dimmable-image">
+                <Image src={src} />
+                <div styleName="content">
+                    {content}
+                </div>
+                <div styleName="hover-content">
+                    {hoverContent}
+                </div>
+            </div>
+        );
+    }
+}
