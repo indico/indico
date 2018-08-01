d9eb93681b9e2897bf2435d2798b03a53fcfeeb0
Pedro Ferreira <pedro.ferreira@cern.ch>
2018-08-01 11:22:19 +0200

    Refactor Timeline, split functionality

---
+++ indico/modules/rb_new/client/js/components/BookingTimeline.jsx
@@ -0,0 +1,176 @@
+/* This file is part of Indico.
+ * Copyright (C) 2002 - 2018 European Organization for Nuclear Research (CERN).
+ *
+ * Indico is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Indico is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Indico; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+import _ from 'lodash';
+import moment from 'moment';
+import PropTypes from 'prop-types';
+import React from 'react';
+import {connect} from 'react-redux';
+import {stateToQueryString} from 'redux-router-querystring';
+import {Label, Message} from 'semantic-ui-react';
+import {Translate} from 'indico/react/i18n';
+import {isDateWithinRange, pushStateMergeProps} from '../util';
+import {queryString as queryStringSerializer} from '../serializers/filters';
+import TimelineBase from './TimelineBase';
+import * as actions from '../actions';
+
+import './Timeline.module.scss';
+
+
+const DATE_FORMAT = 'YYYY-MM-DD';
+const _toMoment = (date) => moment(date, DATE_FORMAT);
+
+
+class BookingTimeline extends React.Component {
+    static propTypes = {
+        minHour: PropTypes.number.isRequired,
+        maxHour: PropTypes.number.isRequired,
+        dateRange: PropTypes.array.isRequired,
+        availability: PropTypes.object.isRequired,
+        pushState: PropTypes.func.isRequired,
+
+        // from redux state
+        isFetching: PropTypes.bool.isRequired,
+        isFetchingRooms: PropTypes.bool.isRequired,
+        recurrenceType: PropTypes.string.isRequired,
+    };
+
+    state = {};
+
+    static getDerivedStateFromProps({dateRange}, state) {
+        if (!_.isEmpty(dateRange) && !isDateWithinRange(state.activeDate, dateRange, _toMoment)) {
+            return {...state, activeDate: _toMoment(dateRange[0])};
+        } else {
+            return state;
+        }
+    }
+
+    calcRows = () => {
+        const {activeDate} = this.state;
+        const {availability, dateRange} = this.props;
+
+        if (!activeDate) {
+            return [];
+        }
+
+        let rows = [];
+
+        if (Object.keys(availability).length === 1) {
+            const roomId = Object.keys(availability)[0];
+            const roomAvailability = availability[roomId];
+            const room = roomAvailability.room;
+
+            rows = dateRange.map((dt) => {
+                const av = {
+                    candidates: roomAvailability.candidates[dt].map((cand) => ({...cand, bookable: true})) || [],
+                    preBookings: roomAvailability.pre_bookings[dt] || [],
+                    bookings: roomAvailability.bookings[dt] || [],
+                    conflicts: roomAvailability.conflicts[dt] || [],
+                    preConflicts: roomAvailability.pre_conflicts[dt] || [],
+                    blockings: roomAvailability.blockings[dt] || [],
+                    nonbookablePeriods: roomAvailability.nonbookable_periods[dt] || [],
+                    unbookableHours: roomAvailability.unbookable_hours || []
+                };
+                return {availability: av, label: dt, conflictIndicator: true, id: dt, room};
+            });
+        } else {
+            const dt = activeDate.format('YYYY-MM-DD');
+            rows = Object.values(availability).map((roomAvailability) => {
+                const av = {
+                    candidates: roomAvailability.candidates[dt].map((cand) => ({...cand, bookable: true})) || [],
+                    preBookings: roomAvailability.pre_bookings[dt] || [],
+                    bookings: roomAvailability.bookings[dt] || [],
+                    conflicts: roomAvailability.conflicts[dt] || [],
+                    preConflicts: roomAvailability.pre_conflicts[dt] || [],
+                    blockings: roomAvailability.blockings[dt] || [],
+                    nonbookablePeriods: roomAvailability.nonbookable_periods[dt] || [],
+                    unbookableHours: roomAvailability.unbookable_hours || []
+                };
+
+                const room = roomAvailability.room;
+                return {availability: av, label: room.full_name, conflictIndicator: true, id: room.id, room};
+            });
+        }
+
+        return rows;
+    };
+
+    openBookingModal = (room) => {
+        const {pushState} = this.props;
+        pushState(`/book/${room.id}/confirm`, true);
+    };
+
+    render() {
+        const {dateRange, maxHour, minHour, isFetching, isFetchingRooms, recurrenceType} = this.props;
+        const {activeDate} = this.state;
+        const legend = (
+            <>
+                <Label color="green">{Translate.string('Available')}</Label>
+                <Label color="orange">{Translate.string('Booked')}</Label>
+                <Label styleName="pre-booking">{Translate.string('Pre-Booking')}</Label>
+                <Label color="red">{Translate.string('Conflict')}</Label>
+                <Label styleName="pre-booking-conflict">{Translate.string('Conflict with Pre-Booking')}</Label>
+                <Label styleName="blocking">{Translate.string('Blocked')}</Label>
+                <Label styleName="unbookable">{Translate.string('Not bookable')}</Label>
+            </>
+        );
+        const emptyMessage = (
+            <Message warning>
+                <Translate>
+                    There are no rooms matching the criteria.
+                </Translate>
+            </Message>
+        );
+
+        return (
+            <TimelineBase rows={this.calcRows()}
+                          legend={legend}
+                          emptyMessage={emptyMessage}
+                          onClick={this.openBookingModal}
+                          dateRange={dateRange}
+                          minHour={minHour}
+                          maxHour={maxHour}
+                          activeDate={activeDate}
+                          onDateChange={(newDate) => {
+                              this.setState({
+                                  activeDate: newDate
+                              });
+                          }}
+                          isFetching={isFetching}
+                          isFetchingRooms={isFetchingRooms}
+                          recurrenceType={recurrenceType} />
+        );
+    }
+}
+
+export default connect(
+    ({bookRoom}) => {
+        return {
+            ...bookRoom.timeline,
+            recurrenceType: bookRoom.filters.recurrence.type,
+            queryString: stateToQueryString(bookRoom, queryStringSerializer),
+            isFetchingRooms: bookRoom.rooms.isFetching
+        };
+    },
+    dispatch => ({
+        resetBookingState: () => {
+            dispatch(actions.resetBookingState());
+        },
+        dispatch
+    }),
+    pushStateMergeProps
+)(BookingTimeline);
---
+++ indico/modules/rb_new/client/js/components/TimelineBase.jsx
@@ -0,0 +1,152 @@
+/* This file is part of Indico.
+ * Copyright (C) 2002 - 2018 European Organization for Nuclear Research (CERN).
+ *
+ * Indico is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Indico is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Indico; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+import _ from 'lodash';
+import moment from 'moment';
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Button, Container, Label, Loader, Message, Segment} from 'semantic-ui-react';
+import DatePicker from 'rc-calendar/lib/Picker';
+import Calendar from 'rc-calendar';
+import {Translate} from 'indico/react/i18n';
+import TimelineContent from './TimelineContent';
+import {isDateWithinRange} from '../util';
+
+import './Timeline.module.scss';
+
+
+const DATE_FORMAT = 'YYYY-MM-DD';
+const _toMoment = (date) => moment(date, DATE_FORMAT);
+
+export default class TimelineBase extends React.Component {
+    static propTypes = {
+        activeDate: PropTypes.instanceOf(moment),
+        onDateChange: PropTypes.func.isRequired,
+        rows: PropTypes.arrayOf(PropTypes.object).isRequired,
+        legend: PropTypes.node.isRequired,
+        emptyMessage: PropTypes.node,
+        minHour: PropTypes.number.isRequired,
+        maxHour: PropTypes.number.isRequired,
+        step: PropTypes.number,
+        onClick: PropTypes.func,
+        dateRange: PropTypes.array.isRequired,
+        isFetching: PropTypes.bool.isRequired,
+        isFetchingRooms: PropTypes.bool.isRequired,
+        recurrenceType: PropTypes.string.isRequired,
+    };
+
+    static defaultProps = {
+        emptyMessage: (
+            <Message warning>
+                <Translate>
+                    There are no rooms matching the criteria.
+                </Translate>
+            </Message>
+        ),
+        step: 2,
+        onClick: null,
+        activeDate: null
+    };
+
+    calendarDisabledDate = (date) => {
+        const {dateRange} = this.props;
+        if (!date) {
+            return false;
+        }
+        return !isDateWithinRange(date, dateRange, _toMoment);
+    };
+
+    changeSelectedDate = (mode) => {
+        const {activeDate, dateRange, onDateChange} = this.props;
+        const index = dateRange.findIndex((dt) => _toMoment(dt).isSame(activeDate)) + (mode === 'next' ? 1 : -1);
+        onDateChange(_toMoment(dateRange[index]));
+    };
+
+    onSelect = (date) => {
+        const {dateRange, onDateChange} = this.props;
+        const startDate = _toMoment(dateRange[0]);
+        const endDate = _toMoment(dateRange[dateRange.length - 1]);
+
+        if (date.isBefore(_toMoment(startDate)) || date.isAfter(_toMoment(endDate))) {
+            return;
+        } else if (isDateWithinRange(date, dateRange, _toMoment)) {
+            onDateChange(date);
+        }
+    };
+
+    renderTimeline = () => {
+        const {activeDate, dateRange, legend, maxHour, minHour, onClick, recurrenceType, rows, step} = this.props;
+        const hourSeries = _.range(minHour, maxHour + step, step);
+        const calendar = <Calendar disabledDate={this.calendarDisabledDate} onChange={this.onSelect} />;
+        const startDate = _toMoment(dateRange[0]);
+        const endDate = _toMoment(dateRange[dateRange.length - 1]);
+
+        return (
+            <>
+                <Segment styleName="legend" basic>
+                    <Label.Group as="span" size="large" styleName="labels">
+                        {legend}
+                    </Label.Group>
+                    {rows.length > 1 && (
+                        <Button.Group floated="right" size="small">
+                            <Button icon="left arrow"
+                                    onClick={() => this.changeSelectedDate('prev')}
+                                    disabled={activeDate.clone().subtract(1, 'day').isBefore(startDate)} />
+                            <DatePicker calendar={calendar}>
+                                {
+                                    () => (
+                                        <Button primary>
+                                            {activeDate.format(DATE_FORMAT)}
+                                        </Button>
+                                    )
+                                }
+                            </DatePicker>
+                            <Button icon="right arrow"
+                                    onClick={() => this.changeSelectedDate('next')}
+                                    disabled={activeDate.clone().add(1, 'day').isAfter(endDate)} />
+                        </Button.Group>
+                    )}
+                </Segment>
+                <div styleName="timeline">
+                    <TimelineContent rows={rows}
+                                     hourSeries={hourSeries}
+                                     recurrenceType={recurrenceType}
+                                     onClick={onClick} />
+                </div>
+            </>
+        );
+    };
+
+    renderContent = () => {
+        const {isFetching, isFetchingRooms, emptyMessage, rows} = this.props;
+        if (isFetching || isFetchingRooms) {
+            return <Loader active />;
+        } else if (!_.isEmpty(rows)) {
+            return this.renderTimeline();
+        } else {
+            return emptyMessage;
+        }
+    };
+
+    render() {
+        return (
+            <Container>
+                {this.renderContent()}
+            </Container>
+        );
+    }
+}
