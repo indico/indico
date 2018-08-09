184498b4d9d3ed9fb81780bfa5042c3f0dc67040
Adrian Moennich <adrian.moennich@cern.ch>
2018-08-09 15:22:36 +0200

    Move menu signal handlers (core -> modules/core)

--- indico/modules/core/__init__.py
+++ indico/modules/core/__init__.py
@@ -21,7 +21,19 @@ from flask import session
 from indico.core import signals
 from indico.util.i18n import _
 from indico.web.flask.util import url_for
-from indico.web.menu import SideMenuItem
+from indico.web.menu import SideMenuItem, TopMenuItem, TopMenuSection
+from indico.web.util import url_for_index
+
+
+@signals.menu.sections.connect_via('top-menu')
+def _topmenu_sections(sender, **kwargs):
+    yield TopMenuSection('services', _('Services'), 60)
+    yield TopMenuSection('help', _('Help'), -10)  # put the help section always last
+
+
+@signals.menu.items.connect_via('top-menu')
+def _topmenu_items(sender, **kwargs):
+    yield TopMenuItem('home', _('Home'), url_for_index(), 100)
 
 
 @signals.menu.items.connect_via('admin-sidemenu')
