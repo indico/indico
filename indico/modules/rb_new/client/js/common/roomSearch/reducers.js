fdfcdf64daa2ef39e8aec46b7dad5ee7956e423a
Adrian Moennich <adrian.moennich@cern.ch>
2018-09-14 12:47:49 +0200

    Move roomList stuff to roomSearch & add namespace
    we'll need the same for bookRoom

---
+++ indico/modules/rb_new/client/js/common/roomSearch/reducers.js
@@ -0,0 +1,47 @@
+/* This file is part of Indico.
+ * Copyright (C) 2002 - 2018 European Organization for Nuclear Research (CERN).
+ *
+ * Indico is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Indico is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Indico; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {combineReducers} from 'redux';
+import {requestReducer} from 'indico/utils/redux';
+
+import filterReducerFactory, {initialRoomFilterStateFactory} from '../../reducers/roomBooking/filters';
+import {mapReducerFactory} from '../../reducers/roomBooking/map';
+import {roomSearchActionsFactory} from './actions';
+
+
+export function roomSearchReducerFactory(namespace) {
+    const actions = roomSearchActionsFactory(namespace);
+    return combineReducers({
+        search: combineReducers({
+            request: requestReducer(
+                actions.SEARCH_ROOMS_REQUEST,
+                actions.SEARCH_ROOMS_SUCCESS,
+                actions.SEARCH_ROOMS_ERROR
+            ),
+            results: (state = [], action) => {
+                switch (action.type) {
+                    case actions.SEARCH_RESULTS_RECEIVED:
+                        return action.data;
+                    default:
+                        return state;
+                }
+            }
+        }),
+        filters: filterReducerFactory(namespace, initialRoomFilterStateFactory),
+        map: mapReducerFactory(namespace),
+    });
+}
