9146c2c940efd35948569495d22f978093fa49f2
Adrian Moennich <adrian.moennich@cern.ch>
2018-10-10 17:29:13 +0200

    Exclude inactive rooms from room picker
    Also sort the rooms properly

--- indico/modules/rb_new/controllers/backend/locations.py
+++ indico/modules/rb_new/controllers/backend/locations.py
@@ -17,8 +17,10 @@
 from __future__ import unicode_literals
 
 from flask import jsonify
+from sqlalchemy.orm import contains_eager
 
-from indico.modules.rb import Location
+from indico.core.db import db
+from indico.modules.rb import Location, Room
 from indico.modules.rb.controllers import RHRoomBookingBase
 from indico.modules.rb_new.operations.locations import get_equipment_types
 from indico.modules.rb_new.schemas import aspects_schema, locations_schema
@@ -26,7 +28,11 @@ from indico.modules.rb_new.schemas import aspects_schema, locations_schema
 
 class RHLocations(RHRoomBookingBase):
     def _process(self):
-        locations = Location.query.all()
+        locations = (Location.query
+                     .join(Room, (Location.id == Room.location_id) & Room.is_active)
+                     .options(contains_eager('rooms').noload('*'))
+                     .order_by(Location.name, db.func.indico.natsort(Room.full_name))
+                     .all())
         return jsonify(locations_schema.dump(locations).data)
 
 
