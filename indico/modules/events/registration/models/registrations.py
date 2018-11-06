66b0fca133b9ad90207589b4ac3f1ba7a2ba34e5
penelopec <penelope@fnal.gov>
2018-11-06 10:32:40 -0600

    Update base_price_info column

--- indico/modules/events/registration/models/registrations.py
+++ indico/modules/events/registration/models/registrations.py
@@ -147,10 +147,11 @@ class Registration(db.Model):
         db.String,
         nullable=False
     )
-    #: Base registration fee information (used by PayPal)
+    #: Base registration fee information
     base_price_info = db.Column(
         db.String,
-        nullable=True
+        nullable=False
+        default=''
     )
     #: The date/time when the registration was recorded
     submitted_dt = db.Column(
