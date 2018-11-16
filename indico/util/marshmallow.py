b2e737e36dd43f03185720f92795e3b19276cfca
Adrian Moennich <adrian.moennich@cern.ch>
2018-11-16 17:12:39 +0100

    Add ModelList field for webargs

--- indico/util/marshmallow.py
+++ indico/util/marshmallow.py
@@ -17,8 +17,9 @@
 from __future__ import absolute_import, unicode_literals
 
 from marshmallow import ValidationError
-from marshmallow.fields import DateTime
+from marshmallow.fields import DateTime, Field
 from marshmallow.utils import from_iso
+from sqlalchemy import inspect
 
 
 def _naive_isoformat(dt, **unused):
@@ -48,3 +49,49 @@ class NaiveDateTime(UnicodeDateTime):
     DATEFORMAT_DESERIALIZATION_FUNCS = {
         'iso': _naive_from_iso,
     }
+
+
+class ModelList(Field):
+    """Deserializer for a list of database objects.
+
+    This is meant to be used in webargs, so serialization is not supported.
+    Use the normal types/ModelSchema if you need to serialize.
+    """
+
+    default_error_messages = {
+        'not_found': '"{value}" does not exist',
+        'type': 'Invalid input type.'
+    }
+
+    def __init__(self, model, column=None, column_type=None, **kwargs):
+        self.model = model
+        if column:
+            self.column = getattr(model, column)
+            # Custom column -> most likely a string value
+            self.column_type = column_type or unicode
+        else:
+            pks = inspect(model).primary_key
+            assert len(pks) == 1
+            self.column = pks[0]
+            # Default PK -> most likely an ID
+            self.column_type = column_type or int
+        super(ModelList, self).__init__(**kwargs)
+
+    def _serialize(self, value, attr, obj):
+        raise NotImplementedError
+
+    def _deserialize(self, value, attr, data):
+        if not value:
+            return []
+        try:
+            value = map(self.column_type, value)
+        except (TypeError, ValueError):
+            self.fail('type')
+        requested = set(value)
+        objs = self.model.query.filter(self.column.in_(value)).all()
+        found = {getattr(x, self.column.key) for x in objs}
+        invalid = requested - found
+        if invalid:
+            self.fail('not_found', value=next(iter(invalid)))
+        assert found == requested, 'Unexpected objects found'
+        return objs
