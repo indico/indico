c2db5f4bc67c4d4774c0caea72e1190c8c1dc0ad
Adrian Moennich <adrian.moennich@cern.ch>
2018-11-21 15:07:53 +0100

    Refresh data after leaving RB administration

--- indico/modules/rb_new/client/js/setup.jsx
+++ indico/modules/rb_new/client/js/setup.jsx
@@ -10,6 +10,7 @@ import createRBStore, {history} from './store';
 import {init} from './actions';
 import {selectors as configSelectors} from './common/config';
 import {selectors as userSelectors} from './common/user';
+import {actions as roomsActions} from './common/rooms';
 
 import 'semantic-ui-css/semantic.css';
 import '../styles/main.scss';
@@ -19,6 +20,17 @@ export default function setup(overrides = {}, postReducers = []) {
     document.addEventListener('DOMContentLoaded', () => {
         const appContainer = document.getElementById('rb-app-container');
         const store = createRBStore(overrides, postReducers);
+
+        let oldPath = history.location.pathname;
+        history.listen(({pathname: newPath}) => {
+            if (oldPath.startsWith('/admin') && !newPath.startsWith('/admin')) {
+                // user left the admin area so we need to reload some data that might have been changed
+                // TODO: add more things here once admins can change them (e.g. map areas)
+                store.dispatch(roomsActions.fetchEquipmentTypes());
+                store.dispatch(roomsActions.fetchRooms());
+            }
+            oldPath = newPath;
+        });
 
         store.dispatch(init());
         setupUserMenu(
