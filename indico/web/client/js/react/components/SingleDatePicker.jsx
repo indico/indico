d3969bc0045b4b43a2a0f61efceb6e6989b004d5
Michal Kolodziejski <michal.kolodziejski@cern.ch>
2018-11-23 11:45:17 +0100

    Allow to disable dates in SingleDatePicker

--- indico/web/client/js/react/components/SingleDatePicker.jsx
+++ indico/web/client/js/react/components/SingleDatePicker.jsx
@@ -16,6 +16,7 @@
  */
 
 import 'react-dates/initialize';
+import PropTypes from 'prop-types';
 import React from 'react';
 import {SingleDatePicker as ReactDatesSinglePicker} from 'react-dates';
 
@@ -24,6 +25,14 @@ import './style/dates.scss';
 
 
 export default class SingleDatePicker extends React.Component {
+    static propTypes = {
+        disabledDate: PropTypes.func,
+    };
+
+    static defaultProps = {
+        disabledDate: null,
+    };
+
     state = {
         focused: false,
     };
@@ -34,12 +43,17 @@ export default class SingleDatePicker extends React.Component {
 
     render() {
         const {focused} = this.state;
+        const {disabledDate} = this.props;
         const filteredProps = Object.entries(this.props).filter(([name]) => {
             return !['name', 'value', 'onBlur', 'onChange', 'onFocus', 'label', 'disabledDate'].includes(name);
         }).reduce((acc, curr) => {
             const [key, value] = curr;
             return {...acc, [key]: value};
         }, {});
+
+        if (disabledDate) {
+            filteredProps.isOutsideRange = disabledDate;
+        }
 
         return (
             <ReactDatesSinglePicker focused={focused}
