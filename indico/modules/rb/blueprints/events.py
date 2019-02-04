0e059b8dc6747fa8e91cc8a50a07152b852cf4f2
Marco Vidal Garcia <marco.vidal@cern.ch>
2019-02-04 17:07:03 +0100

    Rename old urls

--- indico/modules/rb/blueprints/events.py
+++ indico/modules/rb/blueprints/events.py
@@ -24,31 +24,34 @@ event_mgmt = IndicoBlueprint('event_mgmt', __name__, url_prefix='/event/<confId>
 
 
 # Booking and event assignment list
-event_mgmt.add_url_rule('/rooms/choose-event', 'rooms_choose_event', event.RHRoomBookingEventChooseEvent)
+event_mgmt.add_url_rule('/rooms-old/', 'rooms_booking_list', event.RHRoomBookingEventBookingList)
+event_mgmt.add_url_rule('/rooms-old/choose-event', 'rooms_choose_event', event.RHRoomBookingEventChooseEvent)
 
 # View/modify booking
-event_mgmt.add_url_rule('/rooms/booking/<roomLocation>/<int:resvID>/', 'rooms_booking_details',
+event_mgmt.add_url_rule('/rooms-old/booking/<roomLocation>/<int:resvID>/', 'rooms_booking_details',
                         event.RHRoomBookingEventBookingDetails)
-event_mgmt.add_url_rule('/rooms/booking/<roomLocation>/<int:resvID>/modify', 'rooms_booking_modify',
+event_mgmt.add_url_rule('/rooms-old/booking/<roomLocation>/<int:resvID>/modify', 'rooms_booking_modify',
                         event.RHRoomBookingEventBookingModifyBooking, methods=('GET', 'POST'))
-event_mgmt.add_url_rule('/rooms/booking/<roomLocation>/<int:resvID>/clone', 'rooms_booking_clone',
+event_mgmt.add_url_rule('/rooms-old/booking/<roomLocation>/<int:resvID>/clone', 'rooms_booking_clone',
                         event.RHRoomBookingEventBookingCloneBooking, methods=('GET', 'POST'))
-event_mgmt.add_url_rule('/rooms/booking/<roomLocation>/<int:resvID>/accept', 'rooms_booking_accept',
+event_mgmt.add_url_rule('/rooms-old/booking/<roomLocation>/<int:resvID>/accept', 'rooms_booking_accept',
                         event.RHRoomBookingEventAcceptBooking, methods=('POST',))
-event_mgmt.add_url_rule('/rooms/booking/<roomLocation>/<int:resvID>/cancel', 'rooms_booking_cancel',
+event_mgmt.add_url_rule('/rooms-old/booking/<roomLocation>/<int:resvID>/cancel', 'rooms_booking_cancel',
                         event.RHRoomBookingEventCancelBooking, methods=('POST',))
-event_mgmt.add_url_rule('/rooms/booking/<roomLocation>/<int:resvID>/reject', 'rooms_booking_reject',
+event_mgmt.add_url_rule('/rooms-old/booking/<roomLocation>/<int:resvID>/reject', 'rooms_booking_reject',
                         event.RHRoomBookingEventRejectBooking, methods=('POST',))
-event_mgmt.add_url_rule('/rooms/booking/<roomLocation>/<int:resvID>/<date>/cancel', 'rooms_booking_occurrence_cancel',
+event_mgmt.add_url_rule('/rooms-old/booking/<roomLocation>/<int:resvID>/<date>/cancel',
+                        'rooms_booking_occurrence_cancel',
                         event.RHRoomBookingEventCancelBookingOccurrence, methods=('POST',))
-event_mgmt.add_url_rule('/rooms/booking/<roomLocation>/<int:resvID>/<date>/reject', 'rooms_booking_occurrence_reject',
+event_mgmt.add_url_rule('/rooms-old/booking/<roomLocation>/<int:resvID>/<date>/reject',
+                        'rooms_booking_occurrence_reject',
                         event.RHRoomBookingEventRejectBookingOccurrence, methods=('POST',))
 
 # Book room
-event_mgmt.add_url_rule('/rooms/room/<roomLocation>/<int:roomID>/book', 'rooms_room_book',
+event_mgmt.add_url_rule('/rooms-old/room/<roomLocation>/<int:roomID>/book', 'rooms_room_book',
                         event.RHRoomBookingEventNewBookingSimple, methods=('GET', 'POST'))
-event_mgmt.add_url_rule('/rooms/book', 'rooms_book', event.RHRoomBookingEventNewBooking, methods=('GET', 'POST'))
+event_mgmt.add_url_rule('/rooms-old/book', 'rooms_book', event.RHRoomBookingEventNewBooking, methods=('GET', 'POST'))
 
 # Room details
-event_mgmt.add_url_rule('/rooms/room/<roomLocation>/<int:roomID>/', 'rooms_room_details',
+event_mgmt.add_url_rule('/rooms-old/room/<roomLocation>/<int:roomID>/', 'rooms_room_details',
                         event.RHRoomBookingEventRoomDetails)
