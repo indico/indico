1e28c9429c29fc3cb390d9bedd20ee66de693baf
Marco Vidal Garcia <marco.vidal@cern.ch>
2019-05-13 17:44:21 +0200

    Fix timeline legend and date picker wrapping
    fixes #3915

--- indico/modules/rb/client/js/common/timeline/DateNavigator.jsx
+++ indico/modules/rb/client/js/common/timeline/DateNavigator.jsx
@@ -164,7 +164,7 @@ export default class DateNavigator extends React.Component {
     renderModeSwitcher(disabled) {
         const {mode} = this.props;
         return !!mode && (
-            <Button.Group size="tiny" style={{marginRight: 10}}>
+            <Button.Group size="tiny" styleName="date-navigator-item">
                 <Button content={Translate.string('Day')}
                         onClick={() => this.handleModeChange('days')}
                         primary={mode === 'days'}
@@ -250,7 +250,7 @@ export default class DateNavigator extends React.Component {
         const nextDisabled = disabled || !this.isValidChange(1, mode);
 
         return (
-            <Button.Group size="tiny">
+            <Button.Group size="tiny" styleName="date-navigator-item">
                 <Button icon="left arrow"
                         onClick={() => this.changeSelectedDate('prev')}
                         disabled={prevDisabled} />
