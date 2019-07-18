f9a62534f77f02d7cb1fd0845b3f3002a9382ac5
Adrian Moennich <adrian.moennich@cern.ch>
2019-07-18 11:04:09 +0200

    Use outdated-browser-rework with browserslist data

--- indico/web/client/js/outdatedbrowser.js
+++ indico/web/client/js/outdatedbrowser.js
@@ -5,8 +5,35 @@
 // modify it under the terms of the MIT License; see the
 // LICENSE file for more details.
 
-import 'outdatedbrowser/outdatedbrowser/outdatedbrowser.css';
+import preval from 'preval.macro';
+import outdatedBrowser from 'outdated-browser-rework';
+import 'outdated-browser-rework/dist/style.css';
+import './outdatedbrowser.css';
 
-// outdatedbrowser has to be loaded with exports-loader
-// in order to be available from 'window.outdatedBrowser'
-window.outdatedBrowser = require('exports-loader?outdatedBrowser!outdatedbrowser');
+// get list of supported browsers at build time
+const browserSupport = preval`
+  module.exports = require('./supported-browsers');
+`;
+
+outdatedBrowser({
+  backgroundColor: '#0b63a5',
+  isUnknownBrowserOK: true,
+  browserSupport,
+  // override language so we never get a default message
+  language: 'en',
+  messages: {
+    en: {
+      outOfDate: '⚠ Your browser is out of date ⚠',
+      unsupported: `⚠ Internet Explorer is not supported anymore ⚠`,
+      update: {
+        web:
+          'Indico may not work correctly in this browser.<br><br>' +
+          'If you are using Internet Explorer, please use Firefox, Chrome or Edge instead.',
+        googlePlay: 'Please install Chrome or Firefox from Google Play',
+        appStore: 'Please update iOS from the Settings App',
+      },
+      url: null,
+      close: 'Close',
+    },
+  },
+});
---
+++ indico/web/client/js/supported-browsers.js
@@ -0,0 +1,60 @@
+// This file is part of Indico.
+// Copyright (C) 2002 - 2019 CERN
+//
+// Indico is free software; you can redistribute it and/or
+// modify it under the terms of the MIT License; see the
+// LICENSE file for more details.
+
+// XXX: This script runs at build time. It is not transpiled so it can
+// only use features natively support by NodeJS!
+
+/* eslint-disable import/no-commonjs, import/unambiguous */
+/* global module:false, __dirname:false */
+
+const path = require('path');
+const browserslist = require('browserslist');
+const semver = require('semver');
+const BROWSERSLIST_PATH = path.join(__dirname, '..', '..', '..', '..', '.browserslistrc');
+
+const browsers = browserslist(null, {
+  env: 'production',
+  config: BROWSERSLIST_PATH,
+}).reduce((accum, text) => {
+  // get only the lowest version of a browser
+  const parts = text.split(' ');
+  const browser = parts[0];
+  let version = parts[1];
+
+  // version ranges - take the lowest
+  if (version.includes('-')) {
+    version = version.split('-')[0];
+  }
+  if (accum[browser]) {
+    if (semver.gt(semver.coerce(version), semver.coerce(accum[browser]))) {
+      // found a higher version, ignore it
+      return accum;
+    }
+  }
+  accum[browser] = version;
+  return accum;
+}, {});
+
+const convert = version => {
+  const [, major, minor] = version.match(/^(\d+)(?:\.(\d+))?$/);
+  if (minor === undefined) {
+    return +major;
+  } else {
+    return {major: +major, minor: +minor};
+  }
+};
+
+const mapped = {
+  'Chrome': convert(browsers.chrome),
+  'Edge': convert(browsers.edge),
+  'Safari': convert(browsers.safari),
+  'Mobile Safari': convert(browsers.ios_saf),
+  'Firefox': convert(browsers.firefox),
+  'IE': false,
+};
+
+module.exports = mapped;
