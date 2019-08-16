79cf4d5b6ca28e2a83ba64f3e2db0ada856b93bd
Adrian Moennich <adrian.moennich@cern.ch>
2019-08-16 15:55:17 +0200

    Improve create_entry fixture
    It only worked for the dummy_event...

--- indico/modules/events/timetable/testing/fixtures.py
+++ indico/modules/events/timetable/testing/fixtures.py
@@ -13,11 +13,11 @@ from indico.modules.events.timetable.models.entries import TimetableEntry
 
 
 @pytest.fixture
-def create_entry(db, dummy_event):
+def create_entry(db):
     """Returns a a callable which lets you create timetable"""
 
     def _create_entry(obj, start_dt):
-        entry = TimetableEntry(event=dummy_event, object=obj, start_dt=start_dt)
+        entry = TimetableEntry(event=obj.event, object=obj, start_dt=start_dt)
         db.session.add(entry)
         db.session.flush()
         return entry
