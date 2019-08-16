ecb6ddb74699dfa4007c6c577db2929b84d1f3d0
Adrian Moennich <adrian.moennich@cern.ch>
2019-08-16 15:57:03 +0200

    Add tests for get_booking_params_for_event

--- indico/modules/rb/util_test.py
+++ indico/modules/rb/util_test.py
@@ -5,11 +5,19 @@
 # modify it under the terms of the MIT License; see the
 # LICENSE file for more details.
 
+from __future__ import unicode_literals
+
+from datetime import date, datetime, time, timedelta
+
 import pytest
+import pytz
 
 from indico.modules.rb import rb_settings
-from indico.modules.rb.util import rb_check_user_access, rb_is_admin
+from indico.modules.rb.util import get_booking_params_for_event, rb_check_user_access, rb_is_admin
 from indico.testing.util import bool_matrix
+
+
+pytest_plugins = ('indico.modules.rb.testing.fixtures', 'indico.modules.events.timetable.testing.fixtures')
 
 
 @pytest.mark.parametrize(('is_rb_admin', 'acl_empty', 'in_acl', 'expected'), bool_matrix('...', expect=any))
@@ -27,3 +35,73 @@ def test_rb_check_user_access(db, mocker, dummy_user, dummy_group, is_rb_admin, 
 def test_rb_is_admin(create_user, is_admin, is_rb_admin, expected):
     user = create_user(1, admin=is_admin, rb_admin=is_rb_admin)
     assert rb_is_admin(user) == expected
+
+
+@pytest.mark.parametrize(('start_dt', 'end_dt', 'expected_params'), (
+    # single-day event
+    (datetime(2019, 8, 16, 10, 0), datetime(2019, 8, 16, 13, 0), {'recurrence': 'single',
+                                                                  'interval': 'week',
+                                                                  'number': 1,
+                                                                  'sd': '2019-08-16',
+                                                                  'ed': None,
+                                                                  'st': '10:00',
+                                                                  'et': '13:00'}),
+    # multi-day event
+    (datetime(2019, 8, 16, 10, 0), datetime(2019, 8, 18, 13, 0), {'recurrence': 'daily',
+                                                                  'interval': 'week',
+                                                                  'number': 1,
+                                                                  'sd': '2019-08-16',
+                                                                  'ed': '2019-08-18',
+                                                                  'st': '10:00',
+                                                                  'et': '13:00'}),
+    # end time < start time
+    (datetime(2019, 8, 16, 16, 0), datetime(2019, 8, 18, 13, 0), {'sd': '2019-08-16'})
+))
+def test_get_booking_params_for_event_same_times(create_event, dummy_room, start_dt, end_dt, expected_params):
+    start_dt = pytz.utc.localize(start_dt)
+    end_dt = pytz.utc.localize(end_dt)
+    event = create_event(start_dt=start_dt, end_dt=end_dt, room=dummy_room)
+    params = get_booking_params_for_event(event)
+    assert params == {
+        'type': 'same_times',
+        'params': dict({
+            'link_id': event.id,
+            'link_type': 'event',
+            'text': dummy_room.name,
+        }, **expected_params)
+    }
+
+
+@pytest.mark.parametrize(('start_time', 'end_time', 'expected_params'), (
+    # start time < end time
+    (time(10), time(13), {'interval': 'week', 'number': 1, 'recurrence': 'single', 'st': '10:00', 'et': '13:00'}),
+    # end time < start time
+    (time(15), time(13), {}),
+))
+def test_get_booking_params_for_event_multiple_times(create_event, create_contribution, create_entry, dummy_room,
+                                                     start_time, end_time, expected_params):
+    start_dt = pytz.utc.localize(datetime.combine(date(2019, 8, 16), start_time))
+    end_dt = pytz.utc.localize(datetime.combine(date(2019, 8, 18), end_time))
+    event = create_event(start_dt=start_dt, end_dt=end_dt, room=dummy_room)
+    c1 = create_contribution(event, 'C1', timedelta(minutes=30))
+    c2 = create_contribution(event, 'C2', timedelta(minutes=120))
+    c3 = create_contribution(event, 'C3', timedelta(minutes=30))
+    create_entry(c1, pytz.utc.localize(datetime(2019, 8, 17, 9, 0)))
+    create_entry(c2, pytz.utc.localize(datetime(2019, 8, 17, 18, 0)))
+    create_entry(c3, pytz.utc.localize(datetime(2019, 8, 17, 19, 0)))
+    params = get_booking_params_for_event(event)
+    assert params == {
+        'type': 'mixed_times',
+        'params': {
+            'link_type': 'event',
+            'link_id': event.id,
+            'text': dummy_room.name,
+        },
+        'time_info': [
+            (date(2019, 8, 16), dict({'sd': '2019-08-16'}, **expected_params)),
+            # this day has timetable entries -> not using the event defaults
+            (date(2019, 8, 17), {'interval': 'week', 'number': 1, 'recurrence': 'single',
+                                 'sd': '2019-08-17', 'st': '09:00', 'et': '20:00'}),
+            (date(2019, 8, 18), dict({'sd': '2019-08-18'}, **expected_params))
+        ]
+    }
