65d1b3c6d46e65a5dd874d0b7365abc9a3b7adee
Adrian Moennich <adrian.moennich@cern.ch>
2019-08-20 16:45:55 +0200

    Include SUI CSS before our custom CSS
    Otherwise some rules from our own CSS are overridden by the ones from
    SUI, e.g. for the color of `a` tags.

--- indico/web/views.py
+++ indico/web/views.py
@@ -216,9 +216,9 @@ class WPBase(WPBundleMixin):
     @classproperty
     @classmethod
     def bundles(cls):
-        _bundles = ('common.css', 'common.js', 'jquery.css', 'jquery.js', 'main.css', 'main.js', 'module_core.js',
-                    'module_events.creation.js', 'module_attachments.js', 'outdatedbrowser.js', 'outdatedbrowser.css',
-                    'react.js', 'semantic-ui.js', 'semantic-ui.css')
+        _bundles = ('common.css', 'common.js', 'react.js', 'semantic-ui.js', 'semantic-ui.css', 'jquery.css',
+                    'jquery.js', 'main.css', 'main.js', 'module_core.js', 'module_events.creation.js',
+                    'module_attachments.js', 'outdatedbrowser.js', 'outdatedbrowser.css')
         if not g.get('static_site'):
             _bundles += ('ckeditor.js',)
         return _bundles
