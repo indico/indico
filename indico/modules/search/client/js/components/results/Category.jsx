b91f7fe38b7697e0ae1c0567423e67354290db1a
Aristofanis Chionis <aristofanis.chionis@cern.ch>
2019-08-30 11:14:16 +0200

    Fix Category path Rendering

--- indico/modules/search/client/js/components/results/Category.jsx
+++ indico/modules/search/client/js/components/results/Category.jsx
@@ -1,20 +1,41 @@
 import React from 'react';
-import {List} from 'semantic-ui-react';
+import {List, Breadcrumb} from 'semantic-ui-react';
 import PropTypes from 'prop-types';
-import '../ResultList.module.scss';
+import './Category.module.scss';
 
-const Category = ({title, path, url}) => (
-  <>
-    <List.Header>
-      <a href={`https://blackhole.cern.ch${url}`}>{title}</a>
-    </List.Header>
-    <List.Description>
-      <List>
-        <List.Item>{path.join(' » ')}</List.Item>
-      </List>
-    </List.Description>
-  </>
-);
+const Category = ({title, path, url}) => {
+  const array = path.slice(0, path.length - 1);
+
+  return (
+    <>
+      <List.Header>
+        <a href={`https://blackhole.cern.ch${url}`}>{title}</a>
+      </List.Header>
+      {path.length !== 0 && (
+        <List.Description>
+          <span>&nbsp;</span>
+          <Breadcrumb>
+            {array.map(item => (
+              <Breadcrumb.Section key={item}>
+                <span styleName="category">
+                  <a href="https://www.google.com">
+                    {item}
+                    <span>&nbsp;</span>
+                  </a>
+                  {' » '}
+                  <span>&nbsp;</span>
+                </span>
+              </Breadcrumb.Section>
+            ))}
+            <Breadcrumb.Section active styleName="category">
+              {path[path.length - 1]}
+            </Breadcrumb.Section>
+          </Breadcrumb>
+        </List.Description>
+      )}
+    </>
+  );
+};
 
 Category.propTypes = {
   title: PropTypes.string.isRequired,
