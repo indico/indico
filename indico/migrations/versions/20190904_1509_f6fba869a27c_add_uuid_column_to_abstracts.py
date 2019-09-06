7a4977c8cd83386f78640de7d7b62db901626ae0
Michal Kolodziejski <michal.kolodziejski@cern.ch>
2019-09-06 13:59:13 +0200

    Add uuid to an Abstract

---
+++ indico/migrations/versions/20190904_1509_f6fba869a27c_add_uuid_column_to_abstracts.py
@@ -0,0 +1,26 @@
+"""Add uuid column to event_abstracts.abstracts
+
+Revision ID: f6fba869a27c
+Revises: 0f20a5e4018e
+Create Date: 2019-09-04 15:09:33.244329
+"""
+
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy.dialects import postgresql
+
+
+# revision identifiers, used by Alembic.
+revision = 'f6fba869a27c'
+down_revision = '0f20a5e4018e'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column('abstracts', sa.Column('uuid', postgresql.UUID(), nullable=True), schema='event_abstracts')
+    op.create_index(None, 'abstracts', ['uuid'], unique=True, schema='event_abstracts')
+
+
+def downgrade():
+    op.drop_column('abstracts', 'uuid', schema='event_abstracts')
