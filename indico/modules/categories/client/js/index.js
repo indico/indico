f431c386e961f9d3653850d33566c5eec7f64809
Aristofanis Chionis <aristofanis.chionis@cern.ch>
2019-09-19 15:37:48 +0200

    Place Search Bar in the main page

--- indico/modules/categories/client/js/index.js
+++ indico/modules/categories/client/js/index.js
@@ -5,15 +5,29 @@
 // modify it under the terms of the MIT License; see the
 // LICENSE file for more details.
 
+import searchUrl from 'indico-url:search.search';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import './display';
+import SearchBar from 'indico/modules/search/components/SearchBar';
 import {setMomentLocale} from 'indico/utils/date';
 import CategoryStatistics from './components/CategoryStatistics';
 import {LocaleContext} from './context.js';
+import './display';
 
 (function(global) {
+  document.addEventListener('DOMContentLoaded', () => {
+    const domContainer = document.querySelector('#search-box');
+    ReactDOM.render(
+      React.createElement(SearchBar, {
+        onSearch: keyword => {
+          window.location = searchUrl({q: keyword});
+        },
+        searchTerm: '',
+      }),
+      domContainer
+    );
+  });
   global.setupCategoryStats = function setupCategoryStats() {
     document.addEventListener('DOMContentLoaded', async () => {
       const rootElement = document.querySelector('#category-stats-root');
