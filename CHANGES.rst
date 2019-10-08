cb397fafa9a9be725f237afb16e0a53ee65ba7eb
Adrian Moennich <adrian.moennich@cern.ch>
2019-10-08 18:36:08 +0200

    Release 2.1.10

--- CHANGES.rst
+++ CHANGES.rst
@@ -5,7 +5,7 @@ Changelog
 Version 2.1.10
 --------------
 
-*Unreleased*
+*Released on October 08, 2019*
 
 Security fixes
 ^^^^^^^^^^^^^^
--- indico/__init__.py
+++ indico/__init__.py
@@ -17,7 +17,7 @@
 from indico.util.mimetypes import register_custom_mimetypes
 
 
-__version__ = '2.1.10-dev'
+__version__ = '2.1.10'
 
 register_custom_mimetypes()
 
