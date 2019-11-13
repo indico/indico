f25dbf846c697d28be2bd2953b520c0912cb8b65
Adrian Moennich <adrian.moennich@cern.ch>
2019-11-13 11:51:26 +0100

    Allow revisions with (new,replaced) states

--- indico/migrations/versions/20191108_1713_4e459d27adab_add_tables_for_editing_revisions.py
+++ indico/migrations/versions/20191108_1713_4e459d27adab_add_tables_for_editing_revisions.py
@@ -48,7 +48,7 @@ def upgrade():
         sa.ForeignKeyConstraint(['editable_id'], ['event_editing.editables.id']),
         sa.ForeignKeyConstraint(['editor_id'], ['users.users.id']),
         sa.ForeignKeyConstraint(['submitter_id'], ['users.users.id']),
-        sa.CheckConstraint('(initial_state=1 AND final_state=0) OR (initial_state=2) OR '
+        sa.CheckConstraint('(initial_state=1 AND final_state IN (0,1)) OR (initial_state=2) OR '
                            '(initial_state=3 AND (final_state IN (3,4)))', name='valid_state_combination'),
         sa.PrimaryKeyConstraint('id'),
         schema='event_editing'
