9955b2ab920ac8af753937e79a038b78754ba4d7
Pedro Ferreira <pedro.ferreira@cern.ch>
2019-11-18 14:45:56 +0100

    Add basic editable page

--- indico/modules/events/editing/controllers/frontend.py
+++ indico/modules/events/editing/controllers/frontend.py
@@ -6,3 +6,16 @@
 # LICENSE file for more details.
 
 from __future__ import unicode_literals
+
+from indico.modules.events.editing.controllers.base import RHContributionEditableBase
+from indico.modules.events.editing.views import WPEditing
+
+
+class RHEditableTimeline(RHContributionEditableBase):
+    def _process(self):
+        return WPEditing.render_template(
+            'editing.html',
+            self.event,
+            editable=self.editable,
+            contribution=self.contrib
+        )
