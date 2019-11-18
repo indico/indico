e4152614d71a078984145a19f3c5f3b3ca7b1a8a
Pedro Ferreira <pedro.ferreira@cern.ch>
2019-11-18 11:55:49 +0100

    FileManager: make reducers fully non-mutating

--- indico/modules/events/editing/client/js/components/FileManager/reducer.js
+++ indico/modules/events/editing/client/js/components/FileManager/reducer.js
@@ -9,67 +9,72 @@ import _ from 'lodash';
 import {combineReducers} from 'redux';
 import * as actions from './actions';
 
-export default combineReducers({
-  fileTypes: (state = [], action) => {
-    const fileType = _.find(state, {id: action.fileTypeId});
-    switch (action.type) {
-      case actions.MARK_UPLOADED:
-        action.file.state = 'added';
-        if (fileType.allowMultipleFiles) {
-          fileType.files = fileType.files.concat(action.file);
-        } else {
-          fileType.files = [action.file];
-        }
-        return [...state];
+/**
+ *
+ * @param {Array} files - the array of files to look into
+ * @param {String} uuid - the uuid of the file to look for
+ * @param {Function} func - function used to "reduce" the file in question
+ */
+function replaceFile(files, uuid, func) {
+  return files.map(file => (file.uuid === uuid ? func(file) : file)).filter(file => !!file);
+}
 
-      case actions.MARK_DELETED: {
-        const file = _.find(fileType.files, {uuid: action.fileId});
-        if (file.claimed) {
-          file.state = 'deleted';
-        } else {
-          _.pull(fileType.files, file);
-        }
-        return [...state];
-      }
+/**
+ * Reducer for arrays of files within a fileType (`fileType.files`).
+ * @param {Array} state - the array of file objects which will be reduced
+ * @param {Object} action - the action which will be processed
+ */
+function fileListReducer(state, action, allowMultipleFiles) {
+  switch (action.type) {
+    case actions.MARK_UPLOADED: {
+      const newFile = {...action.file, state: 'added'};
+      return allowMultipleFiles ? [...state, newFile] : [newFile];
+    }
 
-      case actions.MARK_MODIFIED: {
-        const currentFile = _.find(fileType.files, {uuid: action.fileId});
-        fileType.files = fileType.files.map(file => {
-          if (file.uuid === action.fileId) {
-            return {
+    case actions.MARK_DELETED:
+      return replaceFile(state, action.fileId, file =>
+        file.claimed ? {...file, state: 'deleted'} : null
+      );
+
+    case actions.MARK_MODIFIED:
+      return replaceFile(state, action.fileId, file =>
+        file.claimed
+          ? {
+              ...action.file,
               state: 'modified',
               // if the current file is already a "replacement", then let's preserve
               // the "pointer" to the original file (we only allow undo to the initial file)
-              originalFile: currentFile.originalFile || currentFile,
-              ...action.file,
-            };
-          } else {
-            return file;
-          }
-        });
-        return [...state];
-      }
+              originalFile: file.originalFile || file,
+            }
+          : null
+      );
 
-      case actions.UNDELETE: {
-        const file = _.find(fileType.files, {uuid: action.fileId});
-        file.state = null;
-        return [...state];
-      }
+    case actions.UNDELETE:
+      return replaceFile(state, action.fileId, file => ({...file, state: null}));
 
-      case actions.REVERT: {
-        fileType.files = fileType.files.map(file => {
-          if (file.uuid === action.fileId) {
-            return {state: null, ...file.originalFile};
-          } else {
-            return file;
-          }
-        });
-        return [...state];
-      }
+    case actions.REVERT:
+      return replaceFile(state, action.fileId, file => ({
+        ...file.originalFile,
+        state: null,
+      }));
 
-      default:
-        return state;
-    }
+    default:
+      return state;
+  }
+}
+
+export default combineReducers({
+  fileTypes: (state = [], action) => {
+    return state.map(fileType => {
+      if (fileType.id === action.fileTypeId) {
+        return {
+          ...fileType,
+          files: fileListReducer(fileType.files, action, fileType.allowMultipleFiles),
+        };
+      } else {
+        return fileType;
+      }
+    });
   },
   uploads: (state = {}, action) => {
     switch (action.type) {
