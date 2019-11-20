376ed30b3b2d4f988b21a82fd4a1be69ffec53e4
Pedro Ferreira <pedro.ferreira@cern.ch>
2019-11-20 16:11:07 +0100

    Add jest/enzyme test environment

---
+++ __mocks__/react.js
@@ -0,0 +1,40 @@
+// This file is part of Indico.
+// Copyright (C) 2002 - 2019 CERN
+//
+// Indico is free software; you can redistribute it and/or
+// modify it under the terms of the MIT License; see the
+// LICENSE file for more details.
+
+/**
+ * This module mocks some of React's hooks while providing
+ * a way to tap into some of the internal methods (e.g. dispatch)
+ */
+
+import React from 'react';
+
+const mockDispatches = [];
+
+export * from 'react';
+
+export function useReducer(reducer, initialState) {
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+  const mockDispatch = jest.fn(action => {
+    dispatch(action);
+  });
+
+  mockDispatches.push(mockDispatch);
+  return [state, mockDispatch];
+}
+
+export function resetMocks() {
+  while (mockDispatches.length) {
+    mockDispatches.pop();
+  }
+}
+
+export default {
+  ...React,
+  useReducer,
+  resetMocks,
+  mockDispatches,
+};
