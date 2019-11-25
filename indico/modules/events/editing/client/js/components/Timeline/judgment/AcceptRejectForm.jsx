02215904caf03cf7740aad6388c4af39b64fa0f3
Pedro Ferreira <pedro.ferreira@cern.ch>
2019-11-25 11:07:49 +0100

    Move JudgmentModal to JudgmentBox

---
+++ indico/modules/events/editing/client/js/components/Timeline/judgment/AcceptRejectForm.jsx
@@ -0,0 +1,64 @@
+// This file is part of Indico.
+// Copyright (C) 2002 - 2019 CERN
+//
+// Indico is free software; you can redistribute it and/or
+// modify it under the terms of the MIT License; see the
+// LICENSE file for more details.
+
+import React from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import PropTypes from 'prop-types';
+import {Form as FinalForm} from 'react-final-form';
+import {Form} from 'semantic-ui-react';
+
+import {FinalSubmitButton, FinalTextArea} from 'indico/react/forms';
+import {Translate} from 'indico/react/i18n';
+
+import {reviewEditable} from '../../../actions';
+import * as selectors from '../../../selectors';
+import {blockPropTypes} from '../util';
+
+import './JudgmentBox.module.scss';
+
+export default function AcceptRejectForm({block, action, setLoading}) {
+  const {eventId, contributionId, editableType} = useSelector(selectors.getStaticData);
+  const dispatch = useDispatch();
+
+  return (
+    <FinalForm
+      onSubmit={async formData => {
+        setLoading(true);
+        const ret = await dispatch(
+          reviewEditable(eventId, contributionId, editableType, block, {...formData, action})
+        );
+        if (ret.error) {
+          setLoading(false);
+          return ret.error;
+        }
+      }}
+      subscription={{submitting: true}}
+    >
+      {({handleSubmit}) => (
+        <>
+          <Form id="judgment-form" onSubmit={handleSubmit}>
+            <FinalTextArea
+              name="comment"
+              placeholder={Translate.string('Leave a comment...')}
+              hideValidationError
+            />
+            <div>TODO: Tags field</div>
+          </Form>
+          <div styleName="judgment-submit-button">
+            <FinalSubmitButton form="judgment-form" label={Translate.string('Confirm')} />
+          </div>
+        </>
+      )}
+    </FinalForm>
+  );
+}
+
+AcceptRejectForm.propTypes = {
+  block: PropTypes.shape(blockPropTypes).isRequired,
+  action: PropTypes.oneOf(['accept', 'reject']).isRequired,
+  setLoading: PropTypes.func.isRequired,
+};
---
+++ indico/modules/events/editing/client/js/components/Timeline/judgment/JudgmentBox.jsx
@@ -0,0 +1,82 @@
+// This file is part of Indico.
+// Copyright (C) 2002 - 2019 CERN
+//
+// Indico is free software; you can redistribute it and/or
+// modify it under the terms of the MIT License; see the
+// LICENSE file for more details.
+
+import _ from 'lodash';
+import React, {useState, useMemo} from 'react';
+import PropTypes from 'prop-types';
+import {Button, Dropdown} from 'semantic-ui-react';
+
+import {Translate} from 'indico/react/i18n';
+
+import {blockPropTypes} from '../util';
+import AcceptRejectForm from './AcceptRejectForm';
+
+import './JudgmentBox.module.scss';
+
+export default function JudgmentBox({block, onClose, judgmentType: _judgmentType, options}) {
+  const [judgmentType, setJudgmentType] = useState(_judgmentType);
+  const [loading, setLoading] = useState(false);
+  const optionsMap = useMemo(() => _.keyBy(options, 'value'), [options]);
+
+  const option = optionsMap[judgmentType];
+
+  return (
+    <>
+      <div styleName="choice-bar">
+        <h3>
+          <Translate>Your decision</Translate>
+        </h3>
+        <div>
+          <Dropdown
+            value={judgmentType}
+            text={option.text}
+            direction="left"
+            disabled={loading}
+            button
+            floating
+            styleName="judgment-type-button"
+            className={option.class}
+          >
+            <Dropdown.Menu>
+              {options.map(({value, text}) => (
+                <Dropdown.Item
+                  key={value}
+                  selected={value === judgmentType}
+                  onClick={() => {
+                    setJudgmentType(value);
+                  }}
+                  text={text}
+                />
+              ))}
+            </Dropdown.Menu>
+          </Dropdown>
+          <Button icon="delete" disabled={loading} onClick={onClose} />
+        </div>
+      </div>
+      {(judgmentType === 'accept' || judgmentType === 'reject') && (
+        <AcceptRejectForm block={block} action={judgmentType} setLoading={setLoading} />
+      )}
+    </>
+  );
+}
+
+JudgmentBox.propTypes = {
+  block: PropTypes.shape(blockPropTypes).isRequired,
+  judgmentType: PropTypes.string.isRequired,
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      value: PropTypes.string,
+      text: PropTypes.string,
+      class: PropTypes.string,
+    })
+  ).isRequired,
+  onClose: PropTypes.func,
+};
+
+JudgmentBox.defaultProps = {
+  onClose: null,
+};
