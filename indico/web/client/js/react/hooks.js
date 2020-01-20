18cbb14148156903144e742fc2ce49c1bd7f1c8f
Adrian Moennich <adrian.moennich@cern.ch>
2020-01-20 14:13:17 +0100

    Add useTimeout hook

--- indico/web/client/js/react/hooks.js
+++ indico/web/client/js/react/hooks.js
@@ -121,3 +121,28 @@ export function useIndicoAxios({camelize, ...args}) {
   }
   return {response, error, loading, reFetch, data, lastData: lastData.current};
 }
+
+/**
+ * React hook to use `setTimeout` inside a react component.
+ *
+ * @param {Function} callback - the function to run at the end
+ * @param {Number} delay - the duration of the timeout (`null` to disable)
+ *
+ * Based on https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+ */
+export function useTimeout(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest callback
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the actual timeout
+  useEffect(() => {
+    if (delay !== null) {
+      const id = setTimeout(() => savedCallback.current(), delay);
+      return () => clearTimeout(id);
+    }
+  }, [delay]);
+}
