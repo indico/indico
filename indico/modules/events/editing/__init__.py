546675ec21a1c18367aa7b4346e36b79233be629
Panagiota Poulopoulou <panagiota.poulopoulou@cern.ch>
2020-03-04 16:48:35 +0100

    Allow enabling Editing module from Editing page
    Rename "Paper editing" to just "Editing" for general cases

--- indico/modules/events/editing/__init__.py
+++ indico/modules/events/editing/__init__.py
@@ -25,9 +25,9 @@ logger = Logger.get('events.editing')
 
 class EditingFeature(EventFeature):
     name = 'editing'
-    friendly_name = _('Paper Editing')
-    description = _('Gives event managers the opportunity to let contributors submit papers to be edited and '
-                    'eventually published.')
+    friendly_name = _('Editing')
+    description = _('Gives event managers the opportunity to let contributors submit papers and/or slides to be edited '
+                    'and eventually published.')
 
     @classmethod
     def is_allowed_for_event(cls, event):
@@ -58,7 +58,7 @@ def _get_management_permissions(sender, **kwargs):
 def _extend_event_management_menu(sender, event, **kwargs):
     if not event.can_manage(session.user) or not EditingFeature.is_allowed_for_event(event):
         return
-    return SideMenuItem('editing', _('Paper Editing'), url_for('event_editing.dashboard', event),
+    return SideMenuItem('editing', _('Editing'), url_for('event_editing.dashboard', event),
                         section='organization')
 
 
