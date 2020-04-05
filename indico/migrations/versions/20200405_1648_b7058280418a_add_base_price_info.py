59323cc973c500cbb1069aee6f9d055b480e96ee
penelope <penelope@fnal.gov>
2020-04-05 17:13:18 -0500

    Add base_price_info column

--- indico/migrations/versions/20200405_1648_b7058280418a_add_base_price_info.py
+++ indico/migrations/versions/20200405_1648_b7058280418a_add_base_price_info.py
@@ -1,8 +1,8 @@
-"""Add base_price_info column
+"""Add base_price_info
 
-Revision ID: 5961a6ace73b
-Revises: 7e03b2262e9e
-Create Date: 2018-11-02 13:54:39.610928
+Revision ID: b7058280418a
+Revises: 620b312814f3
+Create Date: 2020-04-05 16:48:55.885158
 """
 
 import sqlalchemy as sa
@@ -10,8 +10,8 @@ from alembic import op
 
 
 # revision identifiers, used by Alembic.
-revision = '5961a6ace73b'
-down_revision = '7e03b2262e9e'
+revision = 'b7058280418a'
+down_revision = '620b312814f3'
 branch_labels = None
 depends_on = None
 
