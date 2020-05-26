ff38bc45f1213a52a347383638341753b35aae1d
Adrian Moennich <adrian.moennich@cern.ch>
2020-05-26 21:51:45 +0200

    Add secure_client_filename util

--- indico/util/fs_test.py
+++ indico/util/fs_test.py
@@ -7,14 +7,30 @@
 
 import pytest
 
-from indico.util.fs import secure_filename
+from indico.util.fs import secure_client_filename, secure_filename
+
+
+@pytest.mark.parametrize(('filename', 'expected'), (
+    (u'', u'file'),
+    (None, u'file'),
+    (u'.', u'_'),
+    (u'..', u'__'),
+    (u'../', u'.._'),
+    (u'foo.txt', u'foo.txt'),
+    (u'../../../etc/passwd', u'.._.._.._etc_passwd'),
+    (u'm\xf6p.txt', u'm\xf6p.txt'),
+    (u'/m\xf6p.txt', u'_m\xf6p.txt'),
+    (ur'c:\test.txt', u'c_test.txt'),
+    (r'spacy   \filename', u'spacy _filename'),
+))
+def test_secure_client_filename(filename, expected):
+    assert secure_client_filename(filename) == expected
 
 
 @pytest.mark.parametrize(('filename', 'expected'), (
     ('', 'fallback'),
     (None, 'fallback'),
     ('foo.txt', 'foo.txt'),
-    ('', 'fallback'),
     ('../../../etc/passwd', 'etc_passwd'),
     (u'm\xf6p.txt', 'moep.txt'),
     (u'm\xf6p.txt'.encode('utf-8'), 'moep.txt'),
