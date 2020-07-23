37c999c3ea9635db0531e9f996d05fbb7447cd5f
Adrian Moennich <adrian.moennich@cern.ch>
2020-07-23 20:00:44 +0200

    Fail CI in case of outdated header

--- bin/maintenance/update_header.py
+++ bin/maintenance/update_header.py
@@ -126,6 +126,7 @@ def _update_header(project, file_path, year, substring, regex, data):
         print(cformat('%{green!}Updating header of %{blue!}{}').format(os.path.relpath(file_path)))
         with open(file_path, 'wb') as file_write:
             file_write.write(content)
+        return True
     elif not found:
         print('Missing header in {}'.format(os.path.relpath(file_path)))
         return True
