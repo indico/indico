d4e3fd966606db8717f0ccfe3177db8ef67ea7ec
Pedro Lourenço <pedro.lourenco@cern.ch>
2020-07-27 18:21:40 +0200

    Fix a bug causing a revision to lose its comments

--- indico/modules/events/editing/client/js/editing/timeline/__tests__/selectors.test.js
+++ indico/modules/events/editing/client/js/editing/timeline/__tests__/selectors.test.js
@@ -13,7 +13,15 @@ describe('timeline selectors', () => {
     const revisions = [
       {
         id: 1,
-        comments: '',
+        comments: [
+          {
+            id: 1,
+            text: 'revision replaced',
+            user: {
+              fullName: 'Indico Janitor',
+            },
+          },
+        ],
         initialState: {
           name: InitialRevisionState.new,
         },
@@ -23,7 +31,7 @@ describe('timeline selectors', () => {
       },
       {
         id: 2,
-        comments: '',
+        comments: [],
         initialState: {
           name: InitialRevisionState.ready_for_review,
         },
@@ -36,6 +44,7 @@ describe('timeline selectors', () => {
     expect(result).toHaveLength(1);
     expect(result[0].id).toBe(1);
     expect(result[0].items).toStrictEqual([
+      expect.objectContaining({text: 'revision replaced'}),
       expect.objectContaining({state: FinalRevisionState.replaced}),
       expect.objectContaining({state: FinalRevisionState.needs_submitter_changes}),
     ]);
@@ -45,7 +54,7 @@ describe('timeline selectors', () => {
     const revisions = [
       {
         id: 1,
-        comments: '',
+        comments: [],
         initialState: {
           name: InitialRevisionState.ready_for_review,
         },
@@ -55,7 +64,7 @@ describe('timeline selectors', () => {
       },
       {
         id: 2,
-        comments: '',
+        comments: [],
         initialState: {
           name: InitialRevisionState.needs_submitter_confirmation,
         },
@@ -65,7 +74,7 @@ describe('timeline selectors', () => {
       },
       {
         id: 3,
-        comments: '',
+        comments: [],
         initialState: {
           name: InitialRevisionState.ready_for_review,
         },
@@ -86,5 +95,83 @@ describe('timeline selectors', () => {
     ]);
     expect(result[2].id).toBe(3);
     expect(result[2].items).toHaveLength(0);
+  });
+
+  it('should render comments correctly', () => {
+    const revisions = [
+      {
+        id: 1,
+        comments: [
+          {
+            id: 1,
+            text: 'first comment',
+            user: {
+              fullName: 'Indico Janitor',
+            },
+          },
+        ],
+        initialState: {
+          name: InitialRevisionState.new,
+        },
+        finalState: {
+          name: FinalRevisionState.replaced,
+        },
+      },
+      {
+        id: 2,
+        comments: [
+          {
+            id: 1,
+            text: 'my test comment',
+            user: {
+              fullName: 'Indico Janitor',
+            },
+          },
+          {
+            id: 2,
+            text: 'foo',
+            user: {
+              fullName: 'Indico Janitor',
+            },
+          },
+        ],
+        initialState: {
+          name: InitialRevisionState.ready_for_review,
+        },
+        finalState: {
+          name: FinalRevisionState.needs_submitter_changes,
+        },
+      },
+      {
+        id: 3,
+        comments: [
+          {
+            id: 2,
+            text: 'done',
+            user: {
+              fullName: 'Indico Janitor',
+            },
+          },
+        ],
+        initialState: {
+          name: InitialRevisionState.ready_for_review,
+        },
+        finalState: {
+          name: FinalRevisionState.none,
+        },
+      },
+    ];
+    const result = processRevisions(revisions);
+    expect(result).toHaveLength(2);
+    expect(result[0].id).toBe(1);
+    expect(result[0].items).toStrictEqual([
+      expect.objectContaining({text: 'first comment'}),
+      expect.objectContaining({state: FinalRevisionState.replaced}),
+      expect.objectContaining({text: 'my test comment'}),
+      expect.objectContaining({text: 'foo'}),
+      expect.objectContaining({state: FinalRevisionState.needs_submitter_changes}),
+    ]);
+    expect(result[1].id).toBe(3);
+    expect(result[1].items).toStrictEqual([expect.objectContaining({text: 'done'})]);
   });
 });
