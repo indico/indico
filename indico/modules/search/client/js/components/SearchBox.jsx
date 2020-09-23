e633869de1903f8d8f84a2348db8ed82ce97f95e
Adrian Moennich <adrian.moennich@cern.ch>
2020-09-23 17:25:38 +0200

    Fix/add missing headers

--- indico/modules/search/client/js/components/SearchBox.jsx
+++ indico/modules/search/client/js/components/SearchBox.jsx
@@ -1,3 +1,10 @@
+// This file is part of Indico.
+// Copyright (C) 2002 - 2020 CERN
+//
+// Indico is free software; you can redistribute it and/or
+// modify it under the terms of the MIT License; see the
+// LICENSE file for more details.
+
 import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import {Form} from 'semantic-ui-react';
