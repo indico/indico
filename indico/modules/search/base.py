b739cf1c52f9406a3a50c93f04c42fb34b6a568e
Pedro Lourenço <pedro.lourenco@cern.ch>
2021-01-19 10:12:51 +0000

    Update the search interface

---
+++ indico/modules/search/base.py
@@ -0,0 +1,23 @@
+from indico.core import signals
+from indico.util.signals import values_from_signal
+from indico.util.struct.enum import IndicoEnum
+
+
+def get_search_provider():
+    providers = signals.get_search_providers.send()
+    return values_from_signal(providers, as_list=True)[0] if len(providers) else None
+
+
+class SearchTarget(int, IndicoEnum):
+    category = 1
+    event = 2
+    contribution = 3
+    attachment = 4
+
+
+class IndicoSearchProvider(object):
+    RESULTS_PER_PAGE = 10
+
+    @classmethod
+    def search(cls, query, access, object_type=SearchTarget.event, page=1):
+        raise NotImplementedError()
