0810111cc5d1a5f08bfdfe226e5dc62cb42c3d55
Adrian Moennich <adrian.moennich@cern.ch>
2021-01-21 13:04:15 +0100

    Move package metadata from setup.py to setup.cfg
    Except for some dynamic parts...

--- setup.py
+++ setup.py
@@ -5,14 +5,12 @@
 # modify it under the terms of the MIT License; see the
 # LICENSE file for more details.
 
-import ast
 import json
 import os
-import re
 import subprocess
 from distutils.command.build import build
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
 
 def read_requirements_file(fname):
@@ -22,12 +20,6 @@ def read_requirements_file(fname):
 
 def get_requirements():
     return read_requirements_file(os.path.join(os.path.dirname(__file__), 'requirements.txt'))
-
-
-def get_version():
-    _version_re = re.compile(r'__version__\s+=\s+(.*)')
-    with open('indico/__init__.py') as f:
-        return str(ast.literal_eval(_version_re.search(f.read()).group(1)))
 
 
 class BuildWithTranslations(build):
@@ -63,31 +55,7 @@ if os.environ.get('READTHEDOCS') != 'True':
 
 if __name__ == '__main__':
     setup(
-        name='indico',
-        version=get_version(),
         cmdclass=cmdclass,
-        description='Indico is a full-featured conference lifecycle management and meeting/lecture scheduling tool',
-        long_description_content_type='text/markdown',
-        author='Indico Team',
-        author_email='indico-team@cern.ch',
-        url='https://getindico.io',
-        download_url='https://github.com/indico/indico/releases',
-        long_description="Indico allows you to schedule conferences, from single talks to complex meetings with "
-                         "sessions and contributions. It also includes an advanced user delegation mechanism, "
-                         "allows paper reviewing, archival of conference information and electronic proceedings",
-        license='MIT',
-        zip_safe=False,
-        packages=find_packages(include=('indico', 'indico.*',)),
-        include_package_data=True,
         install_requires=get_requirements(),
         python_requires=('~=3.9' if 'READTHEDOCS' not in os.environ else None),
-        classifiers=[
-            'Environment :: Web Environment',
-            'License :: OSI Approved :: MIT License',
-            'Programming Language :: Python :: 3.9',
-        ],
-        entry_points={
-            'console_scripts': {'indico = indico.cli.core:cli'},
-            'celery.commands': {'unlock = indico.core.celery.cli:UnlockCommand'},
-            'pytest11': {'indico = indico.testing.pytest_plugin'},
-        })
+    )
