74fa0bd1f26b1338a1767fb6d75fcd906820ced6
Adrian Moennich <adrian.moennich@cern.ch>
2021-02-11 10:13:41 +0100

    Fix event creation

--- indico/modules/events/__init__.py
+++ indico/modules/events/__init__.py
@@ -142,7 +142,7 @@ def _handle_legacy_ids(app, **kwargs):
 
     # Endpoints which need to deal with non-standard event "ids" because they might be shorturls.
     # Those endpoints handle legacy event ids on their own so we ignore them here.
-    _non_standard_id_endpoints = {'events.shorturl', 'events.display', 'events.display_overview'}
+    _non_standard_id_endpoints = {'events.shorturl', 'events.display', 'events.display_overview', 'events.create'}
 
     # Match event ids which are either not purely numeric or have a leading zero without being exactly `0`
     _legacy_event_id_re = re.compile(r'/event/((?=0[^/]|\d*[^\d/])[^/]*)')
