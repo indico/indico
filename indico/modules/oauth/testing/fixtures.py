e944f133d525ecc298d77f400beaef00c25b5b74
Adrian Moennich <adrian.moennich@cern.ch>
2021-02-12 16:00:51 +0100

    Use sane defaults in create_application fixture

--- indico/modules/oauth/testing/fixtures.py
+++ indico/modules/oauth/testing/fixtures.py
@@ -19,8 +19,8 @@ def create_application(db):
 
     def _create_application(name, **params):
         params.setdefault('client_id', str(uuid4()))
-        params.setdefault('default_scopes', 'read:user')
-        params.setdefault('redirect_uris', 'http://localhost:10500')
+        params.setdefault('default_scopes', ['read:user'])
+        params.setdefault('redirect_uris', ['http://localhost:10500/'])
         params.setdefault('is_trusted', True)
         application = OAuthApplication(name=name, **params)
         db.session.add(application)
