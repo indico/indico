b204f758b5f4c869f867e9e9fbd2eddb084dcbbe
Javier Ferrer <javier.ferrer@cern.ch>
2021-02-15 09:49:58 +0000

    Add attach_ical flag to Reminder

---
+++ indico/migrations/versions/20210211_1613_26985db8ed12_add_attach_ical.py
@@ -0,0 +1,25 @@
+"""add_attach_ical
+
+Revision ID: 26985db8ed12
+Revises: e4fb983dc64c
+Create Date: 2021-02-11 16:13:25.061874
+"""
+
+import sqlalchemy as sa
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = '26985db8ed12'
+down_revision = 'e4fb983dc64c'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column('reminders', sa.Column('attach_ical', sa.Boolean(), nullable=False, server_default='false'), schema='events')
+    op.alter_column('reminders', 'attach_ical', server_default=None, schema='events')
+
+
+def downgrade():
+    op.drop_column('reminders', 'attach_ical', schema='events')
