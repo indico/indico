f8e0d77a6d24217d0567da7c73f236f541a3149f
Alejandro Avilés <alejandro.aviles@un.org>
2021-02-18 12:53:59 +0100

    Avoid querying active registrations if not needed

--- indico/modules/events/registration/fields/choices.py
+++ indico/modules/events/registration/fields/choices.py
@@ -128,6 +128,8 @@ class ChoiceBaseField(RegistrationFormBillableItemsField):
 
     def get_places_used(self):
         places_used = Counter()
+        if not any(x.get('places_limit') for x in self.form_item.versioned_data['choices']):
+            return dict(places_used)
         for registration in self.form_item.registration_form.active_registrations:
             if self.form_item.id not in registration.data_by_field:
                 continue
@@ -416,6 +418,8 @@ class AccommodationField(RegistrationFormBillableItemsField):
 
     def get_places_used(self):
         places_used = Counter()
+        if not any(x.get('places_limit') for x in self.form_item.versioned_data['choices']):
+            return dict(places_used)
         for registration in self.form_item.registration_form.active_registrations:
             if self.form_item.id not in registration.data_by_field:
                 continue
