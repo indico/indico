aeee94f34f07d745802c5ae1c6f422f1eed4adbd
Adrian Moennich <adrian.moennich@cern.ch>
2021-03-15 18:00:55 +0100

    Fix static program page having header+footer

--- indico/modules/events/tracks/controllers.py
+++ indico/modules/events/tracks/controllers.py
@@ -132,6 +132,8 @@ class RHDeleteTrack(RHManageTrackBase):
 
 
 class RHDisplayTracks(RHDisplayEventBase):
+    view_class = WPDisplayTracks
+
     def _process(self):
         program = track_settings.get(self.event, 'program')
         render_mode = track_settings.get(self.event, 'program_render_mode')
@@ -141,7 +143,7 @@ class RHDisplayTracks(RHDisplayEventBase):
                   .all())
         track_groups = self.event.track_groups
         items = sorted(tracks + track_groups, key=attrgetter('position'))
-        return WPDisplayTracks.render_template('display.html', self.event, program=program, items=items)
+        return self.view_class.render_template('display.html', self.event, program=program, items=items)
 
 
 class RHTracksPDF(RHDisplayEventBase):
