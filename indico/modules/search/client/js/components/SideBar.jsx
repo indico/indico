af8c778aeb9540ccf8232d9f74b276201d094134
Pedro Lourenço <pedro.lourenco@cern.ch>
2021-03-25 14:58:43 +0000

    Refactor the search client
    Update the sidebar

--- indico/modules/search/client/js/components/SideBar.jsx
+++ indico/modules/search/client/js/components/SideBar.jsx
@@ -6,130 +6,84 @@
 // LICENSE file for more details.
 
 import PropTypes from 'prop-types';
-import React, {useState} from 'react';
+import React from 'react';
 import {Table, Checkbox} from 'semantic-ui-react';
-import {useQueryParam, StringParam} from 'use-query-params';
 
 import './SideBar.module.scss';
 
-export default function SideBar({filterType}) {
-  const [speakerValue, setSpeakerValue] = useState('');
-  const [affiliationValue, setAffiliationValue] = useState('');
-
-  const [speakerFilter, setSpeakerFilter] = useQueryParam('speaker', StringParam);
-  const [affiliationFilter, setAffiliationFilter] = useQueryParam('affiliation', StringParam);
-
-  const addSearchFilter = (value, type) => {
-    if (type === 'speaker') {
-      setSpeakerFilter(speakerValue === value ? '' : value, 'pushIn');
-      setSpeakerValue(speakerValue === value ? '' : value);
-    } else if (type === 'affiliation') {
-      setAffiliationFilter(affiliationValue === value ? '' : value, 'pushIn');
-      setAffiliationValue(affiliationValue === value ? '' : value);
-    }
+export default function SideBar({query, aggregations, onChange}) {
+  const handleChange = (type, value) => {
+    onChange && onChange(type in query && query[type] === value ? undefined : value, type);
   };
 
-  const handleChange = (e, {value}) => {
-    addSearchFilter(value.name, value.type);
-  };
-
-  // it's going to change soon
-  if (filterType !== 'Contributions') {
-    return;
-  }
   return (
     <div styleName="sidebar">
-      <Table celled padded>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>Speakers</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-
-        <Table.Body>
-          <Table.Row>
-            <Table.Cell>
-              <Checkbox
-                label="Aristofanis (45)"
-                name="Aristofanis"
-                value={{name: 'Aristofanis', type: 'speaker'}}
-                checked={speakerValue === 'Aristofanis'}
-                onChange={handleChange}
-              />
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell>
-              <Checkbox
-                label="Pedro (4555)"
-                name="Pedro"
-                value={{name: 'Pedro', type: 'speaker'}}
-                checked={speakerValue === 'Pedro'}
-                onChange={handleChange}
-              />
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell>
-              <Checkbox
-                label="Adrian (455)"
-                name="Adrian"
-                value={{name: 'Adrian', type: 'speaker'}}
-                checked={speakerValue === 'Adrian'}
-                onChange={handleChange}
-              />
-            </Table.Cell>
-          </Table.Row>
-        </Table.Body>
-      </Table>
-
-      <Table celled padded>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>Affiliations</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-
-        <Table.Body>
-          <Table.Row>
-            <Table.Cell>
-              <Checkbox
-                label="Natalia's (123)"
-                name="Natalia"
-                value={{name: 'Natalia', type: 'affiliation'}}
-                checked={affiliationValue === 'Natalia'}
-                onChange={handleChange}
-              />
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell>
-              <Checkbox
-                label="Marco's (321)"
-                name="Marco"
-                value={{name: 'Marco', type: 'affiliation'}}
-                checked={affiliationValue === 'Marco'}
-                onChange={handleChange}
-              />
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell>
-              <Checkbox
-                label="Giota's (333)"
-                name="Giota"
-                value={{name: 'Giota', type: 'affiliation'}}
-                checked={affiliationValue === 'Giota'}
-                onChange={handleChange}
-              />
-            </Table.Cell>
-          </Table.Row>
-        </Table.Body>
-      </Table>
+      {aggregations
+        .filter(({buckets}) => buckets && buckets.length)
+        .map(({key, label, buckets}) => (
+          <AggregationList
+            key={key}
+            name={key}
+            title={label}
+            items={buckets}
+            query={query}
+            onChange={handleChange}
+          />
+        ))}
     </div>
   );
 }
 
+function AggregationList({name, title, items, query, onChange}) {
+  const _items = items
+    .filter(b => b.key)
+    .map(({key, fromAsString: from, toAsString: to, docCount: count}) => ({
+      key: from || to ? `[${from || '*'} TO ${to || '*'}]` : key,
+      label: key.charAt(0).toUpperCase() + key.slice(1),
+      count,
+    }));
+
+  return (
+    <Table fixed singleLine celled padded>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell styleName="capitalize">{title}</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {_items.map(({key, label, count}) => (
+          <Table.Row key={key}>
+            <Table.Cell>
+              <Checkbox
+                styleName="checkbox"
+                label={`${label} (${count})`}
+                checked={name in query && query[name] === key}
+                onChange={() => onChange(name, key)}
+              />
+            </Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table>
+  );
+}
+
+AggregationList.propTypes = {
+  name: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  items: PropTypes.array.isRequired,
+  query: PropTypes.object.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
 SideBar.propTypes = {
-  filterType: PropTypes.string.isRequired,
+  query: PropTypes.object,
+  aggregations: PropTypes.array,
+  onChange: PropTypes.func,
+};
+
+SideBar.defaultProps = {
+  query: {},
+  aggregations: [],
+  onChange: undefined,
 };
