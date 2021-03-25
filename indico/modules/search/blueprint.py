36c285818d9ca74a8a3fb5dc834edd0399f04d4e
Pedro Lourenço <pedro.lourenco@cern.ch>
2021-03-25 11:02:06 +0000

    Update the search controller into a single endpoint

--- indico/modules/search/blueprint.py
+++ indico/modules/search/blueprint.py
@@ -5,8 +5,7 @@
 # modify it under the terms of the MIT License; see the
 # LICENSE file for more details.
 
-from indico.modules.search.controllers import (RHAPISearchAttachment, RHAPISearchCategory, RHAPISearchContribution,
-                                               RHAPISearchEvent, RHSearchDisplay)
+from indico.modules.search.controllers import RHAPISearch, RHSearchDisplay
 from indico.web.flask.wrappers import IndicoBlueprint
 
 
@@ -16,7 +15,5 @@ _bp = IndicoBlueprint('search', __name__, template_folder='templates', virtual_t
 _bp.add_url_rule('/search/', 'search', RHSearchDisplay)
 
 # Search APIs
-_bp.add_url_rule('/search/api/category', 'api_search_category', RHAPISearchCategory)
-_bp.add_url_rule('/search/api/event', 'api_search_event', RHAPISearchEvent)
-_bp.add_url_rule('/search/api/contribution', 'api_search_contribution', RHAPISearchContribution)
-_bp.add_url_rule('/search/api/attachment', 'api_search_attachment', RHAPISearchAttachment)
+_bp.add_url_rule('/search/api/<any(event,category,contribution,attachment,event_note):type>',
+                 'api_search', RHAPISearch)
