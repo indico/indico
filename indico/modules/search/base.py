3a1353f337bfa57244e78bd090484718f0944d98
Pedro Lourenço <pedro.lourenco@cern.ch>
2021-03-25 11:02:06 +0000

    Update the search controller into a single endpoint

--- indico/modules/search/base.py
+++ indico/modules/search/base.py
@@ -1,3 +1,10 @@
+# This file is part of Indico.
+# Copyright (C) 2002 - 2021 CERN
+#
+# Indico is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see the
+# LICENSE file for more details.
+
 from indico.core import signals
 from indico.util.enum import IndicoEnum
 from indico.util.signals import values_from_signal
@@ -12,11 +19,13 @@ class SearchTarget(int, IndicoEnum):
     category = 1
     event = 2
     contribution = 3
-    attachment = 4
+    subcontribution = 4
+    event_note = 5
+    attachment = 6
 
 
 class IndicoSearchProvider:
     RESULTS_PER_PAGE = 10
 
-    def search(self, query, access, object_type=SearchTarget.event, page=1):
+    def search(self, query, access, object_type=SearchTarget.event, page=1, params=None):
         raise NotImplementedError()
