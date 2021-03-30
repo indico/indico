3b45da4880e3e6c7b75b15e5a939e07ae19af6b6
Jan Schill <janschill@me.com>
2021-03-30 15:40:24 +0200

    Integrate solid-comment persistent layer

--- indico/modules/events/blueprint.py
+++ indico/modules/events/blueprint.py
@@ -5,12 +5,19 @@
 # modify it under the terms of the MIT License; see the
 # LICENSE file for more details.
 
+from flask import jsonify, request, session
+from webargs import fields
+from werkzeug.exceptions import Forbidden
+
 from indico.modules.events.controllers.admin import (RHCreateEventLabel, RHCreateReferenceType, RHDeleteEventLabel,
                                                      RHDeleteReferenceType, RHEditEventLabel, RHEditReferenceType,
                                                      RHEventLabels, RHReferenceTypes)
+from indico.modules.events.controllers.base import RHDisplayEventBase
 from indico.modules.events.controllers.creation import RHCreateEvent
 from indico.modules.events.controllers.display import RHEventAccessKey, RHEventMarcXML, RHExportEventICAL
 from indico.modules.events.controllers.entry import event_or_shorturl
+from indico.modules.events.settings import EventSettingsProxy
+from indico.web.args import use_kwargs
 from indico.web.flask.util import make_compat_redirect_func, redirect_view
 from indico.web.flask.wrappers import IndicoBlueprint
 
@@ -69,3 +76,35 @@ _compat_bp.add_url_rule('/conferenceDisplay.py/overview', 'display_overview_modp
 _compat_bp.add_url_rule('/event/<int:event_id>/my-conference/', 'display_mystuff',
                         make_compat_redirect_func(_bp, 'display'))
 _compat_bp.add_url_rule('/myconference.py', 'display_mystuff_modpython', make_compat_redirect_func(_bp, 'display'))
+
+
+solid_comments = EventSettingsProxy('solid_comments', {
+    'comment_ids': [],
+})
+
+class RHEventSolidComments(RHDisplayEventBase):
+    def _check_access(self):
+        RHDisplayEventBase._check_access(self)
+        print(session.user)
+        if not session.user and request.method != 'GET':
+            raise Forbidden
+
+    def _process_GET(self):
+        print(session.user)
+        return jsonify(solid_comments.get(self.event, 'comment_ids'))
+
+    @use_kwargs({
+        'comment_id': fields.String(required=True)
+    }, location='json')
+    def _process_POST(self, comment_id):
+        comment_ids = solid_comments.get(self.event, 'comment_ids')
+        if comment_id in comment_ids:
+            return '', 409
+        comment_ids.append(comment_id)
+        solid_comments.set(self.event, 'comment_ids', comment_ids)
+        return '', 201
+        # alternatively (if you need it), return the jsonified list like in GET
+
+
+# in case the blueprint you use already has a prefix with the event id, just put `/solid-comments` below
+_bp.add_url_rule('/event/<int:event_id>/solid-comments', 'solid_comments', RHEventSolidComments, methods=('GET', 'POST'))
