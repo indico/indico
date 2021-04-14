4ee3e441a4704c11916b22e427f323cfcd0be38c
Adrian Moennich <adrian.moennich@cern.ch>
2021-04-14 18:00:19 +0200

    Remove unused import

--- indico/modules/search/client/js/index.jsx
+++ indico/modules/search/client/js/index.jsx
@@ -7,7 +7,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 import SearchApp from './components/SearchApp';
 
