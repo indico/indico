9aec42a5677fa3f26e52db5b44f771da2c896799
Adrian <adrian@planetcoding.net>
2021-04-14 15:17:10 +0200

    Update indico/modules/rb/__init__.py

--- indico/modules/rb/__init__.py
+++ indico/modules/rb/__init__.py
@@ -75,7 +75,7 @@ def _topmenu_items(sender, **kwargs):
 @signals.menu.items.connect_via('event-management-sidemenu')
 def _sidemenu_items(sender, event, **kwargs):
     if config.ENABLE_ROOMBOOKING and event.can_manage(session.user):
-        yield SideMenuItem('room_booking', _('Room bBooking'), url_for('rb.event_booking_list', event), 50,
+        yield SideMenuItem('room_booking', _('Room booking'), url_for('rb.event_booking_list', event), 50,
                            icon='location')
 
 
