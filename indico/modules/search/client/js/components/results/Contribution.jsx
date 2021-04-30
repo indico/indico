922d80f1709f3d2c12f2ceba055c366f99848688
Pedro Lourenço <pedro.lourenco@cern.ch>
2021-04-30 09:50:19 +0100

    Move placeholders to the search provider (#4887)
    * Use InternalSearch as default search provider
    * Move placeholders to the search provider
    * Fix subcontribution URLs
    * Add placeholder validation
    * Make PlaceholderSchema attributes required
    * Enforce placeholder validation
    * Use dataclass for search placeholders
    
    Co-authored-by: Adrian Moennich <adrian.moennich@cern.ch>

--- indico/modules/search/client/js/components/results/Contribution.jsx
+++ indico/modules/search/client/js/components/results/Contribution.jsx
@@ -6,6 +6,7 @@
 // LICENSE file for more details.
 
 import contributionURL from 'indico-url:contributions.display_contribution';
+import subcontributionURL from 'indico-url:contributions.display_subcontribution';
 
 import PropTypes from 'prop-types';
 import React from 'react';
@@ -17,30 +18,38 @@ import PersonList from './PersonList';
 
 import '../ResultList.module.scss';
 
-const Contribution = ({eventId, contributionId, title, startDt, persons}) => (
-  <div styleName="item">
-    <List.Header styleName="header">
-      <a href={contributionURL({event_id: eventId, contrib_id: contributionId})}>{title}</a>
-    </List.Header>
-    <List.Description styleName="description">
-      {persons.length !== 0 && (
-        <List.Item>
-          <PersonList persons={persons} />
-        </List.Item>
-      )}
-      {startDt && (
-        <List.Item>
-          <Icon name="calendar alternate outline" />
-          {serializeDate(toMoment(startDt), 'DD MMMM YYYY HH:mm')}
-        </List.Item>
-      )}
-    </List.Description>
-  </div>
-);
+function Contribution({eventId, contributionId, subcontributionId, title, startDt, persons}) {
+  const urlParams = {event_id: eventId, contrib_id: contributionId};
+  const url = subcontributionId
+    ? subcontributionURL({...urlParams, subcontrib_id: subcontributionId})
+    : contributionURL(urlParams);
+
+  return (
+    <div styleName="item">
+      <List.Header styleName="header">
+        <a href={url}>{title}</a>
+      </List.Header>
+      <List.Description styleName="description">
+        {persons.length !== 0 && (
+          <List.Item>
+            <PersonList persons={persons} />
+          </List.Item>
+        )}
+        {startDt && (
+          <List.Item>
+            <Icon name="calendar alternate outline" />
+            {serializeDate(toMoment(startDt), 'DD MMMM YYYY HH:mm')}
+          </List.Item>
+        )}
+      </List.Description>
+    </div>
+  );
+}
 
 Contribution.propTypes = {
   title: PropTypes.string.isRequired,
   contributionId: PropTypes.number.isRequired,
+  subcontributionId: PropTypes.number,
   eventId: PropTypes.number.isRequired,
   startDt: PropTypes.string,
   persons: PropTypes.arrayOf(
@@ -54,6 +63,7 @@ Contribution.propTypes = {
 Contribution.defaultProps = {
   startDt: undefined,
   persons: [],
+  subcontributionId: undefined,
 };
 
 export default Contribution;
