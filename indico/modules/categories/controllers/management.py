86e861dd6d65fbd206791ae1f8d41f07d1b6c948
Adrian Moennich <adrian.moennich@cern.ch>
2021-06-02 16:51:35 +0200

    Fix moving categories if target contains events

--- indico/modules/categories/controllers/management.py
+++ indico/modules/categories/controllers/management.py
@@ -268,8 +268,6 @@ class RHMoveCategoryBase(RHManageCategoryBase):
             self.target_category = Category.get_or_404(int(target_category_id), is_deleted=False)
             if not self.target_category.can_manage(session.user):
                 raise Forbidden(_("You are not allowed to manage the selected destination."))
-            if self.target_category.events:
-                raise BadRequest(_("The destination already contains an event."))
 
 
 class RHMoveCategory(RHMoveCategoryBase):
