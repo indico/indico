560207f3330e698297809e2a5bf177244069cc2d
Pedro Lourenço <pedro.lourenco@cern.ch>
2021-07-15 15:16:16 +0100

    Include file headers

--- indico/modules/categories/models/event_move_request.py
+++ indico/modules/categories/models/event_move_request.py
@@ -1,3 +1,10 @@
+# This file is part of Indico.
+# Copyright (C) 2002 - 2021 CERN
+#
+# Indico is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see the
+# LICENSE file for more details.
+
 from indico.core.db import db
 from indico.core.db.sqlalchemy import PyIntEnum, UTCDateTime
 from indico.modules.events import EventLogKind, EventLogRealm
