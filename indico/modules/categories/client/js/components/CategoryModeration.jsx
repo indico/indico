543ae4ce45ccfe86aeabc72ff4e22bc0a38b4c12
Pedro Lourenço <pedro.lourenco@cern.ch>
2021-07-15 15:16:16 +0100

    Include file headers

--- indico/modules/categories/client/js/components/CategoryModeration.jsx
+++ indico/modules/categories/client/js/components/CategoryModeration.jsx
@@ -1,3 +1,10 @@
+// This file is part of Indico.
+// Copyright (C) 2002 - 2021 CERN
+//
+// Indico is free software; you can redistribute it and/or
+// modify it under the terms of the MIT License; see the
+// LICENSE file for more details.
+
 import eventRequestsURL from 'indico-url:categories.api_event_move_requests';
 import eventURL from 'indico-url:events.display';
 
