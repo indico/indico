d39194f254f1b614a9d87677045808d86a18832f
Pedro Lourenço <pedro.lourenco@cern.ch>
2021-07-28 18:14:09 +0100

    Update the move request list loading state

--- indico/modules/categories/client/js/components/CategoryModeration.jsx
+++ indico/modules/categories/client/js/components/CategoryModeration.jsx
@@ -10,7 +10,7 @@ import eventURL from 'indico-url:events.display';
 
 import PropTypes from 'prop-types';
 import React, {useState} from 'react';
-import {Button, Checkbox, Image, Table} from 'semantic-ui-react';
+import {Button, Checkbox, Image, Placeholder, Table} from 'semantic-ui-react';
 
 import {handleSubmitError} from 'indico/react/forms';
 import {useIndicoAxios} from 'indico/react/hooks';
@@ -19,7 +19,65 @@ import {indicoAxios} from 'indico/utils/axios';
 import {serializeDate} from 'indico/utils/date';
 import './CategoryModeration.module.scss';
 
-function EventRequestList({requests, onSubmit}) {
+function PlaceholderTableRow() {
+  return (
+    <Table.Row>
+      <Table.Cell collapsing>
+        <Checkbox />
+      </Table.Cell>
+      <Table.Cell colSpan="4">
+        <Placeholder fluid>
+          <Placeholder.Header image>
+            <Placeholder.Line />
+            <Placeholder.Line />
+          </Placeholder.Header>
+        </Placeholder>
+      </Table.Cell>
+    </Table.Row>
+  );
+}
+
+function RequestTableRow({submitter, event, submittedDt, selected, onSelect}) {
+  const {fullName, avatarURL, affiliation} = submitter;
+  return (
+    <Table.Row>
+      <Table.Cell collapsing>
+        <Checkbox checked={selected} onChange={onSelect} />
+      </Table.Cell>
+      <Table.Cell styleName="user">
+        <Image styleName="avatar" src={avatarURL} size="mini" avatar />
+        <span>
+          {fullName} {affiliation && `(${affiliation})`}
+        </span>
+      </Table.Cell>
+      <Table.Cell>
+        <a href={eventURL({event_id: event.id})}>{event.title}</a>
+      </Table.Cell>
+      <Table.Cell>{serializeDate(submittedDt)}</Table.Cell>
+    </Table.Row>
+  );
+}
+
+RequestTableRow.propTypes = {
+  submitter: PropTypes.shape({
+    fullName: PropTypes.string,
+    avatarURL: PropTypes.string,
+    affiliation: PropTypes.string,
+  }).isRequired,
+  event: PropTypes.shape({
+    id: PropTypes.number,
+    title: PropTypes.string,
+  }).isRequired,
+  submittedDt: PropTypes.string.isRequired,
+  selected: PropTypes.bool,
+  onSelect: PropTypes.func.isRequired,
+};
+
+RequestTableRow.defaultProps = {
+  selected: false,
+};
+
+function RequestList({requests, onSubmit, loading}) {
   const [selected, _setSelected] = useState(new Set());
   const isAnySelected = selected.size > 0;
 
@@ -37,8 +95,8 @@ function EventRequestList({requests, onSubmit}) {
       return set;
     });
 
-  if (requests.length === 0) {
-    return <Translate as="p">There are no event move requests</Translate>;
+  if (requests.length === 0 && !loading) {
+    return <Translate as="p">There are no event move requests.</Translate>;
   }
 
   return (
@@ -48,33 +106,25 @@ function EventRequestList({requests, onSubmit}) {
           <Table.HeaderCell />
           <Translate as={Table.HeaderCell}>User</Translate>
           <Translate as={Table.HeaderCell}>Event</Translate>
-          <Translate as={Table.HeaderCell}>State</Translate>
           <Translate as={Table.HeaderCell}>Submitted Date</Translate>
         </Table.Row>
       </Table.Header>
       <Table.Body>
-        {requests.map(({id, submitter, event, state, submittedDt}) => (
-          <Table.Row key={id}>
-            <Table.Cell collapsing>
-              <Checkbox
-                checked={selected.has(id)}
-                onChange={() => select(id)}
-                disabled={state !== 'pending'}
-              />
-            </Table.Cell>
-            <Table.Cell styleName="user">
-              <Image styleName="avatar" src={submitter.avatarURL} size="mini" avatar />
-              <span>
-                {submitter.fullName} {submitter.affiliation && `(${submitter.affiliation})`}
-              </span>
-            </Table.Cell>
-            <Table.Cell>
-              <a href={eventURL({event_id: event.id})}>{event.title}</a>
-            </Table.Cell>
-            <Table.Cell>{state}</Table.Cell>
-            <Table.Cell>{serializeDate(submittedDt)}</Table.Cell>
-          </Table.Row>
-        ))}
+        {loading ? (
+          <PlaceholderTableRow />
+        ) : (
+          requests.map(({id, submitter, event, submittedDt}) => (
+            <RequestTableRow
+              key={id}
+              id={id}
+              event={event}
+              submitter={submitter}
+              submittedDt={submittedDt}
+              selected={selected.has(id)}
+              onSelect={() => select(id)}
+            />
+          ))
+        )}
       </Table.Body>
       <Table.Footer fullWidth>
         <Table.Row>
@@ -103,13 +153,18 @@ function EventRequestList({requests, onSubmit}) {
   );
 }
 
-EventRequestList.propTypes = {
+RequestList.propTypes = {
   requests: PropTypes.array.isRequired,
   onSubmit: PropTypes.func.isRequired,
+  loading: PropTypes.bool,
+};
+
+RequestList.defaultProps = {
+  loading: false,
 };
 
 export default function CategoryModeration({categoryId}) {
-  const {data, reFetch} = useIndicoAxios({
+  const {data, loading, reFetch} = useIndicoAxios({
     url: eventRequestsURL({category_id: categoryId}),
     trigger: categoryId,
     camelize: true,
@@ -125,7 +180,9 @@ export default function CategoryModeration({categoryId}) {
     }
   };
 
-  return <EventRequestList requests={data || []} onSubmit={setRequestsState} />;
+  return (
+    <RequestList requests={data || []} onSubmit={setRequestsState} loading={!data || loading} />
+  );
 }
 
 CategoryModeration.propTypes = {
