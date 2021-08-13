ec31ea8ee0efb406abd7071a6a17139f767d46fa
Javier Ferrer <javier.ferrer@cern.ch>
2021-08-13 16:34:18 +0200

    Update alembic revision after rebase

--- indico/migrations/versions/20210813_1633_dc53d6e8c576_make_event_category_nullable.py
+++ indico/migrations/versions/20210813_1633_dc53d6e8c576_make_event_category_nullable.py
@@ -1,7 +1,7 @@
 """Make event category nullable
 
 Revision ID: dc53d6e8c576
-Revises: 1cec32e42f65
+Revises: 9d00917b2fa8
 Create Date: 2021-07-07 16:48:03.719901
 """
 
@@ -10,7 +10,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = 'dc53d6e8c576'
-down_revision = '1cec32e42f65'
+down_revision = '9d00917b2fa8'
 branch_labels = None
 depends_on = None
 
