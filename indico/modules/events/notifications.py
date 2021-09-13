4abe87e2b99a5d6fe8fe1f120638b0fd65eea443
Tomáš Roun <tomas.roun@cern.ch>
2021-09-13 17:47:16 +0200

    Add email notifications for moderated categories (#5057)
    * Fix incorrect docstring
    * Notify requestors when a move request is accepted/rejected
    * Notify managers when a move request is created
    * Add snapshot tests for event request emails
    * Update move request email tests
    * Group emails by target category
    
    Co-authored-by: Adrian Moennich <adrian.moennich@cern.ch>

--- indico/modules/events/notifications.py
+++ indico/modules/events/notifications.py
@@ -5,12 +5,28 @@
 # modify it under the terms of the MIT License; see the
 # LICENSE file for more details.
 
+import itertools
+from operator import attrgetter
+
 from sqlalchemy.orm import joinedload
 
 from indico.core.db.sqlalchemy.principals import PrincipalType
 from indico.core.notifications import make_email, send_email
 from indico.modules.categories.models.categories import Category
 from indico.web.flask.templating import get_template_module
+
+
+def _get_emails_from_category(category, ignore_notify_managers=False):
+    emails = set(category.event_creation_notification_emails)
+
+    if category.notify_managers or ignore_notify_managers:
+        for manager in category.get_manager_list():
+            if manager.principal_type in (PrincipalType.user, PrincipalType.email):
+                emails.add(manager.email)
+            elif manager.principal_type in (PrincipalType.local_group, PrincipalType.multipass_group):
+                emails.update(x.email for x in manager.get_members())
+
+    return emails
 
 
 def notify_event_creation(event, occurrences=None):
@@ -27,14 +43,47 @@ def notify_event_creation(event, occurrences=None):
              filter(Category.notify_managers | (Category.event_creation_notification_emails != []))
              .options(joinedload('acl_entries')))
     for cat in query:
-        emails.update(cat.event_creation_notification_emails)
-        if cat.notify_managers:
-            for manager in cat.get_manager_list():
-                if manager.principal_type in (PrincipalType.user, PrincipalType.email):
-                    emails.add(manager.email)
-                elif manager.principal_type in (PrincipalType.local_group, PrincipalType.multipass_group):
-                    emails.update(x.email for x in manager.get_members())
+        emails.update(_get_emails_from_category(cat))
 
     if emails:
         template = get_template_module('events/emails/event_creation.txt', event=event, occurrences=occurrences)
         send_email(make_email(bcc_list=emails, template=template))
+
+
+def notify_move_request_creation(events, target_category, comment=''):
+    """Send email notifications when a move request is created.
+
+    :param events: List of events requested to move.
+    :param target_category: The category to move the events into.
+    :param comment: Optional requestor's comment.
+    """
+    emails = set()
+    query = (target_category.chain_query
+             .filter(Category.notify_managers | (Category.event_creation_notification_emails != []))
+             .options(joinedload('acl_entries')))
+
+    # Walk up the category chain
+    for cat in reversed(query.all()):
+        emails.update(_get_emails_from_category(cat, ignore_notify_managers=True))
+
+        if emails:
+            template = get_template_module('events/emails/move_request_creation.txt',
+                                           events=events, target_category=target_category, comment=comment)
+            send_email(make_email(bcc_list=emails, template=template))
+            break
+
+
+def notify_move_request_closure(move_requests, accept, reason=''):
+    """Send email notifications when a move request is accepted/rejected.
+
+    :param move_requests: List of `EventMoveRequest` that were accepted/rejected.
+    :param accept: Whether the requests were accepted.
+    :param reason: Optional reason for rejection.
+    """
+
+    move_requests = sorted(move_requests, key=attrgetter('requestor.id', 'category.id'))
+    for (requestor, category), requests in itertools.groupby(move_requests, attrgetter('requestor', 'category')):
+        events = [rq.event for rq in requests]
+        template = get_template_module('events/emails/move_request_closure.txt',
+                                       events=events, target_category=category, accept=accept, reason=reason)
+        send_email(make_email(to_list=requestor.email, template=template))
---
+++ indico/modules/events/notifications_test.py
@@ -0,0 +1,46 @@
+# This file is part of Indico.
+# Copyright (C) 2002 - 2021 CERN
+#
+# Indico is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see the
+# LICENSE file for more details.
+
+from pathlib import Path
+
+import pytest
+
+from indico.web.flask.templating import get_template_module
+
+
+@pytest.mark.parametrize(('num_events', 'comment', 'snapshot_name'), (
+    (1, '', 'move-request-new-single-nocomment.txt'),
+    (2, '', 'move-request-new-multi-nocomment.txt'),
+    (1, 'Please move this.\nThanks!', 'move-request-new-single-comment.txt'),
+    (2, 'Please move this.\nThanks!', 'move-request-new-multi-comment.txt'),
+))
+def test_move_request_creation_email_plaintext(snapshot, create_category, create_event, num_events, comment,
+                                               snapshot_name):
+    cat = create_category(id_=314, title='target')
+    events = [create_event(id_=n, title=f'test {n}') for n in range(1, num_events + 1)]
+    template = get_template_module('events/emails/move_request_creation.txt',
+                                   events=events, target_category=cat, comment=comment)
+    snapshot.snapshot_dir = Path(__file__).parent / 'templates/emails/tests'
+    snapshot.assert_match(template.get_body(), snapshot_name)
+
+
+@pytest.mark.parametrize(('accept', 'num_events', 'reason', 'snapshot_name'), (
+    (False, 1, '', 'move-request-rejected-single-noreason.txt'),
+    (False, 2, '', 'move-request-rejected-multi-noreason.txt'),
+    (False, 1, 'We do not want your event.\nSorry not sorry.', 'move-request-rejected-single-reason.txt'),
+    (False, 2, 'We do not want your event.\nSorry not sorry.', 'move-request-rejected-multi-reason.txt'),
+    (True, 1, '', 'move-request-accepted-single.txt'),
+    (True, 2, '', 'move-request-accepted-multi.txt'),
+))
+def test_move_request_closed_email_plaintext(snapshot, create_category, create_event, accept, num_events, reason,
+                                             snapshot_name):
+    cat = create_category(id_=314, title='target')
+    events = [create_event(id_=n, title=f'test {n}') for n in range(1, num_events + 1)]
+    template = get_template_module('events/emails/move_request_closure.txt',
+                                   events=events, target_category=cat, accept=accept, reason=reason)
+    snapshot.snapshot_dir = Path(__file__).parent / 'templates/emails/tests'
+    snapshot.assert_match(template.get_body(), snapshot_name)
