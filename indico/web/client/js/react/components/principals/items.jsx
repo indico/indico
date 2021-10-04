20b8c7b2c7da270b96df1574d9d6992a2481f3ae
Pedro Lourenço <pedro.lourenco@cern.ch>
2021-10-04 18:19:46 +0100

    Update Principal components
    Create PersonListItem

--- indico/web/client/js/react/components/principals/items.jsx
+++ indico/web/client/js/react/components/principals/items.jsx
@@ -7,7 +7,7 @@
 
 import PropTypes from 'prop-types';
 import React from 'react';
-import {Icon, List, Loader, Popup, Image} from 'semantic-ui-react';
+import {Icon, List, Loader, Popup, Image, Label} from 'semantic-ui-react';
 
 import {Translate} from 'indico/react/i18n';
 
@@ -16,19 +16,15 @@ import {PrincipalType} from './util';
 import './items.module.scss';
 
 export const PendingPrincipalListItem = ({type}) => (
-  <List.Item>
-    <div styleName="item">
-      <div styleName="icon">
-        <Icon name={PrincipalType.getIcon(type)} size="large" />
-      </div>
-      <div styleName="content">
-        <List.Content>{PrincipalType.getPendingText(type)}</List.Content>
-      </div>
-      <div styleName="loader">
-        <Loader active inline size="small" />
-      </div>
+  <PrincipalItem as={List.Item}>
+    <div styleName="icon">
+      <Icon name={PrincipalType.getIcon(type)} size="large" />
     </div>
-  </List.Item>
+    <PrincipalItem.Content name={PrincipalType.getPendingText(type)} />
+    <div styleName="loader">
+      <Loader active inline size="small" />
+    </div>
+  </PrincipalItem>
 );
 
 PendingPrincipalListItem.propTypes = {
@@ -37,6 +33,90 @@ PendingPrincipalListItem.propTypes = {
 
 PendingPrincipalListItem.defaultProps = {
   type: PrincipalType.user,
+};
+
+const PrincipalItemIcon = ({type, meta, invalid, avatarURL}) =>
+  type === PrincipalType.eventRole || type === PrincipalType.categoryRole ? (
+    <div styleName="event-role">
+      <span style={meta.style}>{meta.code}</span>
+    </div>
+  ) : (
+    <div styleName="icon">
+      {invalid ? (
+        <Popup
+          trigger={
+            <Icon.Group size="large">
+              <Icon name={PrincipalType.getIcon(type)} />
+              <Icon name="exclamation triangle" color="orange" corner />
+            </Icon.Group>
+          }
+        >
+          {PrincipalType.getDeletedText(type)}
+        </Popup>
+      ) : avatarURL ? (
+        <Image src={avatarURL} size="mini" avatar />
+      ) : (
+        <Icon name={PrincipalType.getIcon(type)} size="large" />
+      )}
+    </div>
+  );
+
+PrincipalItemIcon.propTypes = {
+  type: PropTypes.string,
+  meta: PropTypes.object,
+  invalid: PropTypes.bool,
+  avatarURL: PropTypes.string,
+};
+
+PrincipalItemIcon.defaultProps = {
+  type: PrincipalType.user,
+  meta: {},
+  invalid: false,
+  avatarURL: null,
+};
+
+const PrincipalItemContent = ({name, detail, children}) => (
+  <div styleName="content">
+    <List.Content>{name}</List.Content>
+    {detail && (
+      <List.Description>
+        <small>{detail}</small>
+      </List.Description>
+    )}
+    {children}
+  </div>
+);
+
+PrincipalItemContent.propTypes = {
+  name: PropTypes.string.isRequired,
+  detail: PropTypes.string,
+  children: PropTypes.node,
+};
+
+PrincipalItemContent.defaultProps = {
+  detail: null,
+  children: null,
+};
+
+export const PrincipalItem = ({as, children, className, ...rest}) => {
+  const Component = as || 'div';
+  return (
+    <Component styleName="item" className={className} {...rest}>
+      {children}
+    </Component>
+  );
+};
+
+PrincipalItem.propTypes = {
+  as: PropTypes.elementType,
+  children: PropTypes.node,
+  className: PropTypes.string,
+};
+
+PrincipalItem.defaultProps = {
+  as: null,
+  children: null,
+  className: null,
 };
 
 export const PrincipalListItem = ({
@@ -57,76 +137,45 @@ export const PrincipalListItem = ({
   actions,
   avatarURL,
 }) => (
-  <List.Item>
-    <div styleName="item">
-      {type === PrincipalType.eventRole || type === PrincipalType.categoryRole ? (
-        <div styleName="event-role">
-          <span style={meta.style}>{meta.code}</span>
-        </div>
-      ) : (
-        <div styleName="icon">
-          {invalid ? (
-            <Popup
-              trigger={
-                <Icon.Group size="large">
-                  <Icon name={PrincipalType.getIcon(type)} />
-                  <Icon name="exclamation triangle" color="orange" corner />
-                </Icon.Group>
-              }
-            >
-              {PrincipalType.getDeletedText(type)}
-            </Popup>
-          ) : avatarURL ? (
-            <Image src={avatarURL} size="mini" avatar />
-          ) : (
-            <Icon name={PrincipalType.getIcon(type)} size="large" />
-          )}
-        </div>
-      )}
-      <div styleName="content">
-        <List.Content>{name}</List.Content>
-        {detail && (
-          <List.Description>
-            <small>{detail}</small>
-          </List.Description>
-        )}
-        {actions && <div>{actions}</div>}
-      </div>
-      {!readOnly && (
-        <div styleName="actions">
-          {type === PrincipalType.user &&
-            !isPendingUser &&
-            (favorite ? (
-              <Icon
-                styleName="button favorite active"
-                name="star"
-                size="large"
-                onClick={onDelFavorite}
-                disabled={disabled}
-              />
-            ) : (
-              <Icon
-                styleName="button favorite"
-                name="star outline"
-                size="large"
-                onClick={onAddFavorite}
-                disabled={disabled}
-              />
-            ))}
-          {canDelete && (
+  <PrincipalItem as={List.Item}>
+    <PrincipalItem.Icon type={type} meta={meta} invalid={invalid} avatarURL={avatarURL} />
+    <PrincipalItem.Content name={name} detail={detail}>
+      {actions && <div>{actions}</div>}
+    </PrincipalItem.Content>
+    {!readOnly && (
+      <div styleName="actions">
+        {type === PrincipalType.user &&
+          !isPendingUser &&
+          (favorite ? (
             <Icon
-              styleName="button delete"
-              name="remove"
+              styleName="button favorite active"
+              name="star"
               size="large"
-              onClick={onDelete}
+              onClick={onDelFavorite}
               disabled={disabled}
             />
-          )}
-          {search}
-        </div>
-      )}
-    </div>
-  </List.Item>
+          ) : (
+            <Icon
+              styleName="button favorite"
+              name="star outline"
+              size="large"
+              onClick={onAddFavorite}
+              disabled={disabled}
+            />
+          ))}
+        {canDelete && (
+          <Icon
+            styleName="button delete"
+            name="remove"
+            size="large"
+            onClick={onDelete}
+            disabled={disabled}
+          />
+        )}
+        {search}
+      </div>
+    )}
+  </PrincipalItem>
 );
 
 PrincipalListItem.propTypes = {
@@ -166,25 +215,105 @@ PrincipalListItem.defaultProps = {
   favorite: false,
 };
 
-export const EmptyPrincipalListItem = ({search}) => (
-  <List.Item>
-    <div styleName="item">
-      <div styleName="icon">
-        <Icon name="user outline" size="large" />
-      </div>
-      <div styleName="content">
-        <List.Content>
-          <Translate>Nobody</Translate>
-        </List.Content>
-        <List.Description>
-          <small>
-            <Translate>Select a user</Translate>
-          </small>
-        </List.Description>
-      </div>
-      {search && <div styleName="actions">{search}</div>}
+export const PersonListItem = ({
+  type,
+  meta,
+  invalid,
+  name,
+  detail,
+  avatarURL,
+  children,
+  canDelete,
+  onDelete,
+  onEdit,
+  disabled,
+  roles,
+  onChangeRoles,
+}) => (
+  <PrincipalItem as={List.Item}>
+    <PrincipalItem.Icon type={type} meta={meta} invalid={invalid} avatarURL={avatarURL} />
+    <PrincipalItem.Content name={name} detail={detail}>
+      {children}
+    </PrincipalItem.Content>
+    <div styleName="roles">
+      {roles &&
+        roles.map(({value, label, active}, idx) => (
+          <Label
+            as="a"
+            size="small"
+            key={value}
+            color={active ? 'blue' : undefined}
+            onClick={() =>
+              onChangeRoles &&
+              onChangeRoles(roles.map((r, i) => (i === idx ? {...r, active: !active} : r)))
+            }
+          >
+            {label}
+          </Label>
+        ))}
     </div>
-  </List.Item>
+    <div styleName="actions">
+      <Icon
+        styleName="button edit"
+        name="pencil alternate"
+        size="large"
+        onClick={onEdit}
+        disabled={disabled}
+      />
+      {canDelete && (
+        <Icon
+          styleName="button delete"
+          name="remove"
+          size="large"
+          onClick={onDelete}
+          disabled={disabled}
+        />
+      )}
+    </div>
+  </PrincipalItem>
+);
+
+PersonListItem.propTypes = {
+  type: PrincipalType.propType,
+  invalid: PropTypes.bool,
+  name: PropTypes.string.isRequired,
+  detail: PropTypes.string,
+  meta: PropTypes.object,
+  onDelete: PropTypes.func,
+  canDelete: PropTypes.bool,
+  onEdit: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
+  avatarURL: PropTypes.string,
+  children: PropTypes.node,
+  roles: PropTypes.array,
+  onChangeRoles: PropTypes.func,
+};
+
+PersonListItem.defaultProps = {
+  type: PrincipalType.user,
+  canDelete: true,
+  disabled: false,
+  invalid: false,
+  detail: null,
+  meta: {},
+  avatarURL: null,
+  onDelete: null,
+  children: null,
+  roles: null,
+  onChangeRoles: null,
+};
+
+export const EmptyPrincipalListItem = ({search}) => (
+  <PrincipalItem as={List.Item}>
+    <div styleName="icon">
+      <Icon name="user outline" size="large" />
+    </div>
+    <PrincipalItem.Content
+      name={Translate.string('Nobody')}
+      detail={Translate.string('Select a user')}
+    />
+    {search && <div styleName="actions">{search}</div>}
+  </PrincipalItem>
 );
 
 EmptyPrincipalListItem.propTypes = {
@@ -194,3 +323,6 @@ EmptyPrincipalListItem.propTypes = {
 EmptyPrincipalListItem.defaultProps = {
   search: null,
 };
+
+PrincipalItem.Icon = PrincipalItemIcon;
+PrincipalItem.Content = PrincipalItemContent;
