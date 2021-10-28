bc32eea49c260ff61791b2062fe2ec0ef04875e9
Tomas Roun <tomas.roun@cern.ch>
2021-10-28 16:52:04 +0200

    Add unit tests for registrations

---
+++ indico/modules/events/registration/privacy_test.py
@@ -0,0 +1,62 @@
+# This file is part of Indico.
+# Copyright (C) 2002 - 2021 CERN
+#
+# Indico is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see the
+# LICENSE file for more details.
+
+import pytest
+
+from indico.modules.events.features.util import set_feature_enabled
+from indico.modules.events.registration.models.registrations import (PublishRegistrationsMode, Registration,
+                                                                     RegistrationState)
+
+
+pytest_plugins = 'indico.modules.events.registration.testing.fixtures'
+
+
+@pytest.mark.usefixtures('dummy_reg')
+def test_registration_publishable(dummy_event, dummy_regform):
+    set_feature_enabled(dummy_event, 'registration', True)
+
+    reg = dummy_event.registrations.one()
+
+    assert dummy_regform.publish_registrations_mode == PublishRegistrationsMode.hide_all
+    assert not reg.consented_to_publish
+    assert reg.is_active
+    assert reg.state == RegistrationState.complete
+    assert not reg.is_publishable
+
+    dummy_regform.publish_registrations_mode = PublishRegistrationsMode.show_all
+    assert reg.is_publishable
+
+    dummy_regform.publish_registrations_mode = PublishRegistrationsMode.show_with_consent
+    assert not reg.is_publishable
+
+    reg.consented_to_publish = True
+    assert reg.is_publishable
+
+
+@pytest.mark.usefixtures('dummy_reg')
+def test_registration_publishable_query(dummy_event, dummy_regform):
+    set_feature_enabled(dummy_event, 'registration', True)
+
+    def _get_publishable():
+        return Registration.query.with_parent(dummy_event).filter(Registration.is_publishable)
+
+    reg = dummy_event.registrations.one()
+
+    assert dummy_regform.publish_registrations_mode == PublishRegistrationsMode.hide_all
+    assert not reg.consented_to_publish
+    assert reg.is_active
+    assert reg.state == RegistrationState.complete
+    assert not _get_publishable().all()
+
+    dummy_regform.publish_registrations_mode = PublishRegistrationsMode.show_all
+    assert _get_publishable().all()
+
+    dummy_regform.publish_registrations_mode = PublishRegistrationsMode.show_with_consent
+    assert not _get_publishable().all()
+
+    reg.consented_to_publish = True
+    assert _get_publishable().all()
