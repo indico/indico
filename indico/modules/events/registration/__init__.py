7ed342a204e6a80aaf77989b894cc38d4225ac02
Segilola Mustapha <54508387+SegiNyn@users.noreply.github.com>
2022-02-02 14:49:00 +0100

    Consider all values from signal

--- indico/modules/events/registration/__init__.py
+++ indico/modules/events/registration/__init__.py
@@ -108,7 +108,8 @@ def _extend_event_menu(sender, **kwargs):
 
     def _visible_participant_list(event):
         override = values_from_signal(signals.event.visible_participant_list.send(sender, event=event))
-        return event.has_feature('registration') if not override else override.pop()
+        override = all(override) if override else None
+        return event.has_feature('registration') if override is None else override
 
     yield MenuEntryData(_('Registration'), 'registration', 'event_registration.display_regform_list', position=10,
                         visible=_visible_registration, hide_if_restricted=False)
