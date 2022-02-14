0af19b55e6c13bc33930a98c4f7336ba4347f83c
Vasant Vohra <vasantvohra@gmail.com>
2022-02-14 17:22:01 +0530

    Add before_reminder_make_email signal (#5242)

---
+++ indico/core/signals/event/reminder.py
@@ -0,0 +1,16 @@
+# This file is part of Indico.
+# Copyright (C) 2002 - 2022 CERN
+#
+# Indico is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see the
+# LICENSE file for more details.
+
+from indico.core.signals.event import _signals
+
+
+before_reminder_make_email = _signals.signal('before-reminder-make-email', '''
+Executed before a reminder email is created. The `EventReminder` object is the sender.
+The parameters to create an email (`to_list`, `from_address`, `template` and `attachments`)
+are passed as kwargs; the signal can return a dict used to update the params which will then
+be passed to the `make_email` call.
+''')
