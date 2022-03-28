6aa43dbf98e6ffea65bcd5cf0e7e32cb2b906284
Duarte Galvao <duarte.galvao@cern.ch>
2022-03-28 17:00:32 +0200

    Add visibility duration field to privacy widget

--- indico/web/client/js/react/components/WTFParticipantVisibilityField.jsx
+++ indico/web/client/js/react/components/WTFParticipantVisibilityField.jsx
@@ -7,7 +7,7 @@
 
 import PropTypes from 'prop-types';
 import React, {useMemo, useState, useEffect} from 'react';
-import {Dropdown, Form, Icon, Message} from 'semantic-ui-react';
+import {Dropdown, Form, Icon, Input, Message} from 'semantic-ui-react';
 
 import {Translate} from 'indico/react/i18n';
 
@@ -15,6 +15,7 @@ export default function WTFParticipantVisibilityField({fieldId, wrapperId, value
   const parentElement = useMemo(() => document.getElementById(wrapperId), [wrapperId]);
   const [participantVisibility, setParticipantVisibility] = useState(values[0]);
   const [publicVisibility, setPublicVisibility] = useState(values[1]);
+  const [visibilityDuration, setVisibilityDuration] = useState(values[2]);
 
   // Trigger change only after the DOM has changed
   useEffect(() => {
@@ -46,8 +47,8 @@ export default function WTFParticipantVisibilityField({fieldId, wrapperId, value
 
   return (
     <div>
-      <div style={{display: 'flex', alignItems: 'center', gap: '0 10px'}}>
-        <Form.Field style={{flexBasis: '50%'}}>
+      <div style={{display: 'flex', alignItems: 'center', flexWrap: 'wrap', gap: '10px'}}>
+        <Form.Field style={{flexBasis: '49%'}}>
           <label>
             <Translate>Visibility to participants</Translate>
           </label>
@@ -66,7 +67,7 @@ export default function WTFParticipantVisibilityField({fieldId, wrapperId, value
             value={participantVisibility}
           />
         </Form.Field>
-        <Form.Field style={{flexBasis: '50%'}}>
+        <Form.Field style={{flexBasis: '49%'}}>
           <label>
             <Translate>Visibility to everyone</Translate>
           </label>
@@ -79,6 +80,21 @@ export default function WTFParticipantVisibilityField({fieldId, wrapperId, value
             options={publicOptions}
             selection
             value={publicVisibility}
+          />
+        </Form.Field>
+        <Form.Field style={{flexGrow: 1}}>
+          <label>
+            <Translate>Visibility duration (months)</Translate>
+          </label>
+          <Input
+            type="number"
+            placeholder={Translate.string('Permanent')}
+            step="1"
+            min="0"
+            value={visibilityDuration}
+            onChange={(evt, {value}) => setVisibilityDuration(value)}
+            disabled={participantVisibility === 'hide_all'}
+            fluid
           />
         </Form.Field>
       </div>
