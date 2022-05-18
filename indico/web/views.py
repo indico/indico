7df437469b6788ab236a996b7dadd39e134a866d
Pedro Lourenço <pedro.lourenco@cern.ch>
2022-05-18 11:25:02 +0200

    Remove all occurrences of prior ckeditor

--- indico/web/views.py
+++ indico/web/views.py
@@ -217,8 +217,6 @@ class WPBase(WPBundleMixin):
         _bundles = ('common.css', 'common.js', 'react.css', 'react.js', 'semantic-ui.js', 'semantic-ui.css',
                     'jquery.css', 'jquery.js', 'main.css', 'main.js', 'module_core.js', 'module_events.creation.js',
                     'module_attachments.js', 'outdatedbrowser.js', 'outdatedbrowser.css')
-        if not g.get('static_site'):
-            _bundles += ('ckeditor.js',)
         return _bundles
 
     def _get_head_content(self):
