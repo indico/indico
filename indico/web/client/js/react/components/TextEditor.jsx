8f65270b03deaf5af12e38b07d45b314a4a0a55c
Pedro Lourenço <pedro.lourenco@cern.ch>
2022-05-18 10:45:42 +0200

    Update dimension props in react component

--- indico/web/client/js/react/components/TextEditor.jsx
+++ indico/web/client/js/react/components/TextEditor.jsx
@@ -3,9 +3,16 @@ import ClassicEditor from 'ckeditor';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import {getConfig, onReady} from 'indico/ckeditor';
+import {getConfig} from 'indico/ckeditor';
 
 export default function TextEditor({width, height, ...rest}) {
+  const onReady = editor => {
+    editor.editing.view.change(writer => {
+      writer.setStyle('width', width, editor.editing.view.document.getRoot());
+      writer.setStyle('height', height, editor.editing.view.document.getRoot());
+    });
+  };
+
   return (
     <CKEditor editor={ClassicEditor} onReady={onReady} configuration={{...getConfig(), ...rest}} />
   );
@@ -17,6 +24,6 @@ TextEditor.propTypes = {
 };
 
 TextEditor.defaultProps = {
-  height: 400,
+  height: '400px',
   width: undefined,
 };
