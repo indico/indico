ed980400a6871ee283ba753a05623063b762c257
Tomas Roun <tomas.roun@cern.ch>
2022-07-04 15:45:51 +0200

    Add missing file headers

--- indico/modules/events/registration/client/js/form_submission/Captcha.jsx
+++ indico/modules/events/registration/client/js/form_submission/Captcha.jsx
@@ -1,3 +1,10 @@
+// This file is part of Indico.
+// Copyright (C) 2002 - 2022 CERN
+//
+// Indico is free software; you can redistribute it and/or
+// modify it under the terms of the MIT License; see the
+// LICENSE file for more details.
+
 import generateCaptcha from 'indico-url:event_registration.api_generate_captcha';
 
 import PropTypes from 'prop-types';
