364afce1815fcb763aed64a304bc7d76a3cb692a
Pedro Ferreira <pedro.ferreira@cern.ch>
2022-07-14 18:04:54 +0200

    Add monaco editor to receipt template interface

--- indico/modules/receipts/client/js/templates/TemplatePane.jsx
+++ indico/modules/receipts/client/js/templates/TemplatePane.jsx
@@ -7,29 +7,57 @@
 
 import templateListURL from 'indico-url:receipts.template_list';
 
+import Editor from '@monaco-editor/react';
 import _ from 'lodash';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {Form as FinalForm} from 'react-final-form';
 import {Link} from 'react-router-dom';
-import {Form} from 'semantic-ui-react';
+import {Button, Form, Message} from 'semantic-ui-react';
 
 import {ManagementPageSubTitle} from 'indico/react/components';
-import {FinalInput, FinalSubmitButton, FinalTextArea} from 'indico/react/forms';
+import {FinalField, FinalInput, FinalSubmitButton, parsers} from 'indico/react/forms';
 import {Translate} from 'indico/react/i18n';
+import Nexus from 'indico/react/util/Nexus';
 
 import {targetLocatorSchema, templateSchema} from './util';
 import './TemplatePane.module.scss';
 
-export default function TemplatePage({template, onSubmit, targetLocator}) {
-  const initValues = template ? _.pick(template, ['title', 'html', 'css', 'yaml']) : {};
+const FILES = {
+  html: {
+    syntax: 'html',
+    fileName: 'template.html',
+  },
+  css: {
+    syntax: 'css',
+    fileName: 'theme.css',
+  },
+  yaml: {
+    syntax: 'yaml',
+    fileName: 'metadata.yaml',
+  },
+};
+
+export default function TemplatePane({template, onSubmit, targetLocator, editorHeight}) {
+  const [currentFileExt, setFileExt] = useState('html');
+  const codeValues = _.pick(template, ['html', 'css', 'yaml']);
+
+  const editorRef = useRef(null);
+
+  useEffect(() => {
+    editorRef.current?.focus();
+  }, [currentFileExt]);
 
   return (
     <>
       <ManagementPageSubTitle title={Translate.string('Add Receipt / Certificate template')} />
-      <FinalForm onSubmit={onSubmit} initialValues={initValues}>
-        {fprops => (
-          <Form onSubmit={fprops.handleSubmit}>
+      <FinalForm
+        onSubmit={onSubmit}
+        initialValues={{title: template?.title || null, ...codeValues}}
+        initialValuesEqual={_.isEqual}
+      >
+        {({handleSubmit}) => (
+          <Form onSubmit={handleSubmit} subscription={{dirty: true}}>
             <FinalInput
               name="title"
               label="Title"
@@ -38,25 +66,51 @@ export default function TemplatePage({template, onSubmit, targetLocator}) {
               required
               rows={24}
             />
-            <Form.Group widths="equal">
-              <FinalTextArea
-                name="html"
-                label={Translate.string('HTML / Jinja')}
-                type="text"
-                required
-                rows={24}
-              />
-              <div styleName="stacked-fields">
-                <FinalTextArea name="css" label={Translate.string('CSS')} type="text" rows={10} />
-                <FinalTextArea
-                  name="yaml"
-                  label={Translate.string('Metadata (YAML)')}
-                  nullIfEmpty
-                  type="text"
-                  rows={10}
-                />
-              </div>
-            </Form.Group>
+            <Button.Group attached="top">
+              {['html', 'css', 'yaml'].map(format => (
+                <Button
+                  type="button"
+                  primary={format === currentFileExt}
+                  key={format}
+                  onClick={() => setFileExt(format)}
+                >
+                  {FILES[format].fileName}
+                </Button>
+              ))}
+            </Button.Group>
+            {['html', 'css', 'yaml'].map(fileExt => (
+              <FinalField
+                key={fileExt}
+                name={fileExt}
+                initialValue={template[fileExt]}
+                parse={parsers.nullIfEmpty}
+              >
+                {({input, meta}) => (
+                  <>
+                    <Nexus
+                      target={<div style={{height: `${editorHeight}px`, width: '100%'}} />}
+                      open={fileExt === currentFileExt}
+                    >
+                      <Editor
+                        height={`${editorHeight}px`}
+                        theme="vs-dark"
+                        path={FILES[fileExt].fileName}
+                        defaultLanguage={FILES[fileExt].syntax}
+                        value={input.value}
+                        onChange={input.onChange}
+                        onMount={editor => {
+                          editorRef.current = editor;
+                        }}
+                        options={{minimap: {enabled: false}}}
+                      />
+                    </Nexus>
+                    <Message error visible={!!meta.submitError}>
+                      {meta.submitError}
+                    </Message>
+                  </>
+                )}
+              </FinalField>
+            ))}
             <Form.Group styleName="buttons">
               <FinalSubmitButton label={Translate.string('Submit')} />
               <Link to={templateListURL(targetLocator)} className="ui button">
@@ -70,12 +124,14 @@ export default function TemplatePage({template, onSubmit, targetLocator}) {
   );
 }
 
-TemplatePage.propTypes = {
+TemplatePane.propTypes = {
   onSubmit: PropTypes.func.isRequired,
   template: templateSchema,
   targetLocator: targetLocatorSchema.isRequired,
+  editorHeight: PropTypes.number,
 };
 
-TemplatePage.defaultProps = {
-  template: null,
+TemplatePane.defaultProps = {
+  template: {},
+  editorHeight: 800,
 };
