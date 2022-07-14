17fa2cfd439a7ff45436e11e29b9a97fcfb705c1
Pedro Lourenço <pedro.lourenco@cern.ch>
2022-07-14 10:13:51 +0200

    Update TextEditor component

--- indico/web/client/js/react/components/TextEditor.jsx
+++ indico/web/client/js/react/components/TextEditor.jsx
@@ -8,29 +8,55 @@
 import {CKEditor} from '@ckeditor/ckeditor5-react';
 import ClassicEditor from 'ckeditor';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, {useMemo} from 'react';
+import {Dimmer, Loader} from 'semantic-ui-react';
 
 import {getConfig} from 'indico/ckeditor';
 
-export default function TextEditor({width, height, ...rest}) {
+export default function TextEditor({
+  value,
+  width,
+  height,
+  onReady: _onReady,
+  config: _config,
+  loading,
+  ...rest
+}) {
+  const config = useMemo(() => getConfig(_config), [_config]);
   const onReady = editor => {
     editor.editing.view.change(writer => {
       writer.setStyle('width', width, editor.editing.view.document.getRoot());
       writer.setStyle('height', height, editor.editing.view.document.getRoot());
     });
+    if (_onReady) {
+      _onReady(editor);
+    }
   };
 
   return (
-    <CKEditor editor={ClassicEditor} onReady={onReady} configuration={{...getConfig(), ...rest}} />
+    <Dimmer.Dimmable>
+      <Dimmer inverted active={loading}>
+        <Loader />
+      </Dimmer>
+      <CKEditor editor={ClassicEditor} data={value} onReady={onReady} config={config} {...rest} />
+    </Dimmer.Dimmable>
   );
 }
 
 TextEditor.propTypes = {
-  width: PropTypes.number,
-  height: PropTypes.number,
+  width: PropTypes.string,
+  height: PropTypes.string,
+  onReady: PropTypes.func,
+  value: PropTypes.string,
+  config: PropTypes.object,
+  loading: PropTypes.bool,
 };
 
 TextEditor.defaultProps = {
   height: '400px',
   width: undefined,
+  onReady: undefined,
+  value: undefined,
+  config: undefined,
+  loading: false,
 };
