2e61ebca9a74c321d10f831e524889eccebeedde
Pedro Ferreira <pedro.ferreira@cern.ch>
2022-07-19 10:16:02 +0200

    Error handling in receipt preview

--- indico/modules/receipts/client/js/templates/Previewer.jsx
+++ indico/modules/receipts/client/js/templates/Previewer.jsx
@@ -9,21 +9,43 @@ import _ from 'lodash';
 import PropTypes from 'prop-types';
 import React, {useEffect, useState} from 'react';
 import {pdfjs, Document, Page} from 'react-pdf';
-import {Loader, Pagination} from 'semantic-ui-react';
+import {Loader, Message, Pagination} from 'semantic-ui-react';
 
 import './TemplatePane.module.scss';
 
+import {Translate} from 'indico/react/i18n';
 import {indicoAxios, handleAxiosError} from 'indico/utils/axios';
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
+const MESSAGE_HEADERS = {
+  yaml: Translate.string('YAML Metadata'),
+  html: Translate.string('HTML Code'),
+  css: Translate.string('CSS Stylesheet'),
+  jinja: Translate.string('Jinja Template'),
+};
+
 const debouncedRequest = _.throttle(
-  (setContent, url, data) =>
+  (setContent, setError, url, data) =>
     (async () => {
       try {
+        setError(null);
         const {data: content} = await indicoAxios.post(url, data, {responseType: 'arraybuffer'});
         setContent(content);
       } catch (error) {
-        handleAxiosError(error);
+        const {
+          response: {status, data},
+        } = error;
+        if (status === 422) {
+          const decoder = new TextDecoder('utf-8');
+          const json = JSON.parse(decoder.decode(data));
+          if (json.webargs_errors) {
+            setError(json.webargs_errors);
+          } else {
+            setError({jinja: [json.error.message]});
+          }
+        } else {
+          handleAxiosError(error);
+        }
         return;
       }
     })(),
@@ -35,6 +57,7 @@ export default function Previewer({url, data}) {
   const [loading, setLoading] = useState(false);
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     setLoading(true);
@@ -43,37 +66,53 @@ export default function Previewer({url, data}) {
         setLoading(false);
         setContent(c);
       },
+      setError,
       url,
       data
     );
   }, [url, data]);
 
   return (
-    <div styleName={loading ? 'loading' : null}>
-      <Pagination
-        disabled={numPages < 2}
-        totalPages={numPages}
-        boundaryRange={0}
-        activePage={pageNumber}
-        onPageChange={(__, {activePage}) => {
-          setPageNumber(activePage);
-        }}
-        firstItem={null}
-        lastItem={null}
-        secondary
-        styleName="pagination"
-      />
-      <Document
-        file={content}
-        loading={<Loader active />}
-        onLoadSuccess={({numPages: n}) => {
-          setNumPages(n);
-          setPageNumber(1);
-        }}
-      >
-        <Page pageNumber={pageNumber} />
-      </Document>
-    </div>
+    <>
+      {error &&
+        Object.entries(error).map(([entry, errs]) => (
+          <Message key={entry} error visible={!!error}>
+            <Message.Header>{MESSAGE_HEADERS[entry]}</Message.Header>
+            <ul>
+              {errs.map(err => (
+                <li key={err}>{err}</li>
+              ))}
+            </ul>
+          </Message>
+        ))}
+      <div styleName={loading ? 'loading' : null}>
+        {numPages && (
+          <Pagination
+            disabled={numPages < 2}
+            totalPages={numPages}
+            boundaryRange={0}
+            activePage={pageNumber}
+            onPageChange={(__, {activePage}) => {
+              setPageNumber(activePage);
+            }}
+            firstItem={null}
+            lastItem={null}
+            secondary
+            styleName="pagination"
+          />
+        )}
+        <Document
+          file={content}
+          loading={<Loader active />}
+          onLoadSuccess={({numPages: n}) => {
+            setNumPages(n);
+            setPageNumber(1);
+          }}
+        >
+          <Page pageNumber={pageNumber} />
+        </Document>
+      </div>
+    </>
   );
 }
 
