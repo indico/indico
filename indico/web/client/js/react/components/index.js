904a4d0f8ce68dfe4121ec4ba70767783dbb3567
Adrian Moennich <adrian.moennich@cern.ch>
2022-09-28 15:42:51 +0200

    Add ConfirmButton component

--- indico/web/client/js/react/components/index.js
+++ indico/web/client/js/react/components/index.js
@@ -13,6 +13,7 @@ export {
   ACLField,
   TrackACLField,
 } from './principals';
+export {default as ConfirmButton} from './ConfirmButton';
 export {default as IButton} from './IButton';
 export {default as MessageBox} from './MessageBox';
 export {default as Modal} from './Modal';
