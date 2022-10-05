024d11c888a4168e75ad6565c0f43ec683937754
Adrian Moennich <adrian.moennich@cern.ch>
2022-10-05 11:20:01 +0200

    Force no locale (for now) when forcing event locale
    There's no concept of an event locale yet

--- indico/modules/events/models/events.py
+++ indico/modules/events/models/events.py
@@ -45,7 +45,7 @@ from indico.util.caching import memoize_request
 from indico.util.date_time import get_display_tz, now_utc, overlaps
 from indico.util.decorators import strict_classproperty
 from indico.util.enum import RichIntEnum
-from indico.util.i18n import _
+from indico.util.i18n import _, force_locale
 from indico.util.string import format_repr, text_to_repr
 from indico.web.flask.util import url_for
 
@@ -1071,8 +1071,9 @@ class Event(SearchableTitleMixin, DescriptionMixin, LocationMixin, ProtectionMan
 
     @contextmanager
     def force_event_locale(self):
-        # TODO: Implement force event locale
-        yield None
+        # TODO: once we have an event locale, force that one
+        with force_locale(None):
+            yield
 
 
 Event.register_location_events()
