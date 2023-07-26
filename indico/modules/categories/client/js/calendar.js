a011cbe815a698faeb340f38982e8c9214e6c468
Adrian Moennich <adrian.moennich@cern.ch>
2023-07-26 18:45:27 +0200

    Update fullcalendar to latest version
    This removes the last dependency on legacy node-sass/libsass

--- indico/modules/categories/client/js/calendar.js
+++ indico/modules/categories/client/js/calendar.js
@@ -5,56 +5,47 @@
 // modify it under the terms of the MIT License; see the
 // LICENSE file for more details.
 
-import 'fullcalendar';
-import 'fullcalendar/dist/fullcalendar.css';
+/* global handleAjaxError:false, ajaxDialog:false */
+
+import {Calendar} from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+
+import {$T} from 'indico/utils/i18n';
 
 (function(global) {
-  global.setupCategoryCalendar = function setupCategoryCalendar(elementId, categoryURL) {
+  global.setupCategoryCalendar = function setupCategoryCalendar(
+    containerSelector,
+    tz,
+    categoryURL
+  ) {
     const cachedEvents = {};
-
-    $(elementId).fullCalendar({
+    const container = document.querySelector(containerSelector);
+    const calendar = new Calendar(container, {
+      plugins: [dayGridPlugin],
+      initialView: 'dayGridMonth',
+      timeZone: tz,
       firstDay: 1,
-      height: 800,
+      height: 850,
       eventOrder: 'start',
       eventTextColor: '#FFF',
-      timeFormat: 'HH:mm',
+      eventTimeFormat: {
+        // most convoluted way of writing HH:MM...
+        hour: '2-digit',
+        minute: '2-digit',
+        meridiem: false,
+        hour12: false,
+      },
       nextDayThreshold: '00:00',
-      eventLimit: 5,
+      dayMaxEvents: true,
       buttonText: {
         today: $T.gettext('Today'),
       },
-      eventLimitClick(cellInfo) {
-        const content = $('<ul>');
-        const events = cellInfo.segs.sort((a, b) => {
-          const aTitle = a.footprint.eventInstance.def.title.toLowerCase();
-          const bTitle = b.footprint.eventInstance.def.title.toLowerCase();
-          if (aTitle < bTitle) {
-            return -1;
-          }
-          if (aTitle > bTitle) {
-            return 1;
-          }
-          return 0;
-        });
-        events.forEach(hiddenSegment => {
-          const li = $('<li>');
-          const eventLink = $('<a>', {
-            href: hiddenSegment.footprint.eventInstance.def.url,
-            text: hiddenSegment.footprint.eventInstance.def.title,
-          });
-          li.append(eventLink);
-          content.append(li);
-        });
-        ajaxDialog({
-          dialogClasses: 'all-events-dialog',
-          title: $T.gettext('Events happening on {0}').format(cellInfo.date.format('MMMM Do YYYY')),
-          content,
-        });
-      },
-      events(start, end, timezone, callback) {
+      events({start, end}, successCallback, failureCallback) {
         function updateCalendar(data) {
-          callback(data.events);
-          const toolbarGroup = $(elementId).find('.fc-toolbar .fc-right');
+          successCallback(data.events);
+          const toolbarGroup = $(containerSelector).find(
+            '.fc-toolbar .fc-toolbar-chunk:last-child'
+          );
           const ongoingEventsInfo = $('<a>', {
             href: '#',
             class: 'ongoing-events-info',
@@ -83,23 +74,39 @@ import 'fullcalendar/dist/fullcalendar.css';
           }
         }
 
+        start = start.toISOString().substring(0, 10);
+        end = end.toISOString().substring(0, 10);
         const key = `${start}-${end}`;
         if (cachedEvents[key]) {
           updateCalendar(cachedEvents[key]);
         } else {
           $.ajax({
             url: categoryURL,
-            data: {start: start.format('YYYY-MM-DD'), end: end.format('YYYY-MM-DD')},
+            data: {start, end},
             dataType: 'json',
             contentType: 'application/json',
-            error: handleAjaxError,
             complete: IndicoUI.Dialogs.Util.progress(),
             success(data) {
               updateCalendar(data);
               cachedEvents[key] = data;
             },
+            error(error) {
+              failureCallback(new Error('Loading events failed'));
+              handleAjaxError(error);
+            },
           });
         }
+      },
+    });
+    calendar.render();
+
+    $('.js-calendar-datepicker').datepicker({
+      dateFormat: 'yy-mm-dd',
+      beforeShowDay(date) {
+        return [date.getDate() === 1, ''];
+      },
+      onSelect(date) {
+        calendar.gotoDate(date);
       },
     });
   };
