15430e2b5b408944d217344f92bb0b1e6b5c7ee9
Tomas Roun <tomas.roun@cern.ch>
2023-08-18 10:37:05 +0200

    Add tests

--- indico/web/rh_test.py
+++ indico/web/rh_test.py
@@ -12,7 +12,9 @@ import pytest
 from werkzeug.exceptions import Forbidden
 
 from indico.core import signals
-from indico.web.rh import RH
+from indico.core.errors import IndicoError
+from indico.web.flask.util import make_view_func
+from indico.web.rh import RH, cors
 
 
 @pytest.mark.usefixtures('request_context')
@@ -70,3 +72,55 @@ def test_before_check_access_signal_many_handlers_forbidden(mocker, signal_rv_1,
         with signals.rh.before_check_access.connected_to(lambda *args, **kwargs: signal_rv_2):
             with expectation:
                 rh.process()
+
+
+def test_cors_usage(mocker):
+    # Ensure `cors` can be used both as `@cors` and `@cors(...)`
+    @cors
+    class DummyRH(RH):
+        def _process(self):
+            pass
+
+    assert DummyRH._CORS == {}
+
+    @cors()
+    class DummyRH(RH):
+        def _process(self):
+            pass
+
+    assert DummyRH._CORS == {}
+
+    @cors(origins='localhost')
+    class DummyRH(RH):
+        def _process(self):
+            pass
+
+    assert DummyRH._CORS == {'origins': 'localhost'}
+
+
+def test_cors_headers(app):
+    @cors(origins='localhost')
+    class DummyRH(RH):
+        def _process(self):
+            pass
+
+    app.add_url_rule('/test/cors/headers', 'test_cors_headers', make_view_func(DummyRH))
+
+    with app.test_client() as client:
+        resp = client.options('/test/cors/headers')
+        # Ensure the CORS headers are set
+        assert resp.headers['Access-Control-Allow-Origin'] == 'localhost'
+
+
+def test_cors_headers_set_after_error(app):
+    @cors(origins='localhost')
+    class DummyRH(RH):
+        def _process(self):
+            raise IndicoError('something went wrong')
+
+    app.add_url_rule('/test/cors/error', 'test_cors_error', make_view_func(DummyRH))
+
+    with app.test_client() as client:
+        resp = client.get('/test/cors/error')
+        # Ensure the CORS headers are set even if an error was raised
+        assert resp.headers['Access-Control-Allow-Origin'] == 'localhost'
