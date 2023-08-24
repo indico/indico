indico/indico

commit 9fbfe7f9d38041173fd838162e75c201e2e09a7c

Refactor "action menu" in list of registrants (#5904)
Co-authored-by: Adrian <adrian.moennich@cern.ch>

indico/modules/events/registration/controllers/management/reglists.py
@@ -5,9 +5,11 @@
 # modify it under the terms of the MIT License; see the
 # LICENSE file for more details.
 
+import itertools
 import os
 import uuid
 from io import BytesIO
+from operator import attrgetter
 
 from flask import flash, jsonify, redirect, render_template, request, session
 from sqlalchemy.orm import joinedload, subqueryload
@@ -42,7 +44,8 @@ from indico.modules.events.registration.models.items import PersonalDataType, Re
 from indico.modules.events.registration.models.registrations import Registration, RegistrationData, RegistrationState
 from indico.modules.events.registration.notifications import notify_registration_state_update
 from indico.modules.events.registration.settings import event_badge_settings
-from indico.modules.events.registration.util import (create_registration, generate_spreadsheet_from_registrations,
+from indico.modules.events.registration.util import (ActionMenuEntry, create_registration,
+                                                     generate_spreadsheet_from_registrations,
                                                      get_flat_section_submission_data, get_initial_form_values,
                                                      get_ticket_attachments, get_title_uuid, get_user_data,
                                                      import_registrations_from_csv, make_registration_schema)
@@ -53,6 +56,7 @@ from indico.util.fs import secure_filename
 from indico.util.i18n import _, ngettext
 from indico.util.marshmallow import Principal
 from indico.util.placeholders import replace_placeholders
+from indico.util.signals import values_from_signal
 from indico.util.spreadsheets import send_csv, send_xlsx
 from indico.web.args import parser, use_kwargs
 from indico.web.flask.templating import get_template_module
@@ -84,12 +88,67 @@ class RHRegistrationsListManage(RHManageRegFormBase):
         if self.list_generator.static_link_used:
             return redirect(self.list_generator.get_list_url())
         reg_list_kwargs = self.list_generator.get_list_kwargs()
-        badge_templates = [tpl for tpl in set(self.event.designer_templates) | get_inherited_templates(self.event)
+        regform = self.regform
+
+        event = regform.event
+        badge_templates = [tpl for tpl in set(event.designer_templates) | get_inherited_templates(event)
                            if tpl.type == TemplateType.badge]
-        has_tickets = any(tpl.is_ticket for tpl in badge_templates)
-        has_badges = any(not tpl.is_ticket for tpl in badge_templates)
+
+        action_menu_items = [
+            ActionMenuEntry(
+                _('E-mail'),
+                'mail',
+                url=url_for('.email_registrants', regform),
+                dialog_title=_('Send e-mail'),
+                weight=100,
+                hide_if_locked=True,
+            ), ActionMenuEntry(
+                _('Download Attachments'),
+                'attachment',
+                url=url_for('.registrations_attachments_export', regform),
+                weight=60,
+                extra_classes='js-submit-list-form regform-download-attachments',
+            ),
+            ActionMenuEntry(
+                _('Edit Tags'),
+                'tag',
+                url=url_for('.manage_registration_tags_assign', regform),
+                weight=50,
+                # XXX this is a hack to keep the item disabled when there are no tags defined
+                requires_selected=bool(regform.event.registration_tags),
+                reload_page=True
+            )
+        ]
+
+        # the regform has at least one ticket template
+        if any(tpl.is_ticket for tpl in badge_templates):
+            action_menu_items.append(ActionMenuEntry(
+                _('Print Badges'),
+                'id-badge',
+                url=url_for('.registrations_config_badges', regform),
+                weight=90,
+            ))
+
+        # the regform has at least one badge template
+        if any(not tpl.is_ticket for tpl in badge_templates):
+            action_menu_items.append(ActionMenuEntry(
+                _('Print Tickets'),
+                'ticket',
+                url=url_for('.registrations_config_tickets', regform),
+                weight=80,
+            ))
+
+        action_menu_items = sorted(
+            itertools.chain(
+                action_menu_items,
+                values_from_signal(signals.event.registrant_list_action_menu.send(regform), as_list=True)
+            ),
+            key=attrgetter('weight'),
+            reverse=True
+        )
+
         return WPManageRegistration.render_template('management/regform_reglist.html', self.event,
-                                                    has_badges=has_badges, has_tickets=has_tickets, **reg_list_kwargs)
+                                                    action_menu_items=action_menu_items, **reg_list_kwargs)
 
 
 class RHRegistrationsListCustomize(RHManageRegFormBase):

indico/modules/events/registration/templates/management/regform_reglist.html
@@ -118,60 +118,31 @@
                         {%- trans %}Actions{% endtrans -%}
                     </button>
                     <ul class="i-dropdown">
-                        <li class="hide-if-locked">
-                            <a href="#" class="icon-mail js-requires-selected-row disabled"
-                               data-href="{{ url_for('.email_registrants', regform) }}"
-                               data-title="{% trans %}Send email{% endtrans %}"
-                               data-params-selector="#registration-list tr input[type=checkbox]:checked"
-                               data-method="POST"
-                               data-ajax-dialog>
-                                {%- trans %}Email{% endtrans -%}
-                            </a>
-                        </li>
-                        {% if has_badges %}
-                            <li>
-                                <a href="#" class="icon-id-badge js-requires-selected-row disabled"
-                                   style="padding-left: 3px;"
-                                   data-href="{{ url_for('.registrations_config_badges', regform) }}"
-                                   data-title="{% trans %}Print Badges{% endtrans %}"
-                                   data-params-selector="#registration-list tr input[type=checkbox]:checked"
-                                   data-method="POST"
-                                   data-ajax-dialog>
-                                    {%- trans %}Print Badges{% endtrans -%}
+                        {% for item in action_menu_items %}
+                            <li {% if item.hide_if_locked %}class="hide-if-locked"{% endif %}>
+                                <a href="#"
+                                   class="icon-{{ item.icon_name }}
+                                          disabled
+                                          {{ 'js-requires-selected-row' if item.requires_selected }}
+                                          {{ item.extra_classes }}"
+                                   data-weight="{{ item.weight }}"
+                                   data-params="{{ item.params | tojson | forceescape }}"
+                                   {% if item.type == "callback" %}
+                                       data-params-selector="#registration-list tr input[type=checkbox]:checked"
+                                       data-callback="{{ item.callback }}"
+                                   {% elif item.type == "ajax-dialog" %}
+                                       data-href="{{ item.url }}"
+                                       data-title="{{ item.dialog_title or item.text }}"
+                                       data-params-selector="#registration-list tr input[type=checkbox]:checked"
+                                       data-method="POST"
+                                       data-ajax-dialog
+                                       {{ 'data-reload-after' if item.reload_page }}
+                                   {% endif %}
+                                >
+                                    {{ item.text }}
                                 </a>
                             </li>
-                        {% endif %}
-                        {% if has_tickets %}
-                            <li>
-                                <a href="#" class="icon-ticket js-requires-selected-row disabled"
-                                   data-href="{{ url_for('.registrations_config_tickets', regform) }}"
-                                   data-title="{% trans %}Print Tickets{% endtrans %}"
-                                   data-params-selector="#registration-list tr input[type=checkbox]:checked"
-                                   data-method="POST"
-                                   data-ajax-dialog>
-                                    {%- trans %}Print Tickets{% endtrans -%}
-                                </a>
-                            </li>
-                        {% endif %}
-                        <li>
-                            <a href="#"
-                               class="icon-attachment js-requires-selected-row disabled js-submit-list-form regform-download-attachments"
-                               data-href="{{ url_for('.registrations_attachments_export', regform) }}">
-                                {%- trans %}Download attachments{% endtrans -%}
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#"
-                               class="icon-tag {% if event.registration_tags %}js-requires-selected-row{% endif %} disabled"
-                               data-href="{{ url_for('.manage_registration_tags_assign', regform) }}"
-                               data-title="{% trans %}Edit tags{% endtrans %}"
-                               data-params-selector="#registration-list tr input[type=checkbox]:checked"
-                               data-method="POST"
-                               data-ajax-dialog
-                               data-reload-after>
-                                {%- trans %}Edit tags{% endtrans -%}
-                            </a>
-                        </li>
+                        {% endfor %}
                     </ul>
                     {% set pending_registrations = registrations | selectattr('state.name', 'equalto', 'pending') | list %}
                     {% if (regform.moderation_enabled or pending_registrations) and not event.is_locked %}

indico/modules/events/registration/util.py
@@ -6,6 +6,7 @@
 # LICENSE file for more details.
 
 import csv
+import dataclasses
 import itertools
 from operator import attrgetter
 
@@ -51,6 +52,23 @@ from indico.util.i18n import _
 from indico.util.signals import values_from_signal
 from indico.util.spreadsheets import csv_text_io_wrapper, unique_col
 from indico.util.string import camelize_keys, validate_email, validate_email_verbose
+
+
+@dataclasses.dataclass
+class ActionMenuEntry:
+    text: str
+    icon_name: str
+    # _: dataclasses.KW_ONLY  # TODO uncomment once we require python3.10+; until then consider everything below kw-only
+    weight: int = 0
+    dialog_title: str = None  # defaults to `text`
+    type: str = 'ajax-dialog'  # use 'callback' to call a global JS function
+    params: dict = dataclasses.field(default_factory=dict)
+    url: str = ''
+    callback: str = ''
+    requires_selected: bool = True
+    reload_page: bool = False
+    hide_if_locked: bool = False
+    extra_classes: str = ''
 
 
 def import_user_records_from_csv(fileobj, columns):

indico/core/signals/event/registration.py
@@ -127,3 +127,9 @@ Called after a permanent UUID is assigned to an accompanying person. The sender 
 ``AccompanyingPersonSchema`` object. The temporary ID is passed in the ``temporary_id``
 kwarg and the permanent UUID is passed in the ``permanent_id`` kwarg.
 ''')
+
+registrant_list_action_menu = _signals.signal('registrant-list-action-menu', '''
+Called when composing the list of menu items to be displayed under the "Actions" button
+at the top of the list of registrants/participants. The `sender` is the corresponding
+registration form.
+''')

indico/web/client/js/jquery/utils/declarative.js
@@ -111,6 +111,9 @@ import {$T} from '../../utils/i18n';
       'button[data-content][data-ajax-dialog]',
       'input:button[data-content][data-ajax-dialog]',
       'a[data-content][data-ajax-dialog]',
+      'button[data-callback]',
+      'input:button[data-callback]',
+      'a[data-callback]'
     ];
     $('body').on('click', selectors.join(', '), function(e) {
       e.preventDefault();
@@ -119,6 +122,7 @@ import {$T} from '../../utils/i18n';
         return;
       }
       var url = $this.data('href');
+      var callback = $this.data('callback');
       var method = ($this.data('method') || 'GET').toUpperCase();
       var params = $this.data('params') || {};
       var paramsSelector = $this.data('params-selector');
@@ -223,6 +227,11 @@ import {$T} from '../../utils/i18n';
               }
             },
           });
+          return;
+        }
+
+        if (callback) {
+          window[callback](params);
           return;
         }
 

indico/web/client/styles/modules/_event_management.scss
@@ -743,3 +743,7 @@ form.horizontal .clone-event-dialog-footer .form-field {
 .import-event-dates {
   color: $dark-blue;
 }
+
+.list.registrations .i-dropdown .icon-id-badge::before {
+  padding-left: 4px;
+}

commit 1a3e438c0bb4a8eb6799dd856058c26898c011ed

Fix 'download attachments' dialog remaining open
This broke in #5904 when cleaning up; it is actually the one case where
no ajax dialog is used...

indico/modules/events/registration/controllers/management/reglists.py
@@ -105,6 +105,7 @@ class RHRegistrationsListManage(RHManageRegFormBase):
             ), ActionMenuEntry(
                 _('Download Attachments'),
                 'attachment',
+                type='href-custom',
                 url=url_for('.registrations_attachments_export', regform),
                 weight=60,
                 extra_classes='js-submit-list-form regform-download-attachments',

indico/modules/events/registration/templates/management/regform_reglist.html
@@ -130,6 +130,8 @@
                                    {% if item.type == "callback" %}
                                        data-params-selector="#registration-list tr input[type=checkbox]:checked"
                                        data-callback="{{ item.callback }}"
+                                   {% elif item.type == "href-custom" %}
+                                       data-href="{{ item.url }}"
                                    {% elif item.type == "ajax-dialog" %}
                                        data-href="{{ item.url }}"
                                        data-title="{{ item.dialog_title or item.text }}"

indico/modules/events/registration/util.py
@@ -61,7 +61,7 @@ class ActionMenuEntry:
     # _: dataclasses.KW_ONLY  # TODO uncomment once we require python3.10+; until then consider everything below kw-only
     weight: int = 0
     dialog_title: str = None  # defaults to `text`
-    type: str = 'ajax-dialog'  # use 'callback' to call a global JS function
+    type: str = 'ajax-dialog'  # use 'callback' to call a global JS function or 'href-custom' for a data-href
     params: dict = dataclasses.field(default_factory=dict)
     url: str = ''
     callback: str = ''