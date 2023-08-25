168c8fa7f035302597b5a4f66560d4ecf260b78e
Adrian Moennich <adrian.moennich@cern.ch>
2023-08-25 14:30:29 +0200

    Update alembic revision

---
+++ indico/migrations/versions/20230825_1430_13d8ba15a83b_add_recurrence_weekdays_to_reservations.py
@@ -0,0 +1,31 @@
+"""add recurrence_weekdays to reservations
+
+Revision ID: 13d8ba15a83b
+Revises: 0c44046dc1be
+Create Date: 2023-07-27 11:15:22.561512
+"""
+
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy.dialects import postgresql
+
+
+# revision identifiers, used by Alembic.
+revision = '13d8ba15a83b'
+down_revision = '0c44046dc1be'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column('reservations', sa.Column('recurrence_weekdays', postgresql.ARRAY(sa.String()), nullable=True),
+                  schema='roombooking')
+    op.create_check_constraint(
+        'valid_recurrence_weekdays', 'reservations',
+        "recurrence_weekdays::text[] <@ ARRAY['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']::text[]",
+        schema='roombooking'
+    )
+
+
+def downgrade():
+    op.drop_column('reservations', 'recurrence_weekdays', schema='roombooking')
