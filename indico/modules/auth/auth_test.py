3a16d3a25ce44801531fbb49312e08b775322987
Christopher Bartz <christopher.bartz@canonical.com>
2023-08-28 09:41:03 +0200

    Add test for process_identity

---
+++ indico/modules/auth/auth_test.py
@@ -0,0 +1,47 @@
+# This file is part of Indico.
+# Copyright (C) 2002 - 2023 CERN
+#
+# Indico is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see the
+# LICENSE file for more details.
+
+from datetime import datetime, timezone
+
+import pytest
+from flask import session
+from flask_multipass import IdentityInfo, Multipass
+from flask_multipass.providers.static import StaticIdentityProvider
+
+from indico.modules.auth import process_identity
+
+
+@pytest.mark.usefixtures('database')
+def test_process_identity_sets_hard_expiry(app):
+    """Test that hard expiry is set on the session object if it is present in the multipass data."""
+    dt_now = datetime.now(timezone.utc)
+    identity_info = IdentityInfo(
+        provider=StaticIdentityProvider(
+            multipass=Multipass(), name='static', settings={}
+        ),
+        identifier='static', multipass_data={'session_expiry': dt_now}
+    )
+
+    with app.test_request_context('/ping', method='GET'):
+        process_identity(identity_info)
+
+        assert session.hard_expiry == dt_now
+
+
+@pytest.mark.usefixtures('database')
+def test_process_identity_raises_exc_for_non_dt_hard_expiry(app):
+    """Test that a ValueError is raised if the hard expiry is not a datetime object."""
+    identity_info = IdentityInfo(
+        provider=StaticIdentityProvider(
+            multipass=Multipass(), name='static', settings={}
+        ),
+        identifier='static', multipass_data={'session_expiry': 12345}
+    )
+
+    with app.test_request_context('/ping', method='GET'):
+        with pytest.raises(ValueError):
+            process_identity(identity_info)
