229835181404385ae02d49f8ffcd8a6e23646e90
Tomas Roun <tomas.roun@cern.ch>
2023-09-08 11:16:50 +0200

    Prefix MM fields with 'fields.'

--- indico/modules/events/surveys/schemas.py
+++ indico/modules/events/surveys/schemas.py
@@ -5,7 +5,7 @@
 # modify it under the terms of the MIT License; see the
 # LICENSE file for more details.
 
-from marshmallow.fields import List, Nested, Raw, String
+from marshmallow import fields
 
 from indico.core.marshmallow import mm
 from indico.modules.events.surveys.models.submissions import SurveyAnswer, SurveySubmission
@@ -16,8 +16,8 @@ class SurveyAnswerSchema(mm.SQLAlchemyAutoSchema):
         model = SurveyAnswer
         fields = ('question_id', 'question_title', 'answer')
 
-    answer = Raw(attribute='data')
-    question_title = String(attribute='question.title')
+    answer = fields.Raw(attribute='data')
+    question_title = fields.String(attribute='question.title')
 
 
 class SurveySubmissionSchema(mm.SQLAlchemyAutoSchema):
@@ -26,5 +26,5 @@ class SurveySubmissionSchema(mm.SQLAlchemyAutoSchema):
         fields = ('id', 'survey_id', 'survey_title', 'submitted_dt', 'is_anonymous', 'is_submitted', 'pending_answers',
                   'answers')
 
-    survey_title = String(attribute='survey.title')
-    answers = List(Nested(SurveyAnswerSchema))
+    survey_title = fields.String(attribute='survey.title')
+    answers = fields.List(fields.Nested(SurveyAnswerSchema))
