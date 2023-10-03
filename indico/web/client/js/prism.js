8bda0761883e37d22bf7a0d2d6b15f2b627ae3c4
Adrian Moennich <adrian.moennich@cern.ch>
2023-10-03 16:43:23 +0200

    Add syntax highlighting for more languages
    Also use global prismjs in tinymce instead of the bundled one

--- indico/web/client/js/prism.js
+++ indico/web/client/js/prism.js
@@ -10,13 +10,34 @@
 
 import Prism from 'prismjs';
 import 'prismjs/themes/prism.css';
+import 'prismjs/components/prism-bash';
 import 'prismjs/components/prism-c';
 import 'prismjs/components/prism-cpp';
 import 'prismjs/components/prism-csharp';
+import 'prismjs/components/prism-css';
+import 'prismjs/components/prism-csv';
+import 'prismjs/components/prism-diff';
+import 'prismjs/components/prism-docker';
+import 'prismjs/components/prism-fortran';
+import 'prismjs/components/prism-go';
 import 'prismjs/components/prism-java';
-import 'prismjs/components/prism-python';
-import 'prismjs/components/prism-ruby';
+import 'prismjs/components/prism-json';
+import 'prismjs/components/prism-jsx';
+import 'prismjs/components/prism-julia';
+import 'prismjs/components/prism-latex';
+import 'prismjs/components/prism-lua';
+import 'prismjs/components/prism-matlab';
+import 'prismjs/components/prism-markup';
 import 'prismjs/components/prism-markup-templating'; // dependency for prism-php
+import 'prismjs/components/prism-perl';
 import 'prismjs/components/prism-php';
+import 'prismjs/components/prism-powershell';
+import 'prismjs/components/prism-python';
+import 'prismjs/components/prism-r';
+import 'prismjs/components/prism-ruby';
+import 'prismjs/components/prism-rust';
+import 'prismjs/components/prism-sql';
+import 'prismjs/components/prism-typescript';
+import 'prismjs/components/prism-yaml';
 
 Prism.highlightAll();
