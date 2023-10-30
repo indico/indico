495bc9b66485e9c6ba060ee9dab7b6c14f0c921c
Adrian Moennich <adrian.moennich@cern.ch>
2023-10-30 15:00:27 +0100

    Update alembic revision

--- indico/migrations/versions/20231030_1500_155cfc134f0c_future_events_threshold_setting.py
+++ indico/migrations/versions/20231030_1500_155cfc134f0c_future_events_threshold_setting.py
@@ -1,7 +1,7 @@
 """Setting for future events threshold
 
 Revision ID: 155cfc134f0c
-Revises: 0c44046dc1be
+Revises: 13d8ba15a83b
 Create Date: 2023-09-13 21:16:13.839731
 """
 
@@ -11,13 +11,14 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = '155cfc134f0c'
-down_revision = '0c44046dc1be'
+down_revision = '13d8ba15a83b'
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    op.add_column('categories', sa.Column('show_future_months', sa.Integer(), nullable=False, server_default='0'), schema='categories')
+    op.add_column('categories', sa.Column('show_future_months', sa.Integer(), nullable=False, server_default='0'),
+                  schema='categories')
     op.alter_column('categories', 'show_future_months', server_default=None, schema='categories')
 
 
