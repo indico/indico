74460c0841f74acc7d1d98723d44e4a7db30a6f4
Pedro Ferreira <pedro.ferreira@cern.ch>
2023-11-14 15:29:17 +0100

    VC: add warning when user removes associated entry

--- indico/modules/vc/__init__.py
+++ indico/modules/vc/__init__.py
@@ -5,17 +5,21 @@
 # modify it under the terms of the MIT License; see the
 # LICENSE file for more details.
 
-from flask import has_request_context, render_template, session
+import typing as t
+
+from flask import flash, has_request_context, render_template, session
 
 from indico.core import signals
+from indico.modules.events.contributions.models.contributions import Contribution
 from indico.modules.events.layout import layout_settings
 from indico.modules.events.layout.util import MenuEntryData
+from indico.modules.events.sessions.models.blocks import SessionBlock
 from indico.modules.users import User
 from indico.modules.vc.forms import VCPluginSettingsFormBase
 from indico.modules.vc.models.vc_rooms import VCRoom, VCRoomEventAssociation
 from indico.modules.vc.plugins import VCPluginMixin
 from indico.modules.vc.util import get_managed_vc_plugins, get_vc_plugins
-from indico.util.i18n import _
+from indico.util.i18n import _, ngettext
 from indico.web.flask.templating import get_overridable_template_name, template_hook
 from indico.web.flask.util import url_for
 from indico.web.menu import SideMenuItem, TopMenuItem
@@ -71,9 +75,29 @@ def _extend_event_menu(sender, **kwargs):
 
 @signals.event.contribution_deleted.connect
 @signals.event.session_block_deleted.connect
-def _link_object_deleted(obj, **kwargs):
-    for event_vc_room in obj.vc_room_associations:
+def _link_object_deleted(obj: t.Union[Contribution, SessionBlock], **kwargs):
+    assocs = list(obj.vc_room_associations)
+    for event_vc_room in assocs:
+        # tie the room to the event instead
         event_vc_room.link_object = obj.event
+
+    n_objects = len(assocs)
+    if not n_objects:
+        return
+
+    if isinstance(obj, Contribution):
+        message = ngettext(
+            'There was a VC room associated to this contribution. It has been linked to the event instead',
+            'There were {} VC rooms associated to this contribution. They have been linked to the event instead',
+            n_objects
+        )
+    else:
+        message = ngettext(
+            'There was a VC room associated to this session block. It has been linked to the event instead',
+            'There were {} VC rooms associated to this session block. They have been linked to the event instead',
+            n_objects
+        )
+    flash(message, 'warning')
 
 
 @signals.event.session_deleted.connect
