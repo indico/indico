a8679c5fd168a53421a2035cd16404fed029ccc8
Tomas Roun <tomas.roun@cern.ch>
2023-12-18 15:24:56 +0100

    Fix header

--- indico/modules/designer/models/templates_test.py
+++ indico/modules/designer/models/templates_test.py
@@ -5,7 +5,6 @@
 # modify it under the terms of the MIT License; see the
 # LICENSE file for more details.
 
-
 pytest_plugins = ('indico.modules.designer.testing.fixtures',
                   'indico.modules.events.registration.testing.fixtures')
 
