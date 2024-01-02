8ff31c228008a9847a10cbe70952c160f8c79630
Indico Team <indico-team@cern.ch>
2024-01-02 20:38:40 +0100

    Happy new year 2024 :fireworks:

--- indico/modules/events/abstracts/models/abstracts.py
+++ indico/modules/events/abstracts/models/abstracts.py
@@ -1,5 +1,5 @@
 # This file is part of Indico.
-# Copyright (C) 2002 - 2023 CERN
+# Copyright (C) 2002 - 2024 CERN
 #
 # Indico is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see the
