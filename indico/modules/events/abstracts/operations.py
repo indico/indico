d15effb2f7a2243cd5f6f7761df3769154d5d8dd
Duarte Galvão <duarte.galvao@cern.ch>
2024-01-26 15:03:49 +0100

    Call abstract_updated signal

--- indico/modules/events/abstracts/operations.py
+++ indico/modules/events/abstracts/operations.py
@@ -138,6 +138,7 @@ def update_abstract(abstract, abstract_data, custom_fields_data=None):
         changes.update(set_custom_fields(abstract, custom_fields_data))
     abstract.modified_dt = now_utc()
     db.session.flush()
+    signals.event.abstract_updated.send(abstract)
     logger.info('Abstract %s modified by %s', abstract, session.user)
     log_fields = {
         'title': 'Title',
