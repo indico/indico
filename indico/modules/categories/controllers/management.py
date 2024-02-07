407bc69ce265ae63e739299290bd08fa4099a67a
Giorgio Pieretti <giorgio.pieretti@un.org>
2024-02-07 11:34:43 +0100

    Fix header

--- indico/modules/categories/controllers/management.py
+++ indico/modules/categories/controllers/management.py
@@ -4,6 +4,7 @@
 # Indico is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see the
 # LICENSE file for more details.
+
 import ast
 import os
 import random
