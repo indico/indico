cf0428a7be758b1df4653bf79050b15286f667aa
Giorgio Pieretti <giorgio.pieretti@un.org>
2024-02-12 13:49:06 +0100

    Better indentation

--- indico/modules/events/registration/__init__.py
+++ indico/modules/events/registration/__init__.py
@@ -246,11 +246,12 @@ class RegistrationPermission(ManagementPermission):
 
 @signals.event.updated.connect
 def _patch_google_wallet_class(event, changes, **kwargs):
-    if config.ENABLE_GOOGLE_WALLET:
-        look_for = ['title', 'start_dt', 'end_dt', 'location_data']  # Fields that will update Google Wallet content
-        if [i for i in look_for if i in changes] and event.has_google_wallet_tickets:
-            gwm = GoogleWalletManager(event)
-            if gwm.configured():
-                ticket_class = gwm.create_class_template(gwm.settings['google_wallet_issuer_id'],
-                                                         f'TicketClass-{event.id}')
-                return gwm.patch_class(gwm.settings['google_wallet_issuer_id'], f'TicketClass-{event.id}', ticket_class)
+    if not config.ENABLE_GOOGLE_WALLET:
+        return
+    look_for = ['title', 'start_dt', 'end_dt', 'location_data']  # Fields that will update Google Wallet content
+    if [i for i in look_for if i in changes] and event.has_google_wallet_tickets:
+        gwm = GoogleWalletManager(event)
+        if gwm.configured():
+            ticket_class = gwm.create_class_template(gwm.settings['google_wallet_issuer_id'],
+                                                     f'TicketClass-{event.id}')
+            return gwm.patch_class(gwm.settings['google_wallet_issuer_id'], f'TicketClass-{event.id}', ticket_class)
