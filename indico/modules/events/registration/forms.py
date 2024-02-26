396c4b3d556473e62298e643d7957ea81be0077c
Adrian <adrian@planetcoding.net>
2024-02-26 14:04:21 +0100

    Fix field description
    Co-authored-by: Alejandro Avilés <omegak@gmail.com>

--- indico/modules/events/registration/forms.py
+++ indico/modules/events/registration/forms.py
@@ -310,7 +310,7 @@ class TicketsForm(IndicoForm):
     ticket_google_wallet = BooleanField(_('Export to Google Wallet'), [HiddenUnless('tickets_enabled',
                                                                                     preserve_data=True)],
                                         widget=SwitchWidget(),
-                                        description=_('Allow users to export their to Google Wallet.'))
+                                        description=_('Allow users to export their ticket to Google Wallet.'))
     ticket_on_email = BooleanField(_('Send with an e-mail'), [HiddenUnless('tickets_enabled',
                                                                            preserve_data=True)],
                                    widget=SwitchWidget(),
