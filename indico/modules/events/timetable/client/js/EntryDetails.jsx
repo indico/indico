5580aca01da73d23c46cea79ff0a58c70c34d77b
Duarte Galvão <duarte.galvao@cern.ch>
2024-02-28 15:06:35 +0100

    Add timetable demo based on react-big-calendar
    NOTE: Removed the JS dep changes (adding the fullcalendar library)
    during rebase, to avoid conflicts when rebasing timetable onto master.

---
+++ indico/modules/events/timetable/client/js/EntryDetails.jsx
@@ -0,0 +1,250 @@
+// This file is part of Indico.
+// Copyright (C) 2002 - 2024 CERN
+//
+// Indico is free software; you can redistribute it and/or
+// modify it under the terms of the MIT License; see the
+// LICENSE file for more details.
+
+import PropTypes from 'prop-types';
+import React from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {Accordion, Divider, Header, Icon} from 'semantic-ui-react';
+
+import {Translate} from 'indico/react/i18n';
+
+import * as actions from './actions';
+import AttachmentsDisplay from './components/AttachmentsDisplay';
+import DetailsSegment from './components/DetailsSegment';
+import EntryColorPicker from './components/EntryColorPicker';
+import TimeDisplay from './components/TimeDisplay';
+import * as selectors from './selectors';
+import {entrySchema, entryTypes, formatTitle, handleUnimplemented, isChildOf} from './util';
+
+import './EntryDetails.module.scss';
+
+const entryIcons = {
+  session: 'calendar alternate outline', // XXX: users also looks nice
+  contribution: 'file alternate outline',
+  break: 'coffee',
+};
+
+const detailsPropTypes = {
+  entry: entrySchema.isRequired,
+  uses24HourFormat: PropTypes.bool.isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+
+function ContributionsDisplay({entry, uses24HourFormat, dispatch}) {
+  const children = useSelector(selectors.getChildren);
+  const contribs = children.filter(c => isChildOf(c, entry));
+
+  if (contribs.length === 0) {
+    return (
+      <p>
+        <Translate as="strong">Contributions</Translate>: <Icon name="file alternate outline" />
+        <Translate as="em">None</Translate>
+      </p>
+    );
+  }
+
+  return (
+    <Accordion
+      panels={[
+        {
+          key: 'contribs',
+          title: {
+            content: (
+              <>
+                <Translate as="strong">Contributions</Translate>:{' '}
+                <Icon name="file alternate outline" />
+                {contribs.length}
+              </>
+            ),
+            icon: null,
+          },
+          content: {
+            content: contribs.map(contrib => (
+              <ContributionDetails
+                key={contrib.id}
+                entry={contrib}
+                uses24HourFormat={uses24HourFormat}
+                dispatch={dispatch}
+                title={entryTypes.contribution.formatTitle(contrib)}
+              />
+            )),
+          },
+        },
+      ]}
+    />
+  );
+}
+
+ContributionsDisplay.propTypes = detailsPropTypes;
+
+function SessionDetails({entry, uses24HourFormat, dispatch}) {
+  const {title, slotTitle, code, sessionCode, color} = entry;
+  return (
+    <>
+      <DetailsSegment
+        title={Translate.string('Block')}
+        subtitle={formatTitle(slotTitle, sessionCode)}
+        actions={[
+          {icon: 'edit', title: Translate.string('Edit session block')},
+          {
+            icon: 'trash',
+            title: Translate.string('Delete session block'),
+            onClick: () => dispatch(actions.deleteEntry(entry)),
+          },
+        ]}
+      >
+        <TimeDisplay entry={entry} uses24HourFormat={uses24HourFormat} />
+        <Divider />
+        <ContributionsDisplay
+          entry={entry}
+          uses24HourFormat={uses24HourFormat}
+          dispatch={dispatch}
+        />
+      </DetailsSegment>
+      <DetailsSegment
+        title={Translate.string('Session')}
+        subtitle={formatTitle(title, code)}
+        color={color}
+        actions={[
+          {icon: 'paint brush', title: Translate.string('Change color'), wrapper: EntryColorPicker},
+          {icon: 'edit', title: Translate.string('Edit session'), onClick: handleUnimplemented},
+          {
+            icon: 'shield',
+            title: Translate.string('Manage session protection'),
+            onClick: handleUnimplemented,
+          },
+        ]}
+        entry={entry}
+        dispatch={dispatch}
+      >
+        <AttachmentsDisplay entry={entry} />
+      </DetailsSegment>
+    </>
+  );
+}
+
+SessionDetails.propTypes = detailsPropTypes;
+
+export function ContributionDetails({entry, uses24HourFormat, dispatch, children, ...rest}) {
+  const contribActions = [
+    {
+      icon: 'edit',
+      title: Translate.string('Edit contribution'),
+      onClick: () => dispatch(actions.editEntry('contribution', entry)),
+    },
+    {
+      icon: 'shield',
+      title: Translate.string('Manage contribution protection'),
+      onClick: handleUnimplemented,
+    },
+    {
+      icon: 'clone outline',
+      title: Translate.string('Clone contribution'),
+      onClick: handleUnimplemented,
+    },
+  ];
+  if (!entry.deleted) {
+    contribActions.push({
+      icon: 'trash',
+      title: Translate.string('Unschedule contribution'),
+      onClick: () => dispatch(actions.deleteEntry(entry)),
+    });
+  }
+
+  return (
+    <DetailsSegment
+      title={Translate.string('Contribution')}
+      color={entry.color}
+      actions={contribActions}
+      {...rest}
+    >
+      {children || (
+        <>
+          {!entry.isPoster && <TimeDisplay entry={entry} uses24HourFormat={uses24HourFormat} />}
+          <AttachmentsDisplay entry={entry} />
+        </>
+      )}
+    </DetailsSegment>
+  );
+}
+
+ContributionDetails.propTypes = {
+  ...detailsPropTypes,
+  children: PropTypes.node,
+};
+
+ContributionDetails.defaultProps = {
+  children: null,
+};
+
+function BreakDetails({entry, uses24HourFormat, dispatch}) {
+  return (
+    <DetailsSegment
+      title={Translate.string('Break')}
+      color={entry.color}
+      actions={[
+        {icon: 'paint brush', title: Translate.string('Change color'), wrapper: EntryColorPicker},
+        {icon: 'edit', title: Translate.string('Edit break'), onClick: handleUnimplemented},
+        {
+          icon: 'trash',
+          title: Translate.string('Delete break'),
+          onClick: () => dispatch(actions.deleteEntry(entry)),
+        },
+      ]}
+      entry={entry}
+      dispatch={dispatch}
+    >
+      <TimeDisplay entry={entry} uses24HourFormat={uses24HourFormat} />
+    </DetailsSegment>
+  );
+}
+
+BreakDetails.propTypes = detailsPropTypes;
+
+export default function EntryDetails() {
+  const dispatch = useDispatch();
+  const entry = useSelector(selectors.getSelectedEntry);
+  const {title, slotTitle, description, code, sessionCode, type} = entry;
+
+  // TODO figure this out:
+  const uses24HourFormat = true;
+
+  const PopupContentComponent = {
+    session: SessionDetails,
+    contribution: ContributionDetails,
+    break: BreakDetails,
+  }[type];
+
+  return (
+    <div styleName="details-container">
+      <div styleName="content">
+        <div style={{display: 'flex', justifyContent: 'space-between'}}>
+          <Header
+            size="small"
+            color="blue"
+            icon={entryIcons[type]}
+            content={formatTitle(title, code)}
+            subheader={formatTitle(slotTitle, sessionCode)}
+          />
+          <Icon
+            name="close"
+            color="black"
+            onClick={() => dispatch(actions.selectEntry(null))}
+            title={Translate.string('Close details')}
+            link
+          />
+        </div>
+        {description && <div styleName="description">{description}</div>}
+        <PopupContentComponent
+          entry={entry}
+          uses24HourFormat={uses24HourFormat}
+          dispatch={dispatch}
+        />
+      </div>
+    </div>
+  );
+}
---
+++ indico/modules/events/timetable/client/js/components/DetailsSegment.jsx
@@ -0,0 +1,111 @@
+// This file is part of Indico.
+// Copyright (C) 2002 - 2024 CERN
+//
+// Indico is free software; you can redistribute it and/or
+// modify it under the terms of the MIT License; see the
+// LICENSE file for more details.
+
+import PropTypes from 'prop-types';
+import React from 'react';
+import {Icon, Label, Segment} from 'semantic-ui-react';
+
+import {toClasses} from 'indico/react/util';
+
+import {entryColorSchema, entrySchema} from '../util';
+
+import './DetailsSegment.module.scss';
+
+const actionShape = {
+  icon: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+  wrapper: PropTypes.elementType,
+};
+
+function ActionIcon({icon, onClick, title, color}) {
+  return (
+    <Icon
+      name={icon}
+      onClick={e => {
+        e.stopPropagation();
+        onClick(e);
+      }}
+      style={{color: color?.text || 'rgba(0, 0, 0, 0.6)'}}
+      title={title}
+      link
+    />
+  );
+}
+
+ActionIcon.propTypes = {
+  ...actionShape,
+  color: entryColorSchema,
+};
+
+ActionIcon.defaultProps = {
+  color: null,
+};
+
+export default function DetailsSegment({
+  title,
+  subtitle,
+  color,
+  icon,
+  actions,
+  children,
+  entry,
+  selected,
+  dispatch,
+  ...rest
+}) {
+  return (
+    <Segment style={{borderColor: color?.background}} {...rest}>
+      <Label
+        style={{backgroundColor: color?.background, color: color?.text}}
+        styleName={toClasses({'segment-header': true, selected})}
+        attached="top"
+      >
+        {icon && <Icon name={icon} />}
+        {title}
+        {subtitle && <Label.Detail>{subtitle}</Label.Detail>}
+        <div styleName="actions">
+          {actions.map(({wrapper: Wrapper, ...action}) =>
+            Wrapper ? (
+              <Wrapper
+                key={action.icon}
+                trigger={<ActionIcon {...action} color={color} />}
+                entry={entry}
+                dispatch={dispatch}
+              />
+            ) : (
+              <ActionIcon key={action.icon} {...action} color={color} />
+            )
+          )}
+        </div>
+      </Label>
+      {children}
+    </Segment>
+  );
+}
+
+DetailsSegment.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string,
+  color: entryColorSchema,
+  icon: PropTypes.string,
+  actions: PropTypes.arrayOf(PropTypes.shape(actionShape)),
+  children: PropTypes.node.isRequired,
+  entry: entrySchema,
+  selected: PropTypes.bool,
+  dispatch: PropTypes.func,
+};
+
+DetailsSegment.defaultProps = {
+  subtitle: '',
+  icon: null,
+  actions: [],
+  color: {},
+  entry: null,
+  selected: false,
+  dispatch: () => {},
+};
