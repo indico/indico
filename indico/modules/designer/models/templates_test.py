7e4e7f1f22ed5796a2004b0318d21c9ea81e8931
Tomas Roun <tomas.roun@cern.ch>
2024-03-19 11:10:16 +0100

    Remove unnecessary test

--- indico/modules/designer/models/templates_test.py
+++ indico/modules/designer/models/templates_test.py
@@ -9,20 +9,6 @@ pytest_plugins = ('indico.modules.designer.testing.fixtures',
                   'indico.modules.events.registration.testing.fixtures')
 
 
-def test_template_link_to_regform(dummy_regform, dummy_designer_template):
-    """Ensure that a template can be linked and unlinked from a registration form."""
-    dummy_designer_template.link_regform(dummy_regform)
-    assert dummy_designer_template.registration_form == dummy_regform
-
-    original_data = dummy_designer_template.data
-    # Add regform field placeholders to the template
-    items = {'items': [*original_data['items'], {'type': 'field-1'}, {'type': 'field-2'}]}
-    dummy_designer_template.data = dummy_designer_template.data | items
-
-    dummy_designer_template.unlink_regform()
-    assert dummy_designer_template.registration_form is None
-
-
 def test_template_is_unlinkable(dummy_designer_template):
     """`template.is_unlinkable` should be `False` if the template contains custom registration placeholders."""
     assert dummy_designer_template.is_unlinkable
