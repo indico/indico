b7cce1257cc8375c7814122c2f5e5dcead80fc46
Tomas R <tomas.roun@cern.ch>
2024-03-21 14:26:58 +0100

    Fix missing room id when updating room availability (#6244)
    * Fix missing room id when updating room availability
    * Fix form not updating availability after save

--- indico/modules/rb/client/js/common/rooms/edit/RoomEditModal.jsx
+++ indico/modules/rb/client/js/common/rooms/edit/RoomEditModal.jsx
@@ -210,11 +210,11 @@ function RoomEditModal({roomId, locationId, onClose, afterCreation}) {
       ...basicDetails
     } = changedValues;
     try {
+      let response;
       if (isNewRoom) {
         const payload = {...basicDetails};
         payload.location_id = locationId;
-        const response = await indicoAxios.post(roomsURL(), payload);
-        setNewRoomId(response.data.id);
+        response = await indicoAxios.post(roomsURL(), payload);
       } else if (!_.isEmpty(basicDetails)) {
         await indicoAxios.patch(roomURL({room_id: roomId}), basicDetails);
       }
@@ -229,7 +229,7 @@ function RoomEditModal({roomId, locationId, onClose, afterCreation}) {
       if (bookableHours || nonbookablePeriods) {
         const availability = {bookableHours, nonbookablePeriods};
         await indicoAxios.post(
-          updateRoomAvailabilityURL({room_id: roomId}),
+          updateRoomAvailabilityURL({room_id: isNewRoom ? response.data.id : roomId}),
           snakifyKeys(availability)
         );
       }
@@ -237,6 +237,8 @@ function RoomEditModal({roomId, locationId, onClose, afterCreation}) {
       if (!isNewRoom) {
         setWasEverUpdated(true);
         fetchRoomData();
+      } else {
+        setNewRoomId(response.data.id);
       }
     } catch (e) {
       return handleSubmitError(e);
