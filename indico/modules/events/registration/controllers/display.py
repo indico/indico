40d5e08d42538e864b35e3aefaf980f74345b20f
Giorgio Pieretti <giorgio.pieretti@un.org>
2024-04-08 11:08:00 +0200

    Fix headers

--- indico/modules/events/registration/controllers/display.py
+++ indico/modules/events/registration/controllers/display.py
@@ -4,6 +4,7 @@
 # Indico is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see the
 # LICENSE file for more details.
+
 from uuid import UUID
 
 from flask import flash, jsonify, redirect, request, session
