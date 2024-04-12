e56f7d80e477f44bdd47e10c2d449862c24bbf9d
Duarte Galvão <duarte.galvao@cern.ch>
2024-04-12 13:02:45 +0100

    Add locations endpoint for locationfield

--- indico/web/client/js/react/components/LocationField.jsx
+++ indico/web/client/js/react/components/LocationField.jsx
@@ -5,11 +5,11 @@
 // modify it under the terms of the MIT License; see the
 // LICENSE file for more details.
 
-import locationsURL from 'indico-url:rb.locations'; // TODO replace this with proper endpoint
+import locationsURL from 'indico-url:event_management.api_locations';
 
 import PropTypes from 'prop-types';
 import React from 'react';
-import {Dropdown, Form} from 'semantic-ui-react';
+import {Dropdown, Form, Icon, Popup} from 'semantic-ui-react';
 
 import {FinalField} from 'indico/react/forms';
 import {useIndicoAxios} from 'indico/react/hooks';
@@ -17,31 +17,24 @@ import {Translate} from 'indico/react/i18n';
 
 import './LocationField.module.scss';
 
-export default function LocationField({
-  value,
-  onChange,
-  disabled,
-  required,
-  editAddress,
-  allowLocationInheritance,
-}) {
-  const {data: locations, loading} = useIndicoAxios(locationsURL(), {camelize: true}); // TODO check if we need to handle 404s if RB is disabled
-
-  const venues = locations?.map(({id, name}) => ({key: id, text: name, value: id})) || [];
-  const rooms =
-    locations?.flatMap(location => [
-      {
-        key: `_loc:${location.id}`,
-        text: location.name,
-        as: Dropdown.Header,
-        disabled: true,
-      },
-      ...location.rooms.map(({id, fullName}) => ({
-        key: id,
-        text: fullName,
-        value: id,
-      })),
-    ]) || [];
+const processLocations = (locations, value) => {
+  if (!locations.length) {
+    return [[], []];
+  }
+  const venues = locations.map(({id, name}) => ({key: id, text: name, value: id}));
+  const rooms = locations.flatMap(location => [
+    {
+      key: `_loc:${location.id}`,
+      text: location.name,
+      as: Dropdown.Header,
+      disabled: true,
+    },
+    ...location.rooms.map(({id, fullName}) => ({
+      key: id,
+      text: fullName,
+      value: id,
+    })),
+  ]);
   if (value.venue_name && !value.venue_id) {
     venues.push({
       key: 'custom',
@@ -58,91 +51,133 @@ export default function LocationField({
       content: Translate.string('Custom room: "{room}"', {room: value.room_name}),
     });
   }
+  return [venues, rooms];
+};
+
+export default function LocationField({value, onChange, disabled, required, editAddress, parent}) {
+  const {data, loading} = useIndicoAxios(locationsURL(), {camelize: true});
+  const locationsEnabled = data?.enabled || false;
+  const locations = data?.locations || [];
+  const [venues, rooms] = processLocations(locations, value);
+
+  const handleChangeVenue = (_, {value: locationId}) => {
+    if (typeof locationId === 'string') {
+      onChange({
+        ...value,
+        venue_id: null,
+        venue_name: locationId,
+        room_id: null,
+        room_name: value.room_id ? '' : value.room_name,
+      });
+      return;
+    }
+    onChange({
+      ...value,
+      venue_id: locationId,
+      venue_name: locations.find(l => l.id === locationId).name,
+      room_id: null,
+      room_name: value.room_id ? '' : value.room_name,
+    });
+  };
+
+  const handleChangeRoom = (_, {value: roomId}) => {
+    if (typeof roomId === 'string') {
+      onChange({
+        ...value,
+        room_id: null,
+        room_name: roomId,
+      });
+      return;
+    }
+    const location = locations.find(l => l.rooms.some(r => r.id === roomId));
+    onChange({
+      ...value,
+      venue_id: location.id,
+      venue_name: location.name,
+      room_id: roomId,
+      room_name: location.rooms.find(r => r.id === roomId).fullName,
+    });
+  };
+
+  const makeOnChange = key => (_, {value: fieldValue}) => onChange({...value, [key]: fieldValue});
 
   return (
     <>
-      <Form.Group widths="equal">
-        <Form.Select
-          placeholder={Translate.string('Venue')}
-          disabled={disabled || value.use_default}
-          required={required && !value.use_default}
-          clearable={!required}
-          options={venues}
-          value={value.venue_id || value.venue_name}
-          onChange={(_, {value: locationId}) => {
-            if (typeof locationId === 'string') {
-              onChange({
-                ...value,
-                venue_id: null,
-                venue_name: locationId,
-                room_id: null,
-                room_name: value.room_id ? '' : value.room_name,
-              });
-              return;
-            }
-            onChange({
-              ...value,
-              venue_id: locationId,
-              venue_name: locations.find(l => l.id === locationId).name,
-              room_id: null,
-              room_name: value.room_id ? '' : value.room_name,
-            });
-          }}
-          loading={loading}
-          selectOnBlur={false}
-          search
-          allowAdditions
-        />
-        <Form.Select
-          styleName="room-dropdown"
-          placeholder={Translate.string('Room')}
-          disabled={disabled || value.use_default}
-          required={required && !value.use_default}
-          clearable={!required}
-          options={rooms}
-          value={value.room_id || value.room_name}
-          onChange={(_, {value: roomId}) => {
-            if (typeof roomId === 'string') {
-              onChange({
-                ...value,
-                room_id: null,
-                room_name: roomId,
-              });
-              return;
-            }
-            const location = locations.find(l => l.rooms.some(r => r.id === roomId));
-            onChange({
-              ...value,
-              venue_id: location.id,
-              venue_name: location.name,
-              room_id: roomId,
-              room_name: location.rooms.find(r => r.id === roomId).fullName,
-            });
-          }}
-          loading={loading}
-          selectOnBlur={false}
-          search
-          allowAdditions
-        />
-      </Form.Group>
+      {locationsEnabled || loading ? (
+        <Form.Group widths="equal">
+          <Form.Select
+            placeholder={Translate.string('Venue')}
+            disabled={disabled || value.use_default}
+            required={required && !value.use_default}
+            clearable={!required}
+            options={venues}
+            value={value.venue_id || value.venue_name}
+            onChange={handleChangeVenue}
+            loading={loading}
+            selectOnBlur={false}
+            search
+            allowAdditions
+          />
+          <Form.Select
+            styleName="room-dropdown"
+            placeholder={Translate.string('Room')}
+            disabled={disabled || value.use_default}
+            required={required && !value.use_default}
+            clearable={!required}
+            options={rooms}
+            value={value.room_id || value.room_name}
+            onChange={handleChangeRoom}
+            loading={loading}
+            selectOnBlur={false}
+            search
+            allowAdditions
+          />
+        </Form.Group>
+      ) : (
+        <Form.Group widths="equal">
+          <Form.Input
+            placeholder={Translate.string('Venue')}
+            value={value.venue_name || ''}
+            onChange={makeOnChange('venue_name')}
+            disabled={disabled || value.use_default}
+            required={required}
+          />
+          <Form.Input
+            placeholder={Translate.string('Room')}
+            value={value.room_name || ''}
+            onChange={makeOnChange('room_name')}
+            disabled={disabled || value.use_default}
+            required={required}
+          />
+        </Form.Group>
+      )}
       {editAddress && (
         <Form.TextArea
           placeholder={Translate.string('Address')}
           value={value.address || ''}
-          onChange={(_, {value: address}) => onChange({...value, address})}
+          onChange={makeOnChange('address')}
           disabled={disabled || value.use_default}
-          required={required && !value.use_default}
+          required={required}
         />
       )}
-      {allowLocationInheritance && (
-        <Form.Checkbox
-          label={Translate.string('Use default')}
-          checked={value.use_default}
-          onChange={(_, {checked}) =>
-            onChange(checked ? {use_default: true} : {...value, use_default: false})
-          }
-          disabled={disabled}
-        />
+      {parent && (
+        <Form.Group>
+          <Form.Checkbox
+            label={Translate.string('Use default')}
+            checked={value.use_default}
+            onChange={(_, {checked}) =>
+              onChange(checked ? {use_default: true} : {...value, use_default: false})
+            }
+            disabled={disabled}
+          />
+          <Popup
+            trigger={<Icon color="grey" name="info circle" />}
+            content={Translate.string("Using default location from '{title}' ({type})", {
+              title: parent.title,
+              type: parent.type,
+            })}
+          />
+        </Form.Group>
       )}
     </>
   );
@@ -161,14 +196,17 @@ LocationField.propTypes = {
   disabled: PropTypes.bool,
   required: PropTypes.bool,
   editAddress: PropTypes.bool,
-  allowLocationInheritance: PropTypes.bool,
+  parent: PropTypes.shape({
+    title: PropTypes.string,
+    type: PropTypes.string,
+  }),
 };
 
 LocationField.defaultProps = {
   disabled: false,
   required: false,
   editAddress: true,
-  allowLocationInheritance: true,
+  parent: null,
 };
 
 export function FinalLocationField({name, ...rest}) {
