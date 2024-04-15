b797cac8bb73077c8f436201f22b1b067d7d4ad8
Duarte Galvão <duarte.galvao@cern.ch>
2024-04-15 11:54:27 +0100

    Add remaining fields to session form

--- indico/modules/events/timetable/client/js/components/SessionForm.jsx
+++ indico/modules/events/timetable/client/js/components/SessionForm.jsx
@@ -1,8 +1,9 @@
 import React, {useState} from 'react';
 import {Button} from 'semantic-ui-react';
 
+import {FinalLocationField, FinalSessionColorPicker} from 'indico/react/components';
 import {FinalInput, FinalTextArea} from 'indico/react/forms';
-import {FinalTimePicker} from 'indico/react/forms/fields';
+import {FinalDropdown, FinalTimePicker} from 'indico/react/forms/fields';
 import {FinalModalForm} from 'indico/react/forms/final-form';
 import {Translate} from 'indico/react/i18n';
 
@@ -22,14 +23,34 @@ export default function SessionForm() {
           <FinalInput name="title" label="Session title" autoFocus required />
           <FinalInput
             name="code"
-            label="The code that will identify the session in the Book of Abstracts."
+            label={Translate.string('Session code')}
+            description={Translate.string(
+              'The code that will identify the session in the Book of Abstracts.'
+            )}
           />
-          <FinalTextArea name="description" label="Description" />
+          <FinalTextArea name="description" label={Translate.string('Description')} />
           <FinalTimePicker
             name="default_contribution_duration"
-            label="Default contribution duration"
+            label={Translate.string('Default contribution duration')}
+            description={Translate.string(
+              'Duration that a contribution created within this session will have by default.'
+            )}
             required
           />
+          <FinalDropdown
+            name="type"
+            label={Translate.string('Type')}
+            placeholder={Translate.string('No type selected')}
+            options={[{text: Translate.string('Poster'), value: 'poster'}]}
+            selection
+          />
+          <FinalLocationField
+            name="location"
+            label={Translate.string('Default location')}
+            description={Translate.string('Default location for blocks inside the session.')}
+            parent={{title: 'TODO Conference name here', type: Translate.string('Event')}}
+          />
+          <FinalSessionColorPicker name="color" label={Translate.string('Colors')} />
         </FinalModalForm>
       )}
     </>
