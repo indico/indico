0c326696a9a531c02798e781be515c790b2cf942
Adrian Moennich <adrian.moennich@cern.ch>
2024-05-22 11:24:23 +0200

    Optimize query in session blocks api

--- indico/modules/events/registration/controllers/api/misc.py
+++ indico/modules/events/registration/controllers/api/misc.py
@@ -8,8 +8,11 @@
 from operator import itemgetter
 
 from flask import session
+from sqlalchemy.orm import joinedload
 
 from indico.modules.events.controllers.base import RHProtectedEventBase
+from indico.modules.events.sessions.models.blocks import SessionBlock
+from indico.modules.events.sessions.models.sessions import Session
 from indico.util.iterables import group_list
 
 
@@ -21,14 +24,21 @@ class RHAPIRegistrationForms(RHProtectedEventBase):
 
 class RHAPIEventSessionBlocks(RHProtectedEventBase):
     def _process(self):
-        schema = [
+        blocks = (SessionBlock.query
+                  .join(Session)
+                  .filter(Session.event == self.event,
+                          ~Session.is_deleted)
+                  .options(joinedload(SessionBlock.timetable_entry).raiseload('*'))
+                  .all())
+        res = [
             {
-                'id': sb.id, 'start_dt': sb.start_dt.astimezone(self.event.tzinfo).date().isoformat(),
+                'id': sb.id,
+                'start_dt': sb.start_dt.astimezone(self.event.tzinfo).date().isoformat(),
                 'start_time': sb.start_dt.astimezone(self.event.tzinfo).strftime('%H:%M'),
                 'end_time': sb.end_dt.astimezone(self.event.tzinfo).strftime('%H:%M'),
-                'full_title': sb.full_title
+                'full_title': sb.full_title,
             }
-            for s in self.event.sessions
-            for sb in s.blocks
-            if s.can_access(session.user)]
-        return group_list(schema, key=itemgetter('start_dt'), sort_by=itemgetter('start_dt'))
+            for sb in blocks
+            if sb.can_access(session.user)
+        ]
+        return group_list(res, key=itemgetter('start_dt'), sort_by=itemgetter('start_dt'))
