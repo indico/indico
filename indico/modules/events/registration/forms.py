b4762d8f55315286dac52dd43e340b6c3d866739
Adrian Moennich <adrian.moennich@cern.ch>
2024-05-28 16:04:20 +0200

    Make UI to copy secret link nicer (also for surveys)

--- indico/modules/events/registration/forms.py
+++ indico/modules/events/registration/forms.py
@@ -65,7 +65,7 @@ class RegistrationFormEditForm(IndicoForm):
                                       description=_('If enabled, registrations require manager approval'))
     private = BooleanField(_('Private'), widget=SwitchWidget(),
                            description=_('The registration form will not be publicly displayed on the event page. '
-                                         'Only people with the secret link will be able to register.'))
+                                         'Only people with the secret link or an invitation will be able to register.'))
     require_login = BooleanField(_('Only logged-in users'), widget=SwitchWidget(),
                                  description=_('Users must be logged in to register'))
     require_user = BooleanField(_('Registrant must have account'), widget=SwitchWidget(),
