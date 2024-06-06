b68cca456bd7dd6a64f1e4ab19d3f6e200d7a64e
Vasant Vohra <vasantvohra@gmail.com>
2024-06-06 17:17:19 +0200

    Add setting to hide person titles (#6104)

--- indico/modules/events/persons/__init__.py
+++ indico/modules/events/persons/__init__.py
@@ -51,5 +51,6 @@ def _get_placeholders(sender, person, event, contribution=None, abstract=None, r
 
 persons_settings = EventSettingsProxy('persons', {
     'disallow_custom_persons': False,  # Disallow manually entering persons on person lists
-    'default_search_external': False,  # Enable "Users with no Indico account" by d efault
+    'default_search_external': False,  # Enable "Users with no Indico account" by default
+    'show_titles': True,  # Whether to show titles for people in the event
 })
--- indico/modules/events/persons/forms.py
+++ indico/modules/events/persons/forms.py
@@ -20,6 +20,9 @@ class ManagePersonListsForm(IndicoForm):
     default_search_external = BooleanField(_('Include users with no Indico account by default'), widget=SwitchWidget(),
                                            description=_('If enabled, searching people for speakers/authors will '
                                                          'include those with no Indico account by default.'))
+    show_titles = BooleanField(_('Show titles'), widget=SwitchWidget(),
+                               description=_('When disabled, the titles of persons participating in the event will not '
+                                             'be displayed in public areas.'))
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
