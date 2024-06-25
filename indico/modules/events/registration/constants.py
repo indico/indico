f38a77f893334808b5981a9e6b958c0f801a4564
Alejandro Avilés <alejandro.aviles@un.org>
2024-06-25 11:01:28 +0200

    Move registration picture sizes to constants

---
+++ indico/modules/events/registration/constants.py
@@ -0,0 +1,12 @@
+# This file is part of Indico.
+# Copyright (C) 2002 - 2024 CERN
+#
+# Indico is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see the
+# LICENSE file for more details.
+
+#: Max size of the longest edge for registration pictures (px)
+REGISTRATION_PICTURE_SIZE = 1000
+
+#: Max size of the longest edge for registration pictures thumbnails (px)
+REGISTRATION_PICTURE_THUMBNAIL_SIZE = 120
