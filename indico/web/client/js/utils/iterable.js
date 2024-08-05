b8fea4ed7da2838539615909ee1879116e9c5db5
Hajime Yamasaki Vukelic <hajime.vukelic@un.org>
2024-08-05 08:28:54 +0200

    Implement DateRange picker
    - Enhance the base custom element to automatically hook up attributes
    - Modify the existing date picker elements to support multiple calendars
    - Add <ind-date-range-picker> element and DateRangePicker component
      (the new component is aliased as DateRangePicker2 because the name
      is already taken)
    - Add FinalDateRangePicker
    - Modify keyboard-based movement to be more logical when using screen
      readers

---
+++ indico/web/client/js/utils/iterable.js
@@ -0,0 +1,12 @@
+// This file is part of Indico.
+// Copyright (C) 2002 - 2024 CERN
+//
+// Indico is free software; you can redistribute it and/or
+// modify it under the terms of the MIT License; see the
+// LICENSE file for more details.
+
+export function* range(n) {
+  for (let i = 0; i < n; i++) {
+    yield i;
+  }
+}
