01cccd3efbc9bd849a7eb72becfde599fb6d0413
Ajob Kustra <ajob.edward.kustra@cern.ch>
2024-08-12 15:51:05 +0200

    footer with anonymous registration mentions

--- indico/modules/events/registration/client/js/components/ParticipantListAccordion.tsx
+++ indico/modules/events/registration/client/js/components/ParticipantListAccordion.tsx
@@ -1,3 +1,11 @@
+// This file is part of Indico.
+// Copyright (C) 2002 - 2024 CERN
+//
+// Indico is free software; you can redistribute it and/or
+// modify it under the terms of the MIT License; see the
+// LICENSE file for more details.
+
+import { Param, Plural, PluralTranslate, Singular } from 'indico/react/i18n';
 import React, { useState } from 'react'
 import {
   AccordionTitle,
@@ -7,69 +15,99 @@ import {
   TableRow,
   TableHeaderCell,
   TableHeader,
-  TableFooter,
   TableCell,
   TableBody,
-  MenuItem,
-  Label,
-  Menu,
   Table,
+  TableFooter,
 } from 'semantic-ui-react'
-import { AccordionTitleProps } from 'semantic-ui-react/dist/commonjs/modules/Accordion/AccordionTitle'
 
 
 interface ParticipantListAccordionProps {
     tables: any
 }
 
-export default function ParticipantListAccordion({ tables }: ParticipantListAccordionProps) {
-    const [activeIndex, setActiveIndex] = useState<number>(0)
+interface AccordionItemProps {
+    index: number;
+    table: any;
+}
+
+function AccordionItem({ index, table }: AccordionItemProps) {
+    const [isActive, setIsActive] = useState(true);
+    const hiddenParticipantsCount = table.num_participants - table.rows.length;
   
-    const handleClick = (
-        e: React.MouseEvent<HTMLDivElement, MouseEvent>,
-        titleProps: AccordionTitleProps
-    ) => {
-        const { index } = titleProps
-        const newIndex = activeIndex === index ? -1 : index
-        setActiveIndex(newIndex as number)
-    }
-
+    const handleClick = () => {
+      setIsActive(!isActive);
+    };
+  
     return (
-        <Accordion styled>
-            { tables.map((table, i) => (
-                <React.Fragment key={i}>
-                    <AccordionTitle
-                        active={activeIndex === i}
-                        index={i}
-                        onClick={handleClick}
-                        key={`t${i}`}
-                    >
-                        <Icon name='dropdown' />
-                        { table.title } ({ table.num_participants })
-                    </AccordionTitle>
-                    <AccordionContent active={activeIndex === i} key={`c${i}`}>
-                        <Table celled>
-                            <TableHeader>
-                                <TableRow>
-                                    { table.headers.map((headerText, j) => (
-                                        <TableHeaderCell key={`${i}-${j}`}>{ headerText }</TableHeaderCell>
-                                    ))}
-                                </TableRow>
-                            </TableHeader>
-
-                            <TableBody>
-                                { table.rows.map((row, j) => (
-                                    <TableRow key={`${i}-${j}`}>
-                                        { row.columns.map((col, k) => (
-                                            <TableCell key={`${i}-${j}-${k}`}>{ col.text }</TableCell>
-                                        ))}
-                                    </TableRow>
-                                ))}
-                            </TableBody>
-                        </Table>
-                    </AccordionContent>
-                </React.Fragment>)
-            )}
-        </Accordion>
-    )
+      <>
+        <AccordionTitle
+          active={isActive}
+          onClick={handleClick}
+        >
+          <Icon name="dropdown" />
+          {table.title} ({table.num_participants})
+        </AccordionTitle>
+        <AccordionContent active={isActive} key={`c${index}`}>
+          <Table celled>
+            <TableHeader>
+              <TableRow>
+                {table.headers.map((headerText: string, j: number) => (
+                  <TableHeaderCell key={j}>
+                    {headerText}
+                  </TableHeaderCell>
+                ))}
+              </TableRow>
+            </TableHeader>
+  
+            <TableBody>
+              {table.rows.map((row: any, j: number) => (
+                <TableRow key={j}>
+                  {row.columns.map((col: any, k: number) => (
+                    <TableCell key={`${j}-${k}`}>
+                      {col.text}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))}
+            </TableBody>
+            
+            { (hiddenParticipantsCount > 0 && table.num_participants != hiddenParticipantsCount) &&
+                <TableFooter>
+                    <TableRow>
+                        <TableHeaderCell colSpan="3">
+                        <PluralTranslate count={hiddenParticipantsCount}>
+                            <Singular>
+                                <Param
+                                    name="visible-participants"
+                                    value={hiddenParticipantsCount}
+                                    />{' '}
+                                participant registered anonymously
+                            </Singular>
+                            <Plural>
+                                <Param
+                                    name="visible-participants"
+                                    value={hiddenParticipantsCount}
+                                    />{' '}
+                                participants registered anonymously
+                            </Plural>
+                            </PluralTranslate>
+                        </TableHeaderCell>
+                    </TableRow>
+                </TableFooter>
+            }
+          </Table>
+        </AccordionContent>
+      </>
+    );
   }
+
+export default function ParticipantListAccordion({ tables }: ParticipantListAccordionProps) {
+return (
+    <Accordion styled className="accordion">
+    {tables.map((table: any, i: number) => (
+        <AccordionItem key={i} index={i} table={table} />
+    ))}
+    </Accordion>
+);
+}
