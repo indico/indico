5e4dd9a667f52200b539078b76b410bbe65993f1
Tomas Roun <tomas.roun@cern.ch>
2024-08-19 14:19:30 +0200

    Display time in entries

--- indico/modules/events/timetable/client/js/Entry.jsx
+++ indico/modules/events/timetable/client/js/Entry.jsx
@@ -5,14 +5,29 @@
 // modify it under the terms of the MIT License; see the
 // LICENSE file for more details.
 
-import React from 'react';
-import {useSelector} from 'react-redux';
+import React, {useEffect, useRef, useState} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
 
+import * as actions from './actions';
 import NewEntryDropdown from './components/NewEntryDropdown';
 import * as selectors from './selectors';
-import {entrySchema, entryTypes, isChildOf} from './util';
+import {entrySchema, entryTypes, getEndDt, isChildOf} from './util';
 
 import './Entry.module.scss';
+import {
+  Button,
+  ButtonGroup,
+  Card,
+  Dropdown,
+  DropdownHeader,
+  DropdownItem,
+  DropdownMenu,
+  Icon,
+  Popup,
+} from 'semantic-ui-react';
+import EntryDetails from './EntryDetails';
+import moment from 'moment';
+import {createPortal} from 'react-dom';
 
 export default function Entry({event: entry}) {
   const {type, title} = entry;
@@ -23,14 +38,47 @@ export default function Entry({event: entry}) {
     return <NewEntryDropdown icon={null} open />;
   }
 
+  const startTime = moment(entry.start).format('HH:mm');
+  const endTime = moment(getEndDt(entry)).format('HH:mm');
+  const duration = entry.duration;
+  const showSingleLine = duration <= 15;
   const hasContribs = contributions.some(c => isChildOf(c, entry));
+
+  const compactTitle = (
+    <div styleName="compact-title" style={{fontSize: 15}}>
+      {type !== 'contribution' && <Icon name={entryTypes[type].icon} />} {title}
+    </div>
+  );
+
+  const fullTitle = (
+    <div styleName="entry-title">
+      {showSingleLine && (
+        <div style={{fontSize: 15, display: 'flex', gap: 6}}>
+          {type !== 'contribution' && <Icon name={entryTypes[type].icon} />}
+          <span>
+            {entryTypes[type].formatTitle(entry)}, {startTime} - {endTime}
+          </span>
+        </div>
+      )}
+      {!showSingleLine && (
+        <>
+          <div style={{fontSize: 15, display: 'flex', gap: 6}}>
+            {type !== 'contribution' && <Icon name={entryTypes[type].icon} />}
+            <span>{entryTypes[type].formatTitle(entry)}</span>
+          </div>
+          <div style={{fontSize: 15, marginTop: 3}}>
+            {startTime} - {endTime}
+          </div>
+        </>
+      )}
+    </div>
+  );
+
   return (
-    <>
-      {displayMode === 'compact' && hasContribs && <div styleName="compact-title">{title}</div>}
-      <div styleName="entry-title">
-        {(displayMode !== 'compact' || !hasContribs) && entryTypes[type].formatTitle(entry)}
-      </div>
-    </>
+    <div>
+      {displayMode === 'compact' && hasContribs && compactTitle}
+      {(displayMode !== 'compact' || !hasContribs) && fullTitle}
+    </div>
   );
 }
 
