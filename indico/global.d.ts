8ea5a4452a83293b328d8d73721bfc0210cf171c
Ajob Kustra <ajob.edward.kustra@cern.ch>
2024-08-23 15:16:22 +0200

    add justification comment for global.d.ts

--- indico/global.d.ts
+++ indico/global.d.ts
@@ -1,3 +1,6 @@
+// Justification of this file found in issue below
+// https://github.com/gajus/babel-plugin-react-css-modules/issues/292
+
 declare namespace React {
   interface Attributes {
     styleName?: string;
