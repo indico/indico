dbd1814495895b711d3085939b452bf8c2fa39b3
Adrian <adrian.moennich@cern.ch>
2024-09-19 17:33:17 +0200

    Fix markdown code block display in survey sections (#6553)
    There was some very old code that replaced linebreaks with html
    linebreaks, but being a markdown string this was unnecessary and
    resulted in `<br>` being shown.

--- indico/modules/events/surveys/fields/base.py
+++ indico/modules/events/surveys/fields/base.py
@@ -16,7 +16,8 @@ from indico.web.forms.widgets import SwitchWidget
 
 class SurveyFieldConfigForm(IndicoForm):
     title = StringField(_('Title'), [DataRequired()], description=_('The title of the question'))
-    description = TextAreaField(_('Description'), description=_("The description (shown below the question's field.)"))
+    description = TextAreaField(_('Description'), description=_("The description (shown below the question's field). "
+                                                                'You may use Markdown for formatting.'))
     is_required = BooleanField(_('Required'), widget=SwitchWidget(),
                                description=_('If the user has to answer the question.'))
 
