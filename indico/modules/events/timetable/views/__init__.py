60630d4c2294615ca1183f5590e11615e54ce5f8
Tomas Roun <tomas.roun@cern.ch>
2024-10-08 15:14:05 +0200

    Sort using session codes

--- indico/modules/events/timetable/views/__init__.py
+++ indico/modules/events/timetable/views/__init__.py
@@ -130,5 +130,5 @@ def inject_meeting_body(event, **kwargs):
 def _entry_title_key(entry):
     obj = entry.object
     if entry.type == TimetableEntryType.SESSION_BLOCK:
-        return (obj.code, obj.full_title)
+        return (obj.session.code, obj.full_title)
     return (obj.title,)
