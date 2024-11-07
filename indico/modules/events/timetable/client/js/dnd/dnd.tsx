5c8cc26eb03ce0605e11f71b0ba886cc7add0bf1
Adrian Moennich <adrian.moennich@cern.ch>
2024-11-07 16:37:20 +0100

    Fix linter errors

--- indico/modules/events/timetable/client/js/dnd/dnd.tsx
+++ indico/modules/events/timetable/client/js/dnd/dnd.tsx
@@ -1,3 +1,10 @@
+// This file is part of Indico.
+// Copyright (C) 2002 - 2024 CERN
+//
+// Indico is free software; you can redistribute it and/or
+// modify it under the terms of the MIT License; see the
+// LICENSE file for more details.
+
 import React, {useCallback, useEffect, useRef, useState, useMemo} from 'react';
 import {createContext, useContextSelector} from 'use-context-selector';
 
