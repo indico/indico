85726b9d6ba410b102568baa24c66316b835a8d6
Ajob Kustra <ajob.edward.kustra@cern.ch>
2024-11-14 17:45:24 +0100

    rm useless assert

--- indico/cli/i18n.py
+++ indico/cli/i18n.py
@@ -392,8 +392,6 @@ def split_po_by_pot(merged_po_path: Path, pot_path: Path, output_po_path: Path):
     with pot_path.open('rb') as f:
         pot_catalog = read_po(f)
 
-    assert None not in (merged_catalog, pot_catalog)
-
     merged_catalog.update(pot_catalog)
 
     with output_po_path.open('wb') as f:
