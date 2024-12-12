a674f56e632491c530f9d91073bd9ceb56fdfafe
Adrian Moennich <adrian.moennich@cern.ch>
2024-12-12 21:24:29 +0100

    TEST: Add linked date fields on /admin/announcement

--- indico/modules/announcement/controllers.py
+++ indico/modules/announcement/controllers.py
@@ -5,6 +5,8 @@
 # modify it under the terms of the MIT License; see the
 # LICENSE file for more details.
 
+from datetime import date
+
 from flask import flash, redirect
 
 from indico.modules.admin import RHAdminBase
@@ -18,9 +20,10 @@ from indico.web.forms.base import FormDefaults
 
 class RHAnnouncement(RHAdminBase):
     def _process(self):
-        form = AnnouncementForm(obj=FormDefaults(**announcement_settings.get_all()))
+        form = AnnouncementForm(obj=FormDefaults(**announcement_settings.get_all(), test_start_date=date(2025, 4, 8)))
         if form.validate_on_submit():
-            announcement_settings.set_multi(form.data)
-            flash(_('Settings have been saved'), 'success')
+            # announcement_settings.set_multi(form.data)
+            # flash(_('Settings have been saved'), 'success')
+            flash(f'{form.test_start_date.data} - {form.test_end_date.data}', 'info')
             return redirect(url_for('announcement.manage'))
         return WPAnnouncement.render_template('settings.html', 'announcement', form=form)
