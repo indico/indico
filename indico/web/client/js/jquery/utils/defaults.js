e9e4daab682c1d93cde575d3446fed265427da72
Indico Team <indico-team@cern.ch>
2025-01-01 19:21:09 +0100

    Happy new year 2025 :fireworks:

--- indico/web/client/js/jquery/utils/defaults.js
+++ indico/web/client/js/jquery/utils/defaults.js
@@ -1,5 +1,5 @@
 // This file is part of Indico.
-// Copyright (C) 2002 - 2024 CERN
+// Copyright (C) 2002 - 2025 CERN
 //
 // Indico is free software; you can redistribute it and/or
 // modify it under the terms of the MIT License; see the
