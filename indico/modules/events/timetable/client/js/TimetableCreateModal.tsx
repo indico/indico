7daa9e2703c1b2026f379593f4c2e77966e41ce0
Ajob Kustra <ajob.edward.kustra@cern.ch>
2025-02-05 14:45:30 +0100

    fixed sizing issue and startdate

--- indico/modules/events/timetable/client/js/TimetableCreateModal.tsx
+++ indico/modules/events/timetable/client/js/TimetableCreateModal.tsx
@@ -1,12 +1,14 @@
 import React, {useState} from 'react';
 import {Modal, Button, Divider} from 'semantic-ui-react';
 
-import {SessionBlockCreateForm} from 'indico/modules/events/sessions/client/js/SessionBlockForm';
-import {FinalInput, FinalTextArea} from 'indico/react/forms';
 import { FinalModalForm } from 'indico/react/forms/final-form';
-import { Translate } from 'indico/react/i18n';
 
-import {ContributionCreateForm} from '../../../contributions/client/js/ContributionForm';
+import {ContributionCreateForm, ContributionFormFields} from '../../../contributions/client/js/ContributionForm';
+import { Translate } from 'indico/react/i18n';
+import {FinalInput, FinalTextArea} from 'indico/react/forms';
+import { FinalDatePicker } from 'indico/react/components';
+import { FinalDuration, FinalField } from 'indico/react/forms/fields';
+// import {SessionBlockCreateForm} from 'indico/modules/events/sessions/client/js/SessionBlockForm';
 
 interface TimetableCreateModalProps {
   open: boolean;
@@ -15,53 +17,19 @@ interface TimetableCreateModalProps {
 }
 
 const TimetableCreateModal: React.FC<TimetableCreateModalProps> = ({open, onClose, newEntry}) => {
-  const [activeForm, setActiveForm] = useState('contribution');
+  console.log('A new entry!', newEntry)
+  const forms = {
+    'Contribution': (
+      <ContributionFormFields
+        eventId={5}
+        initialValues={{duration: newEntry?.duration * 60, start_dt: newEntry?.startDt}}
+      />
+    ),
+    'Session Block': (<FinalInput label="Dummy field for session block" name="sessionBlock" />),
+  }
+  const [activeForm, setActiveForm] = useState(Object.keys(forms)[0]);
 
-  const renderForm = () => {
-    switch (activeForm) {
-      case 'contribution':
-        return (
-          <ContributionCreateForm
-            eventId={5}
-            onClose={onClose}
-            customFields={[
-              {
-                id: 1,
-                fieldType: 'text',
-                title: 'test',
-                description: 'Field for testing',
-                isRequired: false,
-                fieldData: {},
-              },
-            ]}
-            customInitialValues={{duration: newEntry?.duration * 60}}
-            includeModal={false}
-          />
-        );
-        case 'session-block':
-          return (
-            <SessionBlockCreateForm
-              eventId={5}
-              onClose={onClose}
-              customFields={[
-                {
-                  id: 1,
-                  fieldType: 'text',
-                  title: 'test',
-                  description: 'Field for testing',
-                  isRequired: false,
-                  fieldData: {},
-                },
-              ]}
-              customInitialValues={{duration: newEntry?.duration * 60}}
-              includeModal={false}
-            />
-          )
-      // Add other forms here
-      default:
-        return null;
-    }
-  };
+  const renderForm = () => forms[activeForm];
 
   return (
     <FinalModalForm
@@ -70,15 +38,15 @@ const TimetableCreateModal: React.FC<TimetableCreateModalProps> = ({open, onClos
         onClose={onClose}
         initialValues={{}}
         size="small"
-        header={Translate.string('Create new entry')}
+        header={Translate.string('Create new timetable entry')}
     >
       <Button.Group>
-        <Button onClick={() => setActiveForm('contribution')}>Contribution</Button>
-        <Button onClick={() => setActiveForm('session-block')}>Session Block</Button>
+        {Object.keys(forms).map((key) => (
+          <Button key={key} onClick={() => setActiveForm(key)} active={activeForm === key}>{key}</Button>
+        ))}
       </Button.Group>
       <Divider />
-
-      <FinalInput name="maintitle" label={Translate.string('Main Title')} autoFocus required type={undefined} nullIfEmpty={undefined} noAutoComplete={undefined} />
+      {/* <FinalDuration name="duration" label={undefined} defaultValue={undefined} /> */}
       {activeForm && renderForm()}
     </FinalModalForm>
   );
