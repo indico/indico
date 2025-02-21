d4a73772bab56c17772a2bf90be4fb087fc0f3b9
Hajime Yamasaki Vukelic <hajime.vukelic@un.org>
2025-02-21 14:00:22 +0400

    Improve places selection a11y in MultiChoiceInput (#6764)

--- indico/modules/events/registration/client/js/form/fields/MultiChoiceInput.jsx
+++ indico/modules/events/registration/client/js/form/fields/MultiChoiceInput.jsx
@@ -10,9 +10,9 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import {Field} from 'react-final-form';
 import {useSelector} from 'react-redux';
-import {Dropdown, Label} from 'semantic-ui-react';
+import {Label} from 'semantic-ui-react';
 
-import {Checkbox} from 'indico/react/components';
+import {Checkbox, Select} from 'indico/react/components';
 import {FinalCheckbox, FinalField, FinalInput, validators as v} from 'indico/react/forms';
 import {Translate, PluralTranslate, Param} from 'indico/react/i18n';
 
@@ -52,15 +52,16 @@ function MultiChoiceInputComponent({
     if (value[choice.id]) {
       onChange(_.omit(value, choice.id));
     } else {
-      onChange({...value, [choice.id]: +!value[choice.id]});
+      onChange({...value, [choice.id]: 1});
     }
   };
-  const makeHandleSlotsChange = choice => (__, {value: newValue}) => {
+  const makeHandleSlotsChange = choice => evt => {
     markTouched();
-    if (!+newValue) {
+    const selectedSlots = Number(evt.target.value);
+    if (!selectedSlots) {
       onChange(_.omit(value, choice.id));
     } else {
-      onChange({...value, [choice.id]: +newValue});
+      onChange({...value, [choice.id]: selectedSlots});
     }
   };
 
@@ -123,7 +124,7 @@ function MultiChoiceInputComponent({
               {withExtraSlots && (
                 <td>
                   {choice.isEnabled && (
-                    <Dropdown
+                    <Select
                       selection
                       styleName="dropdown"
                       disabled={
@@ -133,17 +134,16 @@ function MultiChoiceInputComponent({
                           (placesUsed[choice.id] || 0) - (existingValue[choice.id] || 0) >=
                             choice.placesLimit)
                       }
-                      value={value[choice.id] || 0}
+                      value={String(value[choice.id] || 0)}
                       onChange={makeHandleSlotsChange(choice)}
                       options={_.range(0, choice.maxExtraSlots + 2).map(i => ({
-                        key: i,
                         value: i,
-                        text: i,
                         disabled:
                           choice.placesLimit > 0 &&
                           (placesUsed[choice.id] || 0) - (existingValue[choice.id] || 0) + i >
                             choice.placesLimit,
                       }))}
+                      required
                     />
                   )}
                 </td>
