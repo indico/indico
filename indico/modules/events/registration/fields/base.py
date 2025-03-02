9eee61fc035b234fe64d847a56209957eeb3e8db
José Molina <jose.molina@unconventional.dev>
2025-03-02 09:40:00 +0100

    Polish code

--- indico/modules/events/registration/fields/base.py
+++ indico/modules/events/registration/fields/base.py
@@ -34,9 +34,7 @@ class FieldSetupSchemaBase(mm.Schema):
                 raise ValueError('The field cannot conditionally depend on itself to be shown')
             used_field_ids.add(field.id)
         regform = self.context['regform']
-        is_same_regform = (RegistrationFormItem.query.filter_by(
-            id=field_id, registration_form_id=regform.id).scalar() is not None)
-        if not is_same_regform:
+        if not RegistrationFormItem.query.filter_by(id=field_id, registration_form_id=regform.id).has_rows():
             raise ValidationError('The field to show does not belong to the same registration form.')
         # Avoid cycles
         next_field_id = field_id
@@ -45,10 +43,7 @@ class FieldSetupSchemaBase(mm.Schema):
                 raise ValidationError('Field conditions may not have cycles (a -> b -> c -> a)')
             else:
                 used_field_ids.add(next_field_id)
-            next_field = RegistrationFormItem.query.filter_by(id=next_field_id).one_or_none()
-            if not next_field:
-                # TODO: there is a field that does not exist in the chain. Should we raise an error in this case?
-                break
+            next_field = RegistrationFormItem.query.filter_by(id=next_field_id).one()
             if next_field.is_manager_only or (next_field.parent is not None and next_field.parent.is_manager_only):
                 raise ValidationError('Field conditions may not depend on fields in manager-only sections')
             next_field_id = next_field.data.get('show_if_field_id')
