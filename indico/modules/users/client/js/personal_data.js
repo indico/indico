97e9ce4edc2b55313d604456ad3ef16a43353594
Dominic Hollis <dominic.hollis@cern.ch>
2025-03-05 14:45:24 +0100

    Merge user delete + block modules

---
+++ indico/modules/users/client/js/personal_data.js
@@ -0,0 +1,12 @@
+// This file is part of Indico.
+// Copyright (C) 2002 - 2025 CERN
+//
+// Indico is free software; you can redistribute it and/or
+// modify it under the terms of the MIT License; see the
+// LICENSE file for more details.
+
+import PersonalDataForm from './PersonalDataForm.jsx';
+import UserBlock from './UserBlock.jsx';
+import UserDelete from './UserDelete.jsx';
+
+export {PersonalDataForm, UserDelete, UserBlock};
