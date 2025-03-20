0c139e1049042740fa70940e07f9c8f4610e8e28
Ajob Kustra <ajob.edward.kustra@cern.ch>
2025-03-20 17:38:36 +0100

    unbehead

--- indico/modules/events/person_link_schemas.py
+++ indico/modules/events/person_link_schemas.py
@@ -1,3 +1,10 @@
+# This file is part of Indico.
+# Copyright (C) 2002 - 2025 CERN
+#
+# Indico is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see the
+# LICENSE file for more details.
+
 import operator
 
 from marshmallow import SchemaOpts, ValidationError, fields, post_dump, post_load, pre_load, validates_schema
