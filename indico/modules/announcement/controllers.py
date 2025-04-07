f4eba41ec6ee0d445fa4defedcb252fdcaa61e5b
Adrian Moennich <adrian.moennich@cern.ch>
2025-04-07 12:04:55 +0200

    Remove testing code
    This reverts commit dc466e43ab429432bd9cfdd48bea8c83a8f6f146.

--- indico/modules/announcement/controllers.py
+++ indico/modules/announcement/controllers.py
@@ -5,27 +5,22 @@
 # modify it under the terms of the MIT License; see the
 # LICENSE file for more details.
 
-from datetime import datetime
-
 from flask import flash, redirect
 
 from indico.modules.admin import RHAdminBase
 from indico.modules.announcement import announcement_settings
 from indico.modules.announcement.forms import AnnouncementForm
 from indico.modules.announcement.views import WPAnnouncement
-from indico.util.date_time import server_to_utc
-# from indico.util.i18n import _
+from indico.util.i18n import _
 from indico.web.flask.util import url_for
 from indico.web.forms.base import FormDefaults
 
 
 class RHAnnouncement(RHAdminBase):
     def _process(self):
-        form = AnnouncementForm(obj=FormDefaults(**announcement_settings.get_all(),
-                                                 test_start_dt=server_to_utc(datetime(2025, 4, 8, 13, 37))))
+        form = AnnouncementForm(obj=FormDefaults(**announcement_settings.get_all()))
         if form.validate_on_submit():
-            # announcement_settings.set_multi(form.data)
-            # flash(_('Settings have been saved'), 'success')
-            flash(f'{form.test_start_dt.data} - {form.test_end_dt.data}', 'info')
+            announcement_settings.set_multi(form.data)
+            flash(_('Settings have been saved'), 'success')
             return redirect(url_for('announcement.manage'))
         return WPAnnouncement.render_template('settings.html', 'announcement', form=form)
