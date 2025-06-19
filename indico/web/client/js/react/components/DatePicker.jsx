0ea9beb630319f6a732a22dee908bee778143ddc
Hajime Yamasaki Vukelic <hajime.vukelic@un.org>
2025-06-19 09:03:49 +0400

    Remove console.log

--- indico/web/client/js/react/components/DatePicker.jsx
+++ indico/web/client/js/react/components/DatePicker.jsx
@@ -27,7 +27,6 @@ export default function DatePicker({
   filter,
   ...inputProps
 }) {
-  console.log('DatePicker', value);
   function handleDateChange(evt) {
     const {date} = evt.target.closest('ind-date-picker');
     const invalid = !!evt.target.value && !date;
