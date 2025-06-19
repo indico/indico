33d90de1650498231745edc338f85aa97718bef2
Adrian Moennich <adrian.moennich@cern.ch>
2025-06-19 13:28:18 +0200

    Make test more concise
    - remove double context (the pytext fixture is sufficient)
    - use variables directly (if you want to be more verbose, you could
      always pass them a kwargs, but it should be fairly clear here, even
      more so since it's just a test)

--- indico/modules/events/operations_test.py
+++ indico/modules/events/operations_test.py
@@ -15,21 +15,15 @@ from indico.modules.events.settings import event_language_settings
 
 
 @pytest.mark.usefixtures('request_context')
-def test_language_settings_cloned(dummy_event, app, dummy_user):
-    with app.test_request_context():
-        # Set up language settings
-        event_language_settings.set(dummy_event, 'default_locale', 'fr_FR')
-        event_language_settings.set(dummy_event, 'enforce_locale', True)
-        event_language_settings.set(dummy_event, 'supported_locales', ['es_ES', 'en_GB'])
-
-        session.set_session_user(dummy_user)
-        start_dt = datetime(2022, 11, 11, 13, 37)
-        cloners = set()
-        n_occurrence = 1
-
-        # Clone the event
-        new_event = clone_event(dummy_event, n_occurrence, start_dt, cloners)
-        # Assert that the language settings are cloned correctly
-        assert event_language_settings.get(new_event, 'default_locale') == 'fr_FR'
-        assert event_language_settings.get(new_event, 'enforce_locale')
-        assert event_language_settings.get(new_event, 'supported_locales') == ['es_ES', 'en_GB']
+def test_language_settings_cloned(dummy_event, dummy_user):
+    # Set up language settings
+    event_language_settings.set(dummy_event, 'default_locale', 'fr_FR')
+    event_language_settings.set(dummy_event, 'enforce_locale', True)
+    event_language_settings.set(dummy_event, 'supported_locales', ['es_ES', 'en_GB'])
+    session.set_session_user(dummy_user)
+    # Clone the event
+    new_event = clone_event(dummy_event, 1, datetime(2025, 6, 19, 13, 37), set())
+    # Assert that the language settings are cloned correctly
+    assert event_language_settings.get(new_event, 'default_locale') == 'fr_FR'
+    assert event_language_settings.get(new_event, 'enforce_locale')
+    assert event_language_settings.get(new_event, 'supported_locales') == ['es_ES', 'en_GB']
