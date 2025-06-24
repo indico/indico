ab51006883e4fc45c9dedcaee1e160447c97a85e
Adrian Moennich <adrian.moennich@cern.ch>
2025-06-24 10:43:25 +0200

    Fix variable name

--- indico/modules/events/registration/controllers/management/regforms.py
+++ indico/modules/events/registration/controllers/management/regforms.py
@@ -378,13 +378,13 @@ class RHRegistrationFormModify(RHManageRegFormBase):
     def _process(self):
         min_data_retention = data_retention_settings.get('minimum_data_retention')
         max_data_retention = data_retention_settings.get('maximum_data_retention') or timedelta(days=3650)
-        regform_retention_days = self.regform.retention_period.days // 7 if self.regform.retention_period else None
+        regform_retention_weeks = self.regform.retention_period.days // 7 if self.regform.retention_period else None
         return WPManageRegistration.render_template('management/regform_modify.html', self.event,
                                                     form_data=get_flat_section_setup_data(self.regform),
                                                     regform=self.regform,
                                                     data_retention_range={'min': min_data_retention.days // 7,
                                                                           'max': max_data_retention.days // 7,
-                                                                          'regform': regform_retention_days},
+                                                                          'regform': regform_retention_weeks},
                                                     has_predefined_affiliations=Affiliation.query.has_rows())
 
 
