a931ead2c9fae555bbfa383950b082424fa8910b
Tamas Mako <1412056+tomako@users.noreply.github.com>
2025-06-30 13:28:21 +0200

    Introduce custom reminder

--- indico/modules/events/reminders/util.py
+++ indico/modules/events/reminders/util.py
@@ -5,20 +5,47 @@
 # modify it under the terms of the MIT License; see the
 # LICENSE file for more details.
 
+from indico.core.db.sqlalchemy.descriptions import RenderMode
 from indico.modules.events.models.events import EventType
+from indico.util.string import html_to_plaintext
 from indico.web.flask.templating import get_template_module
 
 
-def make_reminder_email(event, with_agenda, with_description, note):
-    """Return the template module for the reminder email.
+def get_reminder_email_tpl(event, reminder_type, render_mode, with_agenda, with_description, subject, message):
+    """Return template modules for reminder email in both text/html abd text/plain format if applicable.
+
+    Legacy reminder (text/plain message) -> text/plain email format only
+    Standard reminder (text/html message) -> both text/html and text/plain email format
+    Custom reminder (text/html message) -> text/html email format only
 
     :param event: The event
+    :param reminder_type: standard|custom
+    :param render_mode: plain_text|html
     :param with_agenda: If the event's agenda should be included
-    :param note: A custom message to include in the email
+    :param subject: Subject for the custom reminder
+    :param message: A custom message to include in the email (full email body for custom reminder)
+
+    :return: tuple of templates for text/html and text/plain representation
     """
-    if event.type_ == EventType.lecture:
-        with_agenda = False
-    agenda = event.timetable_entries.filter_by(parent_id=None).all() if with_agenda else None
-    return get_template_module('events/reminders/emails/event_reminder.txt', event=event,
-                               url=event.short_external_url, note=note, with_agenda=with_agenda,
-                               with_description=with_description, agenda=agenda)
+    from indico.modules.events.reminders.models.reminders import ReminderType
+
+    html_tpl = text_tpl = None
+    if reminder_type == ReminderType.standard:
+        if event.type_ == EventType.lecture:
+            with_agenda = False
+        agenda = event.timetable_entries.filter_by(parent_id=None).all() if with_agenda else None
+        note = str(message)  # Legacy reminder (text/plain email only)
+        if render_mode == RenderMode.html:
+            note = html_to_plaintext(note).strip()  # Standard reminder (text/html -> text/plain)
+        text_tpl = get_template_module('events/reminders/emails/event_reminder.txt', event=event,
+                                        url=event.short_external_url, note=note, with_agenda=with_agenda,
+                                        with_description=with_description, agenda=agenda)
+        if render_mode == RenderMode.html:
+            html_tpl = get_template_module('events/reminders/emails/event_reminder.html', event=event,
+                                            url=event.short_external_url, note=message, with_agenda=with_agenda,
+                                            with_description=with_description, agenda=agenda)
+    else:
+        html_tpl = get_template_module('events/reminders/emails/custom_event_reminder.html',
+                                       url=event.short_external_url, subject=subject, message=message)
+
+    return html_tpl, text_tpl
