fa8ce5b9acc3a9e539cebe7639c0a36b25d47e51
Tomas Roun <tomas.roun@cern.ch>
2025-07-03 23:07:02 +0200

    Apply ruff fixes

--- indico/modules/events/timetable/controllers/manage.py
+++ indico/modules/events/timetable/controllers/manage.py
@@ -187,9 +187,9 @@ class RHTimetableBreak(RHManageEventBase):
     def _process_PATCH(self, data):
         with (track_time_changes(), track_location_changes()):
             update_break_entry(self.break_, data)
-        
+
         return BreakSchema(context={'event': self.event}).jsonify(self.break_)
-    
+
     def _process_GET(self):
         return BreakSchema(context={'event': self.event}).jsonify(self.break_)
 
@@ -224,14 +224,14 @@ class RHTimetableContribution(RHManageContributionBase):
             data['references'] = self._get_references(references)
 
         data['person_link_data'] = {v['person_link']: v['is_submitter'] for v in data.pop('person_links', [])}
-        
+
         with (track_time_changes(), track_location_changes()):
             update_contribution(self.contrib, data)
-        
+
         return ContributionSchema(context={'event': self.event}).jsonify(self.contrib)
 
     def _process_GET(self):
-        return ContributionSchema(context={'event': self.event}).jsonify(self.contrib)    
+        return ContributionSchema(context={'event': self.event}).jsonify(self.contrib)
 
     @no_autoflush
     def _get_references(self, data: list[dict]) -> list[ContributionReference]:
@@ -266,9 +266,9 @@ class RHTimetableSessionBlock(RHManageEventBase):
     def _process_PATCH(self, data):
         with (track_time_changes(), track_location_changes()):
             update_session_block(self.session_block, data)
-        
+
         return SessionBlockSchema(context={'event': self.event}).jsonify(self.session_block)
-    
+
     def _process_GET(self):
         return SessionBlockSchema(context={'event': self.event}).jsonify(self.session_block)
 
