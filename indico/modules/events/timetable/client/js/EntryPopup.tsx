8febdf8f3f4ddbb43ac43df4b9b1f9d6a4d06bea
Michel Succar <m.succar@cern.ch>
2025-07-17 14:15:26 +0200

    Fix start day edit when changing day
    Fix children disappearing

--- indico/modules/events/timetable/client/js/EntryPopup.tsx
+++ indico/modules/events/timetable/client/js/EntryPopup.tsx
@@ -33,6 +33,7 @@ import {formatTimeRange} from './i18n';
 import * as selectors from './selectors';
 import {BreakEntry, ContribEntry, BlockEntry, EntryType} from './types';
 import {getEntryColor, mapTTDataToEntry} from './utils';
+import { on } from 'process';
 
 function ColoredDot({color}: {color: string}) {
   return (
@@ -73,7 +74,7 @@ function EntryPopupContent({
   const endTime = moment(entry.startDt).add(entry.duration, 'minutes');
   const eventId = useSelector(selectors.getEventId);
 
-  const onEdit = async (e: MouseEvent) => {
+  const onEdit = async () => {
     const {objId} = entry;
     if (!objId) {
       return;
@@ -89,6 +90,7 @@ function EntryPopupContent({
     data['type'] = type;
     entry = mapTTDataToEntry(data);
     dispatch(actions.setDraftEntry(entry));
+    onClose();
   };
 
   const onDelete = async (e: MouseEvent) => {
