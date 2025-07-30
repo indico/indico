07bf2216633ede1fec4e331694216880907064a9
Marina El Jaouhari <marina.el.jaouhari@cern.ch>
2025-07-30 14:15:14 +0200

    remove console.log

--- indico/modules/events/timetable/client/js/DayTimetable.tsx
+++ indico/modules/events/timetable/client/js/DayTimetable.tsx
@@ -487,7 +487,6 @@ function layoutAfterDropOnBlock(
   const toBlock = entries.find(e => e.id === overId);
 
   if (!toBlock || toBlock.type !== EntryType.SessionBlock) {
-    console.log('Cannot drop on block', who);
     return;
   }
 
