1c02e0384ae057f6d2b33d8e10ec2df670dd5a7b
Adrian <adrian.moennich@cern.ch>
2025-08-01 14:07:55 +0200

    ci: Use more uv and make jobs much faster (#7001)

--- .github/utils/check_version.py
+++ .github/utils/check_version.py
@@ -5,16 +5,38 @@
 # modify it under the terms of the MIT License; see the
 # LICENSE file for more details.
 
-import os
 import sys
-from importlib.util import find_spec
+import tomllib
+from pathlib import Path
 
-from setuptools.config.expand import StaticModule
+from hatchling.version.core import VersionFile
 
 
-package = 'indico'
-sys.path.insert(0, os.getcwd())
-version = StaticModule(package, find_spec(package)).__version__
+def _get_pyproject_version(base_dir: Path):
+    data = tomllib.loads((base_dir / 'pyproject.toml').read_text())
+    # try a static version
+    try:
+        return data['project']['version']
+    except KeyError:
+        pass
+    # lookup dynamic version
+    if 'version' not in data['project'].get('dynamic', ()):
+        print('::error::Version is missing and not dynamic')
+        sys.exit(1)
+    if data['build-system']['build-backend'] != 'hatchling.build':
+        print('::error::Dynamic versions are only supported for hatchling build backend')
+        sys.exit(1)
+    try:
+        version_file_path = data['tool']['hatch']['version']['path']
+    except KeyError:
+        print('::error::Version file path is not defined')
+        sys.exit(1)
+    version_file_pattern = data['tool']['hatch']['version'].get('pattern', True)
+    vf = VersionFile(base_dir, version_file_path)
+    return vf.read(pattern=version_file_pattern)
+
+
+version = _get_pyproject_version(Path('.'))
 tag_version = sys.argv[1]
 
 if tag_version != version:
