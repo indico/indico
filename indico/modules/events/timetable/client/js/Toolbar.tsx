6e6349a1b43d5441454db09ad23734e303df8671
AjobK <ajob.edward.kustra@cern.ch>
2025-08-05 16:28:56 +0200

    rm console.log

--- indico/modules/events/timetable/client/js/Toolbar.tsx
+++ indico/modules/events/timetable/client/js/Toolbar.tsx
@@ -80,7 +80,6 @@ export default function Toolbar({
   };
 
   const scrollByPage = (pageDelta: number, behavior: ScrollBehavior = 'smooth') => _ => {
-    console.log('scroll by page');
     if (daysBarRef && daysBarRef.current) {
       const daysPerPage = Math.floor((daysBarRef.current.clientWidth - gradientWidth) / dayWidth);
       const dayDelta = daysPerPage * pageDelta;
