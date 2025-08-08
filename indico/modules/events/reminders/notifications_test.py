95add1c8c28fc669127c24bf55c233cf079658d8
Adrian Moennich <adrian.moennich@cern.ch>
2025-08-08 18:18:23 +0200

    Add test for multipart generation

--- indico/modules/events/reminders/notifications_test.py
+++ indico/modules/events/reminders/notifications_test.py
@@ -5,11 +5,15 @@
 # modify it under the terms of the MIT License; see the
 # LICENSE file for more details.
 
+import textwrap
 from datetime import datetime, timedelta
 from pathlib import Path
 
 import pytest
 
+from indico.core.db.sqlalchemy.descriptions import RenderMode
+from indico.modules.events.reminders.models.reminders import ReminderType
+from indico.modules.events.reminders.util import get_reminder_email_tpl
 from indico.testing.util import assert_email_snapshot
 from indico.util.string import RichMarkup
 from indico.web.flask.templating import get_template_module
@@ -79,3 +83,23 @@ def test_event_reminder_email_html(snapshot, create_contribution, create_timetab
                                    note=RichMarkup('<p>Meow.</p><p>Nyah!</p>'), with_agenda=True, agenda=agenda)
     snapshot.snapshot_dir = Path(__file__).parent / 'templates/emails/tests'
     assert_email_snapshot(snapshot, template, snapshot_filename, html=True)
+
+
+def test_event_reminder_email_plaintext_from_html(snapshot, create_event):
+    event = create_event(start_dt=datetime(2025, 8, 8, 18, 18),
+                         end_dt=datetime(2025, 8, 8, 19, 19),
+                         title='Baking Cats',
+                         url_shortcut='nyan')
+    note = RichMarkup(textwrap.dedent('''
+        <p><a href="https://www.nyan.cat/">Nyan Nyan <strong>Nyan!</strong></a></p>
+
+        <table>
+            <tr><th>Animal</th><th>Cuteness</th></tr>
+            <tr><td>Kitten</td><td>OVER 9000</td></tr>
+            <tr><td>Molerat</td><td>WTF</td></tr>
+        </table>
+    ''').strip())
+    html_tpl, text_tpl = get_reminder_email_tpl(event, ReminderType.standard, RenderMode.html, False, False, None, note)
+    snapshot.snapshot_dir = Path(__file__).parent / 'templates/emails/tests'
+    assert_email_snapshot(snapshot, text_tpl, 'event_reminder_htmlconv.txt')
+    assert_email_snapshot(snapshot, html_tpl, 'event_reminder_htmlconv.html', html=True)
