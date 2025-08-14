7b74a4ba63d11683feb4be550c604265c6a14c87
Adrian Moennich <adrian.moennich@cern.ch>
2025-08-14 12:07:03 +0200

    Fix querying room/location data
    This was doing a rooms x rooms cross join, resulting in a hugely
    inefficient query returning O(n^2) rows.

--- indico/modules/events/management/controllers/api.py
+++ indico/modules/events/management/controllers/api.py
@@ -6,22 +6,14 @@
 # LICENSE file for more details.
 
 from flask import jsonify
-from marshmallow.fields import Method
-from sqlalchemy.orm import joinedload
+from marshmallow import fields
 
 from indico.core.config import config
-from indico.core.db import db
 from indico.core.marshmallow import mm
 from indico.modules.rb.models.locations import Location
-from indico.modules.rb.models.rooms import Room
-from indico.util.string import natural_sort_key
+from indico.modules.rb.schemas import RoomSchema
+from indico.modules.rb.util import get_locations_with_rooms
 from indico.web.rh import RHProtected
-
-
-class _RoomSchema(mm.SQLAlchemyAutoSchema):
-    class Meta:
-        model = Room
-        fields = ('id', 'full_name')
 
 
 class _LocationSchema(mm.SQLAlchemyAutoSchema):
@@ -29,11 +21,7 @@ class _LocationSchema(mm.SQLAlchemyAutoSchema):
         model = Location
         fields = ('id', 'name', 'rooms')
 
-    rooms = Method('_serialize_rooms')
-
-    def _serialize_rooms(self, location):
-        rooms = sorted(location.rooms, key=lambda room: natural_sort_key(room.full_name))
-        return _RoomSchema(many=True).dump(rooms)
+    rooms = fields.List(fields.Nested(RoomSchema, only=('id', 'full_name')))
 
 
 class RHLocations(RHProtected):
@@ -43,10 +31,4 @@ class RHLocations(RHProtected):
         if not config.ENABLE_ROOMBOOKING:
             return jsonify(enabled=False, locations=[])
 
-        locations = (Location.query
-                     .filter_by(is_deleted=False)
-                     .join(Room, (Location.id == Room.location_id) & ~Room.is_deleted)
-                     .options(joinedload('rooms'))
-                     .order_by(db.func.lower(Location.name))
-                     .all())
-        return jsonify(enabled=True, locations=_LocationSchema(many=True).dump(locations))
+        return jsonify(enabled=True, locations=_LocationSchema(many=True).dump(get_locations_with_rooms()))
