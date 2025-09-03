96f81a756f48b6035cd941fbe3123811c23627b8
Adrian Moennich <adrian.moennich@cern.ch>
2025-09-03 15:09:24 +0200

    Restore fallback to boolean value
    In form setup mode these items are not in the store, and it's cleaner to
    have a bool instead of undefined

--- indico/modules/events/registration/client/js/form_submission/selectors.js
+++ indico/modules/events/registration/client/js/form_submission/selectors.js
@@ -41,13 +41,13 @@ export const getManagement = createSelector(
 /** Indicates whether this registration has already been paid */
 export const getPaid = createSelector(
   getStaticData,
-  staticData => staticData.paid
+  staticData => staticData.paid || false
 );
 
 /** Indicates whether the registration must be bound to a specific email */
 export const getLockEmail = createSelector(
   getStaticData,
-  staticData => staticData.lockEmail
+  staticData => staticData.lockEmail || false
 );
 
 /** Indicates the registration form's publishing to other participants mode */
