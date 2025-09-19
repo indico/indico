582502bcfec295b5f56c09e75d3df5f2e9ace04a
Tomas Roun <tomas.roun@cern.ch>
2025-09-19 15:22:07 +0200

    Fix most TS errors in ContributionForm.tsx

---
+++ indico/web/client/js/react/components/PersonLinkField.d.ts
@@ -0,0 +1,27 @@
+// This file is part of Indico.
+// Copyright (C) 2002 - 2025 CERN
+//
+// Indico is free software; you can redistribute it and/or
+// modify it under the terms of the MIT License; see the
+// LICENSE file for more details.
+
+import {ComponentType} from 'react';
+
+// interface SharedFieldProps {
+
+// FinalContributionPersonLinkField.propTypes = {
+//   ...FinalPersonLinkField.propTypes,
+//   allowAuthors: PropTypes.bool,
+//   allowSubmitters: PropTypes.bool,
+//   defaultIsSubmitter: PropTypes.bool,
+//   defaultIsSpeaker: PropTypes.bool,
+//   defaultIsAuthor: PropTypes.bool,
+// };
+
+// FinalContributionPersonLinkField.defaultProps = {
+//   allowAuthors: true,
+//   allowSubmitters: true,
+//   defaultIsSubmitter: true,
+//   defaultIsSpeaker: true,
+//   defaultIsAuthor: false,
+// };
