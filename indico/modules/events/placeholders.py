43387046461f91fb9cf343ca97a85be038b27409
Tamas Mako <1412056+tomako@users.noreply.github.com>
2025-10-01 21:14:26 +0200

    Add placeholders to event reminders

--- indico/modules/events/placeholders.py
+++ indico/modules/events/placeholders.py
@@ -7,6 +7,7 @@
 
 from markupsafe import Markup
 
+from indico.util.date_time import format_date, format_time
 from indico.util.i18n import _
 from indico.util.placeholders import ParametrizedPlaceholder, Placeholder
 
@@ -18,6 +19,24 @@ class EventTitlePlaceholder(Placeholder):
     @classmethod
     def render(cls, event, **kwargs):
         return event.title
+
+
+class EventStartDatePlaceholder(Placeholder):
+    name = 'event_start_date'
+    description = _('Event start date')
+
+    @classmethod
+    def render(cls, event, **kwargs):
+        return format_date(event.start_dt_local)
+
+
+class EventStartTimePlaceholder(Placeholder):
+    name = 'event_start_time'
+    description = _('Event start time')
+
+    @classmethod
+    def render(cls, event, **kwargs):
+        return format_time(event.start_dt_local)
 
 
 class EventLinkPlaceholder(ParametrizedPlaceholder):
