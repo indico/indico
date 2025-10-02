141e157eeb861de71fc3f3f009b99933a0266ef4
AjobK <ajob.edward.kustra@cern.ch>
2025-10-02 14:38:27 +0200

    add header

--- indico/modules/events/timetable/client/js/TimetableSessionModal.tsx
+++ indico/modules/events/timetable/client/js/TimetableSessionModal.tsx
@@ -1,3 +1,10 @@
+// This file is part of Indico.
+// Copyright (C) 2002 - 2025 CERN
+//
+// Indico is free software; you can redistribute it and/or
+// modify it under the terms of the MIT License; see the
+// LICENSE file for more details.
+
 import sessionURL from 'indico-url:sessions.api_manage_session';
 import sessionColorURL from 'indico-url:sessions.api_random_session_color';
 import locationParentURL from 'indico-url:sessions.api_sessions_location_parent';
