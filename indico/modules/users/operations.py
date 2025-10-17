d10eef858a20e8fc086f2f72c676d854bdcd0201
Tamas Mako <1412056+tomako@users.noreply.github.com>
2025-10-17 18:43:25 +0200

    PR: Force kw-only param

--- indico/modules/users/operations.py
+++ indico/modules/users/operations.py
@@ -112,12 +112,12 @@ def remove_users_from_group(users, group, *, extra_log_data=None):
                  data=extra_log_data, meta={'group_id': group.id})
 
 
-def grant_admin(user, extra_log_data=None):
+def grant_admin(user, *, extra_log_data=None):
     if user.is_admin:
         return
     user.is_admin = True
     session_user = session.user if session else None
-    log_data = extra_log_data or {}
+    log_data = dict(extra_log_data or {})
     if remote_addr := (request.remote_addr if request else None):
         log_data['IP'] = remote_addr
     AppLogEntry.log(AppLogRealm.admin, LogKind.positive, 'Admins',
@@ -129,12 +129,12 @@ def grant_admin(user, extra_log_data=None):
     logger.warning('Admin rights granted to %r by %r [%s]', user, session_user, remote_addr)
 
 
-def revoke_admin(user, extra_log_data=None):
+def revoke_admin(user, *, extra_log_data=None):
     if not user.is_admin:
         return
     user.is_admin = False
     session_user = session.user if session else None
-    log_data = extra_log_data or {}
+    log_data = dict(extra_log_data or {})
     if remote_addr := (request.remote_addr if request else None):
         log_data['IP'] = remote_addr
     AppLogEntry.log(AppLogRealm.admin, LogKind.negative, 'Admins',
