53c095ebe3c730e86b79a20a1e0dbac762cfc8fb
AjobK <ajob.edward.kustra@cern.ch>
2025-10-17 14:51:52 +0200

    Adrian comments

--- indico/modules/events/papers/schemas.py
+++ indico/modules/events/papers/schemas.py
@@ -234,13 +234,12 @@ class PaperFileTypeSchema(mm.SQLAlchemyAutoSchema):
     class Meta:
         model = PaperFileType
         # fields = ('id', 'name', 'extensions', 'allow_multiple_files', 'required', 'publishable', 'is_used',
-        #           'filename_template', 'is_used_in_condition', 'url')
+        #           'filename_template', 'url')
         fields = ('id', 'name', 'extensions', 'allow_multiple_files', 'required', 'publishable',
                   'filename_template', 'url')
 
-    # TODO: (Ajob) Find out what to do exactly with these fields
+    # TODO: (Ajob) Fix is_used fn to work with proper query
     # is_used = Function(lambda ft: EditingRevisionFile.query.with_parent(ft).has_rows())
-    # is_used_in_condition = Function(lambda ft: EditingReviewCondition.query.with_parent(ft).has_rows())
     url = Function(lambda ft: url_for('.api_papers_edit_file_type',
                                              ft.event,
                                              file_type_id=ft.id,
