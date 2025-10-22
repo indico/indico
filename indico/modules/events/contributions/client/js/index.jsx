526160b4b05071c3fca1a6f53d851ba66ab56243
AjobK <ajob.edward.kustra@cern.ch>
2025-10-22 15:58:46 +0200

    Custom button appears in jinja generated content

--- indico/modules/events/contributions/client/js/index.jsx
+++ indico/modules/events/contributions/client/js/index.jsx
@@ -20,6 +20,7 @@ import ReactDOM from 'react-dom';
 import 'indico/modules/events/util/types_dialog';
 import 'indico/react/components/AffiliationPopup';
 import EditableSubmissionButton from 'indico/modules/events/editing/editing/EditableSubmissionButton';
+import PaperSubmissionButton from 'indico/modules/events/papers/components/PaperSubmissionButton';
 import {IButton, ICSCalendarLink} from 'indico/react/components';
 import {Translate} from 'indico/react/i18n';
 import {indicoAxios, handleAxiosError} from 'indico/utils/axios';
@@ -30,6 +31,19 @@ import PublicationButton from './PublicationButton';
 import PublicationSwitch from './PublicationSwitch';
 
 document.addEventListener('DOMContentLoaded', () => {
+  customElements.define(
+    'ind-paper-submission-button',
+    class extends HTMLElement {
+      connectedCallback() {
+        ReactDOM.render(<PaperSubmissionButton />, this);
+      }
+
+      disconnectedCallback() {
+        ReactDOM.unmountComponentAtNode(this);
+      }
+    }
+  );
+
   const calendarContainer = document.querySelector('#contribution-calendar-link');
 
   if (!calendarContainer) {
