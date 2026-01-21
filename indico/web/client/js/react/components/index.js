96011d4526f5d4774733910aa2782ed3d19efcef
AjobK <ajob.edward.kustra@cern.ch>
2026-01-21 16:39:21 +0100

    rm filesubmission module

--- indico/web/client/js/react/components/index.js
+++ indico/web/client/js/react/components/index.js
@@ -34,7 +34,6 @@ export {default as PopoverDropdownMenu} from './PopoverDropdownMenu';
 export {default as ResponsivePopup} from './ResponsivePopup';
 export {default as ListFilter} from './ListFilter';
 export {default as MathJax} from './MathJax';
-export {default as FileSubmission} from './files/FileSubmission';
 export {default as FinalPictureManager} from './pictures/PictureManager';
 export {default as FinalSingleFileManager} from './files/SingleFileManager';
 export {default as FinalSingleFileDrop} from './files/SingleFileDrop';
