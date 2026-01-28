f2c067e0ae5122114a2b4a39a24449cbcbd0a7e4
AjobK <ajob.edward.kustra@cern.ch>
2026-01-28 17:14:18 +0100

    rm empty space, mv fn code directly to new property

--- indico/modules/events/papers/models/revisions.py
+++ indico/modules/events/papers/models/revisions.py
@@ -160,10 +160,10 @@ class PaperRevision(ProposalRevisionMixin, RenderModeMixin, db.Model):
     @property
     def timeline(self):
         return self.get_timeline()
-    
+
     @property
     def publishable_files(self):
-        return self.get_publishable_files()
+        return [paper_file for paper_file in self.files if not paper_file.file_type or paper_file.file_type.publishable]
 
     @paper.setter
     def paper(self, paper):
@@ -217,9 +217,6 @@ class PaperRevision(ProposalRevisionMixin, RenderModeMixin, db.Model):
                 return bool(layout_review)
             elif user in self._contribution.paper_content_reviewers:
                 return bool(content_review)
-
-    def get_publishable_files(self):
-        return [paper_file for paper_file in self.files if not paper_file.file_type or paper_file.file_type.publishable]
 
     def get_spotlight_file(self):
         pdf_files = [paper_file for paper_file in self.publishable_files if paper_file.content_type == 'application/pdf']
