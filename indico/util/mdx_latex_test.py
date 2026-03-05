1dbb12525b3de14229bf4d1ae192988068f975f6
Adrian <adrian.moennich@cern.ch>
2026-03-05 10:25:20 +0100

    Fix LaTeX regexps (#7365)
    - match weird escape syntax
    - correctly match `$$...$$` when extracting math fragments

--- indico/util/mdx_latex_test.py
+++ indico/util/mdx_latex_test.py
@@ -15,6 +15,7 @@ from indico.util.mdx_latex import LaTeXExtension, latex_escape
 
 def test_escape():
     assert latex_escape(r'\naughty') == r'\textbackslash{}naughty'
+    assert latex_escape(r'^^5cnaughty') == r'\textbackslash{}naughty'
     assert (latex_escape(r'this\\is\\harmless') ==
             r'this\textbackslash{}\textbackslash{}is\textbackslash{}\textbackslash{}harmless')
     assert latex_escape(r'\\\extranaughty') == r'\textbackslash{}\textbackslash{}\textbackslash{}extranaughty'
@@ -22,6 +23,7 @@ def test_escape():
 
 def test_escape_math():
     assert latex_escape(r'$\naughty$') == r'\protect $\\naughty$'
+    assert latex_escape(r'$^^5cnaughty$') == r'\protect $\\naughty$'
     assert latex_escape(r'$\\naughty$') == r'\protect $\\naughty$'
     assert latex_escape(r'$harm\\less$') == r'\protect $harm\\less$'
     assert latex_escape(r'$\\\extranaughty$') == r'\protect $\\\\extranaughty$'
