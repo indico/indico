9f7b602edc505b9bf953918ddc85244110d540db
Michel Succar <m.succar@cern.ch>
2026-04-09 17:00:29 +0200

    Fix bug from rename consistency

--- indico/modules/events/papers/client/js/index.js
+++ indico/modules/events/papers/client/js/index.js
@@ -159,7 +159,7 @@ customElements.define(
           editURLFn={editFileTypeURL}
           createURLFn={createFileTypeURL}
           allowDeleteLastType
-          autoSubmissionFromPeerReviewURLfn={() =>
+          autoSubmissionFromPeerReviewURLFn={() =>
             autoSubmissionFromPeerReviewURL({
               type: 'paper',
               event_id: +this.getAttribute('event-id'),
