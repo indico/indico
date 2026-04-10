875357cae265f384c1aaeaabee252d8f0738f78f
Michel Succar <m.succar@cern.ch>
2026-04-10 18:46:27 +0200

    Ignore synced file types when auto submission is off

--- indico/modules/events/papers/controllers/common.py
+++ indico/modules/events/papers/controllers/common.py
@@ -8,11 +8,15 @@
 from indico.modules.events.papers.controllers.base import RHPapersBase
 from indico.modules.events.papers.file_types import PaperFileType
 from indico.modules.events.papers.schemas import PaperFileTypeSchema
+from indico.modules.events.papers.settings import paper_submission_settings
 
 
 class RHPapersFileTypes(RHPapersBase):
     """Return all file types defined in the event for paper reviewing."""
 
     def _process(self):
-        file_types = PaperFileType.query.with_parent(self.event).all()
+        if not paper_submission_settings.get(self.event, 'auto_submission_to_editing'):
+            file_types = PaperFileType.query.with_parent(self.event).filter_by(source_editing_file_type_id=None).all()
+        else:
+            file_types = PaperFileType.query.with_parent(self.event).all()
         return PaperFileTypeSchema(many=True).jsonify(file_types)
