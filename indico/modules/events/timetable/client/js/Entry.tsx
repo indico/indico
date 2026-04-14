8c3d995368ece5cbc3d29e793bbf76638b6162bf
Rene Fernandez <rene.fernandez@cern.ch>
2026-04-14 09:01:42 +0200

    Do not zoom in if it is a poster session block

--- indico/modules/events/timetable/client/js/Entry.tsx
+++ indico/modules/events/timetable/client/js/Entry.tsx
@@ -53,6 +53,9 @@ export function DraggableEntry({id, setDuration, ...rest}: DraggableEntryProps) 
   const isSelected = useSelector((state: ReduxState) =>
     selectors.makeIsSelectedSelector()(state, id)
   );
+  const isPosterBlock = useSelector((state: ReduxState) =>
+    selectors.isPosterSessionBlock(state, id)
+  );
   // Used to determine whether the entry was just clicked or actually dragged
   // if dragged, this prevents selecting the entry on drag end (and thus showing the popup)
   const isClick = useRef<boolean>(true);
@@ -65,7 +68,7 @@ export function DraggableEntry({id, setDuration, ...rest}: DraggableEntryProps) 
   }
 
   function onDoubleClick(evt: React.MouseEvent<HTMLElement>) {
-    if (rest.type !== EntryType.SessionBlock) {
+    if (rest.type !== EntryType.SessionBlock || isPosterBlock) {
       return;
     }
     evt.stopPropagation();
