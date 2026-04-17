7c7e977c83b74468b2ca5fe12ef6e441167a7bfa
Rene Fernandez <rene.fernandez@cern.ch>
2026-04-17 17:15:33 +0200

    Add Toast context and provider

---
+++ indico/modules/events/timetable/client/js/ToastContext.tsx
@@ -0,0 +1,54 @@
+import React, {createContext, useContext, useState, ReactNode} from 'react';
+
+import {Toast, ToastType} from './Toast';
+
+export interface ToastOptions {
+  type?: ToastType;
+  message: ReactNode;
+}
+
+interface ToastContextValue {
+  addToast: (options: ToastOptions) => void;
+  removeToast: (id: number) => void;
+}
+
+interface ToastData extends ToastOptions {
+  id: number;
+}
+
+const ToastContext = createContext<ToastContextValue | null>(null);
+
+export function ToastProvider({children}: {children: ReactNode}) {
+  const [toasts, setToasts] = useState<ToastData[]>([]);
+
+  const removeToast = (id: number) => {
+    setToasts(currentToasts => currentToasts.filter(toast => toast.id !== id));
+  };
+
+  const addToast = (options: ToastOptions) => {
+    const id = Date.now(); // Simple unique ID
+    setToasts(currentToasts => [...currentToasts, {...options, id}]);
+
+    // Auto-remove the toast after a few seconds
+    // TODO: Customize the duration
+    setTimeout(() => {
+      removeToast(id);
+    }, 3000);
+  };
+
+  return (
+    <ToastContext.Provider value={{addToast, removeToast}}>
+      {children}
+      <>
+        {toasts.map(toast => (
+          <Toast key={toast.id} type={toast.type} message={toast.message} />
+        ))}
+      </>
+    </ToastContext.Provider>
+  );
+}
+
+export function useToast() {
+  const context = useContext(ToastContext);
+  return context;
+}
