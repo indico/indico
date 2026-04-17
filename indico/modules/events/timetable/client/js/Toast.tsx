98fefe0dfb7d79f44844c264c88260c7c020ddf1
Rene Fernandez <rene.fernandez@cern.ch>
2026-04-17 16:59:31 +0200

    Add Toast component

---
+++ indico/modules/events/timetable/client/js/Toast.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Icon} from 'semantic-ui-react';
+
+export type ToastType = 'info' | 'success' | 'warning' | 'error';
+
+const ICONS: Record<ToastType, string> = {
+  info: 'info',
+  success: 'check',
+  warning: 'warning',
+  error: 'times',
+};
+
+interface ToastProps {
+  type: ToastType;
+  message: React.ReactNode;
+}
+
+export function Toast({type, message}: ToastProps) {
+  return (
+    <div data-type={type} role="status">
+      <Icon name={ICONS[type]} styleName="toast-icon" />
+      <div styleName="toast-message">{message}</div>
+      <div styleName="toast-progress" />
+    </div>
+  );
+}
