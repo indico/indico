7525d0518d7d082dff3194ffbbb240d5f41cbb81
Francisco Rodrigues <francisco.rodrigues@cern.ch>
2026-05-05 14:03:18 +0200

    Address comments

--- indico/util/marshmallow_test.py
+++ indico/util/marshmallow_test.py
@@ -39,12 +39,12 @@ def test_NaiveDateTime_deserialize():
         field.deserialize(utc_now.isoformat())
 
 
-def test_ModelField_deserialize(dummy_user, dummy_contribution, create_event):
+def test_ModelField_deserialize(dummy_user, dummy_contribution, dummy_event, create_event):
     field = ModelField(User)
-    assert field.deserialize(dummy_user.id).id == dummy_user.id
+    assert field.deserialize(dummy_user.id) == dummy_user
 
     field = ModelField(User, column='first_name')
-    assert field.deserialize(dummy_user.first_name).id == dummy_user.id
+    assert field.deserialize(dummy_user.first_name) == dummy_user
 
     dummy_user.is_deleted = True
     field = ModelField(User, filter_deleted=True)
@@ -52,7 +52,7 @@ def test_ModelField_deserialize(dummy_user, dummy_contribution, create_event):
         field.deserialize(dummy_user.id)
 
     field = ModelField(User, get_query=lambda m, _: m.query.filter_by(first_name=dummy_user.first_name))
-    assert field.deserialize(dummy_user.id).id == dummy_user.id
+    assert field.deserialize(dummy_user.id) == dummy_user
 
     field = ModelField(User, get_query=lambda m, _: m.query.filter_by(first_name='Fake'))
     with pytest.raises(ValidationError):
@@ -61,25 +61,23 @@ def test_ModelField_deserialize(dummy_user, dummy_contribution, create_event):
     field = ModelField(User, none_if_missing=True, filter_deleted=True)
     assert field.deserialize(dummy_user.id) is None
 
-    event1 = create_event(title='event1')
-    event2 = create_event(title='event2')
-    event1.contributions.append(dummy_contribution)
+    other_event = create_event(title='other_event')
 
-    class Schema(mm.SQLAlchemyAutoSchema):
+    class Schema(mm.Schema):
         contrib = ModelField(Contribution, with_parent='event')
 
-    ctx = {'event': event1}
-    assert Schema(context=ctx).load({'contrib': dummy_contribution.id})['contrib'].id == dummy_contribution.id
+    ctx = {'event': dummy_event}
+    assert Schema(context=ctx).load({'contrib': dummy_contribution.id})['contrib'] == dummy_contribution
 
-    ctx['event'] = event2
+    ctx = {'event': other_event}
     with pytest.raises(ValidationError):
         Schema(context=ctx).load({'contrib': dummy_contribution.id})['contrib']
 
 
 def test_ModelField_serialize(dummy_user):
-    obj = type('Test', (), {
+    obj = {
         'user': dummy_user
-    })
+    }
 
     field = ModelField(User)
     assert field.serialize('user', obj) == dummy_user.id
@@ -88,15 +86,18 @@ def test_ModelField_serialize(dummy_user):
     assert field.serialize('user', obj) == dummy_user.first_name
 
 
-def test_ModelList_deserialize(dummy_user, dummy_contribution, create_event):
+def test_ModelList_deserialize(dummy_user, dummy_contribution, dummy_event, create_event):
     field = ModelList(User)
-    assert field.deserialize([dummy_user.id])[0].id == dummy_user.id
+    assert field.deserialize([dummy_user.id]) == [dummy_user]
+
+    field = ModelList(User)
+    assert field.deserialize([dummy_user.id, dummy_user.id]) == [dummy_user]
 
     field = ModelList(User, collection_class=set)
     assert isinstance(field.deserialize([dummy_user.id]), set)
 
     field = ModelList(User, column='first_name')
-    assert field.deserialize([dummy_user.first_name])[0].id == dummy_user.id
+    assert field.deserialize([dummy_user.first_name]) == [dummy_user]
 
     dummy_user.is_deleted = True
     field = ModelList(User, filter_deleted=True)
@@ -104,36 +105,33 @@ def test_ModelList_deserialize(dummy_user, dummy_contribution, create_event):
         field.deserialize([dummy_user.id])
 
     field = ModelList(User, get_query=lambda m, _: m.query.filter_by(first_name=dummy_user.first_name))
-    assert field.deserialize([dummy_user.id])[0].id == dummy_user.id
+    assert field.deserialize([dummy_user.id]) == [dummy_user]
 
     field = ModelList(User, get_query=lambda m, _: m.query.filter_by(first_name='Fake'))
     with pytest.raises(ValidationError):
         field.deserialize([dummy_user.id])
 
-    event1 = create_event(title='event1')
-    event2 = create_event(title='event2')
-    event1.contributions.append(dummy_contribution)
+    other_event = create_event(title='other_event')
 
-    class Schema(mm.SQLAlchemyAutoSchema):
+    class Schema(mm.Schema):
         contribs = ModelList(Contribution, with_parent='event')
 
-    ctx = {'event': event1}
-    assert Schema(context=ctx).load({'contribs': [dummy_contribution.id]})['contribs'][0].id == dummy_contribution.id
+    ctx = {'event': dummy_event}
+    assert Schema(context=ctx).load({'contribs': [dummy_contribution.id]})['contribs'] == [dummy_contribution]
 
-    ctx['event'] = event2
+    ctx = {'event': other_event}
     with pytest.raises(ValidationError):
         Schema(context=ctx).load({'contribs': [dummy_contribution.id]})['contribs']
 
 
 def test_ModelList_serialize(dummy_user):
-    obj = type('Test', (), {
+    obj = {
         'users': [dummy_user, dummy_user]
-    })
+    }
 
     field = ModelList(User)
     serialized = field.serialize('users', obj)
-    assert len(serialized) == 2
-    assert serialized[0] == dummy_user.id
+    assert serialized == [dummy_user.id, dummy_user.id]
 
     field = ModelList(User, column='first_name')
-    assert field.serialize('users', obj)[0] == dummy_user.first_name
+    assert field.serialize('users', obj) == [dummy_user.first_name, dummy_user.first_name]
