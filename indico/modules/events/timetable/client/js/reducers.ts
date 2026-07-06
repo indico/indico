194f3bd35d4d0995ca745eeb18034e2b190ad503
AjobK <ajob.edward.kustra@cern.ch>
2026-07-06 16:28:50 +0200

    fix regression on delete session unscheduled

--- indico/modules/events/timetable/client/js/reducers.ts
+++ indico/modules/events/timetable/client/js/reducers.ts
@@ -357,7 +357,7 @@ export default {
         return {
           ...state,
           entries: newEntries,
-          unscheduled: [state.unscheduled, ...contribsToUnschedule],
+          unscheduled: [...state.unscheduled, ...contribsToUnschedule],
         };
       }
       default:
