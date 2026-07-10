indico/indico

commit 49d59f68d51ed7fd7d60b12cdf49b7d2661cbedb

Support syncing user emails

indico/core/auth.py
@@ -47,7 +47,6 @@ class IndicoMultipass(Multipass):
             return set()
         synced_fields = set(provider.settings.get('synced_fields'))
         synced_fields &= set(current_app.config['MULTIPASS_IDENTITY_INFO_KEYS'])
-        synced_fields.discard('email')
         return synced_fields
 
     def init_app(self, app):

indico/modules/users/controllers.py
@@ -200,7 +200,7 @@ class RHPersonalData(RHUserBase):
                                synced_fields=self.user.synced_fields, synced_values=self.user.synced_values)
         if form.validate_on_submit():
             self.user.synced_fields = form.synced_fields
-            form.populate_obj(self.user, skip=self.user.synced_fields)
+            form.populate_obj(self.user, skip=(self.user.synced_fields | {'email'}))
             self.user.synchronize_data(refresh=True)
             flash(_('Your personal data was successfully updated.'), 'success')
             return redirect(url_for('.user_profile'))
@@ -464,6 +464,10 @@ class RHUserEmailsSetPrimary(RHUserBase):
             if self.user.picture_source in (ProfilePictureSource.gravatar, ProfilePictureSource.identicon):
                 update_gravatars.delay(self.user)
             flash(_('Your primary email was updated successfully.'), 'success')
+            if 'email' in self.user.synced_fields:
+                self.user.synced_fields = self.user.synced_fields - {'email'}
+                flash(_('Email address synchronization has been disabled since you manually changed your primary'
+                        ' email address.'), 'warning')
         return redirect(url_for('.user_emails'))
 
 

indico/modules/users/forms.py
@@ -19,6 +19,7 @@ from indico.modules.users import User
 from indico.modules.users.models.emails import UserEmail
 from indico.modules.users.models.users import NameFormat, UserTitle
 from indico.util.i18n import _, get_all_locales
+from indico.web.flask.util import url_for
 from indico.web.forms.base import IndicoForm, SyncedInputsMixin
 from indico.web.forms.fields import IndicoEnumSelectField, MultiStringField, PrincipalField, PrincipalListField
 from indico.web.forms.util import inject_validators
@@ -33,6 +34,14 @@ class UserDetailsForm(SyncedInputsMixin, IndicoForm):
     affiliation = StringField(_('Affiliation'), widget=SyncedInputWidget())
     address = TextAreaField(_('Address'), widget=SyncedInputWidget(textarea=True))
     phone = StringField(_('Phone number'), widget=SyncedInputWidget())
+    email = StringField(_('Email address'), widget=SyncedInputWidget(), render_kw={'disabled': True})
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.email.description = (
+            _('You can manage your email addresses {link}here{endlink}.')
+            .format(link=f'<a href="{url_for(".user_emails")}">', endlink='</a>')
+        )
 
 
 class UserPreferencesForm(IndicoForm):

indico/modules/users/models/users.py
@@ -138,7 +138,8 @@ syncable_fields = {
     'last_name': _('family name'),
     'affiliation': _('affiliation'),
     'address': _('address'),
-    'phone': _('phone number')
+    'phone': _('phone number'),
+    'email': _('primary email address'),
 }
 
 
@@ -639,7 +640,7 @@ class User(PersonMixin, db.Model):
             query = query.filter_by(revoked_dt=None)
         return query
 
-    def synchronize_data(self, refresh=False):
+    def synchronize_data(self, refresh=False, silent=False):
         """Synchronize the fields of the user from the sync identity.
 
         This will take only into account :attr:`synced_fields`.
@@ -648,20 +649,56 @@ class User(PersonMixin, db.Model):
                         with the sync provider before instead of using
                         the stored data. (Only if the sync provider
                         supports refresh.)
+        :param silent: bool -- Whether to just synchronize but not flash
+                       any messages
         """
+        from indico.modules.users import logger
         identity = self._get_synced_identity(refresh=refresh)
         if identity is None:
+            return
+        if not any(identity.data.values()):
+            # refuse to sync with empty identities, just in case - if there is no
+            # data at all there's a good chance something is wrong!
             return
         for field in self.synced_fields:
             old_value = getattr(self, field)
             new_value = identity.data.get(field) or ''
-            if field in ('first_name', 'last_name') and not new_value:
+            if field == 'email':
+                new_value = new_value.lower()
+            if field in ('first_name', 'last_name', 'email') and not new_value:
                 continue
             if old_value == new_value:
                 continue
-            flash(_("Your {field_name} has been synchronized from '{old_value}' to '{new_value}'.").format(
-                  field_name=syncable_fields[field], old_value=old_value, new_value=new_value))
-            setattr(self, field, new_value)
+            logger.info('Syncing %s for %r from %r to %r', field, self, old_value, new_value)
+            if field == 'email':
+                if not self._synchronize_email(new_value):
+                    if not silent:
+                        flash(_("Your email address could not be synchronized from '{old_value}' to "
+                                "'{new_value}' due to a conflict with another Indico profile.")
+                              .format(old_value=old_value, new_value=new_value), 'warning')
+                    continue
+            else:
+                setattr(self, field, new_value)
+            if not silent:
+                flash(_("Your {field_name} has been synchronized from '{old_value}' to '{new_value}'.").format(
+                    field_name=syncable_fields[field], old_value=old_value, new_value=new_value))
+
+    def _synchronize_email(self, email):
+        from indico.modules.users import logger
+        from indico.modules.users.tasks import update_gravatars
+        from indico.modules.users.util import get_user_by_email
+
+        if email not in self.secondary_emails:
+            if other := get_user_by_email(email):
+                logger.warning('Cannot sync email for %r to %r; already used by %r', self, email, other)
+                return False
+            self.secondary_emails.add(email)
+            signals.users.email_added.send(self, email=email)
+
+        self.make_email_primary(email)
+        if self.picture_source in (ProfilePictureSource.gravatar, ProfilePictureSource.identicon):
+            update_gravatars.delay(self)
+        return True
 
     def _get_synced_identity(self, refresh=False):
         sync_provider = multipass.sync_provider

commit cef2d73c1e34d74d755bb3fc2c6b0eeaec1abe0c

Remove stale claim that email cannot be synced
The `IndicoMultipass.synced_fields` docstring stated that the email can
never be synchronized. This has been inaccurate since email sync was
added in 2021: `email` is part of `MULTIPASS_IDENTITY_INFO_KEYS` and
`User.synchronize_data` handles it via `_synchronize_email`, so it is
synced whenever a provider lists `email` in its `synced_fields`.

Fixes: 49d59f68d51ed ("Support syncing user emails")

indico/core/auth.py
@@ -50,7 +50,6 @@ class IndicoMultipass(Multipass):
         """The keys to be synchronized.
 
         This is the set of keys to be synced to user data.
-        The ``email`` can never be synchronized.
         """
         provider = self.sync_provider
         if provider is None: