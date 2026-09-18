dafbdc439b8bb89dfa2cdb03257ccea903197ca2
Adrian Moennich <adrian.moennich@cern.ch>
2026-09-18 10:26:09 +0200

    Allow max choices on optional select field
    For min choices it makes sense that the fields has to be required, but
    for max choices it doesn't...

--- indico/web/fields/choices.py
+++ indico/web/fields/choices.py
@@ -92,7 +92,7 @@ class MultiSelectConfigForm:
                                uuid_field='id', sortable=True, description=_('Specify the answers the user can select'))
     min_choices = IntegerField(_('Minimum choices'), [HiddenUnless('is_required'), Optional(), NumberRange(min=0)],
                                description=_('The minimum amount of options the user has to choose.'))
-    max_choices = IntegerField(_('Maximum choices'), [HiddenUnless('is_required'), Optional(), NumberRange(min=1)],
+    max_choices = IntegerField(_('Maximum choices'), [Optional(), NumberRange(min=1)],
                                description=_('The maximum amount of options the user may choose.'))
 
     def _validate_min_max_choices(self):
