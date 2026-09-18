49175892d88d8d9f220181f1227f091011bfba79
Adrian Moennich <adrian.moennich@cern.ch>
2026-09-18 11:14:08 +0200

    Explicitly disallow Pillow calling GhostScript

--- indico/__init__.py
+++ indico/__init__.py
@@ -5,6 +5,8 @@
 # modify it under the terms of the MIT License; see the
 # LICENSE file for more details.
 
+from PIL import EpsImagePlugin
+
 from indico.util.mimetypes import register_custom_mimetypes
 from indico.util.network import patch_socket_getaddrinfo
 
@@ -14,3 +16,9 @@ PREFERRED_PYTHON_VERSION_SPEC = '~=3.12.2'
 
 register_custom_mimetypes()
 patch_socket_getaddrinfo()
+
+# We don't expect to ever process EPS files (since we check the image type against a whitelist),
+# but just for added security we explicitly tell Pillow that it cannot call GhostScript. This is
+# done by weasyprint as well, but it's cleaner to modify the global state early on instead of letting
+# another library do it while it's being imported.
+EpsImagePlugin.gs_binary = False
